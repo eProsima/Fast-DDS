12ba6ea5bc1539559183d55ee59165de328c5c64
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-03-11 13:11:10 +0100

    Fixed log for reader
    Former-commit-id: 3145afa292ae3a9c58609b166cc5e23ee220794b

--- src/cpp/utils/RTPSLog.cpp
+++ src/cpp/utils/RTPSLog.cpp
@@ -33,7 +33,7 @@ RTPSLog* RTPSLog::single = NULL;
 void RTPSLog::printError()
 {
 	std::stringstream ss;
-	ss << B_RED << "[  Error] " << DEF <<  RTPSLog::Error.str() << DEF;
+	ss << B_RED << "[eRTPS- Err] " << DEF <<  RTPSLog::Error.str() << DEF;
 	RTPSLog::printString(EPROSIMA_ERROR_VERBOSITY_LEVEL,ss.str());
 	RTPSLog::Error.str("");
 	RTPSLog::Info.clear();
@@ -42,7 +42,7 @@ void RTPSLog::printError()
 void RTPSLog::printWarning()
 {
 	std::stringstream ss;
-		ss << B_YELLOW << "[Warning] " << DEF <<  RTPSLog::Warning.str() << DEF;
+		ss << B_YELLOW << "[eRTPS-Warn] " << DEF <<  RTPSLog::Warning.str() << DEF;
 	RTPSLog::printString(EPROSIMA_WARNING_VERBOSITY_LEVEL,ss.str());
 	RTPSLog::Warning.str("");
 	RTPSLog::Info.clear();
@@ -51,7 +51,7 @@ void RTPSLog::printWarning()
 void RTPSLog::printInfo()
 {
 	std::stringstream ss;
-		ss << B_GREEN << "[   Info] " << DEF <<  RTPSLog::Info.str() << DEF;
+		ss << B_GREEN << "[eRTPS-Info] " << DEF <<  RTPSLog::Info.str() << DEF;
 	RTPSLog::printString(EPROSIMA_INFO_VERBOSITY_LEVEL,ss.str());
 	RTPSLog::Info.str("");
 	RTPSLog::Info.clear();
