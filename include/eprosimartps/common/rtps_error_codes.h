15873d5505d8a541bc6a72958684af3aab99918d
eProsimaUser01 <info@eprosima.com>
2014-03-11 16:10:57 +0100

    Version 0.1.1
    Former-commit-id: 9477e3521bddb2bb5d478f76f204d7f68ebb81fd

--- include/eprosimartps/common/rtps_error_codes.h
+++ include/eprosimartps/common/rtps_error_codes.h
@@ -21,15 +21,28 @@ namespace eprosima{
 namespace rtps{
 
 #define ERR_CDRMESSAGE_BUFFER_FULL 101
+#define ERR_CDRMESSAGE_BUFFER_FULL_STR "CDR message buffer full. Not enough space."
 
 #define ERR_MESSAGE_TOO_SHORT 201
+#define ERR_MESSAGE_TOO_SHORT_STR "Message too short to be RTPS."
+
 #define ERR_MESSAGE_NOT_RTPS 202
+#define ERR_MESSAGE_NOT_RTPS_STR "Message NOT RTPS."
+
 #define ERR_MESSAGE_VERSION_UNSUPPORTED 203
+#define ERR_MESSAGE_VERSION_UNSUPPORTED_STR "Message version not supported."
+
 #define ERR_SUBMSGHEADER_TOO_SHORT 204
+#define ERR_SUBMSGHEADER_TOO_SHORT_STR "Submessage Header too short."
+
 #define ERR_SUBMSG_LENGTH_INVALID 205
+#define ERR_SUBMSG_LENGTH_INVALID_STR "Submessage length invalid."
+
 #define ERR_MESSAGE_INCORRECT_HEADER 206
+#define ERR_MESSAGE_INCORRECT_HEADER_STR "Incorrect Message Header."
 
 #define ERR_PARTICIPANT_INCORRECT_ENDPOINT_TYPE 301
+#define ERR_PARTICIPANT_INCORRECT_ENDPOINT_TYPE_STR "Incorrect Participant Endpoint Type"
 
 }
 }
