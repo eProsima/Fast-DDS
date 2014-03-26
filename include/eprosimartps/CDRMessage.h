1cd015439bbcd894cc468aebea849e3209b10b00
eProsimaUser01 <info@eprosima.com>
2014-03-26 08:26:56 +0100

    Change CDRMessage to inline functions.
    Former-commit-id: b8273d8c0c7b0b3ac1db8739de9aa9ac11b60da0

--- include/eprosimartps/CDRMessage.h
+++ include/eprosimartps/CDRMessage.h
@@ -27,13 +27,10 @@ namespace eprosima {
 namespace rtps {
 
 /**
- * Class CDRMessage, contains static methods to initialize CDRMessage_t and add or read different data types.
+ * Class CDRMessage, contains   inline methods to initialize CDRMessage_t and add or read different data types.
  @ingroup COMMONMODULE
  */
-class CDRMessage {
-public:
-	CDRMessage();
-	virtual ~CDRMessage();
+namespace CDRMessage{
 
 	/** @name Read from a CDRMessage_t.
 	 * Methods to read different data types from a CDR message. Pointers to the message and to the data types are provided.
@@ -46,18 +43,18 @@ public:
 	 */
 
 	/// @{
-	static bool readEntityId(CDRMessage_t* msg,EntityId_t*id);
-	static bool readData(CDRMessage_t* msg,octet* o,uint16_t length);
-	static bool readDataReversed(CDRMessage_t* msg,octet* o,uint16_t length);
-	static bool readInt32(CDRMessage_t* msg,int32_t* lo);
-	static bool readUInt32(CDRMessage_t* msg,uint32_t* ulo);
-	static bool readSequenceNumber(CDRMessage_t* msg,SequenceNumber_t* sn);
-	static bool readInt16(CDRMessage_t* msg,int16_t* i16);
-	static bool readUInt16(CDRMessage_t* msg,uint16_t* i16);
-	static bool readLocator(CDRMessage_t* msg,Locator_t* loc);
-	static bool readOctet(CDRMessage_t* msg,octet* o);
-	static bool readSequenceNumberSet(CDRMessage_t* msg,SequenceNumberSet_t* snset);
-	static bool readTimestamp(CDRMessage_t*msg,Time_t* ts);
+	  inline bool readEntityId(CDRMessage_t* msg,EntityId_t*id);
+	  inline bool readData(CDRMessage_t* msg,octet* o,uint16_t length);
+	  inline bool readDataReversed(CDRMessage_t* msg,octet* o,uint16_t length);
+	  inline bool readInt32(CDRMessage_t* msg,int32_t* lo);
+	  inline bool readUInt32(CDRMessage_t* msg,uint32_t* ulo);
+	  inline bool readSequenceNumber(CDRMessage_t* msg,SequenceNumber_t* sn);
+	  inline bool readInt16(CDRMessage_t* msg,int16_t* i16);
+	  inline bool readUInt16(CDRMessage_t* msg,uint16_t* i16);
+	  inline bool readLocator(CDRMessage_t* msg,Locator_t* loc);
+	  inline bool readOctet(CDRMessage_t* msg,octet* o);
+	  inline bool readSequenceNumberSet(CDRMessage_t* msg,SequenceNumberSet_t* snset);
+	  inline bool readTimestamp(CDRMessage_t*msg,Time_t* ts);
 
 	///@}
 //	/**
@@ -66,20 +63,20 @@ public:
 //	 * @param[in] size Maximum size of the message.
 //	 * @return True if correct.
 //	 */
-//	static bool initCDRMsg(CDRMessage_t* msg,uint size);
+//	  inline bool initCDRMsg(CDRMessage_t* msg,uint size);
 	/**
 	 * Initialize given CDR message with default size. It frees the memory already allocated and reserves new one.
 	 * @param[in,out] msg Pointer to the message to initialize.
 	 * @return True if correct.
 	 */
-	static bool initCDRMsg(CDRMessage_t* msg);
+	  inline bool initCDRMsg(CDRMessage_t* msg);
 	/**
 	 * Append given CDRMessage to existing CDR Message. Joins two messages into the first one if it has space.
 	 * @param[out] first Pointer to first message.
 	 * @param[in] second Pointer to second message.
 	 * @return True if correct.
 	 */
-	static bool appendMsg(CDRMessage_t* first,CDRMessage_t* second);
+	  inline bool appendMsg(CDRMessage_t* first,CDRMessage_t* second);
 
 
 	/** @name Add to a CDRMessage_t.
@@ -93,25 +90,28 @@ public:
 	 */
 	/// @{
 
-	static bool addData(CDRMessage_t*,octet*,uint number_bytes);
-	static bool addDataReversed(CDRMessage_t*,octet*,uint byte_number);
-	static bool addOctet(CDRMessage_t*msg,octet o);
-	static bool addUInt16(CDRMessage_t*msg,uint16_t us);
-	static bool addInt32(CDRMessage_t*msg,int32_t lo);
-	static bool addUInt32(CDRMessage_t*msg,uint32_t lo);
-	static bool addInt64(CDRMessage_t*msg,int64_t lo);
-	static bool addEntityId(CDRMessage_t*msg,EntityId_t* id);
-	static bool addSequenceNumber(CDRMessage_t*msg,SequenceNumber_t* sn);
-	static bool addSequenceNumberSet(CDRMessage_t*msg,SequenceNumberSet_t* sns);
-	static bool addLocator(CDRMessage_t*msg,Locator_t*loc);
-	static bool addParameterStatus(CDRMessage_t*msg,octet status);
-	static bool addParameterKey(CDRMessage_t*msg,InstanceHandle_t* iHandle);
-	static bool addParameterSentinel(CDRMessage_t*msg);
-	static bool addParameterId(CDRMessage_t*msg,ParameterId_t pid);
+	  inline bool addData(CDRMessage_t*,octet*,uint number_bytes);
+	  inline bool addDataReversed(CDRMessage_t*,octet*,uint byte_number);
+	  inline bool addOctet(CDRMessage_t*msg,octet o);
+	  inline bool addUInt16(CDRMessage_t*msg,uint16_t us);
+	  inline bool addInt32(CDRMessage_t*msg,int32_t lo);
+	  inline bool addUInt32(CDRMessage_t*msg,uint32_t lo);
+	  inline bool addInt64(CDRMessage_t*msg,int64_t lo);
+	  inline bool addEntityId(CDRMessage_t*msg,EntityId_t* id);
+	  inline bool addSequenceNumber(CDRMessage_t*msg,SequenceNumber_t* sn);
+	  inline bool addSequenceNumberSet(CDRMessage_t*msg,SequenceNumberSet_t* sns);
+	  inline bool addLocator(CDRMessage_t*msg,Locator_t*loc);
+	  inline bool addParameterStatus(CDRMessage_t*msg,octet status);
+	  inline bool addParameterKey(CDRMessage_t*msg,InstanceHandle_t* iHandle);
+	  inline bool addParameterSentinel(CDRMessage_t*msg);
+	  inline bool addParameterId(CDRMessage_t*msg,ParameterId_t pid);
 	///@}
-};
 
+}
 } /* namespace rtps */
 } /* namespace eprosima */
+
+#include "CDRMessage.hpp"
+
 
 #endif /* CDRMESSAGE_H_ */
