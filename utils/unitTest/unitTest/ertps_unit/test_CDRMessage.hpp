9c5666464ab18de7239c9def76df410e79240015
eProsimaUser01 <info@eprosima.com>
2014-04-02 22:25:16 +0200

    CDRMessage unit test continues
    Former-commit-id: 579cd8a5f7631692b6e500a55a4674786d05e08b

--- utils/unitTest/unitTest/ertps_unit/test_CDRMessage.hpp
+++ utils/unitTest/unitTest/ertps_unit/test_CDRMessage.hpp
@@ -53,7 +53,7 @@ protected:
 
 };
 
-TEST_F(CDRMessageTest, add_octet)
+TEST_F(CDRMessageTest, octet)
 {
 	octet o = 35;
 	saveLengthPos();
@@ -61,16 +61,78 @@ TEST_F(CDRMessageTest, add_octet)
 	EXPECT_TRUE(checkLengthIncrement(1));
 	EXPECT_TRUE(checkPosIncrement(1));
 	ASSERT_EQ(35,t_msg.buffer[t_msg.pos-1]) << "Octet not added correctly";
-}
-
-TEST_F(CDRMessageTest, add_uint16)
-{
-	uint16_t
+	t_msg.pos--;
 	saveLengthPos();
-	CDRMessage::addOctet(&t_msg,o);
-	EXPECT_TRUE(checkLengthIncrement(1));
+	CDRMessage::readOctet(&t_msg,&o);
+	EXPECT_FALSE(checkLengthIncrement(1));
 	EXPECT_TRUE(checkPosIncrement(1));
-	ASSERT_EQ(35,t_msg.buffer[t_msg.pos-1]) << "Octet not added correctly";
+	ASSERT_EQ(35,o) << "Octet not readed correctly";
 }
 
+TEST_F(CDRMessageTest, uint16)
+{
+	uint16_t n = 645;
+	saveLengthPos();
+	CDRMessage::addUInt16(&t_msg,n);
+	EXPECT_TRUE(checkLengthIncrement(2));
+	EXPECT_TRUE(checkPosIncrement(2));
+	t_msg.pos-=2;
+	CDRMessage::readUInt16(&t_msg,&n);
+	ASSERT_EQ(645,n);
+}
+
+TEST_F(CDRMessageTest, uint32)
+{
+	uint32_t n = 123456;
+	saveLengthPos();
+	CDRMessage::addUInt32(&t_msg,n);
+	EXPECT_TRUE(checkLengthIncrement(4));
+	EXPECT_TRUE(checkPosIncrement(4));
+	t_msg.pos-=4;
+	CDRMessage::readUInt32(&t_msg,&n);
+	ASSERT_EQ(123456,n);
+}
+
+
+TEST_F(CDRMessageTest, int32)
+{
+	int32_t n = 123456;
+	saveLengthPos();
+	EXPECT_TRUE(CDRMessage::addInt32(&t_msg,n));
+	EXPECT_TRUE(checkLengthIncrement(4));
+	EXPECT_TRUE(checkPosIncrement(4));
+	t_msg.pos-=4;
+	EXPECT_TRUE(CDRMessage::readInt32(&t_msg,&n));
+	ASSERT_EQ(123456,n);
+	n = -123456;
+	saveLengthPos();
+	EXPECT_TRUE(CDRMessage::addInt32(&t_msg,n));
+	EXPECT_TRUE(checkLengthIncrement(4));
+	EXPECT_TRUE(checkPosIncrement(4));
+	t_msg.pos-=4;
+	EXPECT_TRUE(CDRMessage::readInt32(&t_msg,&n));
+	ASSERT_EQ(-123456,n);
+}
+
+TEST_F(CDRMessageTest, SequenceNumber)
+{
+	SequenceNumber_t seq;
+	seq.high = 10;
+	seq.low = 25;
+	saveLengthPos();
+	EXPECT_TRUE(CDRMessage::addSequenceNumber(&t_msg,&seq));
+	EXPECT_TRUE(checkLengthIncrement(8));
+	EXPECT_TRUE(checkPosIncrement(8));
+	t_msg.pos-=8;
+	CDRMessage::readSequenceNumber(&t_msg,&n);
+	ASSERT_EQ(123456,n);
+	n = -123456;
+	saveLengthPos();
+	CDRMessage::addInt32(&t_msg,n);
+	EXPECT_TRUE(checkLengthIncrement(4));
+	EXPECT_TRUE(checkPosIncrement(4));
+	t_msg.pos-=4;
+	CDRMessage::readInt32(&t_msg,&n);
+	ASSERT_EQ(-123456,n);
+}
 
