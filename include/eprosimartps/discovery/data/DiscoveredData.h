c0740bc1c2f5256354b704ff88ecf18ad01d5ae4
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-05-19 17:13:54 +0200

    DiscoveredData methods to process ParameterList_t
    Former-commit-id: 0d76ef6435f37b2fe031802bcac16b433849b02c
    Former-commit-id: 61a8fda55b7d19bb4ee21a9d715505749807454c

--- include/eprosimartps/discovery/data/DiscoveredData.h
+++ include/eprosimartps/discovery/data/DiscoveredData.h
@@ -25,21 +25,25 @@
 namespace eprosima {
 namespace rtps {
 
-class DiscoveredData {
+class DiscoveredData_t {
 public:
-	DiscoveredData();
-	virtual ~DiscoveredData();
+	DiscoveredData_t();
+	virtual ~DiscoveredData_t();
 
 	GUID_t remoteGuid;
-	LocatorList_t unicastLocatorList;
-	LocatorList_t multicastLocatorList;
+	LocatorList_t m_unicastLocatorList;
+	LocatorList_t m_multicastLocatorList;
+	LocatorList_t m_metatrafficUnicastLocatorList;
+	LocatorList_t m_metatrafficMulticastLocatorList;
+	LocatorList_t m_defaultUnicastLocatorList;
+	LocatorList_t m_defaultMulticastLocatorList;
 	InstanceHandle_t m_key;
 	InstanceHandle_t m_participantKey;
 	std::string m_typeName;
 	std::string m_topicName;
 	uint16_t userDefinedId;
-
-	bool expectsInlineQos;
+	GUID_t m_participantGuid;
+	bool m_expectsInlineQos;
 	TopicKind_t topicKind;
 
 	DurabilityQosPolicy m_durability;
@@ -56,6 +60,9 @@ public:
 	PartitionQosPolicy m_partition;
 	TopicDataQosPolicy m_topicData;
 	GroupDataQosPolicy m_groupData;
+	Duration_t m_leaseDuration;
+	BuiltinEndpointSet_t m_availableBuiltinEndpoints;
+	std::string m_participantName;
 
 	LifespanQosPolicy m_lifespan;
 	TransportPriorityQosPolicy m_transportPriority;
@@ -65,9 +72,22 @@ public:
 	ResourceLimitsQosPolicy m_resourceLimits;
 
 	OwnershipStrengthQosPolicy m_ownershipStrength;
+	ParameterPropertyList_t m_propertyList;
+	ProtocolVersion_t m_protocolVersion;
+	VendorId_t m_VendorId;
 
+};
 
-
+class DiscoveredData
+{
+public:
+	DiscoveredData(){};
+	virtual ~DiscoveredData(){};
+	bool processParameterList(ParameterList_t& param,DiscoveredData_t* data);
+	bool DiscoveredData2WriterData(DiscoveredData_t* ddata,DiscoveredWriterData* wdata);
+	bool DiscoveredData2ReaderData(DiscoveredData_t* ddata,DiscoveredReaderData* wdata);
+	bool DiscoveredData2TopicData(DiscoveredData_t* ddata,DiscoveredTopicData* wdata);
+	bool DiscoveredData2ParticipantData(DiscoveredData_t* ddata,DiscoveredParticipantData* wdata);
 };
 
 } /* namespace rtps */
