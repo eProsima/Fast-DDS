3b84043c8888429a4a14fb59f832c97562ea5561
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-06-03 10:45:39 +0200

    Changed LatencyTest

--- trunk/src/useTests/LatencyTest/LatencyPublisher.h
+++ trunk/src/useTests/LatencyTest/LatencyPublisher.h
@@ -61,7 +61,6 @@ public:
 	{
 		m_sub->readNextData((void*)m_latency_in,&m_info);
 		clock.setTimeNow(&m_t2);
-		cout << "Readed data " << m_latency_in->seqnum << endl;
 //		cout << "mt2 sec "<< m_t2.seconds << endl;
 //		cout << "mt2 sec cast "<< (uint64_t) m_t2.seconds << endl;
 //		cout << "mt2 nsec "<<m_t2.fraction << endl;
@@ -71,11 +70,10 @@ public:
 //		cout << "lantecy time "<< toMicroSec(m_t2)-toMicroSec(m_t1)-overhead_value << endl;
 //		int aux;
 //		std::cin >> aux;
-		m_latency_in->seqnum = -1;
+
 		m_times.push_back(toMicroSec(m_t2)-toMicroSec(m_t1)-overhead_value);
 		m_sub->takeNextData((void*)m_latency_in,&m_info);
-		cout << "Take data " << m_latency_in->seqnum << endl;
-		m_latency_in->seqnum = -1;
+		sema.post();
 	}
 	void onSubscriptionMatched()
 	{
@@ -152,12 +150,13 @@ bool LatencyPublisher::test(uint32_t datasize,uint32_t n_samples)
 		m_latency_out->seqnum++;
 		clock.setTimeNow(&m_t1);
 		m_pub->write((void*)m_latency_out);
-		m_sub->waitForUnreadMessage();
+		sema.wait();
 		if(!(*m_latency_out == *m_latency_in))
 		{
 			cout << "PROBLEM"<<endl;
 			return false;
 		}
+		m_latency_in->seqnum = -1;
 	}
 	int32_t removed=0;
 	m_pub->removeAllChange(&removed);
