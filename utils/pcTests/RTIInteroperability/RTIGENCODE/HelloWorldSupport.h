1f417c28eccfd0995149740645e2b6e0365e03e9
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-07-02 17:25:04 +0200

    Created simple interoperability Test

---
+++ utils/pcTests/RTIInteroperability/RTIGENCODE/HelloWorldSupport.h
@@ -0,0 +1,83 @@
+
+/*
+  WARNING: THIS FILE IS AUTO-GENERATED. DO NOT MODIFY.
+
+  This file was generated from HelloWorld.idl using "rtiddsgen".
+  The rtiddsgen tool is part of the RTI Connext distribution.
+  For more information, type 'rtiddsgen -help' at a command shell
+  or consult the RTI Connext manual.
+*/
+
+#ifndef HelloWorldSupport_1436886087_h
+#define HelloWorldSupport_1436886087_h
+
+/* Uses */
+#include "HelloWorld.h"
+
+
+
+#ifdef __cplusplus
+#ifndef ndds_cpp_h
+  #include "ndds/ndds_cpp.h"
+#endif
+#else
+#ifndef ndds_c_h
+  #include "ndds/ndds_c.h"
+#endif
+#endif
+
+        
+
+/* ========================================================================= */
+/**
+   Uses:     T
+
+   Defines:  TTypeSupport, TDataWriter, TDataReader
+
+   Organized using the well-documented "Generics Pattern" for
+   implementing generics in C and C++.
+*/
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+  /* If the code is building on Windows, start exporting symbols.
+   */
+  #undef NDDSUSERDllExport
+  #define NDDSUSERDllExport __declspec(dllexport)
+
+#ifdef __cplusplus
+  /* If we're building on Windows, explicitly import the superclasses of
+   * the types declared below.
+   */        
+  class __declspec(dllimport) DDSTypeSupport;
+  class __declspec(dllimport) DDSDataWriter;
+  class __declspec(dllimport) DDSDataReader;
+#endif
+
+#endif
+
+#ifdef __cplusplus
+
+DDS_TYPESUPPORT_CPP(SampleTypeTypeSupport, SampleType);
+
+DDS_DATAWRITER_CPP(SampleTypeDataWriter, SampleType);
+DDS_DATAREADER_CPP(SampleTypeDataReader, SampleTypeSeq, SampleType);
+
+
+#else
+
+DDS_TYPESUPPORT_C(SampleTypeTypeSupport, SampleType);
+DDS_DATAWRITER_C(SampleTypeDataWriter, SampleType);
+DDS_DATAREADER_C(SampleTypeDataReader, SampleTypeSeq, SampleType);
+
+#endif
+
+#if (defined(RTI_WIN32) || defined (RTI_WINCE)) && defined(NDDS_USER_DLL_EXPORT)
+  /* If the code is building on Windows, stop exporting symbols.
+   */
+  #undef NDDSUSERDllExport
+  #define NDDSUSERDllExport
+#endif
+
+
+
+#endif  /* HelloWorldSupport_1436886087_h */
