873de7e7d0b5d882a0ecb1e4af9e0bbb9eb8db97
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-07-08 08:24:57 +0200

    Check Qos methods improved

--- src/cpp/qos/ReaderQos.cpp
+++ src/cpp/qos/ReaderQos.cpp
@@ -119,7 +119,11 @@ bool ReaderQos::checkQos()
 		pError("PERSISTENT Durability not supported"<<endl);
 		return false;
 	}
-
+	if(m_destinationOrder.kind == BY_SOURCE_TIMESTAMP_DESTINATIONORDER_QOS)
+	{
+		pError("BY SOURCE TIMESTAMP DestinationOrder not supported"<<endl);
+		return false;
+	}
 
 
 
