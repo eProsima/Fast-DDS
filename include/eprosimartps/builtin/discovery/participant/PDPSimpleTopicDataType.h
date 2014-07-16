af145116a19519ef1485cdc88ba24653abf03685
eProsimaUser01 <info@eprosima.com>
2014-07-16 15:16:32 +0200

    PDPSimpleTopicDataType created

--- include/eprosimartps/builtin/discovery/participant/PDPSimpleTopicDataType.h
+++ include/eprosimartps/builtin/discovery/participant/PDPSimpleTopicDataType.h
@@ -14,14 +14,35 @@
 #ifndef PDPSIMPLETOPICDATATYPE_H_
 #define PDPSIMPLETOPICDATATYPE_H_
 
+#include "eprosimartps/dds/DDSTopicDataType.h"
+#include "eprosimartps/common/types/CDRMessage_t.h"
+
+
+using namespace eprosima::dds;
+
 namespace eprosima {
 namespace rtps {
 
-class PDPSimpleTopicDataType {
+class PDPSimpleTopicDataType : public DDSTopicDataType {
 public:
 	PDPSimpleTopicDataType();
 	virtual ~PDPSimpleTopicDataType();
+
+	bool serialize(void* data,SerializedPayload_t* payload);
+	bool deserialize(SerializedPayload_t* payload,void * data);
+	bool getKey(void*data,InstanceHandle_t* ihandle);
+	CDRMessage_t aux_msg;
+	void* initial_data;
+
 };
+
+
+
+
+
+
+
+
 
 } /* namespace rtps */
 } /* namespace eprosima */
