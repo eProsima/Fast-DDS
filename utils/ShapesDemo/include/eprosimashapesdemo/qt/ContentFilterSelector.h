4b24c1a9af26565ddad7ab7232b6c45f7430728a
eProsimaUser01 <info@eprosima.com>
2014-07-28 15:51:59 +0200

    Finished Resizable content filter

--- utils/ShapesDemo/include/eprosimashapesdemo/qt/ContentFilterSelector.h
+++ utils/ShapesDemo/include/eprosimashapesdemo/qt/ContentFilterSelector.h
@@ -14,32 +14,60 @@
 #include <QWidget>
 #include <QTimer>
 
+enum SD_CONTENTCORNER
+{
+    SD_OUTSIDE,
+    SD_CENTER,
+    SD_TOPLEFT,
+    SD_TOPRIGHT,
+    SD_BOTTOMLEFT,
+    SD_BOTTOMRIGHT
+};
 
+class QFrame;
+class ShapeSubscriber;
 
 /**
  * @brief Class ContentFilterSelector, implements content filter selector.
  */
 class ContentFilterSelector: public QWidget
 {
-   // Q_OBJECT
+    // Q_OBJECT
 public:
     ContentFilterSelector(QWidget* parent=0);
     virtual ~ContentFilterSelector();
 
-//    QSize minimumSizeHint() const;
-//    QSize sizeHint() const;
+    //    QSize minimumSizeHint() const;
+    //    QSize sizeHint() const;
 
+    void assignShapeSubscriber(ShapeSubscriber* ssub);
 protected:
 
 
 private:
 
 
-private slots:
+public slots:
     void mousePressEvent(QMouseEvent* event);
     void mouseMoveEvent(QMouseEvent* event);
     void mouseReleaseEvent(QMouseEvent* event);
+    void paintEvent(QPaintEvent *event);
+private:
+    QPen m_pen;
+    QBrush m_brush;
     QPoint offset;
+    int m_size_x;
+    int m_size_y;
+    bool m_moveFilter;
+    bool m_resizeFilter;
+    SD_CONTENTCORNER getCorner(QMouseEvent* event);
+    QFrame* m_frame;
+    SD_CONTENTCORNER m_corner;
+    ShapeSubscriber* mp_ssub;
+
+    void contentFilterChanged();
+
+
 
 };
 
