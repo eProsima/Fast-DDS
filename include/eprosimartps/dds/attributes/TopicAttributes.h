01a7afd053b439a4385d8e7cb84ad658a71a41c3
eProsimaUser01 <info@eprosima.com>
2014-07-29 08:51:06 +0200

    Topic Attributes != operator

--- include/eprosimartps/dds/attributes/TopicAttributes.h
+++ include/eprosimartps/dds/attributes/TopicAttributes.h
@@ -94,6 +94,23 @@ public:
 	}
 };
 
+
+bool inline operator!=(TopicAttributes& t1, TopicAttributes& t2)
+{
+	if(t1.topicKind != t2.topicKind)
+		return false;
+	if(t1.topicName != t2.topicName)
+		return false;
+	if(t1.topicDataType != t2.topicDataType)
+		return false;
+	if(t1.historyQos.kind != t2.historyQos.kind)
+		return false;
+	if(t1.historyQos.kind == KEEP_LAST_HISTORY_QOS && t1.historyQos.depth != t2.historyQos.depth)
+		return false;
+
+}
+
+
 } /* namespace rtps */
 } /* namespace eprosima */
 
