a818bb085af781976d263cfb391e725b2338d63f
Rafael Lara <RafaelLara@eprosima.com>
2014-10-07 10:59:07 +0200

    - Added rtps project

---
+++ rtpsgen/src/com/eprosima/rtps/solution/Project.java
@@ -0,0 +1,109 @@
+package com.eprosima.rtps.solution;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import com.eprosima.solution.GUIDGenerator;
+import com.eprosima.idl.util.Util;
+
+public class Project extends com.eprosima.solution.Project
+{
+	public Project(String name, String file, HashSet dependencies)
+	{
+        super(name, file, dependencies);
+
+		m_subscribersrcfiles = new ArrayList();
+		m_subscriberincludefiles = new ArrayList();
+		m_publishersrcfiles = new ArrayList();
+		m_publisherincludefiles = new ArrayList();
+	}
+	
+	public void addSubscriberSrcFile(String file)
+	{
+		m_subscribersrcfiles.add(file);
+	}
+	
+	public ArrayList getSubscriberSrcFiles()
+	{
+		return m_subscribersrcfiles;
+	}
+	
+	public void addSubscriberIncludeFile(String file)
+	{
+		m_subscriberincludefiles.add(file);
+	}
+	
+	public ArrayList getSubscriberIncludeFiles()
+	{
+		return m_subscriberincludefiles;
+	}
+	
+	public void addPublisherSrcFile(String file)
+	{
+		m_publishersrcfiles.add(file);
+	}
+	
+	public ArrayList getPublisherSrcFiles()
+	{
+		return m_publishersrcfiles;
+	}
+	
+	public void addPublisherIncludeFile(String file)
+	{
+		m_publisherincludefiles.add(file);
+	}
+	
+	public ArrayList getPublisherIncludeFiles()
+	{
+		return m_publisherincludefiles;
+	}
+	
+	public boolean getContainsInterfaces()
+	{
+		return m_containsInterfaces;
+	}
+	
+	public void setContainsInterfaces(boolean containsInterfaces)
+	{
+		m_containsInterfaces = containsInterfaces;
+	}
+	
+	/*!
+	 * @brief Used in string templates.
+	 */
+	public String getSubscriberGuid()
+	{
+		return GUIDGenerator.genGUID(getFile() + "Subscriber");
+	}
+	
+	/*!
+	 * @brief Used in string templates.
+	 */
+	public String getSubscriberExampleGuid()
+	{
+		return GUIDGenerator.genGUID(getFile() + "SubscriberExample");
+	}
+	
+	/*!
+	 * @brief Used in string templates.
+	 */
+	public String getPublisherGuid()
+	{
+		return GUIDGenerator.genGUID(getFile() + "Publisher");
+	}
+	
+	/*!
+	 * @brief Used in string templates.
+	 */
+	public String getPublisherExampleGuid()
+	{
+		return GUIDGenerator.genGUID(getFile() + "PublisherExample");
+	}
+	
+	private boolean m_containsInterfaces = false;
+	private ArrayList m_subscribersrcfiles = null;
+	private ArrayList m_subscriberincludefiles = null;
+	private ArrayList m_publishersrcfiles = null;
+	private ArrayList m_publisherincludefiles = null;
+	String m_guid = null;
+}
---
+++ rtpsgen/src/com/eprosima/rtps/solution/Solution.java
@@ -0,0 +1,100 @@
+package com.eprosima.rtps.solution;
+
+import java.util.ArrayList;
+
+public class Solution extends com.eprosima.solution.Solution
+{
+    public Solution(String example, String version, boolean serverside, boolean clientside)
+    {
+        super();
+
+        m_publisherside = serverside;
+        m_subscriberside = clientside;
+        m_version = version;
+        m_example = example;
+    }
+
+    public boolean getPublisherside()
+    {
+        return m_publisherside;
+    }
+
+    public boolean getSubscriberside()
+    {
+        return m_subscriberside;
+    }
+
+    @Override
+    public ArrayList<String> getLibraries()
+    {
+        ArrayList<String> libraries = super.getLibraries();
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(int count = 0; count < libraries.size(); ++count)
+        {
+            if(m_example.contains("Win") && libraries.get(count).startsWith("eprosimartps"))
+                ret.add(libraries.get(count) + "-" + m_version);
+            else
+                ret.add(libraries.get(count));
+        }
+
+        return ret;
+    }
+
+    public ArrayList<String> getLibrariesDebug()
+    {
+        ArrayList<String> libraries = super.getLibraries();
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(int count = 0; count < libraries.size(); ++count)
+        {
+            if(m_example.contains("Win") && libraries.get(count).startsWith("eprosimartps"))
+                ret.add(libraries.get(count) + "d-" + m_version);
+            else
+                ret.add(libraries.get(count) + "d");
+        }
+
+        return ret;
+    }
+
+    public ArrayList<String> getLibrariesStatic()
+    {
+        ArrayList<String> libraries = super.getLibraries();
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(int count = 0; count < libraries.size(); ++count)
+        {
+            if(libraries.get(count).startsWith("ndds"))
+                ret.add(libraries.get(count) + "z");
+            else if(m_example.contains("Win") && libraries.get(count).startsWith("eprosimartps"))
+                ret.add("lib" + libraries.get(count) + "-" + m_version);
+            else
+                ret.add("lib" + libraries.get(count));
+        }
+
+        return ret;
+    }
+
+    public ArrayList<String> getLibrariesStaticDebug()
+    {
+        ArrayList<String> libraries = super.getLibraries();
+        ArrayList<String> ret = new ArrayList<String>();
+
+        for(int count = 0; count < libraries.size(); ++count)
+        {
+            if(libraries.get(count).startsWith("ndds"))
+                ret.add(libraries.get(count) + "zd");
+            else if(m_example.contains("Win") && libraries.get(count).startsWith("eprosimartps"))
+                ret.add("lib" + libraries.get(count) + "d-" + m_version);
+            else
+                ret.add("lib" + libraries.get(count) + "d");
+        }
+
+        return ret;
+    }
+
+    private boolean m_publisherside = true;
+    private boolean m_subscriberside = true;
+    private String m_version = null;
+    private String m_example = null;
+}
