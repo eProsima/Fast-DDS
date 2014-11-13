c51d6acace48ac6e817f62032a383f4e0548e848
eProsimaUser01 <info@eprosima.com>
2014-11-13 09:54:21 +0100

    Compiles in VS2013

--- include/eprosimartps/utils/TimedEvent.h
+++ include/eprosimartps/utils/TimedEvent.h
@@ -16,10 +16,10 @@
 #ifndef PERIODICEVENT_H_
 #define PERIODICEVENT_H_
 
-#define  _WINSOCK_DEPRECATED_NO_WARNINGS
+//#define  _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include <algorithm>
-#include <boost\config\compiler\visualc.hpp>
+//#include <algorithm>
+//#include <boost\config\compiler\visualc.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/deadline_timer.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
