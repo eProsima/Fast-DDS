79e22277824333e3bc19a2c779b22f690b2485cf
Gonzalo Rodriguez (Win32) <gonzalorodriguez@eprosima.com>
2014-12-01 11:48:36 +0100

    Fixed rtpsgen templates

--- win32/cpp/FastRTPS/copyLibraries.bat
+++ win32/cpp/FastRTPS/copyLibraries.bat
@@ -1,4 +1,4 @@
-xcopy ..\..\..\lib\i86Win32VS2010\* "C:\Program Files\eProsima\eRTPS\lib\i86Win32VS2010\" /y
-xcopy ..\..\..\lib\x64Win64VS2010\* "C:\Program Files\eProsima\eRTPS\lib\x64Win64VS2010\" /y
-xcopy ..\..\..\lib\i86Win32VS2013\* "C:\Program Files\eProsima\eRTPS\lib\i86Win32VS2013\" /y
-xcopy ..\..\..\lib\x64Win64VS2013\* "C:\Program Files\eProsima\eRTPS\lib\x64Win64VS2013\" /y
+xcopy ..\..\..\lib\i86Win32VS2010\* "C:\Program Files\eProsima\FastRTPS\lib\i86Win32VS2010\" /y
+xcopy ..\..\..\lib\x64Win64VS2010\* "C:\Program Files\eProsima\FastRTPS\lib\x64Win64VS2010\" /y
+xcopy ..\..\..\lib\i86Win32VS2013\* "C:\Program Files\eProsima\FastRTPS\lib\i86Win32VS2013\" /y
+xcopy ..\..\..\lib\x64Win64VS2013\* "C:\Program Files\eProsima\FastRTPS\lib\x64Win64VS2013\" /y
