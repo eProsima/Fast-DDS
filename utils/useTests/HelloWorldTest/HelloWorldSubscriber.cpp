70d9395141a8821a0d6a31731710e3538e6e0d8b
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2014-12-15 16:40:52 +0100

    Now listening in multiple interfaces in multiple IPs IP6 IP4

--- utils/useTests/HelloWorldTest/HelloWorldSubscriber.cpp
+++ utils/useTests/HelloWorldTest/HelloWorldSubscriber.cpp
@@ -47,12 +47,12 @@ HelloWorldSubscriber::HelloWorldSubscriber() {
 	Rparam.topic.resourceLimitsQos.allocated_samples = 20;
 	Rparam.qos.m_reliability.kind = RELIABLE_RELIABILITY_QOS;
 	Locator_t loc;
-	loc.kind = 2;
+	loc.kind = 1;
 	loc.port = 27405;
-	loc.address[0] = 0xff;
-	loc.address[1] = 0x02;
-	loc.address[15] = 0x02;
-	Rparam.multicastLocatorList.push_back(loc);
+	//loc.address[0] = 0xff;
+	//loc.address[1] = 0x02;
+	//loc.address[15] = 0x02;
+	Rparam.unicastLocatorList.push_back(loc);
 	mp_subscriber = Domain::createSubscriber(mp_participant,Rparam,(SubscriberListener*)&m_listener);
 
 }
--- utils/useTests/HelloWorldTest/HelloWorld_main.cpp
+++ utils/useTests/HelloWorldTest/HelloWorld_main.cpp
@@ -19,8 +19,11 @@
 #include "fastrtps/utils/eClock.h"
 #include "fastrtps/utils/RTPSLog.h"
 
-using namespace eprosima;
+#include "fastrtps/rtps/rtps_all.h"
 
+using namespace eprosima;
+using namespace fastrtps;
+using namespace rtps;
 int main(int argc, char** argv)
 {
 	Log::setVerbosity(VERB_INFO);
@@ -44,22 +47,42 @@ int main(int argc, char** argv)
 	{
 	case 1:
 	{
-		HelloWorldPublisher mypub;
-		for(int i = 0;i<10;++i)
-		{
-			if(mypub.publish())
-			{
-//				int aux;
-//				std::cin>>aux;
-				eClock::my_sleep(500);
-			}
-			else
-			{
-				//cout << "Sleeping till discovery"<<endl;
-				eClock::my_sleep(200);
-				--i;
-			}
-		}
+		RTPSParticipantAttributes patt;
+		patt.builtin.use_SIMPLE_RTPSParticipantDiscoveryProtocol = false;
+		patt.builtin.use_WriterLivelinessProtocol = false;
+		RTPSParticipant* part = RTPSDomain::createParticipant(patt);
+		WriterAttributes watt;
+		watt.endpoint.reliabilityKind = BEST_EFFORT;
+		HistoryAttributes hatt;
+		WriterHistory* hist = new WriterHistory(hatt);
+		RTPSWriter* writer=RTPSDomain::createRTPSWriter(part,watt,hist);
+		RemoteReaderAttributes ratt;
+		Locator_t loc;
+		loc.set_IP4_address(192,168,56,101);
+		loc.port = 27405;
+		ratt.endpoint.unicastLocatorList.push_back(loc);
+		writer->matched_reader_add(ratt);
+		CacheChange_t* change = writer->new_change(ALIVE);
+		change->serializedPayload.length = 2;
+		hist->add_change(change);
+
+//
+//		HelloWorldPublisher mypub;
+//		for(int i = 0;i<10;++i)
+//		{
+//			if(mypub.publish())
+//			{
+////				int aux;
+////				std::cin>>aux;
+//				eClock::my_sleep(500);
+//			}
+//			else
+//			{
+//				//cout << "Sleeping till discovery"<<endl;
+//				eClock::my_sleep(200);
+//				--i;
+//			}
+	//}
 		break;
 	}
 	case 2:
