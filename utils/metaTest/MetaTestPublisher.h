75fbc39b7b509dc7eee9ff5eea07bff7848d0319
Gonzalo Rodriguez <gonzalorodriguez@eprosima.com>
2015-01-07 09:51:34 +0100

    Changes in comments

---
+++ utils/metaTest/MetaTestPublisher.h
@@ -0,0 +1,27 @@
+/*************************************************************************
+ * Copyright (c) 2015 eProsima. All rights reserved.
+ *
+ * This copy of eProsima Fast RTPS is licensed to you under the terms described in the
+ * FASTRTPS_LIBRARY_LICENSE file included in this distribution.
+ *
+ *************************************************************************/
+
+/**
+ * @file MetaTestPublisher.h
+ *
+ */
+
+#ifndef METATESTPUBLISHER_H_
+#define METATESTPUBLISHER_H_
+
+namespace eprosima {
+
+class MetaTestPublisher {
+public:
+	MetaTestPublisher();
+	virtual ~MetaTestPublisher();
+};
+
+} /* namespace eprosima */
+
+#endif /* METATESTPUBLISHER_H_ */
---
+++ utils/metaTest/MetaTestSubscriber.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+ * Copyright (c) 2015 eProsima. All rights reserved.
+ *
+ * This copy of eProsima Fast RTPS is licensed to you under the terms described in the
+ * FASTRTPS_LIBRARY_LICENSE file included in this distribution.
+ *
+ *************************************************************************/
+
+/**
+ * @file MetaTestSubscriber.cpp
+ *
+ */
+
+#include "MetaTestSubscriber.h"
+
+namespace eprosima {
+
+MetaTestSubscriber::MetaTestSubscriber() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MetaTestSubscriber::~MetaTestSubscriber() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace eprosima */
