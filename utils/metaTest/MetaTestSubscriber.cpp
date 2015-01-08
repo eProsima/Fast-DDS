669d709a7d5a8a64ba076540ee2bb2f12c35c30a
eProsimaUser01 <info@eprosima.com>
2015-01-08 14:29:34 +0100

    MetaTest structure beginned

--- utils/metaTest/MetaTestSubscriber.cpp
+++ utils/metaTest/MetaTestSubscriber.cpp
@@ -15,7 +15,7 @@
 
 namespace eprosima {
 
-MetaTestSubscriber::MetaTestSubscriber():
+MetaTestSubscriber::MetaTestSubscriber():mp_par(nullptr),
 		mp_pub(nullptr),
 		mp_sub(nullptr)
 {
@@ -25,6 +25,39 @@ MetaTestSubscriber::MetaTestSubscriber():
 
 MetaTestSubscriber::~MetaTestSubscriber() {
 	// TODO Auto-generated destructor stub
+}
+
+bool MetaTestSubscriber::init()
+{
+	ParticipantAttributes Patt;
+	Patt.rtps.builtin.domainId = 50;
+	Patt.rtps.builtin.leaseDuration = c_TimeInfinite;
+	Patt.rtps.setName("MetaPublisher");
+
+	mp_par = Domain::createParticipant(Patt);
+
+	PublisherAttributes Wparam;
+	Wparam.topic.topicName = "metaTest_S2P";
+	Wparam.topic.topicDataType = "MetaTestInfo";
+
+	mp_pub = Domain::createPublisher(mp_par,Wparam,&m_publisten);
+
+
+	SubscriberAttributes Rparam;
+	Rparam.topic.topicName = "metaTest_P2S";
+	Rparam.topic.topicDataType = "MetaTestInfo";
+
+	mp_sub = Domain::createSubscriber(mp_par,Rparam,&m_sublisten);
+
+	if(mp_pub == nullptr || mp_sub == nullptr)
+		return false;
+	return true;
+}
+
+void MetaTestSubscriber::run()
+{
+
+
 }
 
 } /* namespace eprosima */
--- utils/metaTest/MetaTestSubscriber.h
+++ utils/metaTest/MetaTestSubscriber.h
@@ -15,6 +15,8 @@
 #define METATESTSUBSCRIBER_H_
 
 #include "fastrtps/fastrtps_fwd.h"
+#include "fastrtps/publisher/PublisherListener.h"
+#include "fastrtps/subscriber/SubscriberListener.h"
 using namespace eprosima::fastrtps;
 
 namespace eprosima {
@@ -27,8 +29,35 @@ public:
 	void run();
 
 private:
+	Participant* mp_par;
 	Publisher* mp_pub;
 	Subscriber* mp_sub;
+
+	class MyPubListen : public PublisherListener
+	{
+	public:
+		MyPubListen():n_matched(0){};
+		~MyPubListen(){};
+		void onPublicationMatched(Publisher* pub,MatchingInfo info)
+		{
+			if(info.status == MATCHED_MATCHING) n_matched++;
+			else if(info.status == REMOVED_MATCHING) n_matched--;
+		}
+		int n_matched;
+	}m_publisten;
+	class MySubListen : public SubscriberListener
+	{
+	public:
+		MySubListen():n_matched(0){};
+		~MySubListen(){};
+		void onSubscriptionMatched(Publisher* pub,MatchingInfo info)
+		{
+			if(info.status == MATCHED_MATCHING) n_matched++;
+			else if(info.status == REMOVED_MATCHING) n_matched--;
+		}
+		void onNewDataMessage(Subscriber* sub);
+		int n_matched;
+	}m_sublisten;
 };
 
 } /* namespace eprosima */
