18814f0a1a88fa5f9c9ee9aeb2d78822160c1f21
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2015-07-29 09:15:24 +0200

    CMake support

--- cmake/dev/eprosima_libraries.cmake
+++ cmake/dev/eprosima_libraries.cmake
@@ -79,24 +79,24 @@ macro(install_eprosima_fastcdr)
 
         if(EPROSIMA_INSTALLER)
             install(DIRECTORY ${PROJECT_BINARY_DIR}/../i86Win32VS2010/external/fastcdr/install/lib/i86Win32VS2010
-                DESTINATION ${LIB_INSTALL_DIR}
+                DESTINATION lib
                 COMPONENT libraries_i86Win32VS2010
                 )
             install(DIRECTORY ${PROJECT_BINARY_DIR}/../x64Win64VS2010/external/fastcdr/install/lib/x64Win64VS2010
-                DESTINATION ${LIB_INSTALL_DIR}
+                DESTINATION lib
                 COMPONENT libraries_x64Win64VS2010
                 )
             install(DIRECTORY ${PROJECT_BINARY_DIR}/../i86Win32VS2013/external/fastcdr/install/lib/i86Win32VS2013
-                DESTINATION ${LIB_INSTALL_DIR}
+                DESTINATION lib
                 COMPONENT libraries_i86Win32VS2013
                 )
             install(DIRECTORY ${PROJECT_BINARY_DIR}/../x64Win64VS2013/external/fastcdr/install/lib/x64Win64VS2013
-                DESTINATION ${LIB_INSTALL_DIR}
+                DESTINATION lib
                 COMPONENT libraries_x64Win64VS2013
                 )
         else()
-            install(DIRECTORY ${fastcdr_LIB_DIR}/${MSVC_ARCH}
-                DESTINATION ${LIB_INSTALL_DIR}
+            install(DIRECTORY ${fastcdr_LIB_DIR}
+                DESTINATION lib
                 COMPONENT libraries_${MSVC_ARCH}
                 )
         endif()
