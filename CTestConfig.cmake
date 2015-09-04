99d048d66bef3ba75ff6ef0cbe9b3201d6623d66
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2015-09-04 11:49:27 +0200

    Modified CTest configuration

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,7 @@
 set(CTEST_PROJECT_NAME "fastrtps")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "https")
-set(CTEST_DROP_SITE "sambaserver.eprosima.com:8443")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "sambaserver.eprosima.com:8000")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=fastrtps")
 set(CTEST_DROP_SITE_CDASH TRUE)
