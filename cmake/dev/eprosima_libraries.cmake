72b450b9bfacae5bbc8b77e1ebea9a59abab24d7
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2015-10-09 10:29:23 +0200

    Fixed bug in installation using EPROSIMA_BUILD

--- cmake/dev/eprosima_libraries.cmake
+++ cmake/dev/eprosima_libraries.cmake
@@ -111,6 +111,11 @@ macro(install_eprosima_fastcdr)
                 DESTINATION lib/x64Win64VS2013
                 COMPONENT libraries_x64Win64VS2013
                 )
+        elseif(EPROSIMA_BUILD)
+            install(DIRECTORY ${fastcdr_LIB_DIR}
+                DESTINATION lib/${MSVC_ARCH}
+                COMPONENT libraries_${MSVC_ARCH}
+                )
         else()
             install(DIRECTORY ${fastcdr_LIB_DIR}
                 DESTINATION lib
