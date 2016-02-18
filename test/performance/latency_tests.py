947f258d2ec98bac73e9c336806f06729843f2c4
Rubén Rubio <rubenrubio@eprosima.com>
2016-02-18 12:56:22 +0100

    bug fixed

--- test/performance/latency_tests.py
+++ test/performance/latency_tests.py
@@ -1,12 +1,8 @@
 import shlex, subprocess, time, os, socket, sys
 
-subscriber_command = os.environ.get("LATENCY_TEST_BIN") + " subscriber"
-args = shlex.split(subscriber_command)
-subscriber_proc = subprocess.Popen(args)
-
-publisher_command = os.environ.get("LATENCY_TEST_BIN") + " publisher"
-args = shlex.split(publisher_command)
-publisher_proc = subprocess.Popen(args)
+command = os.environ.get("LATENCY_TEST_BIN")
+subscriber_proc = subprocess.Popen([command, "subscriber"])
+publisher_proc = subprocess.Popen([command, "publisher"])
 
 subscriber_proc.communicate()
 publisher_proc.communicate()
