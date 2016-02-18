3cc4b8747e0530bedf5bf7aaa4693f2ef3d83c3d
Rubén Rubio <rubenrubio@eprosima.com>
2016-02-18 15:22:23 +0100

    missing files

---
+++ test/performance/throughput_tests.py
@@ -0,0 +1,10 @@
+import shlex, subprocess, time, os, socket, sys
+
+command = os.environ.get("THROUGHPUT_TEST_BIN")
+subscriber_proc = subprocess.Popen([command, "subscriber"])
+publisher_proc = subprocess.Popen([command, "publisher"])
+
+subscriber_proc.communicate()
+publisher_proc.communicate()
+
+quit()
