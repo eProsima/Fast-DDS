976760cc658caf5d71fa8ed167fa24e512952c27
PabloMansanet <pablomansanet@eprosima.com>
2016-04-25 15:53:30 +0200

    #refs #1577. Implement more of UDPv4Transport's methods

--- include/fastrtps/transport/UDPv4Transport.h
+++ include/fastrtps/transport/UDPv4Transport.h
@@ -26,39 +26,29 @@ public:
    // Checks whether there are open and bound sockets for the given port.
    virtual bool IsLocatorChannelOpen(Locator_t) const;
 
-   // Must report whether the given locator is supported by this transport (typically inspecting its "kind" value).
    virtual bool IsLocatorSupported(Locator_t) const;
 
-   // Must the channel that maps to/from the given locator. This method must allocate, reserve and mark
-   // any resources that are needed for said channel.
    virtual bool OpenLocatorChannel(Locator_t);
 
-   // Must close the channel that maps to/from the given locator. 
-   // IMPORTANT: It MUST be safe to call this method even during a Send and Receive operation. You must implement
-   // any necessary mutual exclusion and timeout mechanisms to make sure the channel can be closed without damage
    virtual bool CloseLocatorChannel(Locator_t);
 
-   // Must report whether two locators map to the same internal channel.
    virtual bool DoLocatorsMatch(Locator_t, Locator_t) const;
 
-   // Must execute a blocking send, through the outbound channel that maps to the localLocator, targeted to the
-   // remote address defined by remoteLocator. Must be threadsafe between channels, but not necessarily
-   // within the same channel.
    virtual bool Send(const std::vector<char>& sendBuffer, Locator_t localLocator, Locator_t remoteLocator);
 
-   // Must execute a blocking receive, on the inbound channel that maps to the localLocator, receiving from the
-   // address defined by remoteLocator. Must be threadsafe between channels, but not necessarily
-   // within the same channel.
    virtual bool Receive(std::vector<char>& receiveBuffer, Locator_t localLocator, Locator_t remoteLocator);
 
 private:
    TransportDescriptor mDescriptor;
 
    // For UDPv4, the notion of channel corresponds to a port. 
+   // Requesting a port with any IP  will trigger the binding of a socket per interface, plus
+   // a multicast receiver socket.
 	boost::asio::io_service mSendService;
-   std::map<uint16_t, boost::asio::ip::udp::socket> mPortSocketMap;
-   bool OpenAndBindUnicastSocket(uint32_t port);
-   bool IsAddressMulticast(Locator_t);
+   std::map<uint16_t, std::vector<boost::asio::ip::udp::socket> > mPortSocketMap;
+
+   bool OpenAndBindSockets(uint32_t port);
+   boost::asio::ip::udp::socket OpenAndBindSocket(boost::asio::ip::address_v4, uint32_t port);
 };
 
 } // namespace rtps
--- src/cpp/transport/UDPv4Transport.cpp
+++ src/cpp/transport/UDPv4Transport.cpp
@@ -1,5 +1,6 @@
 #include <fastrtps/transport/UDPv4Transport.h>
 #include <utility>
+#include <fastrtps/utils/IPFinder.h>
 #include <fastrtps/utils/RTPSLog.h>
 
 using namespace std;
@@ -26,36 +27,55 @@ bool UDPv4Transport::OpenLocatorChannel(Locator_t locator)
 {
    if (IsLocatorChannelOpen(locator))
       return false;   
-
-   if (IsAddressMulticast(locator))
-      return false;
-   else
-      return OpenAndBindUnicastSocket(locator.port);
+   
+   return OpenAndBindSockets(locator.port);
 }
 
-bool UDPv4Transport::OpenAndBindUnicastSocket(uint32_t port)
+bool UDPv4Transport::OpenAndBindSockets(uint32_t port)
 {
-	const char* const METHOD_NAME = "OpenAndBindUnicastSocket";
+	const char* const METHOD_NAME = "OpenAndBindSockets";
 
+   try 
+   {
+      // Multicast first, in vector index 0
+      mPortSocketMap[port].push_back(OpenAndBindSocket(ip::address_v4::any(), port));
+
+      // Unicast output sockets, one per interface
+      std::vector<IPFinder::info_IP> locNames;
+      IPFinder::getIPs(&locNames);
+      for (const auto& ip : locNames)
+         mPortSocketMap[port].push_back(OpenAndBindSocket(boost::asio::ip::address_v4::from_string(ip.name), port));
+   }
+	catch (boost::system::system_error const& e)
+   {
+	   logInfo(RTPS_MSG_OUT, "UDPv4 Error binding at port: (" << port << ")" << " with boost msg: "<<e.what() , C_YELLOW);
+      mPortSocketMap.erase(port);
+      return false;
+   }
+
+   return true;
+}
+
+boost::asio::ip::udp::socket UDPv4Transport::OpenAndBindSocket(ip::address_v4 ipAddress, uint32_t port)
+{
    ip::udp::socket socket(mSendService);
    socket.open(ip::udp::v4());
    socket.set_option(socket_base::send_buffer_size(mDescriptor.bufferSize));
 
-   auto anyIPAddress = ip::address_v4::any();
-   ip::udp::endpoint endpointSinglePortAnyIP(anyIPAddress, port);
+   ip::udp::endpoint endpoint(ipAddress, port);
+   socket.bind(endpoint);
 
-   try 
-   {
-      socket.bind(endpointSinglePortAnyIP);
-   }
-	catch (boost::system::system_error const& e)
-   {
-	   logInfo(RTPS_MSG_OUT, "UDPv4 Error binding endpoint: (" << endpointSinglePortAnyIP << ")" << " with boost msg: "<<e.what() , C_YELLOW);
-      return false;
-   }
-  
-   mPortSocketMap.emplace(port, move(socket));
-   return true;
+   return socket;
+}
+
+bool UDPv4Transport::DoLocatorsMatch(Locator_t left, Locator_t right) const
+{
+   return left.port == right.port;
+}
+
+bool UDPv4Transport::IsLocatorSupported(Locator_t locator) const
+{
+   return locator.kind == LOCATOR_KIND_UDPv4;
 }
 
 } // namespace rtps
