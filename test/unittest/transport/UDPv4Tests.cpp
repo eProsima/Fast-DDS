be2015c5a49e0195fd815be96c83e6062a64f40c
PabloMansanet <pablomansanet@eprosima.com>
2016-04-28 08:15:02 +0200

    refs #1577. Increase test coverage on UDPv4

--- test/unittest/transport/UDPv4Tests.cpp
+++ test/unittest/transport/UDPv4Tests.cpp
@@ -12,12 +12,10 @@ class UDPv4Tests: public ::testing::Test
    public:
    UDPv4Tests()
    {
+      HELPER_SetDescriptorDefaults();
    }
 
    void HELPER_SetDescriptorDefaults();
-   bool HELPER_OpenGenericOutputChannel(UDPv4Transport& transport, uint16_t port);
-   bool HELPER_OpenGenericInputChannel(UDPv4Transport& transport, uint16_t port);
-   void HELPER_StartSenderMulticastOutput();
 
    UDPv4Transport::TransportDescriptor descriptor;
    unique_ptr<boost::thread> senderThread;
@@ -27,7 +25,6 @@ class UDPv4Tests: public ::testing::Test
 TEST_F(UDPv4Tests, locators_with_kind_1_supported)
 {
    // Given
-   HELPER_SetDescriptorDefaults(); 
    UDPv4Transport transportUnderTest(descriptor);
 
    Locator_t supportedLocator;
@@ -43,7 +40,6 @@ TEST_F(UDPv4Tests, locators_with_kind_1_supported)
 TEST_F(UDPv4Tests, opening_and_closing_output_channel)
 {
    // Given
-   HELPER_SetDescriptorDefaults(); 
    UDPv4Transport transportUnderTest(descriptor);
    Locator_t genericOutputChannelLocator;
    genericOutputChannelLocator.kind = LOCATOR_KIND_UDPv4;
@@ -61,7 +57,6 @@ TEST_F(UDPv4Tests, opening_and_closing_output_channel)
 TEST_F(UDPv4Tests, opening_and_closing_input_channel)
 {
    // Given
-   HELPER_SetDescriptorDefaults(); 
    UDPv4Transport transportUnderTest(descriptor);
    Locator_t multicastFilterLocator;
    multicastFilterLocator.kind = LOCATOR_KIND_UDPv4;
@@ -79,7 +74,6 @@ TEST_F(UDPv4Tests, opening_and_closing_input_channel)
 
 TEST_F(UDPv4Tests, send_and_receive_between_ports)
 {
-   HELPER_SetDescriptorDefaults(); 
    UDPv4Transport transportUnderTest(descriptor);
 
    Locator_t multicastLocator;
@@ -116,27 +110,125 @@ TEST_F(UDPv4Tests, send_and_receive_between_ports)
    receiverThread->join();
 }
 
+TEST_F(UDPv4Tests, send_to_loopback)
+{
+   UDPv4Transport transportUnderTest(descriptor);
+
+   Locator_t multicastLocator;
+   multicastLocator.port = 7410;
+   multicastLocator.kind = LOCATOR_KIND_UDPv4;
+   multicastLocator.set_IP4_address(239, 255, 0, 1);
+
+   Locator_t outputChannelLocator;
+   outputChannelLocator.port = 7400;
+   outputChannelLocator.kind = LOCATOR_KIND_UDPv4;
+   outputChannelLocator.set_IP4_address(127,0,0,1); // Loopback
+   ASSERT_TRUE(transportUnderTest.OpenOutputChannel(outputChannelLocator));
+   ASSERT_TRUE(transportUnderTest.OpenInputChannel(multicastLocator));
+   vector<char> message = { 'H','e','l','l','o' };
+
+   auto sendThreadFunction = [&]()
+   {
+      Locator_t destinationLocator;
+      destinationLocator.port = 7410;
+      destinationLocator.kind = LOCATOR_KIND_UDPv4;
+      EXPECT_TRUE(transportUnderTest.Send(message, outputChannelLocator, multicastLocator));
+   };
+
+   auto receiveThreadFunction = [&]() 
+   {
+      vector<char> receiveBuffer(descriptor.receiveBufferSize);
+      Locator_t remoteLocatorToReceive;
+      EXPECT_TRUE(transportUnderTest.Receive(receiveBuffer, multicastLocator, remoteLocatorToReceive));
+      EXPECT_EQ(message, receiveBuffer);
+   };
+
+   receiverThread.reset(new boost::thread(receiveThreadFunction));      
+   senderThread.reset(new boost::thread(sendThreadFunction));      
+   senderThread->join();
+   receiverThread->join();
+}
+
+TEST_F(UDPv4Tests, send_is_rejected_if_buffer_size_isnt_equal_to_size_specified_in_descriptor)
+{
+   // Given
+   UDPv4Transport transportUnderTest(descriptor);
+   Locator_t genericOutputChannelLocator;
+   genericOutputChannelLocator.kind = LOCATOR_KIND_UDPv4;
+   genericOutputChannelLocator.port = 7400;
+   transportUnderTest.OpenOutputChannel(genericOutputChannelLocator);
+
+   Locator_t destinationLocator;
+   destinationLocator.kind = LOCATOR_KIND_UDPv4;
+   destinationLocator.port = 7410;
+
+   // Then
+   vector<char> receiveBufferWrongSize(descriptor.sendBufferSize + 1);
+   ASSERT_FALSE(transportUnderTest.Send(receiveBufferWrongSize, genericOutputChannelLocator, destinationLocator));
+}
+
+TEST_F(UDPv4Tests, Receive_is_rejected_if_buffer_size_isnt_equal_to_size_specified_in_descriptor)
+{
+   // Given
+   UDPv4Transport transportUnderTest(descriptor);
+   Locator_t genericInputChannelLocator;
+   genericInputChannelLocator.kind = LOCATOR_KIND_UDPv4;
+   genericInputChannelLocator.port = 7410;
+   transportUnderTest.OpenInputChannel(genericInputChannelLocator);
+
+   Locator_t originLocator;
+
+   // Then
+   vector<char> receiveBufferWrongSize(descriptor.sendBufferSize + 1);
+   ASSERT_FALSE(transportUnderTest.Receive(receiveBufferWrongSize, genericInputChannelLocator, originLocator));
+}
+
+TEST_F(UDPv4Tests, Outside_granular_mode_opening_any_output_address_opens_port_for_all_IP_addresses)
+{
+   // Given
+   UDPv4Transport transportUnderTest(descriptor);
+
+   Locator_t specificOutputChannelLocator;
+   specificOutputChannelLocator.kind = LOCATOR_KIND_UDPv4;
+   specificOutputChannelLocator.port = 7410;
+   specificOutputChannelLocator.set_IP4_address(222,222,222,222);
+
+   Locator_t differentOutputChannelLocator;
+   differentOutputChannelLocator.kind = LOCATOR_KIND_UDPv4;
+   differentOutputChannelLocator.port = 7410;
+   differentOutputChannelLocator.set_IP4_address(111,111,111,111);
+
+   // When
+   ASSERT_TRUE(transportUnderTest.OpenOutputChannel(specificOutputChannelLocator));
+
+   // Then
+   ASSERT_TRUE(transportUnderTest.IsOutputChannelOpen(differentOutputChannelLocator));
+}
+
+TEST_F(UDPv4Tests, Outside_granular_mode_RemoteToMainLocal_simply_strips_out_address_leaving_IP_ANY)
+{
+   // Given
+   UDPv4Transport transportUnderTest(descriptor);
+
+   Locator_t remoteLocator;
+   remoteLocator.kind = LOCATOR_KIND_UDPv4;
+   remoteLocator.port = 7410;
+   remoteLocator.set_IP4_address(222,222,222,222);
+
+   // When
+   Locator_t mainLocalLocator = transportUnderTest.RemoteToMainLocal(remoteLocator);
+
+   ASSERT_EQ(mainLocalLocator.port, remoteLocator.port);
+   ASSERT_EQ(mainLocalLocator.kind, remoteLocator.kind);
+
+   ASSERT_EQ(mainLocalLocator.to_IP4_string(), "0.0.0.0");
+}
+
 void UDPv4Tests::HELPER_SetDescriptorDefaults()
 {
    descriptor.sendBufferSize = 5;
    descriptor.receiveBufferSize = 5;
    descriptor.granularMode = false;
-}
-
-bool UDPv4Tests::HELPER_OpenGenericOutputChannel(UDPv4Transport& transport, uint16_t port)
-{
-   Locator_t genericOutputChannelLocator;
-   genericOutputChannelLocator.kind = LOCATOR_KIND_UDPv4;
-   genericOutputChannelLocator.port = port;
-   return transport.OpenInputChannel(genericOutputChannelLocator);
-}
-
-bool UDPv4Tests::HELPER_OpenGenericInputChannel(UDPv4Transport& transport, uint16_t port)
-{
-   Locator_t genericInputChannelLocator;
-   genericInputChannelLocator.kind = LOCATOR_KIND_UDPv4;
-   genericInputChannelLocator.port = port;
-   return transport.OpenInputChannel(genericInputChannelLocator);
 }
 
 int main(int argc, char **argv)
