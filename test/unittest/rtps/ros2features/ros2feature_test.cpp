97f430ebf101958839f418b1d51ed9f6191e8a32
Santiago <santiago@Lonetrek.eProsima>
2016-05-10 12:19:36 +0200

    Added ROS2 Test - Attach a slave listener to the EDPReaderListener

--- test/unittest/rtps/ros2features/ros2feature_test.cpp
+++ test/unittest/rtps/ros2features/ros2feature_test.cpp
@@ -13,7 +13,11 @@
 
 #include "types/HelloWorldType.h"
 #include <string>
+#include <fastrtps/rtps/RTPSDomain.h>
 
+#include <fastrtps/rtps/reader/ReaderListener.h>
+#include <fastrtps/rtps/reader/RTPSReader.h>
+#include <fastrtps/rtps/reader/StatefulReader.h>
 
 #include <boost/interprocess/detail/os_thread_functions.hpp>
 #include <gtest/gtest.h>
@@ -35,16 +39,62 @@ public:
 
 };
 
-/*TEST(ros2features, EDPSlaveReaderAttachment)
+class gettopicnamesandtypesReaderListener:public ReaderListener
 {
+	public:
+		topicnamesandtypesReaderListener():i(0){};
+		rmw_topic_names_and_types* get_topicNtypes{
+			return &topicNtypes;
+		}
+	void onNewCacheChangeAdded(RTPSReader* reader, const CacheChange_t* const change_in){
+		CacheChange_t* change = (CacheChange_t*) change_in;
+		if(change->kind == ALIVE){
+			WriterProxyData proxyData;
+			CDR_Message_t tempMsg;
+			tempMsg.msg_endian = change->serializedPayload.encapsulation == PL_CDR_BE ? BIGEND:LITTLENED;
+			tempMsg.length = change->serializedPayload.length;
+			memcpy(tempMsg.buffer,change->serializedPayload.data,tempMsg.length);
+			if(proxyData.readFromCDRMessage(&tempMsg)){
+				topicNtypes[proxyData.m_topicName].insert(proxyData.m_typeName);		
+			}
+		}
+	}
+	int i;	
+	std::map<std::string,std::set<std::string>> topicNtypes;
 
 
+};
+
+TEST(ros2features, EDPSlaveReaderAttachment)
+{
+	Participant *my_participant;
+	Publisher *my_publisher;
+	ParticipantAttributes p_attr;
+	PublisherAttributes pub_attr;
+	HelloWorldType my_type;
+	pub_dummy_listener my_dummy_listener;
+	ReaderListener slave_listener;
+	bool result;	
+	p_attr.rtps.builtin.domainId = (uint32_t)boost::interprocess::ipcdetail::get_current_process_id() % 230;
+	my_participant = Domain::createParticipant(p_attr);
+
+	ASSERT_NE(my_participant, nullptr);
+	ASSERT_EQ(Domain::registerType(my_participant, &my_type), true);
+
+	pub_attr.topic.topicKind = NO_KEY;
+	pub_attr.topic.topicDataType = "HelloWorldType";
+	my_publisher = Domain::createPublisher(my_participant, pub_attr, &my_dummy_listener);
+	ASSERT_NE(my_publisher, nullptr);
+
+	std::pair<StatefulReader*,StatefulReader*> EDP_Readers = my_participant->getEDPReaders();
+	InfectableReaderListener* target = dynamic_cast<InfectableReaderListener*>(EDP_Readers.first->getListener());
+	target->attachListener(&slave_listener);
+	result = target->hasReaderAttached();
+	ASSERT_EQ(result, true);
 
 
+}true
 
-
-}
-*/
 TEST(ros2features, PubSubPoll)
 {
 	const uint8_t max_elements = 4;
@@ -54,13 +104,13 @@ TEST(ros2features, PubSubPoll)
 	ParticipantAttributes p_attr;
 	HelloWorldType my_type;
 	pub_dummy_listener p_listener_array[max_elements];
-	sub_dummy_listener s_listener_array[max_elements];
+	sub_dummy_listenerstener s_listener_array[max_elements];
 
 	std::string str("HelloWorldType");
 	char *cstr = new  char[str.length()+1];
 	std::strcpy(cstr,str.c_str());
 
-	p_attr.rtps.builtin.domainId = (uint32_t)boost::interprocess::ipcdetail::get_current_process_id() % 230;
+	p_attr.rtps.builtin.domainId = (uint32_t)boost::interprocess::ipcdetail::get_current_process_id() % 230 + 2;
 	my_participant = Domain::createParticipant(p_attr);
 	//Register type
 	
@@ -89,14 +139,55 @@ TEST(ros2features, PubSubPoll)
 	}
 
 }
-/*
 TEST(ros2features, TopicsAndTypesDiscovery){
+	Participant *my_participant;
+	Publisher *my_publisher;
+	ParticipantAttributes p_attr;
+	PublisherAttributes pub_attr;
+	HelloWorldType my_type;
+	pub_dummy_listener my_dummy_listener;
+	gettopicnamesandtypesReaderListener slave_listener;
+	bool result;	
+	p_attr.rtps.builtin.domainId = (uint32_t)boost::interprocess::ipcdetail::get_current_process_id() % 230;
+	my_participant = Domain::createParticipant(p_attr);
+
+	ASSERT_NE(my_participant, nullptr);
+	ASSERT_EQ(Domain::registerType(my_participant, &my_type), true);
+
+	pub_attr.topic.topicKind = NO_KEY;
+	pub_attr.topic.topicDataType = "HelloWorldType";
+	my_publisher = Domain::createPublisher(my_participant, pub_attr, &my_dummy_listener);
+	ASSERT_NE(my_publisher, nullptr);
+
+	std::pair<StatefulReader*,StatefulReader*> EDP_Readers = my_participant->getEDPReaders();
+	InfectableReaderListener* target = dynamic_cast<InfectableReaderListener*>(EDP_Readers.first->getListener());
+	target->attachListener(dynamic_cast<gettopicnamesandtypesReaderListener*>(&slave_listener));
+	result = target->hasReaderAttached();
+	ASSERT_EQ(result,true);
+
+	ASSERT_EQ(target->i,0); //Nothing has been created so far
+
+	Participant *my_participant2;
+	Publisher *my_publisher2;
+	ParticipantAttributes p_attr2;
+	PublisherAttributes pub_attr2;
+	pub_dummy_listener my_dummy_listener2;
+	p_attr2.rtps.builtin.domainId = (uint32_t)boost::interprocess::ipcdetail::get_current_process_id() % 230;
+	my_participant2 = Domain::createParticipant(p_attr2);
+
+	ASSERT_NE(my_participant2, nullptr);
+
+	pub_attr2.topic.topicKind = NO_KEY;
+	pub_attr2.topic.topicDataType = "HelloWorldType";
+	my_publisher2 = Domain::createPublisher(my_participant2, pub_attr2, &my_dummy_listener2);
+	ASSERT_NE(my_publisher2, nullptr);
 
 
+	ASSERT_EQ(target->i,1);
 
 
 }
-*/
+
 int main(int argc, char **argv)
 {
 	testing::InitGoogleTest(&argc, argv);
