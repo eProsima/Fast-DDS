eProsima/Fast-DDS

commit 5778e4133f6422fc786bd5e4a8386cbbeaf526be

StatefulWriter: add new messages to history even if full
If a writer's history fills up, then when a new cache change is
published, the oldest cache change is deleted to make room.

Whether this is correct for a single pub/sub pair is debatable; it may
be preferable to suffer the backpressure.

However, when there are multiple subscribers for a single publisher,
it is almost certainly more correct to allow one "good" subscriber
keep getting reliable data if another "bad" subscriber is either not
keeping up or dies.

With this change, if a second subscriber dies, the CPU usage on the
publisher still increases dramatically (at 1000 msgs/sec with a
history depth of 5000, it goes from ~15% to ~75%), but at least the
data keeps flowing. Doing the history management more efficiently and
detecting the loss of the subscriber might be the next step to reign
in the CPU usage.

src/cpp/rtps/writer/StatefulWriter.cpp
@@ -405,13 +405,40 @@ bool StatefulWriter::clean_history(unsigned int max)
             ackca.push_back(*cit);
     }
 
+    // If there are any entries that are acked by all matched readers, delete
+    // them. Then, delete the oldest unacked entries to get the delete total
+    // to 'max'. Deleting unacked entries happens if a reader goes down and
+    // that is not detected before history fills; we want reliable streams to
+    // live readers to continue uninterrupted. It could also happen in a single
+    // subscriber case if the publish rate exceeds available link capacity; in
+    // that case, something needs to give....
     for(std::vector<CacheChange_t*>::iterator cit = ackca.begin();
             cit != ackca.end(); ++cit)
     {
         mp_history->remove_change_g(*cit);
+        max--;
     }
 
-    return (ackca.size() > 0);
+    // delete oldest samples (just like for StatelessWriter)
+    while (max > 0) {
+        // get oldest change
+        CacheChange_t *cc;
+        if (!mp_history->get_min_change(&cc)) {
+            break; // XXX what can cause this
+        }
+
+        // XXX Remove from all reader's unsent changes? Testing without doing
+        // anything here seems to show it is working, but it seems like there
+        // should be dangling references.
+
+        // delete it
+        if (!mp_history->remove_change(cc)) {
+            break; // XXX what can cause this
+        }
+        max--;
+    }
+
+    return (max == 0);
 }
 
 /*

commit 418ca03040d490117c62f4bac48335d53f7b99c8

StatefulWriter: don't force-clean too much
This makes it so the clean_history done on receipt of an ACKNACK message
does not clear out the entire history. This bug was introduced in commit
5778e41.

src/cpp/rtps/writer/StatefulWriter.cpp
@@ -416,7 +416,8 @@ bool StatefulWriter::clean_history(unsigned int max)
             cit != ackca.end(); ++cit)
     {
         mp_history->remove_change_g(*cit);
-        max--;
+        if (max > 0)
+            max--;
     }
 
     // delete oldest samples (just like for StatelessWriter)