583d0a8089624883dbba1d3ad6d22af2c69e061c
Ricardo González Moreno <RicardoGonzalez@eprosima.com>
2016-05-19 16:26:43 +0200

    Added changes using gmock

--- test/mock/rtps/StatefulReader/fastrtps/rtps/reader/StatefulReader.h
+++ test/mock/rtps/StatefulReader/fastrtps/rtps/reader/StatefulReader.h
@@ -2,6 +2,7 @@
 #define _RTPS_READER_STATEFULREADER_H_
 
 #include <fastrtps/rtps/attributes/ReaderAttributes.h>
+#include <fastrtps/rtps/common/Guid.h>
 
 namespace eprosima
 {
@@ -12,6 +13,9 @@ namespace eprosima
             class StatefulReader
             {
                 public:
+
+                    // In real class, inherited from Endpoint base class.
+                    inline const GUID_t& getGuid() const { return GUID_t(); };
 
                     inline ReaderTimes& getTimes(){return m_times;};
 
