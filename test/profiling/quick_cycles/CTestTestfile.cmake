489fac2d0e3d74ec817319aa64db06b6340897da
PabloMansanet <pablomansanet@eprosima.com>
2016-06-15 15:34:56 +0200

    removed unnecessary files

--- test/profiling/quick_cycles/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/repos/rtps/test/profiling/quick_cycles
-# Build directory: /home/pablo/repos/rtps/build/Debug/test/profiling/quick_cycles
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(profiling "/usr/bin/python3" "/home/pablo/repos/rtps/test/profiling/quick_cycles/profiling.py")
-set_tests_properties(profiling PROPERTIES  ENVIRONMENT "PROFILING_BINS=/home/pablo/repos/rtps/build/Debug/test/profiling/RTPSCommsProfiling;CMAKE_CURRENT_SOURCE_DIR=/home/pablo/repos/rtps/test/profiling/quick_cycles")
