c1073c1df44f97bce9502d32df45230e39dcfb49
PabloMansanet <pablomansanet@eprosima.com>
2016-06-29 08:05:15 +0200

    VS2013 workaround

--- src/cpp/log/Log.cpp
+++ src/cpp/log/Log.cpp
@@ -110,11 +110,15 @@ void Log::KillThread()
    }
    if (mResources.mLoggingThread) 
    {
-      #if !defined(_WIN32) || defined(FASTRTPS_STATIC_LINK) // Workaround for MSVC static destruction
+      // The #ifdef workaround here is due to an unsolved MSVC bug, that Microsoft has announced
+      // they have no intention of solving: https://connect.microsoft.com/VisualStudio/feedback/details/747145
+      // each VS version deals with post-main deallocation of threads in a very different way.
+      #if !defined(_WIN32) || defined(FASTRTPS_STATIC_LINK)
       mResources.mCv.notify_all();
+      #if !(defined(_MSC_VER) && _MSC_VER == 1400)
       mResources.mLoggingThread->join();
       #endif
-    
+      #endif
       mResources.mLoggingThread.reset();
    }
 }
