57f2dfbbffb1f61c9c444c1dfdc2104232a6b484
Ricardo González Moreno <correoricky@gmail.com>
2016-07-27 11:41:56 +0200

    Fixed bad pointer usage.

--- src/cpp/rtps/reader/StatefulReader.cpp
+++ src/cpp/rtps/reader/StatefulReader.cpp
@@ -179,23 +179,38 @@ bool StatefulReader::matched_writer_is_matched(RemoteWriterAttributes& wdata)
 }
 
 
-bool StatefulReader::matched_writer_lookup(GUID_t& writerGUID, WriterProxy** WP)
+bool StatefulReader::matched_writer_lookup(const GUID_t& writerGUID, WriterProxy** WP)
 {
     const char* const METHOD_NAME = "matched_writer_lookup";
-    assert(WP);
 
     boost::lock_guard<boost::recursive_mutex> guard(*mp_mutex);
+
+    bool returnedValue = findWriterProxy(writerGUID, WP);
+
+    if(returnedValue)
+    {
+        logInfo(RTPS_READER,this->getGuid().entityId<<" FINDS writerProxy "<< writerGUID<<" from "<< matched_writers.size());
+    }
+    else
+    {
+        logInfo(RTPS_READER,this->getGuid().entityId<<" NOT FINDS writerProxy "<< writerGUID<<" from "<< matched_writers.size());
+    }
+
+    return returnedValue;
+}
+
+bool StatefulReader::findWriterProxy(const GUID_t& writerGUID, WriterProxy** WP)
+{
+    assert(WP);
 
     for(std::vector<WriterProxy*>::iterator it = matched_writers.begin(); it != matched_writers.end(); ++it)
     {
         if((*it)->m_att.guid == writerGUID)
         {
             *WP = *it;
-            logInfo(RTPS_READER,this->getGuid().entityId<<" FINDS writerProxy "<< writerGUID<<" from "<< matched_writers.size());
             return true;
         }
     }
-    logInfo(RTPS_READER,this->getGuid().entityId<<" NOT FINDS writerProxy "<< writerGUID<<" from "<< matched_writers.size());
     return false;
 }
 
@@ -364,9 +379,15 @@ bool StatefulReader::processHeartbeatMsg(GUID_t &writerGUID, uint32_t hbCount, S
                     }
                     ++notifySeqNum;
 
-                    wpLock.lock();
-                    maxSeqNumAvailable = pWP->available_changes_max();
-                    wpLock.unlock();
+                    // Search again the WriterProxy because could be removed after the unlock.
+                    if(findWriterProxy(proxGUID, &pWP))
+                    {
+                        pWP->getMutex()->lock();
+                        maxSeqNumAvailable = pWP->available_changes_max();
+                        pWP->getMutex()->unlock();
+                    }
+                    else
+                        break;
                 }
             }
         }
@@ -443,10 +464,11 @@ bool StatefulReader::change_received(CacheChange_t* a_change, WriterProxy* prox,
 {
     const char* const METHOD_NAME = "change_received";
 
+
     //First look for WriterProxy in case is not provided
     if(prox == nullptr)
     {
-        if(!this->matched_writer_lookup(a_change->writerGUID,&prox))
+        if(!findWriterProxy(a_change->writerGUID, &prox))
         {
             logInfo(RTPS_READER, "Writer Proxy " << a_change->writerGUID <<" not matched to this Reader "<< m_guid.entityId);
             return false;
@@ -499,9 +521,15 @@ bool StatefulReader::change_received(CacheChange_t* a_change, WriterProxy* prox,
                         }
                         ++notifySeqNum;
 
-                        writerProxyLock.lock();
-                        maxSeqNumAvailable = prox->available_changes_max();
-                        writerProxyLock.unlock();
+                        // Search again the WriterProxy because could be removed after the unlock.
+                        if(findWriterProxy(proxGUID, &prox))
+                        {
+                            prox->getMutex()->lock();
+                            maxSeqNumAvailable = prox->available_changes_max();
+                            prox->getMutex()->unlock();
+                        }
+                        else
+                            break;
                     }
                 }
                 else
@@ -509,7 +537,7 @@ bool StatefulReader::change_received(CacheChange_t* a_change, WriterProxy* prox,
                     //TODO NOTHING; SOME CHANGES ARE MISSING
                 }
 
-		mp_history->postSemaphore();
+                mp_history->postSemaphore();
             }
 
             return true;
