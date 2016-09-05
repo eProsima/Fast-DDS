713caf08c5c7cc997ed9a62846ac0f7188637b60
Santiago Munoz <santiagomunoz@eprosima.com>
2016-09-05 16:17:59 +0200

    New example plus updated input

--- examples/C++/UseCaseDemonstrator/UseCaseLauncher.cpp
+++ examples/C++/UseCaseDemonstrator/UseCaseLauncher.cpp
@@ -62,9 +62,12 @@ int main(){
     std::cout << " 6 - Sample configuration: Fastest" << std::endl;
     std::cout << " 7 - Sample configuration: Triggers" << std::endl;
 
+    std::cout << "Make your choice (1 to 7, q to quit): ";
     validinput = false;
     while(!validinput){
         std::cin >> userchoice;
+        if(userchoice == std::string("q"))
+                return 0;
         int choice;
         try{
             choice = std::stoi(userchoice);
@@ -594,21 +597,292 @@ void incompatible(){
     }
     std::cout << std::endl;
 
-
-
-
 }
 void safest(){
+    
+    samplePubSubType sampleType;
+    sample my_sample;
+    SampleInfo_t sample_info;
 
+    ParticipantAttributes PparamPub;
+    PparamPub.rtps.builtin.domainId = 0;
+    PparamPub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamPub.rtps.setName("PublisherParticipant");
 
+    Participant *PubParticipant = Domain::createParticipant(PparamPub);
+    if(PubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Publisher Participant..." << std::endl;
+        return; 
+    }
+    Domain::registerType(PubParticipant,(TopicDataType*) &sampleType);
+
+    
+    //Publisher config
+    PublisherAttributes Pparam;
+    Pparam.topic.topicDataType = sampleType.getName();
+    Pparam.topic.topicName = "samplePubSubTopic";
+    Pparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Pparam.topic.topicKind = NO_KEY;
+    Pparam.topic.historyQos.kind = KEEP_ALL_HISTORY_QOS;
+    Pparam.qos.m_durability.kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    Pparam.qos.m_reliability.kind = RELIABLE_RELIABILITY_QOS;
+    Pparam.topic.historyQos.depth =  50;
+    Pparam.topic.resourceLimitsQos.max_samples = 100;
+    Pparam.topic.resourceLimitsQos.max_instances = 1;
+    Pparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Best-Effort Publisher..." << std::endl; 
+    Publisher *myPub= Domain::createPublisher(PubParticipant, Pparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "Something went wrong while creating the Publisher..." << std::endl;
+        return;
+    }
+  
+
+    ParticipantAttributes PparamSub;
+    PparamSub.rtps.builtin.domainId = 0;
+    PparamSub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamSub.rtps.setName("SubscriberParticipant");
+
+    Participant *SubParticipant = Domain::createParticipant(PparamSub);
+    if(SubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Subscriber Participant..." << std::endl;
+        return;
+    }
+    Domain::registerType(SubParticipant,(TopicDataType*) &sampleType);
+
+    // Sub
+    SubscriberAttributes Rparam;
+    Rparam.topic.topicDataType = sampleType.getName();
+    Rparam.topic.topicName = "samplePubSubTopic";
+    Rparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Rparam.topic.topicKind = NO_KEY;
+    Rparam.topic.historyQos.kind = KEEP_ALL_HISTORY_QOS;
+    Rparam.qos.m_durability.kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    Rparam.qos.m_reliability.kind = RELIABLE_RELIABILITY_QOS;
+    Rparam.topic.historyQos.depth =  50;
+    Rparam.topic.resourceLimitsQos.max_samples = 100;
+    Rparam.topic.resourceLimitsQos.max_instances = 1;
+    Rparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Subscriber..." << std::endl;
+    Subscriber *mySub1= Domain::createSubscriber(PubParticipant, Rparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "something went wrong while creating the Subscriber..." << std::endl;
+        return;
+    }
+    
+    //Send 20 samples
+    std::cout << "Publishing 10 samples on the topic..." << std::endl;
+    for(uint8_t j=0; j < 10; j++){
+        my_sample.index(j+1); 
+        my_sample.key_value(1);
+        myPub->write(&my_sample);
+    }
+
+    eClock::my_sleep(1500);
+
+    //Read the contents of both histories:
+        std::cout << "The Reliable Subscriber holds: " << std::endl;
+    while(mySub1->readNextData(&my_sample, &sample_info)){
+        std::cout << std::to_string(my_sample.index()) << " ";
+    }
+    std::cout << std::endl;
 
 }
+
 void fastest(){
+
+    samplePubSubType sampleType;
+    sample my_sample;
+    SampleInfo_t sample_info;
+
+    ParticipantAttributes PparamPub;
+    PparamPub.rtps.builtin.domainId = 0;
+    PparamPub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamPub.rtps.setName("PublisherParticipant");
+
+    Participant *PubParticipant = Domain::createParticipant(PparamPub);
+    if(PubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Publisher Participant..." << std::endl;
+        return; 
+    }
+    Domain::registerType(PubParticipant,(TopicDataType*) &sampleType);
+
+    
+    //Publisher config
+    PublisherAttributes Pparam;
+    Pparam.topic.topicDataType = sampleType.getName();
+    Pparam.topic.topicName = "samplePubSubTopic";
+    Pparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Pparam.topic.topicKind = NO_KEY;
+    Pparam.topic.historyQos.kind = KEEP_LAST_HISTORY_QOS;
+    Pparam.qos.m_durability.kind = VOLATILE_DURABILITY_QOS;
+    Pparam.qos.m_reliability.kind = BEST_EFFORT_RELIABILITY_QOS;
+    Pparam.topic.historyQos.depth =  50;
+    Pparam.topic.resourceLimitsQos.max_samples = 100;
+    Pparam.topic.resourceLimitsQos.max_instances = 1;
+    Pparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Best-Effort Publisher..." << std::endl; 
+    Publisher *myPub= Domain::createPublisher(PubParticipant, Pparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "Something went wrong while creating the Publisher..." << std::endl;
+        return;
+    }
+  
+
+    ParticipantAttributes PparamSub;
+    PparamSub.rtps.builtin.domainId = 0;
+    PparamSub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamSub.rtps.setName("SubscriberParticipant");
+
+    Participant *SubParticipant = Domain::createParticipant(PparamSub);
+    if(SubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Subscriber Participant..." << std::endl;
+        return;
+    }
+    Domain::registerType(SubParticipant,(TopicDataType*) &sampleType);
+
+    // Sub
+    SubscriberAttributes Rparam;
+    Rparam.topic.topicDataType = sampleType.getName();
+    Rparam.topic.topicName = "samplePubSubTopic";
+    Rparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Rparam.topic.topicKind = NO_KEY;
+    Rparam.topic.historyQos.kind = KEEP_LAST_HISTORY_QOS;
+    Rparam.qos.m_durability.kind = VOLATILE_DURABILITY_QOS;
+    Rparam.qos.m_reliability.kind = BEST_EFFORT_RELIABILITY_QOS;
+    Rparam.topic.historyQos.depth =  50;
+    Rparam.topic.resourceLimitsQos.max_samples = 100;
+    Rparam.topic.resourceLimitsQos.max_instances = 1;
+    Rparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Subscriber..." << std::endl;
+    Subscriber *mySub1= Domain::createSubscriber(PubParticipant, Rparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "something went wrong while creating the Subscriber..." << std::endl;
+        return;
+    }
+    
+    //Send 20 samples
+    std::cout << "Publishing 10 samples on the topic..." << std::endl;
+    for(uint8_t j=0; j < 10; j++){
+        my_sample.index(j+1); 
+        my_sample.key_value(1);
+        myPub->write(&my_sample);
+    }
+
+    eClock::my_sleep(1500);
+
+    //Read the contents of both histories:
+        std::cout << "The Reliable Subscriber holds: " << std::endl;
+    while(mySub1->readNextData(&my_sample, &sample_info)){
+        std::cout << std::to_string(my_sample.index()) << " ";
+    }
+    std::cout << std::endl;
+
 
 
 
 }
 void triggers(){
+
+    samplePubSubType sampleType;
+    sample my_sample;
+    SampleInfo_t sample_info;
+
+    ParticipantAttributes PparamPub;
+    PparamPub.rtps.builtin.domainId = 0;
+    PparamPub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamPub.rtps.setName("PublisherParticipant");
+
+    Participant *PubParticipant = Domain::createParticipant(PparamPub);
+    if(PubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Publisher Participant..." << std::endl;
+        return; 
+    }
+    Domain::registerType(PubParticipant,(TopicDataType*) &sampleType);
+
+    
+    //Publisher config
+    PublisherAttributes Pparam;
+    Pparam.topic.topicDataType = sampleType.getName();
+    Pparam.topic.topicName = "samplePubSubTopic";
+    Pparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Pparam.topic.topicKind = NO_KEY;
+    Pparam.topic.historyQos.kind = KEEP_ALL_HISTORY_QOS;
+    Pparam.qos.m_durability.kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    Pparam.qos.m_reliability.kind = RELIABLE_RELIABILITY_QOS;
+    Pparam.topic.historyQos.depth =  50;
+    Pparam.topic.resourceLimitsQos.max_samples = 100;
+    Pparam.topic.resourceLimitsQos.max_instances = 1;
+    Pparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Best-Effort Publisher..." << std::endl; 
+    Publisher *myPub= Domain::createPublisher(PubParticipant, Pparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "Something went wrong while creating the Publisher..." << std::endl;
+        return;
+    }
+  
+
+    ParticipantAttributes PparamSub;
+    PparamSub.rtps.builtin.domainId = 0;
+    PparamSub.rtps.builtin.leaseDuration = c_TimeInfinite;
+    PparamSub.rtps.setName("SubscriberParticipant");
+
+    Participant *SubParticipant = Domain::createParticipant(PparamSub);
+    if(SubParticipant == nullptr){
+        std::cout << " Something went wrong while creating the Subscriber Participant..." << std::endl;
+        return;
+    }
+    Domain::registerType(SubParticipant,(TopicDataType*) &sampleType);
+
+    // Sub
+    SubscriberAttributes Rparam;
+    Rparam.topic.topicDataType = sampleType.getName();
+    Rparam.topic.topicName = "samplePubSubTopic";
+    Rparam.historyMemoryPolicy = DYNAMIC_RESERVE_MEMORY_MODE;
+
+    Rparam.topic.topicKind = NO_KEY;
+    Rparam.topic.historyQos.kind = KEEP_ALL_HISTORY_QOS;
+    Rparam.qos.m_durability.kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    Rparam.qos.m_reliability.kind = RELIABLE_RELIABILITY_QOS;
+    Rparam.topic.historyQos.depth =  50;
+    Rparam.topic.resourceLimitsQos.max_samples = 100;
+    Rparam.topic.resourceLimitsQos.max_instances = 1;
+    Rparam.topic.resourceLimitsQos.max_samples_per_instance = 100;
+
+    std::cout << "Creating Subscriber..." << std::endl;
+    Subscriber *mySub1= Domain::createSubscriber(PubParticipant, Rparam, nullptr);
+    if(myPub == nullptr){
+        std::cout << "something went wrong while creating the Subscriber..." << std::endl;
+        return;
+    }
+    
+    //Send 20 samples
+    std::cout << "Publishing 10 samples on the topic..." << std::endl;
+    for(uint8_t j=0; j < 10; j++){
+        my_sample.index(j+1); 
+        my_sample.key_value(1);
+        myPub->write(&my_sample);
+    }
+
+    eClock::my_sleep(1500);
+
+    //Read the contents of both histories:
+        std::cout << "The Reliable Subscriber holds: " << std::endl;
+    while(mySub1->readNextData(&my_sample, &sample_info)){
+        std::cout << std::to_string(my_sample.index()) << " ";
+    }
+    std::cout << std::endl;
+
 
 
 
