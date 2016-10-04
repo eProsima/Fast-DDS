350625e9afc46adb69b22f5566174ed8d3cbac02
Santiago Munoz <santiagomunoz@eprosima.com>
2016-10-04 13:51:31 +0200

    Const members in CryptoKeyExanche

--- include/fastrtps/rtps/security/cryptography/CryptoKeyExchange.h
+++ include/fastrtps/rtps/security/cryptography/CryptoKeyExchange.h
@@ -32,12 +32,13 @@ class CryptoKeyExchange
      * @param local_participant_crypto_tokens (out) Returned CryptoTokenSeq.
      * @param local_participant_crypto CryptoHandle returned by a previous call to register_local_participant.
      * @param remote_participant_crypto CryptoHangle returned by a previous call to register_remote_participant.
+     * @param exception (out) Security exception
      * @return TRUE is successful.
      */
     bool create_local_participant_crypto_tokens(
             ParticipantCryptoTokenSeq &local_participant_crypto_tokens,
-            ParticipantCryptoHandle &local_participant_crypto,
-            ParticipantCryptoHandle &remote_participant_crypto,
+            const ParticipantCryptoHandle &local_participant_crypto,
+            const ParticipantCryptoHandle &remote_participant_crypto,
             SecurityException &exception);
 
     /**
@@ -45,13 +46,13 @@ class CryptoKeyExchange
      * @param local_participant_crypto CryptoHandle returned by a previous call to register_local_participant.
      * @param remote_participant_crypto CryptoHandle returned by a previous call to register_matched_remote_participant.
      * @param remote_participant_tokens CryptoToken sequence received from the remote Participant
-     * @param exception Security exception 
+     * @param exception (out) Security exception 
      * @return TRUE if successful
      */
     bool set_remote_participant_crypto_tokens(
-            ParticipantCryptoHandle &local_participant_crypto,
-            ParticipantCryptoHandle &remote_participant_crypto,
-            ParticipantCryptoTokenSeq &remote_participant_tokens,
+            const ParticipantCryptoHandle &local_participant_crypto,
+            const ParticipantCryptoHandle &remote_participant_crypto,
+            const ParticipantCryptoTokenSeq &remote_participant_tokens,
             SecurityException &exception);
 
     /**
@@ -59,27 +60,27 @@ class CryptoKeyExchange
      * @param local_datawriter_crypto_tokens (out) Returned CryptoSeq
      * @param local_datawriter_crypto CryptoHandle returned by a previous call to register_local_datawriter.
      * @param remote_datawriter_crypto CryptoHandle returned by a previous call to register_matched_remote_datareader
-     * @param exception Security exception
+     * @param exception (out) Security exception
      * @return TRUE if successful
      */
     bool create_local_datawriter_crypto_tokens(
             DataWriterCryptoTokenSeq &local_datawriter_crypto_tokens,
-            DataWriterCryptoHandle &local_datawriter_crypto,
-            DataReaderCryptoHandle &remote_datareader_crypto,
+            const DataWriterCryptoHandle &local_datawriter_crypto,
+            const DataReaderCryptoHandle &remote_datareader_crypto,
             SecurityException &exception);
 
     /**
      * Creates CryptoTokens containing the info to decrypt text encoded by the local DataReader.
-     * @param local_datareader_crypto_tokens
+     * @param local_datareader_crypto_tokens (out)
      * @param local_datareader_crypto
      * @param remote_datawriter_crypto
-     * @param exception Security exception
+     * @param exception (out) Security exception
      * @return TRUE if successful
      */
     bool create_local_datareader_crypto_tokens(
             DataReaderCryptoTokenSeq &local_datareader_crypto_tokens,
-            DataReaderCryptoHandle &local_datareader_crypto,
-            DataWriterCryptoHandle &remote_datawriter_crypto,
+            const DataReaderCryptoHandle &local_datareader_crypto,
+            const DataWriterCryptoHandle &remote_datawriter_crypto,
             SecurityException &exception);
 
     /**
@@ -87,23 +88,23 @@ class CryptoKeyExchange
      * @param local_datawriter_crypto
      * @param remote_datareader_crypto
      * @param remote_datareader_tokens
-     * @param exception Security exception
+     * @param exception (out) Security exception
      * @return TRUE if successful
      */
     bool set_remote_datareader_crypto_tokens(
-            DataWriterCryptoHandle &local_datawriter_crypto,
-            DataReaderCryptoHandle &remote_datareader_crypto,
-            DataReaderCryptoTokenSeq &remote_datareader_tokens,
+            const DataWriterCryptoHandle &local_datawriter_crypto,
+            const DataReaderCryptoHandle &remote_datareader_crypto,
+            const DataReaderCryptoTokenSeq &remote_datareader_tokens,
             SecurityException &exception);
 
     /**
      * Release resources associated with a CryptoTokenSeq
      * @param crypto_tokens
-     * @param exception Security exception
+     * @param exception (out) Security exception
      * @return TRUE if successful
      */
     bool return_crypto_tokens(
-            CryptoTokenSeq &crypto_tokens,
+            const CryptoTokenSeq &crypto_tokens,
             SecurityException *exception);
 
 };
