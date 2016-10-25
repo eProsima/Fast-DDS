fe3098347bd62fb7b738928270596bb2e956ca5c
Santiago Munoz <santiagomunoz@eprosima.com>
2016-10-25 15:30:25 +0200

    RegisterLocalParticipant test

--- test/unittest/security/cryptography/CryptographyPluginTests.hpp
+++ test/unittest/security/cryptography/CryptographyPluginTests.hpp
@@ -1,13 +1,59 @@
 
+
+
+#ifndef _UNITTEST_SECURITY_CRYPTOGRAPHY_CRYPTOGRAPHYPLUGINTESTS_HPP_
+#define _UNITTEST_SECURITY_CRYPTOGRAPHY_CRYPTOGRAPHYPLUGINTESTS_HPP_
+
+#include "../../../../src/cpp/security/cryptography/AESGCMGMAC.h"
+
 #include <gtest/gtest.h>
 
-
+using namespace eprosima::fastrtps::rtps;
+using namespace ::security;
 
 class CryptographyPluginTest : public ::testing::Test{
 
+    protected:
+        virtual void SetUp(){
+            PropertyPolicy m_propertypolicy;
 
+            CryptoPlugin = new AESGCMGMAC(m_propertypolicy);
+            
+        }
+        virtual void TearDown(){
+            delete CryptoPlugin;
 
+        }
 
+    public:
+        CryptographyPluginTest():CryptoPlugin(nullptr){};
 
+        static IdentityHandle* create_placeholder_identityhandle();
+        static PermissionsHandle* create_placeholder_permissionshandle();
+        static PropertySeq* create_placeholder_propertyseq();
+    
+        static bool check_localparticipanthandle(PropertySeq &properties, ParticipantCryptoHandle *local);
+
+        AESGCMGMAC* CryptoPlugin;
 
 };
+
+
+
+TEST_F(CryptographyPluginTest, factory_createLocalParticipantHandle)
+{
+
+    IdentityHandle* i_handle = CryptographyPluginTest::create_placeholder_identityhandle();
+    PermissionsHandle* perm_handle = CryptographyPluginTest::create_placeholder_permissionshandle();
+    PropertySeq* prop_handle = CryptographyPluginTest::create_placeholder_propertyseq();
+    SecurityException exception;
+
+    ParticipantCryptoHandle *target = CryptoPlugin->m_cryptokeyfactory->register_local_participant(*i_handle,*perm_handle,*prop_handle,exception);
+
+    ASSERT_TRUE(target != nullptr);
+
+    //Dissect results to check correct creation
+    ASSERT_TRUE(CryptographyPluginTest::check_localparticipanthandle(*prop_handle, target) );
+
+}
+#endif
--- test/unittest/security/cryptography/builtinAESGCMGMACTests.cpp
+++ test/unittest/security/cryptography/builtinAESGCMGMACTests.cpp
@@ -2,7 +2,70 @@
 
 
 #include <iostream>
+#include <algorithm>
 #include "CryptographyPluginTests.hpp"
+
+IdentityHandle* CryptographyPluginTest::create_placeholder_identityhandle(){
+    IdentityHandle* buffer;
+
+    return buffer;
+}
+
+PermissionsHandle* CryptographyPluginTest::create_placeholder_permissionshandle(){
+    PermissionsHandle* buffer;
+
+    return buffer;
+}
+
+PropertySeq* CryptographyPluginTest::create_placeholder_propertyseq(){
+    PropertySeq* buffer;
+
+    return buffer;
+}
+
+bool CryptographyPluginTest::check_localparticipanthandle(PropertySeq &properties, ParticipantCryptoHandle *local){
+
+    AESGCMGMAC_ParticipantCryptoHandle& local_participant = AESGCMGMAC_ParticipantCryptoHandle::narrow(*local);
+    bool result = true;
+    
+    if(local_participant->ParticipantKeyMaterial == nullptr){
+        std::cout << "LocalParticipantHandle does not have ParticipantKeyMaterial" << std::endl;
+        result = false;
+    }
+    if(local_participant->Participant2ParticipantKeyMaterial.size() != 0){
+        std::cout << "LocalParticipantHandle does have ParticipantKeyMaterial when it should not" << std::endl;
+        result = false;
+    }
+    if(local_participant->Participant2ParticipantKxKeyMaterial.size() != 0){
+        std::cout << "LocalParticipantHandle does have ParticipantKxKeymaterial when it should not" << std::endl;
+        result = false;
+    }
+    
+    if(local_participant->ParticipantKeyMaterial->transformation_kind != std::array<uint8_t,4>(CRYPTO_TRANSFORMATION_KIND_AES128_GCM)){
+        //TODO (Santiago) - Configuration of the transformation kind should be done via PropertySeq. This oughta be implemented in the near future and the test will then must be updated
+        std::cout << "Incorrect transformation_kind present in ParticipantKeyMaterial" << std::endl;
+        result = false;
+    }
+
+    if( std::all_of(local_participant->ParticipantKeyMaterial->master_salt.begin(),local_participant->ParticipantKeyMaterial->master_salt.end(), [](uint8_t i){return i==0;}) ){
+        std::cout << "ParticipantKeyMaterial master_salt is uninitialized" << std::endl;
+        result = false;
+            }
+
+
+    /*if(local_participant->ParticipantKeyMaterial->master_sender_key ==
+
+    if(local_participant->ParticipantKeyMaterial->receiver_specific_key_id
+
+    */
+    if( std::all_of(local_participant->ParticipantKeyMaterial->master_receiver_specific_key.begin(),local_participant->ParticipantKeyMaterial->master_receiver_specific_key.end(), [](uint8_t i){return i==0;}) ) {
+        std::cout << "ParticipantKeyMaterial as receiver_key set when it shouldnt" << std::endl;
+        result = false;
+    }
+
+
+    return result;
+}
 
 TEST_F(CryptographyPluginTest, mocktest){
     uint8_t mock = 7;
