a8f793e090410b0e3ba5d4b321b42988fd295182
Esteve Fernandez <esteve@apache.org>
2017-03-06 14:21:05 +0100

    Updates for NDK 17.
    Added optional support for C++14.
    
    Always assume that asio can use move semantics.
    
    Embed TinyXML2 and Asio if THIRDPARTY is enabled, even if they are found on the system
    
    Switch to asio 1.12.0

--- include/fastrtps/transport/UDPv6Transport.h
+++ include/fastrtps/transport/UDPv6Transport.h
@@ -55,33 +55,20 @@ class UDPv6Transport : public TransportInterface
     {
         public:
 
-#if defined(ASIO_HAS_MOVE)
             SocketInfo(asio::ip::udp::socket& socket) :
                 socket_(std::move(socket)), only_multicast_purpose_(false)
-#else
-            SocketInfo(std::shared_ptr<asio::ip::udp::socket> socket) :
-                socket_(socket), only_multicast_purpose_(false)
-#endif
             {
             }
 
             SocketInfo(SocketInfo&& socketInfo) :
-#if defined(ASIO_HAS_MOVE)
                 socket_(std::move(socketInfo.socket_)),
-#else
-                socket_(socketInfo.socket_),
-#endif
                 only_multicast_purpose_(socketInfo.only_multicast_purpose_)
             {
             }
 
             SocketInfo& operator=(SocketInfo&& socketInfo)
             {
-#if defined(ASIO_HAS_MOVE)
                 socket_ = std::move(socketInfo.socket_);
-#else
-                socket_ = socketInfo.socket_;
-#endif
                 only_multicast_purpose_ = socketInfo.only_multicast_purpose_;
                 return *this;
             }
@@ -101,11 +88,7 @@ class UDPv6Transport : public TransportInterface
                 return only_multicast_purpose_;
             }
 
-#if defined(ASIO_HAS_MOVE)
             asio::ip::udp::socket socket_;
-#else
-            std::shared_ptr<asio::ip::udp::socket> socket_;
-#endif
             bool only_multicast_purpose_;
 
         private:
@@ -214,11 +197,7 @@ private:
    struct LocatorCompare{ bool operator()(const Locator_t& lhs, const Locator_t& rhs) const
                         {return (memcmp(&lhs, &rhs, sizeof(Locator_t)) < 0); } };
    //! For both modes, an input channel corresponds to a port.
-#if defined(ASIO_HAS_MOVE)
    std::map<uint32_t, asio::ip::udp::socket> mInputSockets;
-#else
-   std::map<uint32_t, std::shared_ptr<asio::ip::udp::socket>> mInputSockets;
-#endif
 
    bool IsInterfaceAllowed(const asio::ip::address_v6& ip);
    std::vector<asio::ip::address_v6> mInterfaceWhiteList;
@@ -227,7 +206,6 @@ private:
    bool OpenAndBindOutputSockets(Locator_t& locator);
    bool OpenAndBindInputSockets(uint32_t port, bool is_multicast);
 
-#if defined(ASIO_HAS_MOVE)
    asio::ip::udp::socket OpenAndBindUnicastOutputSocket(const asio::ip::address_v6&, uint32_t& port);
    asio::ip::udp::socket OpenAndBindInputSocket(uint32_t port, bool is_multicast);
 
@@ -235,15 +213,6 @@ private:
                           uint32_t sendBufferSize,
                           const Locator_t& remoteLocator,
                           asio::ip::udp::socket& socket);
-#else
-   std::shared_ptr<asio::ip::udp::socket> OpenAndBindUnicastOutputSocket(const asio::ip::address_v6&, uint32_t& port);
-   std::shared_ptr<asio::ip::udp::socket> OpenAndBindInputSocket(uint32_t port, bool is_multicast);
-
-   bool SendThroughSocket(const octet* sendBuffer,
-                          uint32_t sendBufferSize,
-                          const Locator_t& remoteLocator,
-                          std::shared_ptr<asio::ip::udp::socket> socket);
-#endif
 };
 
 } // namespace rtps
