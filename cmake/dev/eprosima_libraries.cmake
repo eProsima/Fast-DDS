1089b16ecfb7919e0c4441804a473a86f8779ea7
Ricardo González Moreno <correoricky@gmail.com>
2017-10-31 14:24:01 +0100

    Refs #2315. Support for multiple same thirdparty

--- cmake/dev/eprosima_libraries.cmake
+++ cmake/dev/eprosima_libraries.cmake
@@ -15,125 +15,133 @@
 macro(find_eprosima_package package)
     if(NOT (EPROSIMA_INSTALLER AND (MSVC OR MSVC_IDE)))
         if(THIRDPARTY)
-            set(${package}ExternalDir ${PROJECT_BINARY_DIR}/external/${package})
+            set(EPROSIMA_PACKAGE_EXTERNAL_DIR "" CACHE STRING "External directory to compile eprosima libraries")
 
-            if(MINION)
-                set(CMAKE_INSTALL_PREFIX_ "${CMAKE_INSTALL_PREFIX}")
+            if("${EPROSIMA_PACKAGE_EXTERNAL_DIR}" STREQUAL "")
+                set(${package}ExternalDir ${PROJECT_BINARY_DIR}/external/${package})
             else()
-                set(CMAKE_INSTALL_PREFIX_ "${PROJECT_BINARY_DIR}/external/install")
+                set(${package}ExternalDir ${EPROSIMA_PACKAGE_EXTERNAL_DIR}/${package})
             endif()
 
-            set(ANDROID_BUILD_OPTIONS "")
-            if(ANDROID)
-                set(ANDROID_BUILD_OPTIONS "-DANDROID_ABI=${ANDROID_ABI}"
-                    "-DANDROID_NDK=${ANDROID_NDK}"
-                    "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
-                    "-DANDROID_NATIVE_API_LEVEL=${ANDROID_NATIVE_API_LEVEL}"
-                    "-DANDROID_TOOLCHAIN=${ANDROID_TOOLCHAIN}"
-                    "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"               
-                    "-DANDROID_STL=${ANDROID_STL}"
-                    "-DANDROID_PIE=${ANDROID_PIE}"
-                    "-DANDROID_CPP_FEATURES=${ANDROID_CPP_FEATURES}"
-                    "-DANDROID_ALLOW_UNDEFINED_SYMBOLS=${ANDROID_ALLOW_UNDEFINED_SYMBOLS}"
-                    "-DANDROID_ARM_MODE=${ANDROID_ARM_MODE}"
-                    "-DANDROID_ARM_NEON=${ANDROID_ARM_NEON}"
-                    "-DANDROID_DISABLE_NO_EXECUTE=${ANDROID_DISABLE_NO_EXECUTE}"
-                    "-DANDROID_DISABLE_RELRO=${ANDROID_DISABLE_RELRO}"
-                    "-DANDROID_DISABLE_FORMAT_STRING_CHECKS=${ANDROID_DISABLE_FORMAT_STRING_CHECKS}"
-                    "-DANDROID_FUNCTION_LEVEL_LINKING=${ANDROID_FUNCTION_LEVEL_LINKING}"
-                    )
-            endif()
-
-            # Separate CMAKE_PREFIX_PATH
-            string(REPLACE ";" "|" CMAKE_PREFIX_PATH_ "${CMAKE_PREFIX_PATH}")
-            string(REPLACE " " "\\ " CMAKE_C_FLAGS_ "${CMAKE_C_FLAGS}")
-            string(REPLACE " " "\\ " CMAKE_CXX_FLAGS_ "${CMAKE_CXX_FLAGS}")
-
-            set(${package}_CMAKE_ARGS
-                "\${SOURCE_DIR_}"
-                "\${GENERATOR_}"
-                "\${CMAKE_C_COMPILER_}"
-                "\${CMAKE_CXX_COMPILER_}"
-                "-DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS_}"
-                "-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS_}"
-                ${BUILD_OPTION}
-                ${ANDROID_BUILD_OPTIONS}
-                "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-                "-DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}"
-                "-DMINION=ON"
-                "-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
-                "-DBIN_INSTALL_DIR:PATH=${BIN_INSTALL_DIR}"
-                "-DINCLUDE_INSTALL_DIR:PATH=${INCLUDE_INSTALL_DIR}"
-                "-DLIB_INSTALL_DIR:PATH=${LIB_INSTALL_DIR}"
-                "-DLICENSE_INSTALL_DIR:PATH=licenses"
-                "\${CMAKE_INSTALL_PREFIX_}"
-                "\${CMAKE_PREFIX_PATH_}"
-                )
-            list(APPEND ${package}_CMAKE_ARGS LIST_SEPARATOR "|")
-
-            file(MAKE_DIRECTORY ${${package}ExternalDir})
-            file(WRITE ${${package}ExternalDir}/CMakeLists.txt
-                "cmake_minimum_required(VERSION 2.8.12)\n"
-                "include(ExternalProject)\n"
-                "set(SOURCE_DIR_ \"${PROJECT_SOURCE_DIR}/thirdparty/${package}\")\n"
-                "set(GENERATOR_ -G \"${CMAKE_GENERATOR}\")\n"
-                "set(CMAKE_INSTALL_PREFIX_ \"-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX_}\")\n"
-                "set(CMAKE_PREFIX_PATH_ -DCMAKE_PREFIX_PATH=\"${CMAKE_PREFIX_PATH_}\")\n"
-                "set(CMAKE_C_COMPILER_ \"-DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}\")\n"
-                "set(CMAKE_CXX_COMPILER_ \"-DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}\")\n"
-                "ExternalProject_Add(${package}\n"
-                "CONFIGURE_COMMAND \"${CMAKE_COMMAND}\"\n"
-                "${${package}_CMAKE_ARGS}\n"
-                "DOWNLOAD_COMMAND \"\"\n"
-                "UPDATE_COMMAND cd \"${PROJECT_SOURCE_DIR}\" && git submodule update --recursive --init \"thirdparty/${package}\"\n"
-                "SOURCE_DIR \${SOURCE_DIR_}\n"
-                "BINARY_DIR \"${${package}ExternalDir}/build\"\n"
-                ")\n")
-
-            execute_process(COMMAND ${CMAKE_COMMAND}
-                -G ${CMAKE_GENERATOR}
-                ${BUILD_OPTION}
-                ${ANDROID_BUILD_OPTIONS}
-                WORKING_DIRECTORY ${${package}ExternalDir}
-                RESULT_VARIABLE EXECUTE_RESULT
-                )
-
-            if(NOT EXECUTE_RESULT EQUAL 0)
-                message(FATAL_ERROR "Cannot configure Git submodule ${package}")
-            endif()
-
-            if(MSVC OR MSVC_IDE)
-                if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-                    set(BUILD_TYPE_GENERATION "Release")
+            if(NOT EXISTS "${${package}ExternalDir}/cmake_install.cmake")
+                if(MINION)
+                    set(CMAKE_INSTALL_PREFIX_ "${CMAKE_INSTALL_PREFIX}")
                 else()
-                    set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
+                    set(CMAKE_INSTALL_PREFIX_ "${PROJECT_BINARY_DIR}/external/install")
                 endif()
 
-                execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Debug
+                set(ANDROID_BUILD_OPTIONS "")
+                if(ANDROID)
+                    set(ANDROID_BUILD_OPTIONS "-DANDROID_ABI=${ANDROID_ABI}"
+                        "-DANDROID_NDK=${ANDROID_NDK}"
+                        "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}"
+                        "-DANDROID_NATIVE_API_LEVEL=${ANDROID_NATIVE_API_LEVEL}"
+                        "-DANDROID_TOOLCHAIN=${ANDROID_TOOLCHAIN}"
+                        "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"               
+                        "-DANDROID_STL=${ANDROID_STL}"
+                        "-DANDROID_PIE=${ANDROID_PIE}"
+                        "-DANDROID_CPP_FEATURES=${ANDROID_CPP_FEATURES}"
+                        "-DANDROID_ALLOW_UNDEFINED_SYMBOLS=${ANDROID_ALLOW_UNDEFINED_SYMBOLS}"
+                        "-DANDROID_ARM_MODE=${ANDROID_ARM_MODE}"
+                        "-DANDROID_ARM_NEON=${ANDROID_ARM_NEON}"
+                        "-DANDROID_DISABLE_NO_EXECUTE=${ANDROID_DISABLE_NO_EXECUTE}"
+                        "-DANDROID_DISABLE_RELRO=${ANDROID_DISABLE_RELRO}"
+                        "-DANDROID_DISABLE_FORMAT_STRING_CHECKS=${ANDROID_DISABLE_FORMAT_STRING_CHECKS}"
+                        "-DANDROID_FUNCTION_LEVEL_LINKING=${ANDROID_FUNCTION_LEVEL_LINKING}"
+                        )
+                endif()
+
+                # Separate CMAKE_PREFIX_PATH
+                string(REPLACE ";" "|" CMAKE_PREFIX_PATH_ "${CMAKE_PREFIX_PATH}")
+                string(REPLACE " " "\\ " CMAKE_C_FLAGS_ "${CMAKE_C_FLAGS}")
+                string(REPLACE " " "\\ " CMAKE_CXX_FLAGS_ "${CMAKE_CXX_FLAGS}")
+
+                set(${package}_CMAKE_ARGS
+                    "\${SOURCE_DIR_}"
+                    "\${GENERATOR_}"
+                    "\${CMAKE_C_COMPILER_}"
+                    "\${CMAKE_CXX_COMPILER_}"
+                    "-DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS_}"
+                    "-DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS_}"
+                    ${BUILD_OPTION}
+                    ${ANDROID_BUILD_OPTIONS}
+                    "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+                    "-DCMAKE_CXX_COMPILER_LAUNCHER=${CMAKE_CXX_COMPILER_LAUNCHER}"
+                    "-DMINION=ON"
+                    "-DEPROSIMA_INSTALLER_MINION=${EPROSIMA_INSTALLER_MINION}"
+                    "-DBIN_INSTALL_DIR:PATH=${BIN_INSTALL_DIR}"
+                    "-DINCLUDE_INSTALL_DIR:PATH=${INCLUDE_INSTALL_DIR}"
+                    "-DLIB_INSTALL_DIR:PATH=${LIB_INSTALL_DIR}"
+                    "-DLICENSE_INSTALL_DIR:PATH=licenses"
+                    "\${CMAKE_INSTALL_PREFIX_}"
+                    "\${CMAKE_PREFIX_PATH_}"
+                    )
+                list(APPEND ${package}_CMAKE_ARGS LIST_SEPARATOR "|")
+
+                file(MAKE_DIRECTORY ${${package}ExternalDir})
+                file(WRITE ${${package}ExternalDir}/CMakeLists.txt
+                    "cmake_minimum_required(VERSION 2.8.12)\n"
+                    "include(ExternalProject)\n"
+                    "set(SOURCE_DIR_ \"${PROJECT_SOURCE_DIR}/thirdparty/${package}\")\n"
+                    "set(GENERATOR_ -G \"${CMAKE_GENERATOR}\")\n"
+                    "set(CMAKE_INSTALL_PREFIX_ \"-DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX_}\")\n"
+                    "set(CMAKE_PREFIX_PATH_ -DCMAKE_PREFIX_PATH=\"${CMAKE_PREFIX_PATH_}\")\n"
+                    "set(CMAKE_C_COMPILER_ \"-DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}\")\n"
+                    "set(CMAKE_CXX_COMPILER_ \"-DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}\")\n"
+                    "ExternalProject_Add(${package}\n"
+                    "CONFIGURE_COMMAND \"${CMAKE_COMMAND}\"\n"
+                    "${${package}_CMAKE_ARGS}\n"
+                    "DOWNLOAD_COMMAND \"\"\n"
+                    "UPDATE_COMMAND cd \"${PROJECT_SOURCE_DIR}\" && git submodule update --recursive --init \"thirdparty/${package}\"\n"
+                    "SOURCE_DIR \${SOURCE_DIR_}\n"
+                    "BINARY_DIR \"${${package}ExternalDir}/build\"\n"
+                    ")\n")
+
+                execute_process(COMMAND ${CMAKE_COMMAND}
+                    -G ${CMAKE_GENERATOR}
+                    ${BUILD_OPTION}
+                    ${ANDROID_BUILD_OPTIONS}
                     WORKING_DIRECTORY ${${package}ExternalDir}
                     RESULT_VARIABLE EXECUTE_RESULT
                     )
 
                 if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package} in Debug mode")
+                    message(FATAL_ERROR "Cannot configure Git submodule ${package}")
                 endif()
 
-                execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION}
-                    WORKING_DIRECTORY ${${package}ExternalDir}
-                    RESULT_VARIABLE EXECUTE_RESULT
-                    )
+                if(MSVC OR MSVC_IDE)
+                    if("${CMAKE_BUILD_TYPE}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+                        set(BUILD_TYPE_GENERATION "Release")
+                    else()
+                        set(BUILD_TYPE_GENERATION ${CMAKE_BUILD_TYPE})
+                    endif()
 
-                if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
-                endif()
-            else()
-                execute_process(COMMAND ${CMAKE_COMMAND} --build .
-                    WORKING_DIRECTORY ${${package}ExternalDir}
-                    RESULT_VARIABLE EXECUTE_RESULT
-                    )
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build . --config Debug
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
 
-                if(NOT EXECUTE_RESULT EQUAL 0)
-                    message(FATAL_ERROR "Cannot build Git submodule ${package}")
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package} in Debug mode")
+                    endif()
+
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build . --config ${BUILD_TYPE_GENERATION}
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
+
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package} in ${BUILD_TYPE_GENERATION} mode")
+                    endif()
+                else()
+                    execute_process(COMMAND ${CMAKE_COMMAND} --build .
+                        WORKING_DIRECTORY ${${package}ExternalDir}
+                        RESULT_VARIABLE EXECUTE_RESULT
+                        )
+
+                    if(NOT EXECUTE_RESULT EQUAL 0)
+                        message(FATAL_ERROR "Cannot build Git submodule ${package}")
+                    endif()
                 endif()
             endif()
 
