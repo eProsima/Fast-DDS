834b98e44bf1e452489413eb75193e1e2a3578ae
Miguel Company <miguelcompany@eprosima.com>
2018-02-21 15:55:55 +0100

    Refs #2535. - Fixed hasChanged value when setting whole list of partition names - Added XML loading functions to PubSubReader and PubSubWriter - Regression test added

--- test/blackbox/BlackboxTests.cpp
+++ test/blackbox/BlackboxTests.cpp
@@ -1972,6 +1972,41 @@ BLACKBOXTEST(BlackBox, LocalInitialPeers)
     reader.block_for_all();
 }
 
+// Regression test of Refs #2535, github micro-RTPS #1
+BLACKBOXTEST(BlackBox, PubXmlLoadedPartition)
+{
+    PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+
+    reader.partition("A").init();
+
+    ASSERT_TRUE(reader.isInitialized());
+
+    const std::string xml = R"(<profiles>
+  <publisher profile_name="partition_publisher_profile">
+    <topic>
+      <name>)" + writer.topic_name() + R"(</name>
+      <dataType>HelloWorldType</dataType>
+    </topic>
+    <qos>
+      <partition>
+        <names>
+          <name>A</name>
+        </names>
+      </partition>
+    </qos>
+    </publisher>
+</profiles>)";
+
+    writer.load_publisher_attr(xml).init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    reader.waitDiscovery();
+    writer.waitDiscovery();
+}
+
+
 #if HAVE_SECURITY
 
 BLACKBOXTEST(BlackBox, BuiltinAuthenticationPlugin_PKIDH_validation_ok)
