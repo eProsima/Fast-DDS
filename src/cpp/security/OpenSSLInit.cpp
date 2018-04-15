9d368bf1a28c793e0c047719ad71e580fd734b89
Mikael Arguedas <mikael@osrfoundation.org>
2018-04-14 22:30:56 -0700

    fix deprecated openssl macros

--- src/cpp/security/OpenSSLInit.cpp
+++ src/cpp/security/OpenSSLInit.cpp
@@ -14,7 +14,11 @@ class OpenSSLInit
 
         ~OpenSSLInit()
         {
+#if OPENSSL_VERSION_NUMBER < 0x10000000L
             ERR_remove_state(0);
+#elif OPENSSL_VERSION_NUMBER < 0x10100000L
+            ERR_remove_thread_state(NULL);
+#endif
             ENGINE_cleanup();
             RAND_cleanup();
             CRYPTO_cleanup_all_ex_data();
