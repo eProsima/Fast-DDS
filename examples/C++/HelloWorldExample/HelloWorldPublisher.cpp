76647f137bf22600592624a6bd5d4f6bb285712b
Luis Gasco <luisgasco@eprosima.com>
2018-05-07 10:27:30 +0200

    Refs #2864 Removed unused defaultSendPort param.

--- examples/C++/HelloWorldExample/HelloWorldPublisher.cpp
+++ examples/C++/HelloWorldExample/HelloWorldPublisher.cpp
@@ -41,7 +41,7 @@ bool HelloWorldPublisher::init()
     m_Hello.index(0);
     m_Hello.message("HelloWorld");
     ParticipantAttributes PParam;
-    PParam.rtps.defaultSendPort = 11511;
+    PParam.rtps.use_IP6_to_send = true;
     PParam.rtps.builtin.use_SIMPLE_RTPSParticipantDiscoveryProtocol = true;
     PParam.rtps.builtin.use_SIMPLE_EndpointDiscoveryProtocol = true;
     PParam.rtps.builtin.m_simpleEDP.use_PublicationReaderANDSubscriptionWriter = true;
