3d143f9b2fc09d1c84ef96be32c5bb3a87eb9a1b
Luis Gasco <luisgasco@eprosima.com>
2018-05-14 08:20:13 +0200

    Refs #2872

--- include/fastrtps/transport/tcp/RemoteLocator.h
+++ include/fastrtps/transport/tcp/RemoteLocator.h
@@ -7,6 +7,9 @@ namespace eprosima{
 namespace fastrtps{
 namespace rtps{
 
+/**
+ * Locator_t subclass that doesn't compare logical port.
+ * */
 class RemoteLocator_t : public Locator_t
 {
 public:
@@ -25,7 +28,7 @@ inline bool operator<(const RemoteLocator_t &loc1, const RemoteLocator_t &loc2)
             return true;
     }
 
-    if(loc1.get_Logical_port() < loc2.get_Logical_port())
+    if(loc1.get_TCP_port() < loc2.get_TCP_port())
         return true;
 
     return false;
@@ -35,7 +38,7 @@ inline bool operator==(const RemoteLocator_t&loc1, const RemoteLocator_t& loc2)
 {
     if(loc1.kind!=loc2.kind)
         return false;
-    if(loc1.get_Logical_port() != loc2.get_Logical_port())
+    if(loc1.get_TCP_port() != loc2.get_TCP_port())
         return false;
     if(!std::equal(loc1.address,loc1.address+16,loc2.address))
         return false;
