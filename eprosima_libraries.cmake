5ff22247a4b77946b083760ad30de1aa70bdc980
Ricardo González Moreno <correoricky@gmail.com>
2018-07-04 16:40:12 +0200

    Squashed 'cmake/common/' changes from 90161276..17e21e6f
    17e21e6f Merge branch 'feature/vs2017' into 'master'
    75479759 Updated cmake/common
    1a597d21 Refactoring on cmake files
    06125198 Merge branch 'feature/epro-lib-options' into 'master'
    c67faa53 Updating rpcdds to fastrtps 1.6.0
    0b1b7769 Merge branch 'feature/update_gtest' into 'master'
    e7a8cf75 Added old behaviour with dependencies in gtest
    bcb1c9c4 add thirdparty directory to CMAKE_REFIX_PATH only if THIRDPARTY option is specified
    d43a9878 Refs #3010. Fixing windows installer.
    f7b5146f Updates for NDK 17.
    2a493f59 Refs #3009. Fixing errors creating linux installer
    
    git-subtree-dir: cmake/common
    git-subtree-split: 17e21e6f9e26be6e19e0aed7d496ca308144224d

--- eprosima_libraries.cmake
+++ eprosima_libraries.cmake
@@ -14,30 +14,47 @@
 
 macro(eprosima_find_package package)
 
-    if(NOT ${package}_FOUND AND NOT (EPROSIMA_INSTALLER AND (MSVC OR MSVC_IDE)))
+    if(NOT ${package}_FOUND)
 
         # Parse arguments.
         set(options REQUIRED)
-        cmake_parse_arguments(FIND "${options}" "" "" ${ARGN})
+        set(multiValueArgs OPTIONS)
+        cmake_parse_arguments(FIND "${options}" "" "${multiValueArgs}" ${ARGN})
+
+        option(THIRDPARTY "Activate the use of internal thirdparties" OFF)
+        option(THIRDPARTY_UPDATE "Activate the auto update of internal thirdparties" ON)
+
+        if(EPROSIMA_BUILD)
+            set(THIRDPARTY ON)
+        endif()
 
         option(THIRDPARTY_${package} "Activate the use of internal thirdparty ${package}" OFF)
 
-        find_package(${package} QUIET)
+        if(NOT EPROSIMA_INSTALLER)
+            find_package(${package} QUIET)
+        endif()
+
         if(NOT ${package}_FOUND AND (THIRDPARTY OR THIRDPARTY_${package}))
             set(SUBDIRECTORY_EXIST TRUE)
-            if(NOT EXISTS "${PROJECT_SOURCE_DIR}/thirdparty/${package}/CMakeLists.txt")
+            if(THIRDPARTY_UPDATE OR NOT EXISTS "${PROJECT_SOURCE_DIR}/thirdparty/${package}/CMakeLists.txt")
+                message(STATUS "${package} thirdparty is being updated...")
                 execute_process(
                     COMMAND git submodule update --recursive --init "thirdparty/${package}"
                     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                     RESULT_VARIABLE EXECUTE_RESULT
                     )
                 if(NOT EXECUTE_RESULT EQUAL 0)
-                    set(SUBDIRECTORY_EXIST FALSE)
                     message(WARNING "Cannot configure Git submodule ${package}")
+                    if(NOT EXISTS "${PROJECT_SOURCE_DIR}/thirdparty/${package}/CMakeLists.txt")
+                        set(SUBDIRECTORY_EXIST FALSE)
+                    endif()
                 endif()
             endif()
 
             if(SUBDIRECTORY_EXIST)
+                foreach(opt_ ${FIND_OPTIONS})
+                    set(${opt_} ON)
+                endforeach()
                 add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/${package})
                 set(${package}_FOUND TRUE)
                 if(NOT IS_TOP_LEVEL)
@@ -72,10 +89,10 @@ macro(eprosima_find_thirdparty package thirdparty_name)
             else()
                 message(FATAL_ERROR "Cannot configure Git submodule ${package}")
             endif()
+            set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${PROJECT_SOURCE_DIR}/thirdparty/${thirdparty_name})
+            set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${PROJECT_SOURCE_DIR}/thirdparty/${thirdparty_name}/${thirdparty_name})
         endif()
 
-        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${PROJECT_SOURCE_DIR}/thirdparty/${thirdparty_name})
-        set(CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} ${PROJECT_SOURCE_DIR}/thirdparty/${thirdparty_name}/${thirdparty_name})
         find_package(${package} REQUIRED QUIET)
 
     endif()
