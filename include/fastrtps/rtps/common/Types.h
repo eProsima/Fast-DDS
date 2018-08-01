15afe4d757dbd3f5206575cd7b94e6b05329b80a
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-08-01 10:32:46 +0200

    Refs #3267. Implement some modifications from the merge request
    # Conflicts:
    #	include/fastrtps/attributes/PublisherAttributes.h
    #	include/fastrtps/attributes/SubscriberAttributes.h
    #	include/fastrtps/rtps/attributes/RTPSParticipantAttributes.h
    #	include/fastrtps/rtps/common/Locator.h
    
    # Conflicts:
    #	include/fastrtps/rtps/common/Types.h

--- include/fastrtps/rtps/common/Types.h
+++ include/fastrtps/rtps/common/Types.h
@@ -76,7 +76,8 @@ typedef enum TopicKind_t
 }TopicKind_t;
 
 //!Topic discovery kind
-typedef enum TopicDiscoveryKind_t {
+typedef enum TopicDiscoveryKind_t
+{
     NO_CHECK,
     MINIMAL,
     COMPLETE
@@ -134,7 +135,6 @@ struct RTPS_DllAPI ProtocolVersion_t
     {
         return m_major != v.m_major || m_minor != v.m_minor;
     }
-
 };
 
 const ProtocolVersion_t c_ProtocolVersion_2_0(2,0);
