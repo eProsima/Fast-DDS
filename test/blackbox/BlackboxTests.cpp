ecf4862dcb35ec62d6404f44b627294b74170770
Miguel Company <MiguelCompany@eprosima.com>
2018-08-28 13:21:25 +0200

    Refs #3273.Create a test to check that the history of a writer using VOLATILE and BEST_EFFORT is empty at the end of the sending process

--- test/blackbox/BlackboxTests.cpp
+++ test/blackbox/BlackboxTests.cpp
@@ -1297,7 +1297,7 @@ BLACKBOXTEST_F(BlackBoxPersistence, RTPSAsNonReliableWithPersistence)
 
     // Stop and start reader and writer
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    
+
     std::cout << "First round finished." << std::endl;
 
     reader.init();
@@ -5112,6 +5112,42 @@ BLACKBOXTEST(BlackBox, AsyncPubSubAsNonReliableVolatileHelloworld)
     ASSERT_TRUE(data.empty());
     // Block reader until reception finished or timeout.
     reader.block_for_at_least(2);
+}
+
+// Test created to check a bug with writers that use BEST_EFFORT WITH VOLATILE that don't remove messages from history.
+BLACKBOXTEST(BlackBox, AsyncPubSubAsNonReliableVolatileKeepAllHelloworld)
+{
+    RTPSAsSocketReader<HelloWorldType> reader(TEST_TOPIC_NAME);
+    RTPSAsSocketWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+    std::string ip("239.255.1.4");
+
+    reader.reliability(eprosima::fastrtps::rtps::ReliabilityKind_t::BEST_EFFORT).
+        add_to_multicast_locator_list(ip, global_port).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+
+    writer.reliability(eprosima::fastrtps::rtps::ReliabilityKind_t::BEST_EFFORT).
+        durability(eprosima::fastrtps::rtps::DurabilityKind_t::VOLATILE).
+        add_to_multicast_locator_list(ip, global_port).init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    auto data = default_helloworld_data_generator();
+
+    reader.expected_data(data);
+    reader.startReception();
+
+    // Send data
+    writer.send(data);
+    // In this test all data should be sent.
+    ASSERT_TRUE(data.empty());
+    // Block reader until reception finished or timeout.
+    reader.block_for_all();
+
+    // Wait for acks to be sent and check writer history is empty
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    ASSERT_TRUE(writer.is_history_empty());
 }
 
 // Test created to check bug #3290 (ROS2 #539)
