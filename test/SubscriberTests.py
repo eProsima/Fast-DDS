6a0d7c9db8a5c9db4ec51ee55186ef22f7558984
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-09-27 11:20:24 +0200

    Refs #3432. Update python script to fix an issue with the network of docker

--- test/SubscriberTests.py
+++ test/SubscriberTests.py
@@ -16,4 +16,4 @@ else:
 
 os.system("mkdir -p /mnt/jenkins")
 os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
-os.system("./ /mnt/jenkins/LatencyTest subscriber >> sub.log")
+os.system("/mnt/jenkins/LatencyTest subscriber >> sub.log")
