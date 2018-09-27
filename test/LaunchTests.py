2f59c86b9e299a0bdedc59dd7d18e2f7c634b421
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-09-27 08:41:33 +0200

    Refs #3432. Unify pub and sub tests in only one python that manages all the executions at the same time

---
+++ test/LaunchTests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import sys, os, subprocess
+
+pub_str = ""
+sub_str = ""
+user = ""
+password = ""
+
+if len(sys.argv) <= 4:
+	pub_str = "192.168.1.59:2376"
+	sub_str = "192.168.1.59:2376"
+	user = ""
+	password = ""
+#     print ("Error.")
+#   sys.exit(-1)
+else:
+	user = sys.argv[1] 
+	password = sys.argv[2]
+	pub_str = sys.argv[3]
+	sub_str = sys.argv[4]
+
+pub_list = pub_str.split(',')
+sub_list = sub_str.split(',')
+
+for a in pub_list:
+	print("Launch Publisher on: " + a)
+	subprocess.Popen("docker -H" + a + " run --cap-add SYS_ADMIN --security-opt seccomp=unconfined " +
+	"--cap-add DAC_READ_SEARCH ubuntu-test python3 \"/workspace/Multi-Node Manual Linux/test/PublisherTests.py\" " +
+	user + " " + password)
+
+p = None
+for a in sub_list:
+	print("Launch Subscriber on: " + a)
+	p = subprocess.Popen("docker -H" + a + " run --cap-add SYS_ADMIN --security-opt seccomp=unconfined " +
+	"--cap-add DAC_READ_SEARCH -ti ubuntu-test python3 \"/workspace/Multi-Node Manual Linux/test/SubscriberTests.py\" " +
+	user + " " + password)
+
+p.communicate()
