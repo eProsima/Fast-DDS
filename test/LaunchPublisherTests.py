a06d83d1a8152ff5475bd05bb11c08f994b30894
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-09-27 07:25:28 +0200

    Refs #3432. Fix issues with the python scripts

--- test/LaunchPublisherTests.py
+++ test/LaunchPublisherTests.py
@@ -16,11 +16,11 @@ if len(sys.argv) <= 3:
 else:
 	user = sys.argv[1] 
 	password = sys.argv[2]
-    str = sys.argv[3]
+	str = sys.argv[3]
 
 list = str.split(',')
 for a in list:
     print(a)
-    os.system("docker -H".a." run --cap-add SYS_ADMIN --security-opt seccomp=unconfined".
-     "--cap-add DAC_READ_SEARCH -ti ubuntu-test python3 ".
-     "/workspace/Multi-Node Manual Linux/test/PublisherTests.py ".user." ".password)
+    os.system("docker -H" + a + " run --cap-add SYS_ADMIN --security-opt seccomp=unconfined " +
+	"--cap-add DAC_READ_SEARCH -ti ubuntu-test python3 \"/workspace/Multi-Node Manual Linux/test/PublisherTests.py\" " +
+	user + " " + password)
