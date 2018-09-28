5beed508b373dcb6ab21ffcd7ea667d779e3e684
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-09-28 12:44:10 +0200

    Refs #3425 Propagate the folder of the project by all the python scripts

--- test/SubscriberTests.py
+++ test/SubscriberTests.py
@@ -9,18 +9,20 @@ def writeTest(filename, text):
 	with open(filename, "a") as myfile:
 		myfile.write(text + "\n")
 
-if len(sys.argv) <= 2:
+if len(sys.argv) <= 3:
 	print("Error")
 	sys.exit(-1)
 else:
 	user = sys.argv[1]
 	password = sys.argv[2]
+	folder = sys.argv[3]
 
 
 os.system("mkdir -p /mnt/jenkins")
 os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
 writeTest("/mnt/jenkins/sub.log", "Start Tests")
-os.system("cd '/workspace/Multi-Node Manual Linux/test' && python3 GenerateTestsAndXMLs.py")
+os.chdir(folder)
+os.system("python3 GenerateTestsAndXMLs.py " + folder)
 writeTest("/mnt/jenkins/sub.log", "Generated XML files")
-os.system("cd '/workspace/Multi-Node Manual Linux/test' && python3 SubscriberTestList.py /mnt/jenkins/")
+os.system("python3 SubscriberTestList.py /mnt/jenkins/ " + folder)
 writeTest("/mnt/jenkins/sub.log", "Tests Completed")
