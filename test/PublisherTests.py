7113f633a050f73b5452e4df208d50af68f74b82
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-10-01 08:18:32 +0200

    Refs #3425 Add more logs to the python scripts

--- test/PublisherTests.py
+++ test/PublisherTests.py
@@ -17,13 +17,13 @@ else:
 	password = sys.argv[2]
 	folder = sys.argv[3]
 
+os.system("mkdir -p /mnt/jenkins")
+os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
+
 writeTest("/mnt/jenkins/sub.log", "Start Publisher Tests")
 writeTest("/mnt/jenkins/sub.log", "Arguments:")
 for x in sys.argv:
 	writeTest("/mnt/jenkins/sub.log", "\t" + x)
-
-os.system("mkdir -p /mnt/jenkins")
-os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
 writeTest("/mnt/jenkins/pub.log", "Start Tests")
 os.chdir(folder)
 os.system("python3 GenerateTestsAndXMLs.py '" + folder + "'")
--- test/SubscriberTests.py
+++ test/SubscriberTests.py
@@ -17,13 +17,14 @@ else:
 	password = sys.argv[2]
 	folder = sys.argv[3]
 
+os.system("mkdir -p /mnt/jenkins")
+os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
+
 writeTest("/mnt/jenkins/sub.log", "Start Subscriber Tests")
 writeTest("/mnt/jenkins/sub.log", "Arguments:")
 for x in sys.argv:
 	writeTest("/mnt/jenkins/sub.log", "\t" + x)
 
-os.system("mkdir -p /mnt/jenkins")
-os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
 writeTest("/mnt/jenkins/sub.log", "Start Tests")
 os.chdir(folder)
 os.system("python3 GenerateTestsAndXMLs.py '" + folder + "'")
