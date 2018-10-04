1cce29b6f637b8dd54a90e1a1b61d2fb07516098
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-10-04 12:12:01 +0200

    Refs #3432 Improve the copy script to avoid unnecessary files.

--- test/CopyAndRenameCSV.py
+++ test/CopyAndRenameCSV.py
@@ -2,6 +2,9 @@
 
 import sys, os, datetime, shutil
 
+tests_list = ["LatencyTest", "ThroughputTest", "VideoTest", "MemoryTest"]
+filter_list = [["_average", "_minimum"], ["_all_"], [], []]
+
 output_folder = ""
 csvs_folder = ""
 if len(sys.argv) >= 3:
@@ -13,8 +16,25 @@ else:
 
 now = datetime.datetime.now()
 os.system("mkdir -p " + csvs_folder)
-for file in os.listdir(output_folder):
-	if file.endswith(".csv"):
-		shutil.copy(output_folder + "/" + file, csvs_folder + "/" + file[:-4] + now.strftime("%Y-%m-%d_%H-%M-%S") + ".csv")
+
+global_files_list = []
+# Loops for test and combination
+for test_id, test in enumerate(tests_list):
+	for file in os.listdir(output_folder):
+		# Take all the tests by name
+		if file.startswith(test) and file.endswith(".csv"):
+			
+			# Filter files for the test
+			if len(filter_list[test_id]) > 0:
+				for filter_name in filter_list[test_id]:
+					if file.find(filter_name) != -1:
+						global_files_list.append(file)
+						break		
+			else:
+				global_files_list.append(file)
+
+
+for file in global_files_list:
+	shutil.copy(output_folder + "/" + file, csvs_folder + "/" + file[:-4] + now.strftime("%Y-%m-%d_%H-%M-%S") + ".csv")
 
 			
