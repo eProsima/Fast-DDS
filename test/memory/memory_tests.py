40bd4f6a80c205886dd91b4083de132bfa97d4d8
Luis Gasco <luisgasco@eprosima.com>
2018-10-04 07:21:16 +0200

    Refs #3455 Memory test divided by pub and sub, to allow multi machine execution. Added memory tests in GenerationTestsAndXMLs.py

--- test/memory/memory_tests.py
+++ test/memory/memory_tests.py
@@ -18,10 +18,16 @@ command = os.environ.get("MEMORY_TEST_BIN")
 valgrind = os.environ.get("VALGRIND_BIN")
 certs_path = os.environ.get("CERTS_PATH")
 
-valgrind_command_pub_rel = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_pub_rel.out"]
-valgrind_command_sub_rel = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_sub_rel.out"]
-valgrind_command_pub_be = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_pub_be.out"]
-valgrind_command_sub_be = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_sub_be.out"]
+if not valgrind:
+    valgrind = "valgrind"
+
+if len(sys.argv) == 3:
+    command = sys.argv[2]
+
+pubsub = sys.argv[1]
+
+valgrind_command_rel = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_" + pubsub + "_rel.out"]
+valgrind_command_be = [valgrind, "--tool=massif", "--stacks=yes", "--detailed-freq=1", "--max-snapshots=1000", "--massif-out-file=./consumption_" + pubsub + "_be.out"]
 
 security_options = []
 
@@ -29,36 +35,24 @@ if certs_path:
     security_options = ["--security=true", "--certs=" + certs_path]
 
 # Best effort
-subscriber_proc = subprocess.Popen(valgrind_command_sub_be +
-        [command, "subscriber", "--seed", str(os.getpid()), "--hostname"] +
-        security_options)
-publisher_proc = subprocess.Popen(valgrind_command_pub_be +
-        [command, "publisher", "--seed", str(os.getpid()), "--hostname"] +
+proc = subprocess.Popen(valgrind_command_be +
+        [command, pubsub] +
         security_options)
 
-subscriber_proc.communicate()
-publisher_proc.communicate()
+proc.communicate()
 
-py_command = "python3 ./memory_analysis.py ./consumption_pub_be.out ./stack_pub_be.csv"
-p = subprocess.Popen(py_command, shell=True)
-py_command = "python3 ./memory_analysis.py ./consumption_sub_be.out ./stack_sub_be.csv"
+py_command = "python3 ./memory_analysis.py ./consumption_" + pubsub + "_be.out ./stack_" + pubsub + "_be.csv"
 p = subprocess.Popen(py_command, shell=True)
 
 # Reliable
-subscriber_proc = subprocess.Popen(valgrind_command_sub_rel +
-        [command, "subscriber", "-r", "reliable", "--seed", str(os.getpid()), "--hostname"] +
-        security_options)
-publisher_proc = subprocess.Popen(valgrind_command_pub_rel +
-        [command, "publisher", "-r", "reliable", "--seed", str(os.getpid()), "--hostname"] +
+proc = subprocess.Popen(valgrind_command_rel +
+        [command, pubsub, "-r", "reliable"] +
         security_options)
 
-subscriber_proc.communicate()
-publisher_proc.communicate()
+proc.communicate()
 
-py_command = "python3 ./memory_analysis.py ./consumption_pub_rel.out ./stack_pub_rel.csv"
+py_command = "python3 ./memory_analysis.py ./consumption_" + pubsub + "_rel.out ./stack_" + pubsub + "_rel.csv"
 # print("Command: " + py_command)
-p = subprocess.Popen(py_command, shell=True)
-py_command = "python3 ./memory_analysis.py ./consumption_sub_rel.out ./stack_sub_rel.csv"
 p = subprocess.Popen(py_command, shell=True)
 
 quit()
