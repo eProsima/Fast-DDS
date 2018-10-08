cc0f01cd5862c630efbe3c6926e28564b87eed82
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-10-08 11:32:36 +0200

    Refs #3478. Update python scripts to don't mount any shared folder, because the host machines are going to do it

--- test/PublisherTests.py
+++ test/PublisherTests.py
@@ -7,8 +7,6 @@ def signal_handler(sig, frame):
 	sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-user = ""
-password = ""
 folder = ""
 
 def writeTest(filename, text):
@@ -21,16 +19,16 @@ if len(sys.argv) == 1:
 if len(sys.argv) == 2:
     folder = os.path.abspath(sys.argv[1])
 
-if len(sys.argv) == 4:
-    user = sys.argv[1]
-    password = sys.argv[2]
-    folder = os.path.abspath(sys.argv[3])
-    if not os.path.ismount("/mnt/jenkins"):
-        os.system("mkdir -p /mnt/jenkins")
-        os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
-        print("/mnt/jenkins mounted")
-    else:
-        print("/mnt/jenkins is already mounted")
+#if len(sys.argv) == 4:
+#    user = sys.argv[1]
+#    password = sys.argv[2]
+#    folder = os.path.abspath(sys.argv[3])
+#    if not os.path.ismount("/mnt/perfshare"):
+#        os.system("mkdir -p /mnt/perfshare")
+#        os.system("mount -t nfs 192.168.1.4:/var/lib/jenkins/perfshare /mnt/perfshare")
+#        print("/mnt/perfshare mounted")
+#    else:
+#        print("/mnt/perfshare is already mounted")
 
 if not folder:
     print("Error")
@@ -38,14 +36,14 @@ if not folder:
 
 now = datetime.datetime.now()
 
-writeTest("/mnt/jenkins/output/pub.log", "---------------------------------")
-writeTest("/mnt/jenkins/output/pub.log", "Start Publisher Test - " + now.strftime("%Y-%m-%d_%H-%M-%S"))
-os.system("mkdir -p /mnt/jenkins/output")
-writeTest("/mnt/jenkins/output/pub.log", "Start Tests")
+writeTest("/mnt/perfshare/output/pub.log", "---------------------------------")
+writeTest("/mnt/perfshare/output/pub.log", "Start Publisher Test - " + now.strftime("%Y-%m-%d_%H-%M-%S"))
+os.system("mkdir -p /mnt/perfshare/output")
+writeTest("/mnt/perfshare/output/pub.log", "Start Tests")
 os.chdir(folder)
 os.system("python3 GenerateTestsAndXMLs.py '" + folder + "'")
 time.sleep(5)
-writeTest("/mnt/jenkins/output/pub.log", "Generated XML files")
-writeTest("/mnt/jenkins/output/pub.log", "Start Tests")
-os.system("python3 PublisherTestList.py /mnt/jenkins/ '" + folder + "'")
-writeTest("/mnt/jenkins/output/pub.log", "Tests Completed")
+writeTest("/mnt/perfshare/output/pub.log", "Generated XML files")
+writeTest("/mnt/perfshare/output/pub.log", "Start Tests")
+os.system("python3 PublisherTestList.py /mnt/perfshare/ '" + folder + "'")
+writeTest("/mnt/perfshare/output/pub.log", "Tests Completed")
--- test/SubscriberTests.py
+++ test/SubscriberTests.py
@@ -7,8 +7,6 @@ def signal_handler(sig, frame):
 	sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-user = ""
-password = ""
 folder = ""
 
 def writeTest(filename, text):
@@ -21,16 +19,16 @@ if len(sys.argv) == 1:
 if len(sys.argv) == 2:
     folder = os.path.abspath(sys.argv[1])
 
-if len(sys.argv) == 4:
-    user = sys.argv[1]
-    password = sys.argv[2]
-    folder = os.path.abspath(sys.argv[3])
-    if not os.path.ismount("/mnt/jenkins"):
-        os.system("mkdir -p /mnt/jenkins")
-        os.system("mount -t cifs -o username=" + user + ",password=" + password + " //mainserver.intranet.eprosima.com/Public/JenkinsTests /mnt/jenkins")
-        print("/mnt/jenkins mounted")
-    else:
-        print("/mnt/jenkins is already mounted")
+#if len(sys.argv) == 4:
+#    user = sys.argv[1]
+#    password = sys.argv[2]
+#    folder = os.path.abspath(sys.argv[3])
+#    if not os.path.ismount("/mnt/perfshare"):
+#        os.system("mkdir -p /mnt/perfshare")
+#        os.system("mount -t nfs 192.168.1.4:/var/lib/jenkins/perfshare /mnt/perfshare")
+#        print("/mnt/perfshare mounted")
+#    else:
+#        print("/mnt/perfshare is already mounted")
 
 if not folder:
     print("Error")
@@ -38,14 +36,14 @@ if not folder:
 
 now = datetime.datetime.now()
 
-writeTest("/mnt/jenkins/output/sub.log", "---------------------------------")
-writeTest("/mnt/jenkins/output/sub.log", "Start Subscriber Test - " + now.strftime("%Y-%m-%d_%H-%M-%S"))
-os.system("mkdir -p /mnt/jenkins/output")
-writeTest("/mnt/jenkins/output/sub.log", "Start Tests")
+writeTest("/mnt/perfshare/output/sub.log", "---------------------------------")
+writeTest("/mnt/perfshare/output/sub.log", "Start Subscriber Test - " + now.strftime("%Y-%m-%d_%H-%M-%S"))
+os.system("mkdir -p /mnt/perfshare/output")
+writeTest("/mnt/perfshare/output/sub.log", "Start Tests")
 os.chdir(folder)
 os.system("python3 GenerateTestsAndXMLs.py '" + folder + "'")
 time.sleep(5)
-writeTest("/mnt/jenkins/output/sub.log", "Generated XML files")
-writeTest("/mnt/jenkins/output/sub.log", "Start Tests")
-os.system("python3 SubscriberTestList.py /mnt/jenkins/ '" + folder + "'")
-writeTest("/mnt/jenkins/output/sub.log", "Tests Completed")
+writeTest("/mnt/perfshare/output/sub.log", "Generated XML files")
+writeTest("/mnt/perfshare/output/sub.log", "Start Tests")
+os.system("python3 SubscriberTestList.py /mnt/perfshare/ '" + folder + "'")
+writeTest("/mnt/perfshare/output/sub.log", "Tests Completed")
