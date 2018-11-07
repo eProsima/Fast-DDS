446987030b028908cc453ed2c26e37d629085b5b
Juan Carlos Arceredillo <juancarlosarceredillo@eprosima.com>
2018-11-07 16:44:41 +0100

    Refs #3670 Add a new input argument to set the domain of the participants in the latency test

--- test/performance/LatencyTestPublisher.h
+++ test/performance/LatencyTestPublisher.h
@@ -48,118 +48,120 @@ public:
 };
 
 class LatencyTestPublisher {
+
+public:
+    LatencyTestPublisher();
+    virtual ~LatencyTestPublisher();
+
+    eprosima::fastrtps::Participant* mp_participant;
+    eprosima::fastrtps::Publisher* mp_datapub;
+    eprosima::fastrtps::Publisher* mp_commandpub;
+    eprosima::fastrtps::Subscriber* mp_datasub;
+    eprosima::fastrtps::Subscriber* mp_commandsub;
+    std::chrono::steady_clock::time_point t_start_, t_end_;
+    std::chrono::duration<double, std::micro> t_overhead_;
+    int n_subscribers;
+    unsigned int n_samples;
+    eprosima::fastrtps::SampleInfo_t m_sampleinfo;
+    std::vector<std::chrono::duration<double, std::micro>> times_;
+    std::vector<TimeStats> m_stats;
+    std::mutex mutex_;
+    int disc_count_;
+    std::condition_variable disc_cond_;
+    int comm_count_;
+    std::condition_variable comm_cond_;
+    int data_count_;
+    std::condition_variable data_cond_;
+    int m_status;
+    unsigned int n_received;
+    bool n_export_csv;
+    std::string m_exportPrefix;
+    bool init(int n_sub, int n_sam, bool reliable, uint32_t pid, bool hostname, bool export_csv,
+        const std::string& export_prefix,
+        const eprosima::fastrtps::rtps::PropertyPolicy& part_property_policy,
+        const eprosima::fastrtps::rtps::PropertyPolicy& property_policy, bool large_data,
+        const std::string& sXMLConfigFile, bool dynamic_types, int forced_domain);
+    void run();
+    void analyzeTimes(uint32_t datasize);
+    bool test(uint32_t datasize);
+    void printStat(TimeStats& TS);
+
+    class DataPubListener : public eprosima::fastrtps::PublisherListener
+    {
     public:
-        LatencyTestPublisher();
-        virtual ~LatencyTestPublisher();
+        DataPubListener(LatencyTestPublisher* up) :mp_up(up), n_matched(0) {}
+        ~DataPubListener() {}
+        void onPublicationMatched(eprosima::fastrtps::Publisher* pub,
+            eprosima::fastrtps::rtps::MatchingInfo& info);
+        LatencyTestPublisher* mp_up;
+        int n_matched;
+    } m_datapublistener;
 
-        eprosima::fastrtps::Participant* mp_participant;
-        eprosima::fastrtps::Publisher* mp_datapub;
-        eprosima::fastrtps::Publisher* mp_commandpub;
-        eprosima::fastrtps::Subscriber* mp_datasub;
-        eprosima::fastrtps::Subscriber* mp_commandsub;
-        std::chrono::steady_clock::time_point t_start_, t_end_;
-        std::chrono::duration<double, std::micro> t_overhead_;
-        int n_subscribers;
-        unsigned int n_samples;
-        eprosima::fastrtps::SampleInfo_t m_sampleinfo;
-        std::vector<std::chrono::duration<double, std::micro>> times_;
-        std::vector<TimeStats> m_stats;
-        std::mutex mutex_;
-        int disc_count_;
-        std::condition_variable disc_cond_;
-        int comm_count_;
-        std::condition_variable comm_cond_;
-        int data_count_;
-        std::condition_variable data_cond_;
-        int m_status;
-        unsigned int n_received;
-        bool n_export_csv;
-        std::string m_exportPrefix;
-        bool init(int n_sub, int n_sam, bool reliable, uint32_t pid, bool hostname, bool export_csv,
-                const std::string& export_prefix,
-                const eprosima::fastrtps::rtps::PropertyPolicy& part_property_policy,
-                const eprosima::fastrtps::rtps::PropertyPolicy& property_policy, bool large_data,
-                const std::string& sXMLConfigFile, bool dynamic_types);
-        void run();
-        void analyzeTimes(uint32_t datasize);
-        bool test(uint32_t datasize);
-        void printStat(TimeStats& TS);
+    class DataSubListener : public eprosima::fastrtps::SubscriberListener
+    {
+    public:
+        DataSubListener(LatencyTestPublisher* up) :mp_up(up), n_matched(0) {}
+        ~DataSubListener() {}
+        void onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub,
+            eprosima::fastrtps::rtps::MatchingInfo& into);
+        void onNewDataMessage(eprosima::fastrtps::Subscriber* sub);
+        LatencyTestPublisher* mp_up;
+        int n_matched;
+    } m_datasublistener;
 
-        class DataPubListener : public eprosima::fastrtps::PublisherListener
-        {
-            public:
-                DataPubListener(LatencyTestPublisher* up):mp_up(up),n_matched(0){}
-                ~DataPubListener(){}
-                void onPublicationMatched(eprosima::fastrtps::Publisher* pub,
-                        eprosima::fastrtps::rtps::MatchingInfo& info);
-                LatencyTestPublisher* mp_up;
-                int n_matched;
-        } m_datapublistener;
+    class CommandPubListener : public eprosima::fastrtps::PublisherListener
+    {
+    public:
+        CommandPubListener(LatencyTestPublisher* up) :mp_up(up), n_matched(0) {}
+        ~CommandPubListener() {}
+        void onPublicationMatched(eprosima::fastrtps::Publisher* pub,
+            eprosima::fastrtps::rtps::MatchingInfo& info);
+        LatencyTestPublisher* mp_up;
+        int n_matched;
+    } m_commandpublistener;
 
-        class DataSubListener : public eprosima::fastrtps::SubscriberListener
-        {
-            public:
-                DataSubListener(LatencyTestPublisher* up):mp_up(up),n_matched(0){}
-                ~DataSubListener(){}
-                void onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub,
-                        eprosima::fastrtps::rtps::MatchingInfo& into);
-                void onNewDataMessage(eprosima::fastrtps::Subscriber* sub);
-                LatencyTestPublisher* mp_up;
-                int n_matched;
-        } m_datasublistener;
+    class CommandSubListener : public eprosima::fastrtps::SubscriberListener
+    {
+    public:
+        CommandSubListener(LatencyTestPublisher* up) :mp_up(up), n_matched(0) {}
+        ~CommandSubListener() {}
+        void onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub,
+            eprosima::fastrtps::rtps::MatchingInfo& into);
+        void onNewDataMessage(eprosima::fastrtps::Subscriber* sub);
+        LatencyTestPublisher* mp_up;
+        int n_matched;
+    } m_commandsublistener;
 
-        class CommandPubListener : public eprosima::fastrtps::PublisherListener
-        {
-            public:
-                CommandPubListener(LatencyTestPublisher* up):mp_up(up),n_matched(0){}
-                ~CommandPubListener(){}
-                void onPublicationMatched(eprosima::fastrtps::Publisher* pub,
-                        eprosima::fastrtps::rtps::MatchingInfo& info);
-                LatencyTestPublisher* mp_up;
-                int n_matched;
-        } m_commandpublistener;
+    TestCommandDataType command_t;
 
-        class CommandSubListener : public eprosima::fastrtps::SubscriberListener
-        {
-            public:
-                CommandSubListener(LatencyTestPublisher* up):mp_up(up),n_matched(0){}
-                ~CommandSubListener(){}
-                void onSubscriptionMatched(eprosima::fastrtps::Subscriber* sub,
-                        eprosima::fastrtps::rtps::MatchingInfo& into);
-                void onNewDataMessage(eprosima::fastrtps::Subscriber* sub);
-                LatencyTestPublisher* mp_up;
-                int n_matched;
-        } m_commandsublistener;
-
-        TestCommandDataType command_t;
-
-        std::stringstream output_file_minimum;
-        std::stringstream output_file_average;
-        std::stringstream output_file_16;
-        std::stringstream output_file_32;
-        std::stringstream output_file_64;
-        std::stringstream output_file_128;
-        std::stringstream output_file_256;
-        std::stringstream output_file_512;
-        std::stringstream output_file_1024;
-        std::stringstream output_file_2048;
-        std::stringstream output_file_4096;
-        std::stringstream output_file_8192;
-        std::stringstream output_file_16384;
-        std::stringstream output_file_64000;
-        std::stringstream output_file_131072;
-        std::string m_sXMLConfigFile;
-        bool reliable_;
-        //bool dynamic_data = false;
-        // Static Types
-        LatencyDataType latency_t;
-        LatencyType* mp_latency_in;
-        LatencyType* mp_latency_out;
-	// Dynamic Types
-	//eprosima::fastrtps::types::DynamicData* m_DynData_in;
-	//eprosima::fastrtps::types::DynamicData* m_DynData_out;
-	//eprosima::fastrtps::types::DynamicPubSubType m_DynType;
-        //eprosima::fastrtps::types::DynamicType_ptr m_pDynType;
+    std::stringstream output_file_minimum;
+    std::stringstream output_file_average;
+    std::stringstream output_file_16;
+    std::stringstream output_file_32;
+    std::stringstream output_file_64;
+    std::stringstream output_file_128;
+    std::stringstream output_file_256;
+    std::stringstream output_file_512;
+    std::stringstream output_file_1024;
+    std::stringstream output_file_2048;
+    std::stringstream output_file_4096;
+    std::stringstream output_file_8192;
+    std::stringstream output_file_16384;
+    std::stringstream output_file_64000;
+    std::stringstream output_file_131072;
+    std::string m_sXMLConfigFile;
+    bool reliable_;
+    //bool dynamic_data = false;
+    int m_forcedDomain;
+    // Static Types
+    LatencyDataType latency_t;
+    LatencyType* mp_latency_in;
+    LatencyType* mp_latency_out;
+    // Dynamic Types
+    //eprosima::fastrtps::types::DynamicData* m_DynData_in;
+    //eprosima::fastrtps::types::DynamicData* m_DynData_out;
+    //eprosima::fastrtps::types::DynamicPubSubType m_DynType;
+    //eprosima::fastrtps::types::DynamicType_ptr m_pDynType;
 };
 
 
--- test/performance/LatencyTestSubscriber.cpp
+++ test/performance/LatencyTestSubscriber.cpp
@@ -20,6 +20,7 @@
 #include "LatencyTestSubscriber.h"
 #include "fastrtps/log/Log.h"
 #include "fastrtps/log/Colors.h"
+#include <fastrtps/xmlparser/XMLProfileManager.h>
 
 using namespace eprosima;
 using namespace eprosima::fastrtps;
@@ -51,12 +52,11 @@ LatencyTestSubscriber::LatencyTestSubscriber():
     mp_latency(nullptr)//,
     //m_DynData(nullptr)
 {
+    m_forcedDomain = -1;
     m_datapublistener.mp_up = this;
     m_datasublistener.mp_up = this;
     m_commandpublistener.mp_up = this;
     m_commandsublistener.mp_up = this;
-
-
 }
 
 LatencyTestSubscriber::~LatencyTestSubscriber()
@@ -66,7 +66,7 @@ LatencyTestSubscriber::~LatencyTestSubscriber()
 
 bool LatencyTestSubscriber::init(bool echo, int nsam, bool reliable, uint32_t pid, bool hostname,
         const PropertyPolicy& part_property_policy, const PropertyPolicy& property_policy, bool large_data,
-        const std::string& sXMLConfigFile, bool /*dynamic_types*/)
+        const std::string& sXMLConfigFile, bool /*dynamic_types*/, int forced_domain)
 {
     if(!large_data)
     {
@@ -80,6 +80,8 @@ bool LatencyTestSubscriber::init(bool echo, int nsam, bool reliable, uint32_t pi
     m_echo = echo;
     n_samples = nsam;
     //dynamic_data = dynamic_types;
+    m_forcedDomain = forced_domain;
+
 
     //if (dynamic_data)
     //{
@@ -101,13 +103,35 @@ bool LatencyTestSubscriber::init(bool echo, int nsam, bool reliable, uint32_t pi
     // Create RTPSParticipant
     std::string participant_profile_name = "participant_profile";
     ParticipantAttributes PParam;
-    PParam.rtps.builtin.domainId = pid % 230;
+
+    if (m_forcedDomain >= 0)
+    {
+        PParam.rtps.builtin.domainId = m_forcedDomain;
+    }
+    else
+    {
+        PParam.rtps.builtin.domainId = pid % 230;
+    }
     PParam.rtps.setName("Participant_sub");
     PParam.rtps.properties = part_property_policy;
 
     if (m_sXMLConfigFile.length() > 0)
     {
-        mp_participant = Domain::createParticipant(participant_profile_name);
+        if (m_forcedDomain >= 0)
+        {
+            ParticipantAttributes participant_att;
+            if (eprosima::fastrtps::xmlparser::XMLP_ret::XML_OK ==
+                eprosima::fastrtps::xmlparser::XMLProfileManager::fillParticipantAttributes(participant_profile_name,
+                    participant_att))
+            {
+                participant_att.rtps.builtin.domainId = m_forcedDomain;
+                mp_participant = Domain::createParticipant(participant_att);
+            }
+        }
+        else
+        {
+            mp_participant = Domain::createParticipant(participant_profile_name);
+        }
     }
     else
     {
