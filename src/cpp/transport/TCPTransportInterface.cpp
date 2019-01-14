361960bd68706126e320e621d22cc0ebe06c4fbf
LuisGasco <luisgasco@eprosima.com>
2019-01-14 12:32:23 +0100

    Refs #4331 Disabled CRC

--- src/cpp/transport/TCPTransportInterface.cpp
+++ src/cpp/transport/TCPTransportInterface.cpp
@@ -230,21 +230,22 @@ void TCPTransportInterface::DeleteSocket(TCPChannelResource *channelResource)
 
 bool TCPTransportInterface::CheckCRC(const TCPHeader &header, const octet *data, uint32_t size) const
 {
-    uint32_t crc(0);
-    for (uint32_t i = 0; i < size; ++i)
-    {
-        crc = RTCPMessageManager::addToCRC(crc, data[i]);
-    }
-    return crc == header.crc;
+    return true;
+    //uint32_t crc(0);
+    //for (uint32_t i = 0; i < size; ++i)
+    //{
+    //    crc = RTCPMessageManager::addToCRC(crc, data[i]);
+    //}
+    //return crc == header.crc;
 }
 
 void TCPTransportInterface::CalculateCRC(TCPHeader &header, const octet *data, uint32_t size) const
 {
     uint32_t crc(0);
-    for (uint32_t i = 0; i < size; ++i)
-    {
-        crc = RTCPMessageManager::addToCRC(crc, data[i]);
-    }
+    //for (uint32_t i = 0; i < size; ++i)
+    //{
+    //    crc = RTCPMessageManager::addToCRC(crc, data[i]);
+    //}
     header.crc = crc;
 }
 
