32dc6a55e6ae1324b208ea947c3457e13fad1b34
Miguel Company <MiguelCompany@eprosima.com>
2019-02-12 10:33:03 +0100

    Refs #4652. XSD and XMLParser unit tests updated.

--- test/unittest/xmlparser/XMLProfileParserTests.cpp
+++ test/unittest/xmlparser/XMLProfileParserTests.cpp
@@ -408,6 +408,7 @@ TEST_F(XMLProfileParserTests, XMLParserSubscriber)
     EXPECT_EQ(subscriber_atts.historyMemoryPolicy, PREALLOCATED_WITH_REALLOC_MEMORY_MODE);
     EXPECT_EQ(subscriber_atts.getUserDefinedID(), 13);
     EXPECT_EQ(subscriber_atts.getEntityID(), 31);
+    EXPECT_EQ(subscriber_atts.matched_publisher_allocation, ResourceLimitedContainerConfig::fixed_size_configuration(10u));
 }
 
 TEST_F(XMLProfileParserTests, XMLParserDefaultSubscriberProfile)
@@ -472,6 +473,7 @@ TEST_F(XMLProfileParserTests, XMLParserDefaultSubscriberProfile)
     EXPECT_EQ(subscriber_atts.historyMemoryPolicy, PREALLOCATED_WITH_REALLOC_MEMORY_MODE);
     EXPECT_EQ(subscriber_atts.getUserDefinedID(), 13);
     EXPECT_EQ(subscriber_atts.getEntityID(), 31);
+    EXPECT_EQ(subscriber_atts.matched_publisher_allocation, ResourceLimitedContainerConfig::fixed_size_configuration(10u));
 }
 
 #if HAVE_SECURITY
