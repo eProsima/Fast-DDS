1d38bffceec11358213b25381e1d5779f0e3462e
Raquel Alvarez <raquelalvarez@eprosima.com>
2019-03-06 16:26:07 +0100

    Refs #4693 Improving precision of current time

--- src/cpp/rtps/history/WriterHistory.cpp
+++ src/cpp/rtps/history/WriterHistory.cpp
@@ -87,7 +87,7 @@ bool WriterHistory::add_change_(CacheChange_t* a_change, WriteParams &wparams,
 
     ++m_lastCacheChangeSeqNum;
     a_change->sequenceNumber = m_lastCacheChangeSeqNum;
-    a_change->sourceTimestamp = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    a_change->sourceTimestamp = Time_t(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count() * 1e-3);
 
     a_change->write_params = wparams;
     // Updated sample identity
