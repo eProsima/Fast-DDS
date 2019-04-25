ewolfox/Fast-RTPS

Refs #5108 Rebase with develop. Added message if osrf_testing_tools_cpp not found instead making cmake fail.

test/blackbox/PubSubWriter.hpp
@@ -367,13 +367,13 @@ class PubSubWriter
         return *this;
     }
 
-    PubSubWriter& lifespan_period(const eprosima::fastrtps::rtps::Duration_t lifespan_period)
+    PubSubWriter& lifespan_period(const eprosima::fastrtps::Duration_t lifespan_period)
     {
         publisher_attr_.qos.m_lifespan.duration = lifespan_period;
         return *this;
     }
 
-    PubSubWriter& max_blocking_time(const eprosima::fastrtps::rtps::Duration_t time)
+    PubSubWriter& max_blocking_time(const eprosima::fastrtps::Duration_t time)
     {
         publisher_attr_.qos.m_reliability.max_blocking_time = time;
         return *this;

eProsima/Fast-DDS

test/blackbox/PubSubWriter.hpp
@@ -416,14 +416,14 @@ class PubSubWriter
         return *this;
     }
 
-    PubSubWriter& keep_duration(const eprosima::fastrtps::rtps::Duration_t duration)
+    PubSubWriter& keep_duration(const eprosima::fastrtps::Duration_t duration)
     {
         publisher_attr_.qos.m_disablePositiveACKs.enabled = true;
         publisher_attr_.qos.m_disablePositiveACKs.duration = duration;
         return *this;
     }
 
-    PubSubWriter& max_blocking_time(const eprosima::fastrtps::rtps::Duration_t time)
+    PubSubWriter& max_blocking_time(const eprosima::fastrtps::Duration_t time)
     {
         publisher_attr_.qos.m_reliability.max_blocking_time = time;
         return *this;