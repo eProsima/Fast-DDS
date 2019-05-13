from-wind/Fast-RTPS

Refs #2434. Not using operator = on atomic<bool>.

include/fastrtps/transport/ChannelResource.h
@@ -47,12 +47,12 @@ public:
 
     inline bool alive() const
     {
-        return alive_;
+        return alive_.load();
     }
 
     inline virtual void disable()
     {
-        alive_ = false;
+        alive_.exchange(false);
     }
 
     inline CDRMessage_t& message_buffer()

include/fastrtps/transport/tcp/RTCPMessageManager.h
@@ -143,7 +143,7 @@ public:
 
     void dispose()
     {
-        alive_ = false;
+        alive_.exchange(false);
     }
 
 protected:
@@ -213,7 +213,7 @@ protected:
 
     inline bool alive() const
     {
-        return alive_;
+        return alive_.load();
     }
 };
 } /* namespace rtps */

src/cpp/rtps/writer/ReaderProxy.cpp
@@ -68,7 +68,7 @@ void ReaderProxy::start(const RemoteReaderAttributes& reader_attributes)
     reader_attributes_.endpoint.remoteLocatorList.push_back(reader_attributes_.endpoint.multicastLocatorList);
 
     nack_supression_event_->reader_guid(reader_attributes_.guid);
-    timers_enabled_ = (reader_attributes_.endpoint.reliabilityKind == RELIABLE);
+    timers_enabled_.exchange(reader_attributes_.endpoint.reliabilityKind == RELIABLE);
 
     logInfo(RTPS_WRITER, "Reader Proxy started");
 }
@@ -107,7 +107,7 @@ void ReaderProxy::add_change(
     assert(changes_for_reader_.empty() ? true :
         change.getSequenceNumber() > changes_for_reader_.back().getSequenceNumber());
 
-    if (restart_nack_supression && timers_enabled_)
+    if (restart_nack_supression && timers_enabled_.load())
     {
         nack_supression_event_->restart_timer();
     }
@@ -246,7 +246,7 @@ bool ReaderProxy::set_change_to_status(
 {
     if (restart_nack_supression && is_reliable())
     {
-        assert(timers_enabled_);
+        assert(timers_enabled_.load());
         nack_supression_event_->restart_timer();
     }
 

src/cpp/transport/ChannelResource.cpp
@@ -31,7 +31,7 @@ ChannelResource::ChannelResource(ChannelResource&& channelResource)
     , thread_(std::move(channelResource.thread_))
 {
     bool b = channelResource.alive_;
-    alive_ = b;
+    alive_.exchange(b);
     //logInfo(RTPS_MSG_IN, "Created with CDRMessage of size: " << message_buffer_.max_size);
     //message_buffer_ = std::move(channelResource.message_buffer_);
 }
@@ -51,7 +51,7 @@ ChannelResource::~ChannelResource()
 
 void ChannelResource::clear()
 {
-    alive_ = false;
+    alive_.exchange(false);
     if(thread_.joinable())
     {
         thread_.join();

src/cpp/transport/TCPTransportInterface.cpp
@@ -118,7 +118,7 @@ TCPTransportInterface::~TCPTransportInterface()
 void TCPTransportInterface::clean()
 {
     assert(receiver_resources_.size() == 0);
-    alive_ = false;
+    alive_.exchange(false);
 
     if(keep_alive_event_ != nullptr)
     {
@@ -1098,7 +1098,7 @@ void TCPTransportInterface::SocketAccepted(
         const Locator_t& locator,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         if (!error.value())
         {
@@ -1144,7 +1144,7 @@ void TCPTransportInterface::SecureSocketAccepted(
         const Locator_t& locator,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         if (!error.value())
         {
@@ -1189,7 +1189,7 @@ void TCPTransportInterface::SocketConnected(
         const std::weak_ptr<TCPChannelResource>& channel_weak_ptr,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         auto channel = channel_weak_ptr.lock();
 

eProsima/Fast-DDS

include/fastrtps/transport/ChannelResource.h
@@ -47,12 +47,12 @@ public:
 
     inline bool alive() const
     {
-        return alive_;
+        return alive_.load();
     }
 
     inline virtual void disable()
     {
-        alive_ = false;
+        alive_.store(false);
     }
 
     inline CDRMessage_t& message_buffer()

include/fastrtps/transport/tcp/RTCPMessageManager.h
@@ -143,7 +143,7 @@ public:
 
     void dispose()
     {
-        alive_ = false;
+        alive_.store(false);
     }
 
 protected:
@@ -213,7 +213,7 @@ protected:
 
     inline bool alive() const
     {
-        return alive_;
+        return alive_.load();
     }
 };
 } /* namespace rtps */

src/cpp/rtps/writer/ReaderProxy.cpp
@@ -68,7 +68,7 @@ void ReaderProxy::start(const RemoteReaderAttributes& reader_attributes)
     reader_attributes_.endpoint.remoteLocatorList.push_back(reader_attributes_.endpoint.multicastLocatorList);
 
     nack_supression_event_->reader_guid(reader_attributes_.guid);
-    timers_enabled_ = (reader_attributes_.endpoint.reliabilityKind == RELIABLE);
+    timers_enabled_.store(reader_attributes_.endpoint.reliabilityKind == RELIABLE);
 
     logInfo(RTPS_WRITER, "Reader Proxy started");
 }
@@ -107,7 +107,7 @@ void ReaderProxy::add_change(
     assert(changes_for_reader_.empty() ? true :
         change.getSequenceNumber() > changes_for_reader_.back().getSequenceNumber());
 
-    if (restart_nack_supression && timers_enabled_)
+    if (restart_nack_supression && timers_enabled_.load())
     {
         nack_supression_event_->restart_timer();
     }
@@ -246,7 +246,7 @@ bool ReaderProxy::set_change_to_status(
 {
     if (restart_nack_supression && is_reliable())
     {
-        assert(timers_enabled_);
+        assert(timers_enabled_.load());
         nack_supression_event_->restart_timer();
     }
 

src/cpp/transport/ChannelResource.cpp
@@ -31,7 +31,7 @@ ChannelResource::ChannelResource(ChannelResource&& channelResource)
     , thread_(std::move(channelResource.thread_))
 {
     bool b = channelResource.alive_;
-    alive_ = b;
+    alive_.store(b);
     //logInfo(RTPS_MSG_IN, "Created with CDRMessage of size: " << message_buffer_.max_size);
     //message_buffer_ = std::move(channelResource.message_buffer_);
 }
@@ -51,7 +51,7 @@ ChannelResource::~ChannelResource()
 
 void ChannelResource::clear()
 {
-    alive_ = false;
+    alive_.store(false);
     if(thread_.joinable())
     {
         thread_.join();

src/cpp/transport/TCPTransportInterface.cpp
@@ -118,7 +118,7 @@ TCPTransportInterface::~TCPTransportInterface()
 void TCPTransportInterface::clean()
 {
     assert(receiver_resources_.size() == 0);
-    alive_ = false;
+    alive_.store(false);
 
     if(keep_alive_event_ != nullptr)
     {
@@ -1098,7 +1098,7 @@ void TCPTransportInterface::SocketAccepted(
         const Locator_t& locator,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         if (!error.value())
         {
@@ -1144,7 +1144,7 @@ void TCPTransportInterface::SecureSocketAccepted(
         const Locator_t& locator,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         if (!error.value())
         {
@@ -1189,7 +1189,7 @@ void TCPTransportInterface::SocketConnected(
         const std::weak_ptr<TCPChannelResource>& channel_weak_ptr,
         const asio::error_code& error)
 {
-    if(alive_)
+    if(alive_.load())
     {
         auto channel = channel_weak_ptr.lock();
 