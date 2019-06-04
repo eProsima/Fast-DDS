3c6eb3a72e040302df80de3f6f7707ae73ff27f0
Ricardo González Moreno <correoricky@gmail.com>
2019-06-04 09:10:35 +0200

    Refs #5406. Fixed compile error on mac

--- include/fastrtps/utils/TimedConditionVariable.hpp
+++ include/fastrtps/utils/TimedConditionVariable.hpp
@@ -23,7 +23,7 @@
 #include <chrono>
 #include <functional>
 
-#ifdef _WIN32
+#if !defined(__linux__)
 #include <condition_variable>
 #else
 #include <pthread.h>
@@ -32,7 +32,7 @@
 namespace eprosima {
 namespace fastrtps {
 
-#ifdef _WIN32
+#if !defined(__linux__)
 using TimedConditionVariable = std::condition_variable_any;
 #else
 
