e1fc0de596633d374f54c2562bc9ae02264c9802
LuisGasco <luisgasco@eprosima.com>
2019-06-14 12:43:35 +0200

    Refs #5641 Added listeners

---
+++ include/fastrtps/topic/DataWriterListener.hpp
@@ -0,0 +1,86 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file DataWriterListener.hpp
+ */
+
+#ifndef _FASTRTPS_DATAWRITERLISTENER_H_
+#define _FASTRTPS_DATAWRITERLISTENER_H_
+
+#include "../rtps/common/Types.h"
+#include "../rtps/common/MatchingInfo.h"
+#include "../qos/DeadlineMissedStatus.h"
+#include "../qos/LivelinessLostStatus.h"
+
+namespace eprosima {
+namespace fastrtps {
+
+class DataWriter;
+
+/**
+ * Class DataWriterListener, allows the end user to implement callbacks triggered by certain events.
+ * @ingroup FASTRTPS_MODULE
+ * @snippet fastrtps_example.cpp ex_PublisherListener
+ */
+class RTPS_DllAPI DataWriterListener
+{
+public:
+    DataWriterListener(){}
+    virtual ~DataWriterListener(){}
+
+    /**
+     * This method is called when the Publisher is matched (or unmatched) against an endpoint.
+     * @param writer Pointer to the associated Publisher
+     * @param info Information regarding the matched subscriber
+     */
+    virtual void on_publication_matched(
+            DataWriter* writer,
+            rtps::MatchingInfo& info)
+    {
+        (void)writer;
+        (void)info;
+    }
+
+    /**
+     * A method called when a deadline is missed
+     * @param writer Pointer to the associated Publisher
+     * @param status The deadline missed status
+     */
+    virtual void on_offered_deadline_missed(
+            DataWriter* writer,
+            const OfferedDeadlineMissedStatus& status)
+    {
+        (void)writer;
+        (void)status;
+    }
+
+    /**
+     * @brief Method called when the livelivess of a publisher is lost
+     * @param writer The publisher
+     * @param status The liveliness lost status
+     */
+    virtual void on_liveliness_lost(
+            DataWriter* writer,
+            const LivelinessLostStatus& status)
+    {
+        (void)writer;
+        (void)status;
+    }
+};
+
+} /* namespace fastrtps */
+} /* namespace eprosima */
+
+#endif /* _FASTRTPS_DATAWRITERLISTENER_H_ */
