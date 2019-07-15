bf74953ad5210cae1c3bb2251269611af7a910f5
AlvaroMolinillo <alvarogonzalez@eprosima.com>
2019-07-15 09:50:37 +0200

    Refs #5847. New class TopicDescription

---
+++ include/fastdds/topic/TopicDescription.hpp
@@ -0,0 +1,77 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file TopicDescription.hpp
+ */
+
+#ifndef _FASTDDS_TOPIC_DESCRIPTION_HPP_
+#define _FASTDDS_TOPIC_DESCRIPTION_HPP_
+
+#include <string>
+
+namespace eprosima {
+namespace fastdds {
+
+class DomainParticipant;
+
+/**
+ * Class TopicDescription, represents the fact that both publications 
+ * and subscriptions are tied to a single data-type
+ * @ingroup FASTDDS_MODULE
+ */
+class TopicDescription /* : public DomainEntity */
+{
+public:
+    TopicDescription(
+            DomainParticipant* domain_participant,
+            const std::string& name, 
+            const std::string& type_name)
+        : domain_participant_(domain_participant)
+        , name_(name)
+        , type_name_(type_name)
+        //, DomainEntity()
+    {}
+
+    ~TopicDescription(){}
+
+    DomainParticipant* get_participant() const 
+    {
+        return domain_participant_;
+    }
+
+    std::string get_name() const
+    {
+        return name_;
+    }
+
+    std::string get_type_name() const
+    {
+        return type_name_;
+    }
+
+protected:
+
+    DomainParticipant* domain_participant_;
+
+    std::string name_;
+
+    std::string type_name_;
+
+};
+
+} /* namespace fastdds */
+} /* namespace eprosima */
+
+#endif /* _FASTDDS_TYPE_SUPPORT_HPP_ */
