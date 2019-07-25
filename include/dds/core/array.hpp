52f6b08f8425532d07ed1f249eb81502c844b81d
Luis Enrique Muñoz Martín <lemunozm@gmail.com>
2019-07-25 12:03:54 +0200

    Added array & detail/array

--- include/dds/core/array.hpp
+++ include/dds/core/array.hpp
@@ -1,8 +1,8 @@
-/* Copyright 2010, Object Management Group, Inc.
+/*
+ * Copyright 2010, Object Management Group, Inc.
  * Copyright 2010, PrismTech, Corp.
  * Copyright 2010, Real-Time Innovations, Inc.
  * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
- * All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,11 +22,11 @@
 
 #include <dds/core/detail/array.hpp>
 
-namespace dds
-{
-namespace core
-{
+namespace dds {
+namespace core {
+
 using dds::core::detail::array;
+
 }
 }
 
