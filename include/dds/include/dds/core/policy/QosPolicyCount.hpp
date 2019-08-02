72bb2bf4f5910777c98cdf98e63020b23a12f569
LauraMartin <laura@eprosima.com>
2019-08-02 11:03:30 +0200

    Added opensplice psm-api

---
+++ include/dds/include/dds/core/policy/QosPolicyCount.hpp
@@ -0,0 +1,35 @@
+#ifndef OMG_DDS_CORE_POLICY_QOS_POLICY_COUNT_HPP_
+#define OMG_DDS_CORE_POLICY_QOS_POLICY_COUNT_HPP_
+
+/* Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dds/core/policy/detail/QosPolicyCount.hpp>
+
+namespace dds
+{
+namespace core
+{
+namespace policy
+{
+typedef dds::core::policy::detail::QosPolicyCount QosPolicyCount;
+}
+}
+}
+
+#endif /* !defined(OMG_DDS_CORE_POLICY_QOS_POLICY_COUNT_HPP_) */
