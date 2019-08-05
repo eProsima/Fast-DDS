d7a8ad15d518dce5d13cd0ea3b129ea81e33449a
LauraMartin <laura@eprosima.com>
2019-08-05 07:36:14 +0200

    Copyright modified in non-detail classes

---
+++ include/dds/topic/Filter.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DDS_TOPIC_FILTER_HPP_
+#define DDS_TOPIC_FILTER_HPP_
+
+#include <dds/topic/detail/Filter.hpp>
+
+namespace dds
+{
+namespace topic
+{
+typedef dds::topic::detail::Filter Filter;
+}
+}
+
+
+#endif /* DDS_TOPIC_FILTER_HPP_ */
