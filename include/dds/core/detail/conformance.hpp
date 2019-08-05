be8768471db423ba85ff6916bd9e3ebbe58205bc
laura <52071480+laura-eprosima@users.noreply.github.com>
2019-08-05 08:54:03 +0200

    Merge pull request #653 from eProsima/feature/dds-core-xtypes
    Feature/dds-core-xtypes

--- include/dds/core/detail/conformance.hpp
+++ include/dds/core/detail/conformance.hpp
@@ -1,45 +1,44 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
+*/
+
+#ifndef EPROSIMA_DDS_CORE_DETAIL_CONFORMANCE_HPP_
+#define EPROSIMA_DDS_CORE_DETAIL_CONFORMANCE_HPP_
+
+/* The following macros correspond to the compliance "profiles" of the DDS
+ * specification. Implementations shall modify this header to indicate their
+ * level of support:
+ *      - An implementation fully implementing a given profile shall define
+ *        the corresponding macro to "FULL".
+ *      - An implementation partially supporting a given profile shall define
+ *        the corresponding macro to "PARTIAL".
+ *      - And implementation with no support for a given profile shall leave
+ *        the corresponding macro undefined.
  *
+ * DDS Minimum Profile support is required of all DDS implementations;
+ * therefore no corresponding macro is provided.
  */
-#ifndef OSPL_DDS_CORE_DETAIL_CONFORMANCE_HPP_
-#define OSPL_DDS_CORE_DETAIL_CONFORMANCE_HPP_
 
-// Implementation
+//#define OMG_DDS_CONTENT_SUBSCRIPTION_SUPPORT                FULL
+//#define OMG_DDS_MULTI_TOPIC_SUPPORT                         FULL
+//#define OMG_DDS_PERSISTENCE_SUPPORT                         FULL
+//#define OMG_DDS_OWNERSHIP_SUPPORT                           FULL
+//#define OMG_DDS_OBJECT_MODEL_SUPPORT                        FULL
+//#define OMG_DDS_EXTENSIBLE_AND_DYNAMIC_TOPIC_TYPE_SUPPORT   FULL
+#define OMG_DDS_X_TYPES_DYNAMIC_TYPE_SUPPORT                FULL
+//#define OMG_DDS_HAS_PRETTY_PRINT_COUT 1
 
-/**
- * @file
- * @internal
- * @note Values 'set' in this file should be mirrored in etc/doxygen_isocpp2_common.cfg
- * in order to ensure the doxygen documentation includes all supported QoS
- * and features and ting.
- */
-
-#define OMG_DDS_CONTENT_SUBSCRIPTION_SUPPORT                FULL
-// #define OMG_DDS_MULTI_TOPIC_SUPPORT                         FULL
-#define OMG_DDS_PERSISTENCE_SUPPORT                         FULL
-#define OMG_DDS_OWNERSHIP_SUPPORT                           FULL
-#define OMG_DDS_OBJECT_MODEL_SUPPORT                        FULL
-// #define OMG_DDS_EXTENSIBLE_AND_DYNAMIC_TOPIC_TYPE_SUPPORT   FULL
-#define OMG_DDS_X_TYPES_BUILTIN_TOPIC_TYPES_SUPPORT         FULL
-
-#define OMG_DDS_HAS_PRETTY_PRINT_COUT 1
-// End of implementation
-
-#endif /* OSPL_DDS_CORE_DETAIL_CONFORMANCE_HPP_ */
+#endif //EPROSIMA_DDS_CORE_DETAIL_CONFORMANCE_HPP_
