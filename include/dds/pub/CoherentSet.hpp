4ef973b7250b092546469f8c7ae45783cf394d7e
LauraMartin <laura@eprosima.com>
2019-08-05 07:36:14 +0200

    Copyright modified in non-detail classes

---
+++ include/dds/pub/CoherentSet.hpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OMG_DDS_PUB_COHERENT_SET_HPP_
+#define OMG_DDS_PUB_COHERENT_SET_HPP_
+
+#include <dds/pub/detail/CoherentSet.hpp>
+
+namespace dds
+{
+namespace pub
+{
+typedef dds::pub::detail::CoherentSet CoherentSet;
+}
+}
+
+
+#endif /* OMG_DDS_PUB_COHERENT_SET_HPP_ */
