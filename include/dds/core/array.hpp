ad6ca7df54f0f5139d5eb98f26ffe5b7d15b832f
LauraMartin <laura@eprosima.com>
2019-08-05 07:36:14 +0200

    Copyright modified in non-detail classes

---
+++ include/dds/core/array.hpp
@@ -0,0 +1,33 @@
+/* Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef  ORG_OMG_DDS_CORE_ARRAY_HPP_
+#define  ORG_OMG_DDS_CORE_ARRAY_HPP_
+
+#include <dds/core/detail/array.hpp>
+
+namespace dds
+{
+namespace core
+{
+using dds::core::detail::array;
+}
+}
+
+#endif /* ORG_OMG_DDS_CORE_ARRAY_HPP_  */
