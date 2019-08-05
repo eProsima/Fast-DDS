1583d156638dd92997723c502d55102c2e2c8a15
LauraMartin <laura@eprosima.com>
2019-08-05 14:17:49 +0200

    Fix cond, status and policy files

--- include/dds/core/cond/detail/GuardCondition.hpp
+++ include/dds/core/cond/detail/GuardCondition.hpp
@@ -1,67 +1,49 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-#ifndef OSPL_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_
-#define OSPL_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_
+#define EPROSIMA_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_
 
 /**
  * @cond
  * Ignore this file in the API
  */
 
-// Implementation
+//TODO: Add GuardConditionDelegate when is implemented
 
-namespace org
-{
-namespace opensplice
-{
-namespace core
-{
-namespace cond
-{
-class GuardConditionDelegate;
-}
-}
-}
-}
+namespace dds {
+namespace core {
+namespace cond {
 
-namespace dds
-{
-namespace core
-{
-namespace cond
-{
-
-template <typename DELEGATE>
+template<typename DELEGATE>
 class TGuardCondition;
 
-namespace detail
-{
-typedef dds::core::cond::TGuardCondition<org::opensplice::core::cond::GuardConditionDelegate> GuardCondition;
-}
-}
-}
-}
+namespace detail {
 
-// End of implementation
+//TODO: Fix when GuardConditionDelegate is implemented
+//typedef dds::core::cond::TGuardCondition<org::opensplice::core::cond::GuardConditionDelegate> GuardCondition;
+class GuardCondition { };
+
+} //namespace detail
+} //namespace cond
+} //namespace core
+} //namespace dds
+
 
 /** @endcond */
 
-#endif /* OSPL_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_ */
+#endif //EPROSIMA_DDS_CORE_COND_DETAIL_GUARDCONDITION_HPP_
