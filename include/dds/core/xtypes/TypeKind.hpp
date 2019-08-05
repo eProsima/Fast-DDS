f4449f91ee6f39e008794079b0b9a0c845c0298f
laura <52071480+laura-eprosima@users.noreply.github.com>
2019-08-05 08:54:03 +0200

    Merge pull request #653 from eProsima/feature/dds-core-xtypes
    Feature/dds-core-xtypes

--- include/dds/core/xtypes/TypeKind.hpp
+++ include/dds/core/xtypes/TypeKind.hpp
@@ -1,10 +1,8 @@
-#ifndef OMG_DDS_XTYPE_
-#define OMG_DDS_TYPE_OBJECT_HPP_
-
-/* Copyright 2010, Object Management Group, Inc.
+/*
+ * Copyright 2010, Object Management Group, Inc.
  * Copyright 2010, PrismTech, Corp.
  * Copyright 2010, Real-Time Innovations, Inc.
- * All rights reserved.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,47 +17,43 @@
  * limitations under the License.
  */
 
-#include <bitset>
-#include <dds/core/SafeEnumeration.hpp>
+#ifndef OMG_DDS_XTYPES_TYPE_KIND_
+#define OMG_DDS_XTYPES_TYPE_KIND_
+
 #include <dds/core/detail/conformance.hpp>
+#include <dds/core/SafeEnumeration.hpp>
 
+#include <bitset>
 
+#if defined(OMG_DDS_X_TYPES_DYNAMIC_TYPE_SUPPORT)
 
-#if defined(OMG_DDS_X_TYPES_DYNANIC_TYPE_SUPPORT)
+namespace dds {
+namespace core {
+namespace xtypes {
 
-
-// --- Shared meta-data: -------------------------------------------------
-
-namespace dds
-{
-namespace core
-{
-namespace xtypes
-{
-struct TypeKind_def
-{
+struct TypeKind_def {
     enum Type
     {
-        NO_TYPE           =  0, // sentinel indicating "null" value
-        PRIMITIVE_TYPE    = 0x4000,
-        CONSTRUCTED_TYPE  = 0x8000,
-        COLLECTION_TYPE   = 0x0200,
-        AGGREGATION_TYPE  = 0x0100,
-        ANNOTATION_TYPE   = 0x0080,
+        NO_TYPE          = 0, // sentinel indicating "null" value
+        PRIMITIVE_TYPE   = 0x4000,
+        CONSTRUCTED_TYPE = 0x8000,
+        COLLECTION_TYPE  = 0x0200,
+        AGGREGATION_TYPE = 0x0100,
+        ANNOTATION_TYPE  = 0x0080,
 
-        BOOLEAN_TYPE     =  PRIMITIVE_TYPE | 0x0001,
-        UINT_8_TYPE      =  PRIMITIVE_TYPE | 0x0002,
-        INT_16_TYPE      =  PRIMITIVE_TYPE | 0x0003,
-        UINT_16_TYPE     =  PRIMITIVE_TYPE | 0x0004,
-        INT_32_TYPE      =  PRIMITIVE_TYPE | 0x0005,
-        UINT_32_TYPE     =  PRIMITIVE_TYPE | 0x0006,
-        INT_64_TYPE      =  PRIMITIVE_TYPE | 0x0007,
-        UINT_64_TYPE     =  PRIMITIVE_TYPE | 0x0008,
-        FLOAT_32_TYPE    =  PRIMITIVE_TYPE | 0x0009,
-        FLOAT_64_TYPE    =  PRIMITIVE_TYPE | 0x000A,
-        FLOAT_128_TYPE   =  PRIMITIVE_TYPE | 0x000B,
-        CHAR_8_TYPE      =  PRIMITIVE_TYPE | 0x000C,
-        CHAR_32_TYPE     =  PRIMITIVE_TYPE | 0x000D,
+        BOOLEAN_TYPE     = PRIMITIVE_TYPE | 0x0001,
+        UINT_8_TYPE      = PRIMITIVE_TYPE | 0x0002,
+        INT_16_TYPE      = PRIMITIVE_TYPE | 0x0003,
+        UINT_16_TYPE     = PRIMITIVE_TYPE | 0x0004,
+        INT_32_TYPE      = PRIMITIVE_TYPE | 0x0005,
+        UINT_32_TYPE     = PRIMITIVE_TYPE | 0x0006,
+        INT_64_TYPE      = PRIMITIVE_TYPE | 0x0007,
+        UINT_64_TYPE     = PRIMITIVE_TYPE | 0x0008,
+        FLOAT_32_TYPE    = PRIMITIVE_TYPE | 0x0009,
+        FLOAT_64_TYPE    = PRIMITIVE_TYPE | 0x000A,
+        FLOAT_128_TYPE   = PRIMITIVE_TYPE | 0x000B,
+        CHAR_8_TYPE      = PRIMITIVE_TYPE | 0x000C,
+        CHAR_32_TYPE     = PRIMITIVE_TYPE | 0x000D,
 
         ENUMERATION_TYPE = CONSTRUCTED_TYPE | 0x0001,
         BITSET_TYPE      = CONSTRUCTED_TYPE | 0x0002,
@@ -77,12 +71,11 @@ struct TypeKind_def
     };
 };
 
-typedef dds::core::safe_enum<TypeKind_def> TypeKind;
-}
-}
-}
+typedef dds::core::SafeEnum<TypeKind_def> TypeKind;
 
-#endif  // OMG_DDS_X_TYPES_DYNANIC_TYPE_SUPPORT
+} //namespace xtypes
+} //namespace core
+} //namespace dds
 
+#endif // OMG_DDS_X_TYPES_DYNAMIC_TYPE_SUPPORT
 
-#endif // !defined(OMG_DDS_TYPE_OBJECT_HPP_)
--- include/dds/core/xtypes/detail/PrimitiveTypes.hpp
+++ include/dds/core/xtypes/detail/PrimitiveTypes.hpp
@@ -1,69 +1,73 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-#ifndef OMG_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_
-#define OMG_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_
+#define EPROSIMA_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_
 
 #include <dds/core/xtypes/TypeKind.hpp>
 
 // This template specialization have the intent to provide guidelines
 // implementors of this specification. Notice that this define only a subset
 // of primitive types, thus more work for you to do...
+
 namespace dds {
-  namespace core {
-    namespace xtypes {
-      // Notice that const char* const is used instead of std::string
-      // to limit initialization issues with ctors.
-      template<>
-      struct dynamic_type_traits<uint8_t> {
-        static const TypeKind TYPE_ID = TypeKind::UINT_8_TYPE;
-        static const  char* const NAME; // "uint8_t"
-      };
+namespace core {
+namespace xtypes {
 
-      template<>
-      struct dynamic_type_traits<int16_t> {
-        static const TypeKind TYPE_ID = TypeKind::INT_16_TYPE;
-        static const  char* const NAME; // "uint16_t"
-      };
+// Notice that const char* const is used instead of std::string
+// to limit initialization issues with ctors.
 
+template<>
+struct dynamic_type_traits<uint8_t>
+{
+    static constexpr TypeKind TYPE_ID = TypeKind::UINT_8_TYPE;
+    static constexpr const char* NAME = "uint8_t";
+};
 
-      template<>
-      struct dynamic_type_traits<uint16_t> {
-        static const TypeKind TYPE_ID = TypeKind::UINT_16_TYPE;
-        static const  char* const NAME; // "int16_t"
-      };
+template<>
+struct dynamic_type_traits<int16_t>
+{
+    static constexpr TypeKind TYPE_ID = TypeKind::INT_16_TYPE;
+    static constexpr const char* NAME = "int16_t";
+};
 
-      template<>
-      struct dynamic_type_traits<int32_t> {
-        static const TypeKind TYPE_ID = TypeKind::INT_32_TYPE;
-        static const  char* const NAME; // "int16_t"
-      };
+template<>
+struct dynamic_type_traits<uint16_t>
+{
+    static constexpr TypeKind TYPE_ID = TypeKind::UINT_16_TYPE;
+    static constexpr const char* NAME = "uint16_t";
+};
 
-      template<>
-      struct dynamic_type_traits<uint32_t> {
-        static const TypeKind TYPE_ID = TypeKind::UINT_32_TYPE;
-        static const  char* const NAME; // "uint32_t"
-      };
+template<>
+struct dynamic_type_traits<int32_t>
+{
+    static constexpr TypeKind TYPE_ID = TypeKind::INT_32_TYPE;
+    static constexpr const char* NAME = "int32_t";
+};
 
-    }
-  }
-}
+template<>
+struct dynamic_type_traits<uint32_t>
+{
+    static constexpr TypeKind TYPE_ID = TypeKind::UINT_32_TYPE;
+    static constexpr const char* NAME = "uint32_t";
+};
 
+} //namespace xtypes
+} //namespace core
+} //namespace dds
 
-#endif /* OMG_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_ */
+#endif //EPROSIMA_DDS_CORE_XTYPES_DETAIL_PRIMITIVE_TYPES_HPP_
