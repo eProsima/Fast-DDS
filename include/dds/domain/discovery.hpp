ab127aa21043f56868a089ad3cf544194b73b0ed
LauraMartin <laura@eprosima.com>
2019-08-05 07:36:14 +0200

    Copyright modified in non-detail classes

---
+++ include/dds/domain/discovery.hpp
@@ -0,0 +1,73 @@
+/* Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OMG_DDS_DOMAIN_DISCOVERY_HPP_
+#define OMG_DDS_DOMAIN_DISCOVERY_HPP_
+
+#include <dds/core/InstanceHandle.hpp>
+#include <dds/domain/DomainParticipant.hpp>
+
+
+namespace dds {
+namespace domain {
+
+/**
+ * This function enables you to ignore the entity
+ * represented by the given InstanceHandle for the specific
+ * DomainParticipant.
+  *
+ * <b><i>This operation is not yet implemented. It is scheduled for a future release.</i></b>
+ *
+ * @param dp      the DomainParticipant for which the remote
+ *                entity will be ignored
+ *
+ * @param handle  the InstanceHandle of the remote entity that
+ *                has to be ignored
+ *
+ */
+void OMG_DDS_API ignore(
+        const dds::domain::DomainParticipant& dp,
+        const dds::core::InstanceHandle& handle);
+
+/**
+ * This function enables you to ignore a series of entities
+ * whose instance handles are made available via the provided iterators.
+ *
+ * <b><i>This operation is not yet implemented. It is scheduled for a future release.</i></b>
+ *
+ * @param dp      the DomainParticipant for which the remote
+ *                entity will be ignored
+ *
+ * @param begin   the begin iterator for the InstanceHandle
+ *                to ignore
+ *
+ * @param end     the end iterator for the InstanceHandle
+ *                to ignore
+ *
+ */
+template<typename FwdIterator>
+void ignore(
+        const dds::domain::DomainParticipant& dp,
+        FwdIterator begin,
+        FwdIterator end);
+
+
+}
+}
+#endif //OMG_DDS_DOMAIN_DISCOVERY_HPP_
---
+++ include/dds/topic/TTopicDescription.hpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2010, Object Management Group, Inc.
+ * Copyright 2010, PrismTech, Corp.
+ * Copyright 2010, Real-Time Innovations, Inc.
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OMG_DDS_T_TOPIC_TOPIC_DESCRIPTION_HPP_
+#define OMG_DDS_T_TOPIC_TOPIC_DESCRIPTION_HPP_
+
+#include <dds/core/TEntity.hpp>
+#include <dds/topic/TopicTraits.hpp>
+
+namespace dds
+{
+namespace topic
+{
+template <typename DELEGATE>
+class TTopicDescription;
+}
+}
+
+
+/**
+ * @brief
+ * This class is the base for Topic, ContentFilteredTopic and MultiTopic.
+ *
+ * The TopicDescription attribute type_name defines an unique data type that is
+ * made available to the Data Distribution Service when a Topic is created with
+ * that type.<br>
+ * TopicDescription has also a name that allows it to be retrieved locally.
+ *
+ * @see @ref DCPS_Modules_TopicDefinition "Topic Definition"
+ */
+template <typename DELEGATE>
+class dds::topic::TTopicDescription : public virtual dds::core::Reference<DELEGATE>
+{
+public:
+    OMG_DDS_REF_TYPE_PROTECTED_DC(TTopicDescription, dds::core::Reference, DELEGATE)
+    OMG_DDS_IMPLICIT_REF_BASE(TTopicDescription)
+
+public:
+    /** @cond */
+    virtual ~TTopicDescription();
+    /** @endcond */
+
+public:
+    /**
+     * This operation returns the name used to create the TopicDescription.
+     *
+     * @return the TopicDescription name
+     */
+    const std::string& name() const;
+
+    /**
+     * This operation returns the registered name of the data type associated
+     * with the TopicDescription.
+     *
+     * @return the type_name
+     */
+    const std::string& type_name() const;
+
+    /**
+     * This operation returns the DomainParticipant associated with the
+     * TopicDescription.
+     *
+     * Note that there is exactly one DomainParticipant associated with
+     * each TopicDescription.
+     *
+     * @return the DomainParticipant
+     */
+    const dds::domain::DomainParticipant& domain_participant() const;
+
+};
+
+
+#endif /* OMG_DDS_T_TOPIC_TOPIC_DESCRIPTION_HPP_ */
