379d3e3dd9a64dc97145db3a1a19efe8989f23bb
LauraMartin <laura@eprosima.com>
2019-08-27 10:24:11 +0200

    Refs #6171. Fix core folder files

--- include/dds/core/detail/TInstanceHandleImpl.hpp
+++ include/dds/core/detail/TInstanceHandleImpl.hpp
@@ -1,109 +1,108 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-#ifndef OSPL_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_
-#define OSPL_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_
+#define EPROSIMA_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_
 
 /**
  * @file
  */
 #include <dds/core/detail/Value.hpp>
-#include <org/opensplice/core/InstanceHandleDelegate.hpp>
+//#include <org/opensplice/core/InstanceHandleDelegate.hpp>
 
 /*
  * OMG PSM class declaration
  */
-#include <dds/core/TInstanceHandle.hpp>
+#include <dds/core/InstanceHandle.hpp>
 
-// Implementation
-namespace dds
-{
-namespace core
-{
+namespace dds {
+namespace core {
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TInstanceHandle<DELEGATE>::TInstanceHandle() { }
 
-template <typename DELEGATE>
-template <typename ARG0>
-TInstanceHandle<DELEGATE>::TInstanceHandle(const ARG0& arg0) : dds::core::Value<DELEGATE>(arg0) { }
+template<typename DELEGATE>
+template<typename ARG0>
+TInstanceHandle<DELEGATE>::TInstanceHandle(
+        const ARG0& arg0)
+    : dds::core::Value<DELEGATE>(arg0) { }
 
-template <typename DELEGATE>
-TInstanceHandle<DELEGATE>::TInstanceHandle(const dds::core::null_type& nullHandle) : dds::core::Value<DELEGATE>(nullHandle) { }
+template<typename DELEGATE>
+TInstanceHandle<DELEGATE>::TInstanceHandle(
+        const dds::core::null_type& nullHandle)
+    : dds::core::Value<DELEGATE>(nullHandle) { }
 
-template <typename DELEGATE>
-TInstanceHandle<DELEGATE>::TInstanceHandle(const TInstanceHandle& other): dds::core::Value<DELEGATE>(other.delegate()) { }
+template<typename DELEGATE>
+TInstanceHandle<DELEGATE>::TInstanceHandle(
+        const TInstanceHandle& other)
+    : dds::core::Value<DELEGATE>(other.delegate()) { }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TInstanceHandle<DELEGATE>::~TInstanceHandle() { }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TInstanceHandle<DELEGATE>& TInstanceHandle<DELEGATE>::operator=(const TInstanceHandle& that)
 {
-    if(this != &that)
-    {
-        this->delegate() = that.delegate();
-    }
-    return *this;
+    //To implement
 }
 
-template <typename DELEGATE>
-bool TInstanceHandle<DELEGATE>::operator==(const TInstanceHandle& that) const
+template<typename DELEGATE>
+bool TInstanceHandle<DELEGATE>::operator ==(
+        const TInstanceHandle& that) const
 {
-    return this->delegate() == that.delegate();
+    //To implement
 }
 
-template <typename DELEGATE>
-bool TInstanceHandle<DELEGATE>::operator<(const TInstanceHandle& that) const
+template<typename DELEGATE>
+bool TInstanceHandle<DELEGATE>::operator <(
+        const TInstanceHandle& that) const
 {
-    return this->delegate() < that.delegate();
+    //To implement
 }
 
-template <typename DELEGATE>
-bool TInstanceHandle<DELEGATE>::operator>(const TInstanceHandle& that) const
+template<typename DELEGATE>
+bool TInstanceHandle<DELEGATE>::operator >(
+        const TInstanceHandle& that) const
 {
-    return this->delegate() > that.delegate();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 const TInstanceHandle<DELEGATE> TInstanceHandle<DELEGATE>::nil()
 {
-    dds::core::null_type nt;
-    static TInstanceHandle nil_handle(nt);
-    return nil_handle;
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 bool TInstanceHandle<DELEGATE>::is_nil() const
 {
-    return this->delegate().is_nil();
-}
-}
+    //To implement
 }
 
-inline std::ostream& operator << (std::ostream& os, const dds::core::TInstanceHandle<org::opensplice::core::InstanceHandleDelegate>& h)
-{
-    os << h.delegate();
-    return os;
-}
+} //namespace core
+} //namespace dds
 
-// End of implementation
+//TODO: Fix when InstanceHandleDelegate is implemented
+//inline std::ostream& operator <<(
+//        std::ostream& os,
+//        const dds::core::TInstanceHandle<org::opensplice::core::InstanceHandleDelegate>& h)
+//{
+//    os << h.delegate();
+//    return os;
+//}
 
-#endif /* OSPL_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_ */
+#endif //EPROSIMA_DDS_CORE_TINSTANCEHANDLE_IMPL_HPP_
