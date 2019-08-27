b3d578b4331c66093edf8514958cbf0ac6dcd5c1
LauraMartin <laura@eprosima.com>
2019-08-27 09:00:51 +0200

    Refs #6171. Fix policy folder files

--- include/dds/core/policy/detail/CorePolicy.hpp
+++ include/dds/core/policy/detail/CorePolicy.hpp
@@ -1,10 +1,5 @@
-#ifndef OMG_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
-#define OMG_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
-
-/* Copyright 2010, Object Management Group, Inc.
- * Copyright 2010, PrismTech, Corp.
- * Copyright 2010, Real-Time Innovations, Inc.
- * All rights reserved.
+/*
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
+*/
+
+#ifndef EPROSIMA_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
+#define EPROSIMA_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
 
 //TODO: Fix when PolicyDelegate and Policy are implemented
 //#include <org/opensplice/core/policy/PolicyDelegate.hpp>
@@ -145,4 +144,4 @@ class WriterDataLifecycle { };
 
 /** @endcond */
 
-#endif //OMG_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
+#endif //EPROSIMA_DDS_CORE_POLICY_DETAIL_CORE_POLICY_HPP_
