82afcf759adde2c41d3248780086e8b3fdb5e29a
LauraMartin <laura@eprosima.com>
2019-08-28 12:48:32 +0200

    Refs #6171. Fix pub folder files

--- include/dds/pub/detail/TCoherentSetImpl.hpp
+++ include/dds/pub/detail/TCoherentSetImpl.hpp
@@ -1,25 +1,22 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-#ifndef OSPL_DDS_PUB_TCOHERENTSET_IMPL_HPP_
-#define OSPL_DDS_PUB_TCOHERENTSET_IMPL_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_PUB_TCOHERENTSET_IMPL_HPP_
+#define EPROSIMA_DDS_PUB_TCOHERENTSET_IMPL_HPP_
 
 /**
  * @file
@@ -28,36 +25,33 @@
 /*
  * OMG PSM class declaration
  */
-#include <dds/pub/TCoherentSet.hpp>
-#include <org/opensplice/core/ReportUtils.hpp>
+#include <dds/pub/CoherentSet.hpp>
+//#include <org/opensplice/core/ReportUtils.hpp>
 
-// Implementation
 
-namespace dds
+namespace dds {
+namespace pub {
+
+template<typename DELEGATE>
+TCoherentSet<DELEGATE>::TCoherentSet(
+        const dds::pub::Publisher& pub)
+    : dds::core::Value<DELEGATE>(pub)
 {
-namespace pub
-{
+}
 
-template <typename DELEGATE>
-TCoherentSet<DELEGATE>::TCoherentSet(const dds::pub::Publisher& pub) : dds::core::Value<DELEGATE>(pub)  {  }
-
-template <typename DELEGATE>
+template<typename DELEGATE>
 void TCoherentSet<DELEGATE>::end()
 {
-    ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this->delegate().get_publisher().delegate().get());
-    this->delegate().end();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TCoherentSet<DELEGATE>::~TCoherentSet()
 {
-    ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this->delegate().get_publisher().delegate().get());
-    this->delegate().end();
+    //To implement
 }
 
-}
-}
+} //namespace pub
+} //namespace dds
 
-// End of implementation
-
-#endif /* OSPL_DDS_PUB_TCOHERENTSET_HPP_ */
+#endif //EPROSIMA_DDS_PUB_TCOHERENTSET_HPP_
