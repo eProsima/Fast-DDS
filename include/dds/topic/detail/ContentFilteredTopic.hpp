7dcc4743fcb7b8134e2c1ad5c0681f3368c1702d
LauraMartin <laura@eprosima.com>
2019-08-28 12:51:38 +0200

    Refs #6171. Modify topic files due to core references

--- include/dds/topic/detail/ContentFilteredTopic.hpp
+++ include/dds/topic/detail/ContentFilteredTopic.hpp
@@ -26,11 +26,12 @@
 #include <dds/core/types.hpp>
 #include <dds/topic/Topic.hpp>
 #include <dds/topic/Filter.hpp>
-#include <org/opensplice/topic/TopicDescriptionDelegate.hpp>
-#include <org/opensplice/core/ScopedLock.hpp>
+//TODO: Fix when TopicDescriptionDelegate and ScopedLock are implemented
+//#include <org/opensplice/topic/TopicDescriptionDelegate.hpp>
+//#include <org/opensplice/core/ScopedLock.hpp>
 
-#include "u_topic.h"
-#include "v_kernelParser.h"
+//#include "u_topic.h"
+//#include "v_kernelParser.h"
 
 #ifdef OMG_DDS_CONTENT_SUBSCRIPTION_SUPPORT
 
@@ -43,162 +44,163 @@ namespace dds {
 namespace topic {
 namespace detail {
 
-template <typename T>
-class ContentFilteredTopic  : public virtual org::opensplice::topic::TopicDescriptionDelegate
-{
-public:
-    ContentFilteredTopic(
-        const dds::topic::Topic<T>& topic,
-        const std::string& name,
-        const dds::topic::Filter& filter)
-        : org::opensplice::topic::TopicDescriptionDelegate(topic.domain_participant(), name, topic.type_name()),
-          myTopic(topic),
-          myFilter(filter)
-    {
-        ISOCPP_REPORT_STACK_DDS_BEGIN(topic);
-        validate_filter();
-        topic.delegate()->incrNrDependents();
-        this->myParticipant.delegate()->add_cfTopic(*this);
-    }
+//template <typename T>
+//class ContentFilteredTopic  : public virtual org::opensplice::topic::TopicDescriptionDelegate
+//{
+//public:
+//    ContentFilteredTopic(
+//        const dds::topic::Topic<T>& topic,
+//        const std::string& name,
+//        const dds::topic::Filter& filter)
+//        : org::opensplice::topic::TopicDescriptionDelegate(topic.domain_participant(), name, topic.type_name()),
+//          myTopic(topic),
+//          myFilter(filter)
+//    {
+//        ISOCPP_REPORT_STACK_DDS_BEGIN(topic);
+//        validate_filter();
+//        topic.delegate()->incrNrDependents();
+//        this->myParticipant.delegate()->add_cfTopic(*this);
+//    }
 
-    virtual ~ContentFilteredTopic()
-    {
-        if (!this->closed) {
-            try {
-                this->close();
-            } catch (...) {
-                /* Empty: the exception throw should have already traced an error. */
-            }
-        }
-    }
+//    virtual ~ContentFilteredTopic()
+//    {
+//        if (!this->closed) {
+//            try {
+//                this->close();
+//            } catch (...) {
+//                /* Empty: the exception throw should have already traced an error. */
+//            }
+//        }
+//    }
 
-    virtual void close()
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        org::opensplice::core::ScopedObjectLock scopedLock(*this);
+//    virtual void close()
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        org::opensplice::core::ScopedObjectLock scopedLock(*this);
 
-        myTopic.delegate()->decrNrDependents();
+//        myTopic.delegate()->decrNrDependents();
 
-        // Remove the ContentFilteredTopic from the list of topics in its participant.
-        this->myParticipant.delegate()->remove_cfTopic(*this);
+//        // Remove the ContentFilteredTopic from the list of topics in its participant.
+//        this->myParticipant.delegate()->remove_cfTopic(*this);
 
-        org::opensplice::core::ObjectDelegate::close();
-    }
+//        org::opensplice::core::ObjectDelegate::close();
+//    }
 
-    void
-    init(org::opensplice::core::ObjectDelegate::weak_ref_type weak_ref)
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//    void
+//    init(org::opensplice::core::ObjectDelegate::weak_ref_type weak_ref)
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
 
-        /* Set weak_ref before passing ourselves to other isocpp objects. */
-        this->set_weak_ref(weak_ref);
-        /* Register topic at participant. */
-        this->myParticipant.delegate()->add_cfTopic(*this);
-    }
+//        /* Set weak_ref before passing ourselves to other isocpp objects. */
+//        this->set_weak_ref(weak_ref);
+//        /* Register topic at participant. */
+//        this->myParticipant.delegate()->add_cfTopic(*this);
+//    }
 
-private:
-    void validate_filter()
-    {
-        q_expr expr = NULL;
-        uint32_t length;
-        std::vector<c_value> params;
+//private:
+//    void validate_filter()
+//    {
+//        q_expr expr = NULL;
+//        uint32_t length;
+//        std::vector<c_value> params;
 
-        length = myFilter.parameters_length();
-        if (length < 100) {
-            expr = v_parser_parse(myFilter.expression().c_str());
-            if (!expr ) {
-                ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
-                        "filter_expression '%s' is invalid", myFilter.expression().c_str());
-            }
-        } else {
-            ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
-                    "Invalid number of filter_parameters '%d', maximum is 99", length);
-        }
+//        length = myFilter.parameters_length();
+//        if (length < 100) {
+//            expr = v_parser_parse(myFilter.expression().c_str());
+//            if (!expr ) {
+//                ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
+//                        "filter_expression '%s' is invalid", myFilter.expression().c_str());
+//            }
+//        } else {
+//            ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
+//                    "Invalid number of filter_parameters '%d', maximum is 99", length);
+//        }
 
-        u_topic uTopic = (u_topic)(myTopic.delegate()->get_user_handle());
+//        u_topic uTopic = (u_topic)(myTopic.delegate()->get_user_handle());
 
-        params = reader_parameters();
-        if (!u_topicContentFilterValidate2(uTopic, expr, params.empty() ? NULL : &params[0], params.size())) {
-            q_dispose(expr);
-            ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
-                    "filter_expression '%s' is invalid.", myFilter.expression().c_str());
-        }
-        q_dispose(expr);
-    }
+//        params = reader_parameters();
+//        if (!u_topicContentFilterValidate2(uTopic, expr, params.empty() ? NULL : &params[0], params.size())) {
+//            q_dispose(expr);
+//            ISOCPP_THROW_EXCEPTION(ISOCPP_INVALID_ARGUMENT_ERROR,
+//                    "filter_expression '%s' is invalid.", myFilter.expression().c_str());
+//        }
+//        q_dispose(expr);
+//    }
 
-public:
-    std::string reader_expression() const
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        std::string rExpr;
+//public:
+//    std::string reader_expression() const
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        std::string rExpr;
 
-        rExpr += "select * from ";
-        rExpr += myTopic.name();
-        rExpr += " where ";
-        rExpr += myFilter.expression();
-        return rExpr;
-    }
+//        rExpr += "select * from ";
+//        rExpr += myTopic.name();
+//        rExpr += " where ";
+//        rExpr += myFilter.expression();
+//        return rExpr;
+//    }
 
-    std::vector<c_value> reader_parameters() const
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        std::vector<c_value> params;
-        uint32_t n, length;
-        org::opensplice::topic::FilterDelegate::const_iterator paramIterator;
+//    std::vector<c_value> reader_parameters() const
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        std::vector<c_value> params;
+//        uint32_t n, length;
+//        org::opensplice::topic::FilterDelegate::const_iterator paramIterator;
 
-        length = myFilter.parameters_length();
-        if (length != 0) {
-            for (n = 0, paramIterator = myFilter.begin(); n < length; n++, paramIterator++) {
-                params.push_back(c_stringValue(const_cast<char *>(paramIterator->c_str())));
-            }
-        }
-        return params;
-    }
+//        length = myFilter.parameters_length();
+//        if (length != 0) {
+//            for (n = 0, paramIterator = myFilter.begin(); n < length; n++, paramIterator++) {
+//                params.push_back(c_stringValue(const_cast<char *>(paramIterator->c_str())));
+//            }
+//        }
+//        return params;
+//    }
 
-    /**
-    *  @internal Accessor to return the topic filter.
-    * @return The dds::topic::Filter in effect on this topic.
-    */
-    const dds::topic::Filter& filter() const
-    {
-        return myFilter;
-    }
+//    /**
+//    *  @internal Accessor to return the topic filter.
+//    * @return The dds::topic::Filter in effect on this topic.
+//    */
+//    const dds::topic::Filter& filter() const
+//    {
+//        return myFilter;
+//    }
 
-    /**
-     *  @internal Sets the filter parameters for this content filtered topic.
-     * @param begin The iterator holding the first string param
-     * @param end The last item in the string iteration
-     */
-    template <typename FWIterator>
-    void filter_parameters(const FWIterator& begin, const FWIterator& end)
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        ISOCPP_THROW_EXCEPTION(ISOCPP_UNSUPPORTED_ERROR, "Changing of Filter parameters is currently not supported.");
-        myFilter.parameters(begin, end);
-        validate_filter();
-    }
+//    /**
+//     *  @internal Sets the filter parameters for this content filtered topic.
+//     * @param begin The iterator holding the first string param
+//     * @param end The last item in the string iteration
+//     */
+//    template <typename FWIterator>
+//    void filter_parameters(const FWIterator& begin, const FWIterator& end)
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        ISOCPP_THROW_EXCEPTION(ISOCPP_UNSUPPORTED_ERROR, "Changing of Filter parameters is currently not supported.");
+//        myFilter.parameters(begin, end);
+//        validate_filter();
+//    }
 
-    const dds::topic::Topic<T>& topic() const
-    {
-        return myTopic;
-    }
+//    const dds::topic::Topic<T>& topic() const
+//    {
+//        return myTopic;
+//    }
 
-    const std::string& filter_expression() const
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        return myFilter.expression();
-    }
+//    const std::string& filter_expression() const
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        return myFilter.expression();
+//    }
 
-    const dds::core::StringSeq filter_parameters() const
-    {
-        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
-        return dds::core::StringSeq(myFilter.begin(), myFilter.end());
-    }
+//    const dds::core::StringSeq filter_parameters() const
+//    {
+//        ISOCPP_REPORT_STACK_DELEGATE_BEGIN(this);
+//        return dds::core::StringSeq(myFilter.begin(), myFilter.end());
+//    }
 
-private:
-    dds::topic::Topic<T> myTopic;
-    dds::topic::Filter myFilter;
-};
+//private:
+//    dds::topic::Topic<T> myTopic;
+//    dds::topic::Filter myFilter;
+//};
+class ContentFilteredTopic { };
 
 }
 }
--- include/dds/topic/detail/Topic.hpp
+++ include/dds/topic/detail/Topic.hpp
@@ -1,25 +1,22 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-#ifndef OSPL_DDS_TOPIC_DETAIL_TOPIC_HPP_
-#define OSPL_DDS_TOPIC_DETAIL_TOPIC_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_TOPIC_DETAIL_TOPIC_HPP_
+#define EPROSIMA_DDS_TOPIC_DETAIL_TOPIC_HPP_
 
 /**
  * @file
@@ -35,41 +32,27 @@
 #include <dds/topic/qos/TopicQos.hpp>
 #include <dds/topic/AnyTopic.hpp>
 
-#include <org/opensplice/core/config.hpp>
-#include <org/opensplice/topic/TopicTraits.hpp>
-#include <org/opensplice/topic/AnyTopicDelegate.hpp>
-#include <org/opensplice/topic/TopicDescriptionDelegate.hpp>
-
-#include "u_topic.h"
+//TODO: Fix when TopicTraits, AnyTopicDelegate and TopicDescriptionDelegate are implemented
+//#include <org/opensplice/core/config.hpp>
+//#include <org/opensplice/topic/TopicTraits.hpp>
+//#include <org/opensplice/topic/AnyTopicDelegate.hpp>
+//#include <org/opensplice/topic/TopicDescriptionDelegate.hpp>
 
 /**
  * @cond
  * Ignore this file in the API
  */
 
-namespace dds
-{
-namespace topic
-{
-template <typename T>
+namespace dds {
+namespace topic {
+
+template<typename T>
 class TopicListener;
 
-}
-}
+namespace detail {
 
-namespace dds
-{
-namespace topic
-{
-namespace detail
-{
-template <typename T>
-class Topic;
-}
-}
-}
-
-
+template<typename T>
+class Topic { };
 
 /***************************************************************************
  *
@@ -77,64 +60,63 @@ class Topic;
  * Implementation can be found in dds/topic/detail/TTopicImpl.hpp
  *
  ***************************************************************************/
-template <typename T>
-class dds::topic::detail::Topic : public org::opensplice::topic::AnyTopicDelegate
-{
-public:
 
-    typedef typename ::dds::core::smart_ptr_traits< Topic<T> >::ref_type ref_type;
-    typedef typename ::dds::core::smart_ptr_traits< Topic<T> >::weak_ref_type weak_ref_type;
+//TODO: Fix when TopicTraits, AnyTopicDelegate and TopicDescriptionDelegate are implemented
+//template <typename T>
+//class Topic : public org::opensplice::topic::AnyTopicDelegate
+//{
+//public:
 
-    Topic(const dds::domain::DomainParticipant& dp,
-          const std::string& name,
-          const std::string& type_name,
-          const dds::topic::qos::TopicQos& qos,
-          dds::topic::TopicListener<T>* listener,
-          const dds::core::status::StatusMask& mask);
+//    typedef typename ::dds::core::smart_ptr_traits<Topic<T>>::ref_type ref_type;
+//    typedef typename ::dds::core::smart_ptr_traits<Topic<T>>::weak_ref_type weak_ref_type;
 
-    Topic(const dds::domain::DomainParticipant& dp,
-          const std::string& name,
-          const std::string& type_name,
-          const dds::topic::qos::TopicQos& qos,
-          u_topic uTopic);
+//    Topic(
+//            const dds::domain::DomainParticipant& dp,
+//            const std::string& name,
+//            const std::string& type_name,
+//            const dds::topic::qos::TopicQos& qos,
+//            dds::topic::TopicListener<T>* listener,
+//            const dds::core::status::StatusMask& mask);
 
-    virtual ~Topic();
+//    virtual ~Topic();
 
-    virtual void close();
+//    virtual void close();
 
-    void init(ObjectDelegate::weak_ref_type weak_ref);
+//    void init(
+//            ObjectDelegate::weak_ref_type weak_ref);
 
-    dds::topic::Topic<T, dds::topic::detail::Topic> wrapper();
+//    dds::topic::Topic<T, Topic> wrapper();
 
+//    void listener(
+//            dds::topic::TopicListener<T>* listener,
+//            const ::dds::core::status::StatusMask& mask);
 
-public:
+//    dds::topic::TopicListener<T>* listener();
 
-    void listener(dds::topic::TopicListener<T>* listener,
-                  const ::dds::core::status::StatusMask& mask);
+//    virtual void listener_notify(
+//            ObjectDelegate::ref_type source,
+//            uint32_t triggerMask,
+//            void *eventData,
+//            void *listener);
 
-    dds::topic::TopicListener<T>* listener();
+//    dds::topic::TTopicDescription<TopicDescriptionDelegate> clone();
 
-    virtual void listener_notify(ObjectDelegate::ref_type source,
-                                 uint32_t       triggerMask,
-                                 void           *eventData,
-                                 void           *listener);
+//    static dds::topic::Topic<T, Topic> discover_topic(
+//            const dds::domain::DomainParticipant& dp,
+//            const std::string& name,
+//            const dds::core::Duration& timeout);
 
-    dds::topic::TTopicDescription<TopicDescriptionDelegate> clone();
+//    static void discover_topics(
+//            const dds::domain::DomainParticipant& dp,
+//            std::vector<dds::topic::Topic<T, Topic>>& topics,
+//            uint32_t max_size);
 
-    static dds::topic::Topic<T, dds::topic::detail::Topic>
-    discover_topic(const dds::domain::DomainParticipant& dp,
-                   const std::string& name,
-                   const dds::core::Duration& timeout);
+//};
 
-    static void
-    discover_topics(const dds::domain::DomainParticipant& dp,
-                    std::vector<dds::topic::Topic<T, dds::topic::detail::Topic> >& topics,
-                    uint32_t max_size);
-
-};
+} //namespace detail
+} //namespace topic
+} //namespace dds
 
 /** @endcond */
 
-// End of implementation
-
-#endif /* OSPL_DDS_TOPIC_DETAIL_TOPIC_HPP_ */
+#endif //EPROSIMA_DDS_TOPIC_DETAIL_TOPIC_HPP_
