f65507f6fa58a8e19945db02739b135afdc7c132
LauraMartin <laura@eprosima.com>
2019-08-28 12:51:38 +0200

    Refs #6171. Modify topic files due to core references

--- include/dds/topic/TopicDescription.hpp
+++ include/dds/topic/TopicDescription.hpp
@@ -21,14 +21,71 @@
 #define OMG_DDS_TOPIC_TOPIC_DESCRIPTION_HPP_
 
 #include <dds/topic/detail/TopicDescription.hpp>
+#include <dds/domain/DomainParticipant.hpp>
+#include <dds/core/Reference.hpp>
 
 
-namespace dds
+namespace dds {
+namespace topic {
+
+/**
+ * @brief
+ * This class is the base for Topic, ContentFilteredTopic and MultiTopic.
+ *
+ * The TopicDescription attribute type_name defines an unique data type that is
+ * made available to the Data Distribution Service when a Topic is created with
+ * that type.<br>
+ * TopicDescription has also a name that allows it to be retrieved locally.
+ *
+ * @see @ref DCPS_Modules_TopicDefinition "Topic Definition"
+ */
+template<typename DELEGATE>
+class TTopicDescription : public virtual dds::core::Reference<DELEGATE>
 {
-namespace topic
-{
+public:
+    OMG_DDS_REF_TYPE_PROTECTED_DC(
+            TTopicDescription,
+            dds::core::Reference,
+            DELEGATE)
+
+    OMG_DDS_IMPLICIT_REF_BASE(
+            TTopicDescription)
+
+    /** @cond */
+    virtual ~TTopicDescription();
+    /** @endcond */
+
+    /**
+     * This operation returns the name used to create the TopicDescription.
+     *
+     * @return the TopicDescription name
+     */
+    const std::string& name() const;
+
+    /**
+     * This operation returns the registered name of the data type associated
+     * with the TopicDescription.
+     *
+     * @return the type_name
+     */
+    const std::string& type_name() const;
+
+    /**
+     * This operation returns the DomainParticipant associated with the
+     * TopicDescription.
+     *
+     * Note that there is exactly one DomainParticipant associated with
+     * each TopicDescription.
+     *
+     * @return the DomainParticipant
+     */
+    const dds::domain::DomainParticipant& domain_participant() const;
+
+};
+
 typedef ::dds::topic::detail::TopicDescription TopicDescription;
-}
-}
 
-#endif /* OMG_DDS_TOPIC_TOPIC_DESCRIPTION_HPP_ */
+} //namespace topic
+} //namespace dds
+
+#endif //OMG_DDS_TOPIC_TOPIC_DESCRIPTION_HPP_
--- include/dds/topic/detail/Filter.hpp
+++ include/dds/topic/detail/Filter.hpp
@@ -1,27 +1,26 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-#ifndef DDS_TOPIC_DETAIL_QUERY_HPP_
-#define DDS_TOPIC_DETAIL_QUERY_HPP_
+*/
 
-#include <dds/topic/detail/TFilterImpl.hpp>
-#include <org/opensplice/topic/FilterDelegate.hpp>
+#ifndef EPROSIMA_DDS_TOPIC_DETAIL_QUERY_HPP_
+#define EPROSIMA_DDS_TOPIC_DETAIL_QUERY_HPP_
+
+//#include <dds/topic/detail/TFilterImpl.hpp>
+//TODO: Fix when FilterDelegate is implemented
+//#include <org/opensplice/topic/FilterDelegate.hpp>
 
 /**
  * @cond
@@ -31,11 +30,15 @@
 namespace dds {
 namespace topic {
 namespace detail {
-typedef ::dds::topic::TFilter< ::org::opensplice::topic::FilterDelegate > Filter;
-}
-}
-}
+
+//TODO: Fix when FilterDelegate is implemented
+//typedef ::dds::topic::TFilter< ::org::opensplice::topic::FilterDelegate > Filter;
+class Filter { };
+
+} //namespace detail
+} //namespace topic
+} //namespace dds
 
 /** @endcond */
 
-#endif /* DDS_TOPIC_DETAIL_QUERY_HPP_ */
+#endif //EPORSIMA_DDS_TOPIC_DETAIL_QUERY_HPP_
