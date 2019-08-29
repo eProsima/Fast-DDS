cc10d526add3bab547ade1a085f1dc8d055ab544
LauraMartin <laura@eprosima.com>
2019-08-29 11:28:13 +0200

    Refs #6171. Fix sub folder files

--- include/dds/sub/cond/detail/QueryCondition.hpp
+++ include/dds/sub/cond/detail/QueryCondition.hpp
@@ -1,10 +1,5 @@
-#ifndef OMG_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_
-#define OMG_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_
-
-/* Copyright 2010, Object Management Group, Inc.
- * Copyright 2010, PrismTech, Corp.
- * Copyright 2010, Real-Time Innovations, Inc.
- * All rights reserved.
+/*
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,45 +12,46 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
+*/
+
+#ifndef EPROSIMA_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_
+#define EPROSIMA_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_
 
 /**
  * @cond
  * Ignore this file in the API
  */
 
-namespace org
-{
-namespace opensplice
-{
-namespace sub
-{
-namespace cond
-{
-class QueryConditionDelegate;
-}
-}
-}
-}
+//namespace org {
+//namespace opensplice {
+//namespace sub {
+//namespace cond {
+//class QueryConditionDelegate;
 
-namespace dds
-{
-namespace sub
-{
-namespace cond
-{
+//}
+//}
+//}
+//}
 
-template <typename DELEGATE>
+namespace dds {
+namespace sub {
+namespace cond {
+
+template<typename DELEGATE>
 class TQueryCondition;
 
-namespace detail
-{
-typedef dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate> QueryCondition;
-}
-}
-}
-}
+namespace detail {
+
+//TODO: Fix when QueryConditionDelegate is implemented
+//typedef dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate> QueryCondition;
+class QueryCondition;
+
+} //namespace detail
+} //namespace cond
+} //namespace sub
+} //namespace dds
 
 /** @endcond */
 
-#endif /* OMG_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_ */
+#endif //EPROSIMA_DDS_SUB_DETAIL_QUERY_CONDITION_HPP_
