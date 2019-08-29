ce763cf4cea8783fd9f1e0b6979895c8c1d3aef5
LauraMartin <laura@eprosima.com>
2019-08-29 11:15:25 +0200

    Refs #6171. Fix sub folder files

--- include/dds/sub/cond/detail/TQueryConditionImpl.hpp
+++ include/dds/sub/cond/detail/TQueryConditionImpl.hpp
@@ -1,25 +1,22 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
-#ifndef OSPL_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_
-#define OSPL_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_
+*/
+
+#ifndef EPROSIMA_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_
+#define EPROSIMA_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_
 
 /**
  * @file
@@ -28,242 +25,174 @@
 /*
  * OMG PSM class declaration
  */
-#include <dds/sub/cond/TQueryCondition.hpp>
-#include <org/opensplice/sub/cond/QueryConditionDelegate.hpp>
+#include <dds/sub/cond/QueryCondition.hpp>
+//#include <org/opensplice/sub/cond/QueryConditionDelegate.hpp>
 
 // Implementation
 
-namespace dds
-{
-namespace sub
-{
-namespace cond
-{
+namespace dds {
+namespace sub {
+namespace cond {
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::Query& query,
-    const dds::sub::status::DataState& status)
+        const dds::sub::Query& query,
+        const dds::sub::status::DataState& status)
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(query);
-
-    this->set_ref(new DELEGATE(query.data_reader(),
-                               query.expression(), query.delegate()->parameters(), status));
-    this->delegate()->init(this->impl_);
+    //To implement
 }
 
 /** @cond
  * Somehow, these cause functions duplicates in doxygen documentation.
  */
-template <typename DELEGATE>
-template <typename FUN>
+template<typename DELEGATE>
+template<typename FUN>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::Query& query,
-    const dds::sub::status::DataState& status, FUN& functor)
+        const dds::sub::Query& query,
+        const dds::sub::status::DataState& status, FUN& functor)
 {
-	ISOCPP_REPORT_STACK_DDS_BEGIN(query);
-
-    this->set_ref(new DELEGATE(query.data_reader(),
-                               query.expression(), query.delegate()->parameters(), status));
-    this->delegate()->set_handler(functor);
-	this->delegate()->init(this->impl_);
-
+    //To implement
 }
 
-template <typename DELEGATE>
-template <typename FUN>
+template<typename DELEGATE>
+template<typename FUN>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::Query& query,
-    const dds::sub::status::DataState& status, const FUN& functor)
+        const dds::sub::Query& query,
+        const dds::sub::status::DataState& status, const FUN& functor)
 {
-	ISOCPP_REPORT_STACK_DDS_BEGIN(query);
-
-    this->set_ref(new DELEGATE(query.data_reader(),
-                               query.expression(), query.delegate()->parameters(), status));
-    this->delegate()->set_handler(functor);
-    this->delegate()->init(this->impl_);
-
+    //To implement
 }
 /** @endcond */
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::AnyDataReader& dr,
-    const std::string& expression,
-    const std::vector<std::string>& params,
-    const dds::sub::status::DataState& status)
+        const dds::sub::AnyDataReader& dr,
+        const std::string& expression,
+        const std::vector<std::string>& params,
+        const dds::sub::status::DataState& status)
 {
-	ISOCPP_REPORT_STACK_DDS_BEGIN(dr);
-
-    this->set_ref(new DELEGATE(dr, expression, params, status));
-	this->delegate()->init(this->impl_);
+    //To implement
 }
 
 /** @cond
  * Somehow, these cause functions duplicates in doxygen documentation.
  */
-template <typename DELEGATE>
-template <typename FUN>
+template<typename DELEGATE>
+template<typename FUN>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::AnyDataReader& dr,
-    const std::string& expression,
-    const std::vector<std::string>& params,
-    const dds::sub::status::DataState& status,
-    FUN& functor)
+        const dds::sub::AnyDataReader& dr,
+        const std::string& expression,
+        const std::vector<std::string>& params,
+        const dds::sub::status::DataState& status,
+        FUN& functor)
 {
-	ISOCPP_REPORT_STACK_DDS_BEGIN(dr);
-
-    this->set_ref(new DELEGATE(dr, expression, params, status));
-    this->delegate()->set_handler(functor);
-	this->delegate()->init(this->impl_);
+    //To implement
 }
 
-template <typename DELEGATE>
-template <typename FUN>
+template<typename DELEGATE>
+template<typename FUN>
 TQueryCondition<DELEGATE>::TQueryCondition(
-    const dds::sub::AnyDataReader& dr,
-    const std::string& expression,
-    const std::vector<std::string>& params,
-    const dds::sub::status::DataState& status,
-    const FUN& functor)
+        const dds::sub::AnyDataReader& dr,
+        const std::string& expression,
+        const std::vector<std::string>& params,
+        const dds::sub::status::DataState& status,
+        const FUN& functor)
 {
-	ISOCPP_REPORT_STACK_DDS_BEGIN(dr);
-
-    this->set_ref(new DELEGATE(dr, expression, params, status));
-    this->delegate()->set_handler(functor);
-    this->delegate()->init(this->impl_);
+    //To implement
 }
 /** @endcond */
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 TQueryCondition<DELEGATE>::~TQueryCondition()
 {
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 template<typename FWIterator>
-void TQueryCondition<DELEGATE>::parameters(const FWIterator& begin, const FWIterator end)
+void TQueryCondition<DELEGATE>::parameters(
+        const FWIterator& begin,
+        const FWIterator end)
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    std::vector<std::string> params(begin, end);
-    this->delegate()->parameters(params);
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 void TQueryCondition<DELEGATE>::expression(
     const std::string& expr)
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    this->delegate()->expression(expr);
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 const std::string& TQueryCondition<DELEGATE>::expression()
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    return this->delegate()->expression();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 typename TQueryCondition<DELEGATE>::const_iterator TQueryCondition<DELEGATE>::begin() const
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    return this->delegate()->begin();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 typename TQueryCondition<DELEGATE>::const_iterator TQueryCondition<DELEGATE>::end() const
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    return this->delegate()->end();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 typename TQueryCondition<DELEGATE>::iterator TQueryCondition<DELEGATE>::begin()
 {
-    return this->delegate()->begin();
+    //To implement;
 }
 
 template <typename DELEGATE>
 typename TQueryCondition<DELEGATE>::iterator TQueryCondition<DELEGATE>::end()
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    return this->delegate()->end();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 void TQueryCondition<DELEGATE>::add_parameter(
     const std::string& param)
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    this->delegate()->add_parameter(param);
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 uint32_t TQueryCondition<DELEGATE>::parameters_length() const
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-
-    return this->delegate()->parameters_length();
+    //To implement
 }
 
-template <typename DELEGATE>
+template<typename DELEGATE>
 const AnyDataReader& TQueryCondition<DELEGATE>::data_reader() const
 {
-    ISOCPP_REPORT_STACK_DDS_BEGIN(*this);
-    return this->delegate()->data_reader();
+    //To implement
 }
 
-}
-}
-namespace core
+} //namespace cond
+} //namespace sub
+
+namespace core {
+namespace cond {
+template<typename DELEGATE>
+TCondition<DELEGATE>::TCondition(
+        /*const dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate>& h*/)
 {
-namespace cond
-{
-template <typename DELEGATE>
-TCondition<DELEGATE>::TCondition(const dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate>& h)
-{
-    if (h.is_nil()) {
-        /* We got a null object and are not really able to do a typecheck here. */
-        /* So, just set a null object. */
-        *this = dds::core::null;
-    } else {
-        this->::dds::core::Reference<DELEGATE>::impl_ = OSPL_CXX11_STD_MODULE::dynamic_pointer_cast<DELEGATE_T>(h.delegate());
-        if (h.delegate() != this->::dds::core::Reference<DELEGATE>::impl_) {
-            throw dds::core::IllegalOperationError(std::string("Attempted invalid cast: ") + typeid(h).name() + " to " + typeid(*this).name());
-        }
-    }
+    //To implement
 }
 
-template <typename DELEGATE>
-TCondition<DELEGATE>&
-TCondition<DELEGATE>::operator=(const dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate>& rhs)
+template<typename DELEGATE>
+TCondition<DELEGATE>& TCondition<DELEGATE>::operator=(
+        /*const dds::sub::cond::TQueryCondition<org::opensplice::sub::cond::QueryConditionDelegate>& rhs*/)
 {
-    if (this != (TCondition*)&rhs) {
-        if (rhs.is_nil()) {
-            /* We got a null object and are not really able to do a typecheck here. */
-            /* So, just set a null object. */
-            *this = dds::core::null;
-        } else {
-            TCondition other(rhs);
-            /* Dont have to copy when the delegate is the same. */
-            if (other.delegate() != this->::dds::core::Reference<DELEGATE>::impl_) {
-                *this = other;
-            }
-        }
-    }
-    return *this;
+    //To implement
 }
-}
-}
-}
-// End of implementation
 
-#endif /* OSPL_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_ */
+} //namespace cond
+} //namespace core
+} //namespace dds
+
+#endif //EPROSIMA_DDS_SUB_COND_TQUERYCONDITION_IMPL_HPP_
--- include/dds/sub/detail/SampleInfo.hpp
+++ include/dds/sub/detail/SampleInfo.hpp
@@ -1,38 +1,43 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
-#ifndef OMG_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_
-#define OMG_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_
+*/
 
-#include <dds/sub/detail/TSampleInfoImpl.hpp>
-#include <org/opensplice/sub/SampleInfoImpl.hpp>
+#ifndef EPROSIMA_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_
+#define EPROSIMA_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_
+
+//#include <dds/sub/detail/TSampleInfoImpl.hpp>
+//#include <org/opensplice/sub/SampleInfoImpl.hpp>
 
 /**
  * @cond
  * Ignore this file in the API
  */
 
-namespace dds { namespace sub { namespace detail {
-  typedef dds::sub::TSampleInfo<org::opensplice::sub::SampleInfoImpl> SampleInfo;
-} } }
+namespace dds {
+namespace sub {
+namespace detail {
+
+//typedef dds::sub::TSampleInfo<org::opensplice::sub::SampleInfoImpl> SampleInfo;
+class SampleInfo { };
+
+}
+}
+}
 
 /** @endcond */
 
-#endif /* OMG_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_ */
+#endif //EPROSIMA_DDS_SUB_DETAIL_SAMPLE_INFO_HPP_
 
