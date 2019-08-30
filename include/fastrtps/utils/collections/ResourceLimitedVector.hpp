818550fc0ddfc379400ecc31702d0acd0ab9625b
Ricardo González <correoricky@gmail.com>
2019-08-30 09:15:56 +0200

    Added copy constructor to ResourceLimitedVector. [6247] (#681)
    * Refs #6244. Added copy constructor to ResourceLimitedVector.
    
    * Refs #6244. Fixed use of c++14.
    
    * Refs #6244. Applying coding style.

--- include/fastrtps/utils/collections/ResourceLimitedVector.hpp
+++ include/fastrtps/utils/collections/ResourceLimitedVector.hpp
@@ -95,6 +95,15 @@ public:
         collection_.reserve(cfg.initial);
     }
 
+    ResourceLimitedVector(
+            const ResourceLimitedVector& other)
+        : configuration_(other.configuration_)
+        , collection_(other.collection_.get_allocator())
+    {
+        collection_.reserve(other.collection_.capacity());
+        collection_.assign(other.collection_.begin(), other.collection_.end());
+    }
+
     ResourceLimitedVector& operator = (const ResourceLimitedVector& other)
     {
         clear();
