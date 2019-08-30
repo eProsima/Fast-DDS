5617d4e4f4a7545afad920bfbb17fc6a06f5e94e
LauraMartin <laura@eprosima.com>
2019-08-30 11:47:59 +0200

    Refs #6171. All files reviewed

--- include/dds/topic/detail/TopicInstanceImpl.hpp
+++ include/dds/topic/detail/TopicInstanceImpl.hpp
@@ -1,23 +1,20 @@
 /*
- *                         Vortex OpenSplice
+ * Copyright 2019, Proyectos y Sistemas de Mantenimiento SL (eProsima).
  *
- *   This software and documentation are Copyright 2006 to TO_YEAR ADLINK
- *   Technology Limited, its affiliated companies and licensors. All rights
- *   reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
- */
+*/
+
 #ifndef EPROSIMA_DDS_TOPIC_TOPICINSTANCE_HPP_
 #define EPROSIMA_DDS_TOPIC_TOPICINSTANCE_HPP_
 
@@ -30,67 +27,75 @@
  */
 #include <dds/topic/TopicInstance.hpp>
 
-// Implementation
-
 namespace dds {
 namespace topic {
 
 template<typename T>
 TopicInstance<T>::TopicInstance()
-    : h_(dds::core::null) {}
+    : h_(dds::core::null)
+{
+}
 
 template<typename T>
 TopicInstance<T>::TopicInstance(
         const dds::core::InstanceHandle& h)
-    : h_(h), sample_() {}
+    : h_(h), sample_()
+{
+}
 
 template<typename T>
 TopicInstance<T>::TopicInstance(
         const dds::core::InstanceHandle& h,
         const T& sample)
-    : h_(h), sample_(sample) { }
+    : h_(h), sample_(sample)
+{
+}
 
 template<typename T>
 TopicInstance<T>::operator const dds::core::InstanceHandle() const
 {
-    return h_;
+    //To implement
+//    return h_;
 }
 
 template<typename T>
 const dds::core::InstanceHandle TopicInstance<T>::handle() const
 {
-    return h_;
+    //To implement
+//    return h_;
 }
 
 template<typename T>
 void TopicInstance<T>::handle(
         const dds::core::InstanceHandle& h)
 {
-    h_ = h;
+    //To implement
+//    h_ = h;
 }
 
 template<typename T>
 const T& TopicInstance<T>::sample() const
 {
-    return sample_;
+    //To implement
+//    return sample_;
 }
 
 template<typename T>
 T& TopicInstance<T>::sample()
 {
-    return sample_;
+    //To implement
+//    return sample_;
 }
 
 template<typename T>
 void TopicInstance<T>::sample(
         const T& sample)
 {
-    sample_ = sample;
+    //To implement
+//    sample_ = sample;
 }
 
-}
-}
+} //namespace topic
+} //namespace dds
 
-// End of implementation
-
-#endif /* EPROSIMA_DDS_TOPIC_TOPICINSTANCE_HPP_ */
+#endif //EPROSIMA_DDS_TOPIC_TOPICINSTANCE_HPP_
