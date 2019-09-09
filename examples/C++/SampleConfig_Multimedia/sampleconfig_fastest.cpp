47beefd412d86685dd4b4991c67383b385500254
Raquel Alvarez <raquelalvarez@eprosima.com>
2019-09-09 11:24:35 +0200

    Refs #6312 Removing eClock from SampleConfig_Multimedia

--- examples/C++/SampleConfig_Multimedia/sampleconfig_fastest.cpp
+++ examples/C++/SampleConfig_Multimedia/sampleconfig_fastest.cpp
@@ -10,8 +10,6 @@
 #include <fastrtps/Domain.h>
 #include <fastrtps/subscriber/SampleInfo.h>
 
-#include <fastrtps/utils/eClock.h>
-
 #include "samplePubSubTypes.h"
 
 using namespace eprosima::fastrtps;
@@ -98,7 +96,7 @@ void fastest(){
         myPub->write(&my_sample);
     }
 
-    eClock::my_sleep(1500);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
 
     //Read the contents of both histories:
         std::cout << "The Reliable Subscriber (with a history depth of 5) holds: " << std::endl;
