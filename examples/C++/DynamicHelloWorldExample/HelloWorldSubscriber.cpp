16c0bf0b8875515c3b9e98ddcf1c6e74214bbfcc
Raquel Alvarez <raquelalvarez@eprosima.com>
2019-09-09 10:35:16 +0200

    Refs #6312 Removing eClock from DynamicHelloWorld example

--- examples/C++/DynamicHelloWorldExample/HelloWorldSubscriber.cpp
+++ examples/C++/DynamicHelloWorldExample/HelloWorldSubscriber.cpp
@@ -22,7 +22,6 @@
 #include <fastrtps/attributes/SubscriberAttributes.h>
 #include <fastrtps/subscriber/Subscriber.h>
 #include <fastrtps/Domain.h>
-#include <fastrtps/utils/eClock.h>
 #include <fastrtps/types/DynamicTypeBuilderFactory.h>
 #include <fastrtps/types/DynamicDataFactory.h>
 #include <fastrtps/types/DynamicTypeBuilder.h>
@@ -149,5 +148,7 @@ void HelloWorldSubscriber::run(uint32_t number)
 {
     std::cout << "Subscriber running until "<< number << "samples have been received"<<std::endl;
     while(number > this->m_listener.n_samples)
-        eClock::my_sleep(500);
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
 }
