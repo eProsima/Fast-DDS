6faebb32f6f740236e1241105bd5861b6ba63dc9
Laura <laura@eprosima.com>
2019-10-04 09:42:57 +0200

    Refs #6032. Remove unnecessary prints

--- examples/C++/DDS/HelloWorldExample/HelloWorldPublisher.cpp
+++ examples/C++/DDS/HelloWorldExample/HelloWorldPublisher.cpp
@@ -87,8 +87,6 @@ void HelloWorldPublisher::PubListener::on_publication_matched(
         eprosima::fastdds::dds::DataWriter*,
         const eprosima::fastdds::dds::PublicationMatchedStatus &info)
 {
-    std::cout << "Current Count " << info.current_count << " Change " << info.current_count_change << std::endl;
-    std::cout << "Total Count " << info.total_count << " Chnage " << info.total_count_change << std::endl;
     if (info.current_count_change == 1)
     {
         matched_ = info.current_count;
