f21721bcf06d2de403fab63164972c4a795dc982
LuisGasco <luisgasco@eprosima.com>
2019-10-14 07:16:09 +0200

    Refs #6557 Fixing windows Warnings

--- src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
@@ -148,7 +148,7 @@ void PDPListener::onNewCacheChangeAdded(
                 RTPSParticipantListener* listener = parent_pdp_->getRTPSParticipant()->getListener();
                 if (listener != nullptr)
                 {
-                    std::lock_guard<std::mutex> lock(parent_pdp_->callback_mtx_);
+                    std::lock_guard<std::mutex> cb_lock(parent_pdp_->callback_mtx_);
                     ParticipantDiscoveryInfo info(*pdata);
                     info.status = status;
 
