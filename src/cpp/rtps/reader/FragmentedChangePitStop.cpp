09f68483fb7bf53dd550eba8f9e03d91c5e64c9a
Miguel Company <MiguelCompany@eprosima.com>
2019-10-15 12:31:18 +0200

    Refs #6609. Removing vector of fragment state.

--- src/cpp/rtps/reader/FragmentedChangePitStop.cpp
+++ src/cpp/rtps/reader/FragmentedChangePitStop.cpp
@@ -69,51 +69,17 @@ CacheChange_t* FragmentedChangePitStop::process(
 
     original_change_cit->getChange()->received_fragments(fragment_starting_num - 1, fragments_in_submessage);
 
-    bool was_updated = false;
-    for (uint32_t count = (fragment_starting_num - 1); count < (fragment_starting_num - 1) + fragments_in_submessage; ++count)
+    size_t original_offset = size_t(fragment_starting_num - 1) * original_change_cit->getChange()->getFragmentSize();
+
+    memcpy(
+        &original_change_cit->getChange()->serializedPayload.data[original_offset],
+        incoming_change->serializedPayload.data,
+        incoming_change->serializedPayload.length);
+
+    if (original_change_cit->getChange()->is_fully_assembled())
     {
-        if(original_change_cit->getChange()->getDataFragments()->at(count) == ChangeFragmentStatus_t::NOT_PRESENT)
-        {
-            size_t original_offset = size_t(count) * original_change_cit->getChange()->getFragmentSize();
-            size_t incoming_offset = size_t(count - (fragment_starting_num - 1)) * incoming_change->getFragmentSize();
-
-            // All cases minus last fragment.
-            if (count + 1 != original_change_cit->getChange()->getFragmentCount())
-            {
-                memcpy(original_change_cit->getChange()->serializedPayload.data + original_offset,
-                        incoming_change->serializedPayload.data + incoming_offset,
-                        incoming_change->getFragmentSize());
-            }
-            // Last fragment is a special case when copying.
-            else
-            {
-                memcpy(original_change_cit->getChange()->serializedPayload.data + original_offset,
-                        incoming_change->serializedPayload.data + incoming_offset,
-                        original_change_cit->getChange()->serializedPayload.length - original_offset);
-            }
-
-            original_change_cit->getChange()->getDataFragments()->at(count) = ChangeFragmentStatus_t::PRESENT;
-
-            was_updated = true;
-        }
-    }
-
-    // If was updated, check if it is completed.
-    if(was_updated)
-    {
-        auto fit = original_change_cit->getChange()->getDataFragments()->begin();
-        for(; fit != original_change_cit->getChange()->getDataFragments()->end(); ++fit)
-        {
-            if(*fit == ChangeFragmentStatus_t::NOT_PRESENT)
-                break;
-        }
-
-        // If it is completed, return CacheChange_t and remove information.
-        if(fit == original_change_cit->getChange()->getDataFragments()->end())
-        {
-            returnedValue = original_change_cit->getChange();
-            changes_.erase(original_change_cit);
-        }
+        returnedValue = original_change_cit->getChange();
+        changes_.erase(original_change_cit);
     }
 
     return returnedValue;
