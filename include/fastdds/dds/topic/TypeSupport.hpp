d08ab33d356ec99b5a290ef067b06cb3b1973a7c
Luis Gasco <luisgasco@eprosima.com>
2019-10-22 08:41:14 +0200

    Feature/pim typesupport [6639] (#794)
    * Refs #5808 TypeSupport as full class (instead of std::shared_ptr) and still compatible with TopicDataType.
    
    * Refs #5808 Changes to addapt TypeLookup to the new TypeSupport.
    
    * Refs #5808 Fixes and store shared_ptr instead of raw ptr.
    
    * Refs #6639 Requested changes. Extending the change to DomainParticipant.

--- include/fastdds/dds/topic/TypeSupport.hpp
+++ include/fastdds/dds/topic/TypeSupport.hpp
@@ -21,6 +21,7 @@
 
 #include <fastdds/dds/topic/TopicDataType.hpp>
 #include <fastrtps/types/DynamicPubSubType.h>
+#include <fastrtps/types/TypesBase.h>
 #include <string>
 #include <functional>
 #include <memory>
@@ -32,29 +33,35 @@ namespace dds {
 class DomainParticipant;
 
 /**
- * NOTE: This class inherits from std::shared_ptr<TopicDataType>.
  * Class TypeSupport used to provide the DomainRTPSParticipant with the methods to serialize,
  * deserialize and get the key of a specific data type.
  * The user should created a class that inherits from this one,
  * where Serialize and deserialize methods MUST be implemented.
  * @ingroup FASTDDS_MODULE
  */
-class TypeSupport : public std::shared_ptr<fastdds::dds::TopicDataType>
+class TypeSupport
 {
-public:
-    using Base = std::shared_ptr<fastdds::dds::TopicDataType>;
-    using Base::operator->;
-    using Base::operator*;
-    using Base::operator bool;
-    using Base::operator=;
+private:
+    //! Data Type Name
+    std::string data_type_name_;
 
+    //! Type Identifier
+    const fastrtps::types::TypeIdentifier* type_id_;
+
+    // TopicDataType for retro-compatibility
+    std::shared_ptr<fastdds::dds::TopicDataType> topic_data_type_;
+
+public:
     RTPS_DllAPI TypeSupport()
-        : std::shared_ptr<fastdds::dds::TopicDataType>(nullptr)
+        : type_id_(nullptr)
+        , topic_data_type_(nullptr)
     {}
 
     RTPS_DllAPI TypeSupport(
             const TypeSupport& type)
-        : std::shared_ptr<fastdds::dds::TopicDataType>(type)
+        : data_type_name_(type.data_type_name_)
+        , type_id_(type.type_id_)
+        , topic_data_type_(type.topic_data_type_)
     {}
 
     /*!
@@ -65,7 +72,9 @@ public:
      */
     RTPS_DllAPI explicit TypeSupport(
             fastdds::dds::TopicDataType* ptr)
-        : std::shared_ptr<fastdds::dds::TopicDataType>(ptr)
+        : data_type_name_(ptr != nullptr ? ptr->m_topicDataTypeName : "")
+        , type_id_(ptr != nullptr ? ptr->type_id_ : nullptr)
+        , topic_data_type_(std::shared_ptr<fastdds::dds::TopicDataType>(ptr))
     {}
 
     /*!
@@ -75,47 +84,69 @@ public:
      */
     RTPS_DllAPI TypeSupport(
             fastrtps::types::DynamicPubSubType ptr)
-        : std::shared_ptr<fastdds::dds::TopicDataType>(std::make_shared<fastrtps::types::DynamicPubSubType>(std::move(ptr)))
+        : data_type_name_(ptr.m_topicDataTypeName)
+        , type_id_(ptr.type_id_)
+        , topic_data_type_(std::shared_ptr<fastdds::dds::TopicDataType>(
+                  std::make_shared<fastrtps::types::DynamicPubSubType>(std::move(ptr))))
     {}
 
-    RTPS_DllAPI virtual bool register_type(
+    RTPS_DllAPI virtual eprosima::fastrtps::types::ReturnCode_t register_type(
             DomainParticipant* participant,
             std::string type_name) const;
 
     RTPS_DllAPI virtual const std::string& get_type_name() const
     {
-        return get()->m_topicDataTypeName;
+        return data_type_name_;
     }
 
     RTPS_DllAPI virtual bool serialize(
             void* data,
-            fastrtps::rtps::SerializedPayload_t* payload)
+            fastrtps::rtps::SerializedPayload_t* payload) const
     {
-        return get()->serialize(data, payload);
+        if (topic_data_type_ != nullptr)
+        {
+            return topic_data_type_->serialize(data, payload);
+        }
+        return false;
     }
 
     RTPS_DllAPI virtual bool deserialize(
             fastrtps::rtps::SerializedPayload_t* payload,
             void* data)
     {
-        return get()->deserialize(payload, data);
+        if (topic_data_type_ != nullptr)
+        {
+            return topic_data_type_->deserialize(payload, data);
+        }
+        return false;
     }
 
     RTPS_DllAPI virtual std::function<uint32_t()> get_serialized_size_provider(
             void* data)
     {
-        return get()->getSerializedSizeProvider(data);
+        if (topic_data_type_ != nullptr)
+        {
+            topic_data_type_->getSerializedSizeProvider(data);
+        }
+        return [](){ return 0; };
     }
 
     RTPS_DllAPI virtual void* create_data()
     {
-        return get()->createData();
+        if (topic_data_type_ != nullptr)
+        {
+            return topic_data_type_->createData();
+        }
+        return nullptr;
     }
 
     RTPS_DllAPI virtual void delete_data(
-            void * data)
+            void* data)
     {
-        return get()->deleteData(data);
+        if (topic_data_type_ != nullptr)
+        {
+            topic_data_type_->deleteData(data);
+        }
     }
 
     RTPS_DllAPI virtual bool get_key(
@@ -123,20 +154,44 @@ public:
             fastrtps::rtps::InstanceHandle_t* i_handle,
             bool force_md5 = false)
     {
-        return get()->getKey(data, i_handle, force_md5);
+        if (topic_data_type_ != nullptr)
+        {
+            return topic_data_type_->getKey(data, i_handle, force_md5);
+        }
+        return false;
     }
 
     RTPS_DllAPI virtual bool operator==(
             const TypeSupport& type_support)
     {
-        return get()->m_typeSize == type_support->m_typeSize
-               && get()->m_isGetKeyDefined == type_support->m_isGetKeyDefined
-               && get()->m_topicDataTypeName == type_support->m_topicDataTypeName;
+        if (topic_data_type_ != nullptr && type_support.topic_data_type_ != nullptr)
+        {
+            if (topic_data_type_ == type_support.topic_data_type_)
+            {
+                return true;
+            }
+            else
+            {
+                return topic_data_type_->m_typeSize == type_support.topic_data_type_->m_typeSize
+                       && topic_data_type_->m_isGetKeyDefined == type_support.topic_data_type_->m_isGetKeyDefined
+                       && topic_data_type_->m_topicDataTypeName == type_support.topic_data_type_->m_topicDataTypeName;
+            }
+        }
+        return data_type_name_ == type_support.data_type_name_;
     }
 
     RTPS_DllAPI bool empty() const
     {
-        return get() == nullptr;
+        if (topic_data_type_ != nullptr)
+        {
+            return topic_data_type_ == nullptr;
+        }
+        return data_type_name_.empty();
+    }
+
+    RTPS_DllAPI TopicDataType* get() const
+    {
+        return topic_data_type_.get();
     }
 };
 
