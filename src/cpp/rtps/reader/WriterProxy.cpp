dbb6bb3ace637d478d305ff4fc94b3718badc610
Iker Luengo <ikerluengo@eprosima.com>
2019-11-05 17:36:33 +0100

    Correct checking of intra-process writer

--- src/cpp/rtps/reader/WriterProxy.cpp
+++ src/cpp/rtps/reader/WriterProxy.cpp
@@ -112,7 +112,7 @@ void WriterProxy::start(
     guid_as_vector_.push_back(attributes_.guid());
     guid_prefix_as_vector_.push_back(attributes_.guid().guidPrefix);
     is_alive_ = true;
-    is_on_same_process_ = (reader_->getRTPSParticipant()->find_local_writer(attributes_.guid()) != nullptr);
+    is_on_same_process_ = reader_->getRTPSParticipant()->getGuid().is_on_same_process_as(attributes_.guid());
     if (!is_on_same_process_)
     {
         initial_acknack_->restart_timer();
@@ -128,7 +128,7 @@ void WriterProxy::update(const WriterProxyData& attributes)
 
     assert(is_alive_);
     attributes_ = attributes;
-    is_on_same_process_ = (reader_->getRTPSParticipant()->find_local_writer(attributes_.guid()) != nullptr);
+    is_on_same_process_ = reader_->getRTPSParticipant()->getGuid().is_on_same_process_as(attributes_.guid());
     if (is_on_same_process_)
     {
         initial_acknack_->cancel_timer();
