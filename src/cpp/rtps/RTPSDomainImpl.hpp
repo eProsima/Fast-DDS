00bd0561ce99d215eb15338c293ba9818a1f135e
Miguel Company <MiguelCompany@eprosima.com>
2019-11-05 17:28:02 +0100

    Refs #6792. Performance improvement.

--- src/cpp/rtps/RTPSDomainImpl.hpp
+++ src/cpp/rtps/RTPSDomainImpl.hpp
@@ -40,10 +40,10 @@ public:
     {
         for (auto participant : RTPSDomain::m_RTPSParticipants)
         {
-            // Reader found
-            if (auto reader = participant.second->find_local_reader(reader_guid))
+            if (participant.second->getGuid().guidPrefix == reader_guid.guidPrefix)
             {
-                return reader;
+                // Participant found, forward the query
+                return participant.second->find_local_reader(reader_guid); 
             }
         }
 
@@ -53,15 +53,15 @@ public:
     /***
      * @returns A pointer to a local writer given its endpoint guid, or nullptr if not found.
      */
-    static RTPSReader* find_local_writer(
+    static RTPSWriter* find_local_writer(
             const GUID_t& writer_guid)
     {
         for (auto participant : RTPSDomain::m_RTPSParticipants)
         {
-            // Writer found
-            if (auto writer = participant.second->find_local_writer(writer_guid))
+            if (participant.second->getGuid().guidPrefix == writer_guid.guidPrefix)
             {
-                return writer;
+                // Participant found, forward the query
+                return participant.second->find_local_writer(writer_guid);
             }
         }
 
