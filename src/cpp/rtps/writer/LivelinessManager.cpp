86e55b7824f5bc10a06fadf641724efea249cc5c
Ricardo González Moreno <correoricky@gmail.com>
2019-11-14 07:53:07 +0100

    Removing a logError in favor of a logInfo.
    Fixes ros2/rmw_fastrtps#337

--- src/cpp/rtps/writer/LivelinessManager.cpp
+++ src/cpp/rtps/writer/LivelinessManager.cpp
@@ -208,7 +208,7 @@ bool LivelinessManager::assert_liveliness(LivelinessQosPolicyKind kind)
     // Updates the timer owner
     if (!calculate_next())
     {
-        logError(RTPS_WRITER, "Error when restarting liveliness timer: " << writers_.size() << " writers, liveliness " << kind);
+        logInfo(RTPS_WRITER, "Error when restarting liveliness timer: " << writers_.size() << " writers, liveliness " << kind);
         return false;
     }
 
