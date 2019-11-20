f6be2ad0cbaafc6c09b2d3edad6f5c5e49c01b8f
Miguel Company <miguelcompany@eprosima.com>
2019-11-20 14:42:50 +0100

    Space alignment

--- include/fastrtps/utils/collections/foonathan_memory_helpers.hpp
+++ include/fastrtps/utils/collections/foonathan_memory_helpers.hpp
@@ -52,7 +52,7 @@ std::size_t memory_pool_block_size(
 
     return num_elems
            * ((node_size > MemoryPool::min_node_size ? node_size : MemoryPool::min_node_size) // Room for elements
-           * (foonathan::memory::detail::debug_fence_size ? 3 : 1))               // Room for debug info
+           * (foonathan::memory::detail::debug_fence_size ? 3 : 1))                           // Room for debug info
            + foonathan::memory::detail::memory_block_stack::implementation_offset;            // Room for padding
 }
 
