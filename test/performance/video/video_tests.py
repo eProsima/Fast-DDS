99095e7313853b970f22b4263a1ab22b20a58dbb
EduPonz <eduardoponz@eprosima.com>
2019-11-25 15:25:11 +0100

    Refs #6890: Parametrized python to launch separate video tests.

--- test/performance/video/video_tests.py
+++ test/performance/video/video_tests.py
@@ -12,32 +12,141 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import shlex, subprocess, time, os, socket, sys
+import argparse
+import subprocess
+import os
 
-command = os.environ.get("VIDEO_TEST_BIN")
-certs_path = os.environ.get("CERTS_PATH")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        '-x',
+        '--xml_file',
+        help='A Fast-RTPS XML configuration file',
+        required=False
+    )
+    parser.add_argument(
+        '-n',
+        '--number_of_samples',
+        help='The number of measurements to take for each payload',
+        required=False,
+        default=10000
+    )
+    parser.add_argument(
+        '-H',
+        '--height',
+        help='Height of the video',
+        required=False,
+        default=720
+    )
+    parser.add_argument(
+        '-w',
+        '--width',
+        help='Width of the video',
+        required=False,
+        default=1024
+    )
+    parser.add_argument(
+        '-r',
+        '--frame_rate',
+        help='Frame rate of the video [Hz]',
+        required=False,
+        default=30
+    )
+    parser.add_argument(
+        '-t',
+        '--test_duration',
+        help='The duration of the test [s]',
+        required=False,
+        default=2
+    )
+    parser.add_argument(
+        '-s',
+        '--security',
+        action='store_true',
+        help='Enables security (Defaults: disable)',
+        required=False
+    )
+    # Parse arguments
+    args = parser.parse_args()
+    xml_file = args.xml_file
+    samples = str(args.number_of_samples)
+    height = str(args.height)
+    width = str(args.width)
+    frame_rate = str(args.frame_rate)
+    test_duration = str(args.test_duration)
 
-security_options = []
+    # Security flag
+    security = False
+    if args.security:
+        security = True
 
-if certs_path:
-    security_options = ["--security=true", "--certs=" + certs_path]
+    # Environment variables
+    executable = os.environ.get('VIDEO_TEST_BIN')
+    certs_path = os.environ.get('CERTS_PATH')
 
-# Best effort
-subscriber_proc = subprocess.Popen([command, "subscriber", "--seed", str(os.getpid()), "--hostname"] +
-        security_options)
-publisher_proc = subprocess.Popen([command, "publisher", "--seed", str(os.getpid()), "--hostname", "--export_csv"] +
-        security_options)
+    # Security
+    security_options = []
+    if security is True:
+        if certs_path:
+            security_options = ['--security=true', '--certs=' + certs_path]
 
-subscriber_proc.communicate()
-publisher_proc.communicate()
+    # Domain
+    domain = str(os.getpid() % 230)
+    domain_options = ['--domain', domain]
 
-# Reliable
-subscriber_proc = subprocess.Popen([command, "subscriber", "-r", "reliable", "--seed", str(os.getpid()), "--hostname"] +
-        security_options)
-publisher_proc = subprocess.Popen([command, "publisher", "-r", "reliable", "--seed", str(os.getpid()), "--hostname",
-    "--export_csv"] + security_options)
+    # Base of test command for publisher agent
+    pub_command = [
+        executable,
+        'publisher',
+        '--samples',
+        samples,
+        '--testtime',
+        test_duration,
+        '--xml',
+        xml_file,
+        '--width',
+        width,
+        '--height',
+        height,
+        '--rate',
+        frame_rate
+    ]
+    # Base of test command for subscriber agent
+    sub_command = [
+        executable,
+        'subscriber',
+        '--xml',
+        xml_file,
+    ]
 
-subscriber_proc.communicate()
-publisher_proc.communicate()
+    # Manage security
+    if security is True:
+        pub_command += security_options
+        sub_command += security_options
 
-quit()
+    pub_command += domain_options
+    sub_command += domain_options
+
+
+    print('Publisher command: {}'.format(
+            ' '.join(element for element in pub_command)),
+            flush=True
+    )
+    print('Subscriber command: {}'.format(
+            ' '.join(element for element in sub_command)),
+            flush=True
+    )
+
+    # Spawn processes
+    subscriber = subprocess.Popen(pub_command)
+    publisher = subprocess.Popen(sub_command)
+    # Wait until finish
+    subscriber.communicate()
+    publisher.communicate()
+
+    if subscriber.returncode != 0:
+            exit(subscriber.returncode)
+    elif publisher.returncode != 0:
+            exit(publisher.returncode)
