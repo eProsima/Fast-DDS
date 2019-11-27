690f16de23f1784d27e7dbfd97a7cd19c9934961
Ricardo González Moreno <correoricky@gmail.com>
2019-11-27 08:52:16 +0100

    Refs #6929. Improving readerhistory sorting.

--- include/fastdds/rtps/history/ReaderHistory.h
+++ include/fastdds/rtps/history/ReaderHistory.h
@@ -96,10 +96,6 @@ public:
             const GUID_t& writer_guid);
 
     /**
-     * Sort the CacheChange_t from the History by timestamp
-     */
-    RTPS_DllAPI void sortCacheChanges();
-    /**
      * Update the maximum and minimum sequenceNumber cacheChanges.
      */
     RTPS_DllAPI void updateMaxMinSeqNum() override;
