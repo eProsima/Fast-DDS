844eb7ff76fd06bf780bac7b72fab22a00bf0eee
Miguel Company <miguelcompany@eprosima.com>
2020-02-03 16:43:27 +0100

    Removing unnecessary locators when detecting intraprocess only usecase (#917)
    * Clearing locators when detecting intraprocess only config
    
    * Adding is_intraprocess_only() method to RTPSParticipantImpl

--- src/cpp/rtps/participant/RTPSParticipantImpl.cpp
+++ src/cpp/rtps/participant/RTPSParticipantImpl.cpp
@@ -54,6 +54,7 @@
 #include <algorithm>
 
 #include <fastrtps/log/Log.h>
+#include <fastrtps/xmlparser/XMLProfileManager.h>
 
 namespace eprosima {
 namespace fastrtps {
@@ -68,6 +69,15 @@ static EntityId_t TrustedWriter(
         (reader == c_EntityId_SEDPSubReader) ? c_EntityId_SEDPSubWriter :
         (reader == c_EntityId_ReaderLiveliness) ? c_EntityId_WriterLiveliness :
         c_EntityId_Unknown;
+}
+
+static bool should_be_intraprocess_only(
+        const RTPSParticipantAttributes& att)
+{
+    return
+        xmlparser::XMLProfileManager::library_settings().intraprocess_delivery == INTRAPROCESS_FULL &&
+        att.builtin.discovery_config.ignoreParticipantFlags == 
+            (ParticipantFilteringFlags::FILTER_DIFFERENT_HOST | ParticipantFilteringFlags::FILTER_DIFFERENT_PROCESS);
 }
 
 Locator_t& RTPSParticipantImpl::applyLocatorAdaptRule(
@@ -97,6 +107,7 @@ RTPSParticipantImpl::RTPSParticipantImpl(
     , mp_participantListener(plisten)
     , mp_userParticipant(par)
     , mp_mutex(new std::recursive_mutex())
+    , is_intraprocess_only_(should_be_intraprocess_only(PParam))
 {
     // Builtin transport by default
     if (PParam.useBuiltinTransports)
@@ -246,6 +257,13 @@ RTPSParticipantImpl::RTPSParticipantImpl(
             m_att.defaultUnicastLocatorList);
     }
 
+    if (is_intraprocess_only())
+    {
+        m_att.builtin.metatrafficUnicastLocatorList.clear();
+        m_att.defaultUnicastLocatorList.clear();
+        m_att.defaultMulticastLocatorList.clear();
+    }
+    
     createReceiverResources(m_att.builtin.metatrafficMulticastLocatorList, true, false);
     createReceiverResources(m_att.builtin.metatrafficUnicastLocatorList, true, false);
 
