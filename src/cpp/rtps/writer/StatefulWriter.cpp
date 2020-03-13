54eb701bd21a8b8323332a8c0fe3957fc17cda61
Miguel Company <MiguelCompany@eprosima.com>
2020-03-13 09:29:07 +0100

    Refs #7920. Improving sending of heartbeats.

--- src/cpp/rtps/writer/StatefulWriter.cpp
+++ src/cpp/rtps/writer/StatefulWriter.cpp
@@ -776,16 +776,25 @@ void StatefulWriter::send_all_unsent_changes(
         network.select_locators(locator_selector_);
         compute_selected_guids();
 
-        bool force_piggyback_hb = false; // Force piggyback HB if old samples not acknowledged.
+        bool acknack_required = next_all_acked_notify_sequence_ < get_seq_num_min();;
+        bool heartbeat_has_been_sent = false;
 
         RTPSMessageGroup group(mp_RTPSParticipant, this, *this);
-
         RTPSGapBuilder gap_builder(group);
-        SequenceNumber_t seq = next_all_acked_notify_sequence_;
-        SequenceNumber_t last_sequence = mp_history->next_sequence_number();
 
-        force_piggyback_hb = seq < get_seq_num_min();
-        seq = get_seq_num_min();
+        SequenceNumber_t max_removed = biggest_removed_sequence_number_;
+        SequenceNumber_t last_sequence = mp_history->next_sequence_number();
+        SequenceNumber_t min_history_seq = get_seq_num_min();
+        uint32_t history_size = static_cast<uint32_t>(mp_history->getHistorySize());
+
+        if ((next_all_acked_notify_sequence_ < max_removed) &&    // some holes pending acknowledgement
+            (min_history_seq + history_size != last_sequence))    // There is a hole in the history
+        {
+            send_heartbeat_nts_(all_remote_readers_.size(), group, !acknack_required);
+            heartbeat_has_been_sent = true;
+        }
+            
+        SequenceNumber_t seq = get_seq_num_min();
 
         // Add holes in history and send them to all readers
         for (auto cit = mp_history->changesBegin(); cit != mp_history->changesEnd(); cit++)
@@ -859,9 +868,17 @@ void StatefulWriter::send_all_unsent_changes(
         }
 
         // Heartbeat piggyback.
-        if (there_are_remote_readers_)
+        if (!heartbeat_has_been_sent)
         {
-            send_heartbeat_nts_(all_remote_readers_.size(), group, !force_piggyback_hb);
+            if (acknack_required)
+            {
+                send_heartbeat_nts_(all_remote_readers_.size(), group, false);
+            }
+            else
+            {
+                uint32_t last_processed = 0;
+                send_heartbeat_piggyback_nts_(nullptr, group, last_processed);
+            }
         }
 
         group.flush_and_reset();
