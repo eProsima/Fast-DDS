9f8331e409d35d78089cd9f1126989dda3cb5790
Iker Luengo <ikerluengo@eprosima.com>
2020-04-02 19:25:27 +0200

    Fix equality on QosPolicies
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- include/fastdds/dds/core/policy/QosPolicies.hpp
+++ include/fastdds/dds/core/policy/QosPolicies.hpp
@@ -74,7 +74,6 @@ public:
     bool operator ==(
             const QosPolicy& b) const
     {
-        // hastChanged field isn't needed to be compared to being equal two QosPolicy objects.
         return (this->send_always_ == b.send_always_);
     }
 
@@ -464,8 +463,7 @@ public:
     {
         return (this->kind == b.kind) &&
                (this->max_blocking_time == b.max_blocking_time) &&
-               Parameter_t::operator ==(b) &&
-               QosPolicy::operator ==(b);
+               Parameter_t::operator ==(b);
     }
 
     inline void clear() override
