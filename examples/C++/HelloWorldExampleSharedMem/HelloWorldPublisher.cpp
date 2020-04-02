96959e92e406dc56fd3cb74cd0bd7def2710a0a8
AdolfoMartinez <adolfomartinez@eprosima.com>
2020-04-02 13:43:35 +0200

    Refs #8071. HelloWorldExampleSharedMem domain_id changed to 0.
    Signed-off-by: AdolfoMartinez <adolfomartinez@eprosima.com>

--- examples/C++/HelloWorldExampleSharedMem/HelloWorldPublisher.cpp
+++ examples/C++/HelloWorldExampleSharedMem/HelloWorldPublisher.cpp
@@ -48,7 +48,7 @@ bool HelloWorldPublisher::init()
     PParam.rtps.builtin.discovery_config.use_SIMPLE_EndpointDiscoveryProtocol = true;
     PParam.rtps.builtin.discovery_config.m_simpleEDP.use_PublicationReaderANDSubscriptionWriter = true;
     PParam.rtps.builtin.discovery_config.m_simpleEDP.use_PublicationWriterANDSubscriptionReader = true;
-    PParam.rtps.builtin.domainId = 66;
+    PParam.rtps.builtin.domainId = 0;
     PParam.rtps.builtin.discovery_config.leaseDuration = c_TimeInfinite;
 
     PParam.rtps.setName("Participant_pub");
