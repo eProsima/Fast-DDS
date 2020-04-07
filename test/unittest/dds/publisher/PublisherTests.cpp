7fa065058d5c55d6d93003829211dfcec5249684
Laura Martin <laura@eprosima.com>
2020-04-07 12:57:07 +0200

    Refs #8020 Add PIM default_datawriterqos methods unittest
    Signed-off-by: Laura Martin <laura@eprosima.com>

---
+++ test/unittest/dds/publisher/PublisherTests.cpp
@@ -0,0 +1,55 @@
+// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+
+TEST(PublisherTests, ChangeDefaultDataWriterQos)
+{
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0);
+    Publisher* publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+    DataWriterQos qos;
+    publisher->get_default_datawriter_qos(qos);
+    ASSERT_EQ(qos, DATAWRITER_QOS_DEFAULT);
+
+    qos.deadline().period = 260;
+
+    ASSERT_TRUE(publisher->set_default_datawriter_qos(qos) == ReturnCode_t::RETCODE_OK);
+    DataWriterQos wqos;
+    publisher->get_default_datawriter_qos(wqos);
+
+    ASSERT_EQ(qos, wqos);
+    ASSERT_EQ(wqos.deadline().period, 260);
+}
+
+} // namespace dds
+} // namespace fastdds
+} // namespace eprosima
+
+int main(
+        int argc,
+        char** argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
