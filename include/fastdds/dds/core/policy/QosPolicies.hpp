268feb838220e10a50ca6490dfbb45dbf13b1391
Laura Martin <laura@eprosima.com>
2020-04-08 12:23:20 +0200

    Requested Changes
    Signed-off-by: Laura Martin <laura@eprosima.com>

--- include/fastdds/dds/core/policy/QosPolicies.hpp
+++ include/fastdds/dds/core/policy/QosPolicies.hpp
@@ -916,7 +916,7 @@ public:                                                                         
          * @param data data to copy in the newly created object \
          */                                                                            \
         RTPS_DllAPI TClassName(                                                            \
-            const TClassName &data) = default;                                         \
+            const TClassName& data) = default;                                         \
                                                                                        \
         /** \
          * Construct from underlying collection type. \
@@ -927,7 +927,7 @@ public:                                                                         
          * @param data data to copy in the newly created object \
          */                                                                            \
         RTPS_DllAPI TClassName(                                                            \
-            const collection_type &data)                                               \
+            const collection_type& data)                                               \
             : GenericDataQosPolicy(TPid, data)                                             \
         {                                                                                  \
         }                                                                                  \
@@ -944,7 +944,7 @@ public:                                                                         
          * @return reference to the current object. \
          */                                                                            \
         TClassName& operator =(                                                            \
-            const TClassName &b) = default;                                            \
+            const TClassName& b) = default;                                            \
                                                                                        \
     };
 
