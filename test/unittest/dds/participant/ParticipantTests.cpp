1381a14a9633ac1212c39738b392c3759d3e66f3
IkerLuengo <57146230+IkerLuengo@users.noreply.github.com>
2020-04-09 16:08:23 +0200

    Refs 8113. Unit test for delete_subscriber (#1119)
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- test/unittest/dds/participant/ParticipantTests.cpp
+++ test/unittest/dds/participant/ParticipantTests.cpp
@@ -144,6 +144,14 @@ TEST(ParticipantTests, DeletePublisher)
     ASSERT_TRUE(participant->delete_publisher(publisher) == ReturnCode_t::RETCODE_OK);
 }
 
+TEST(ParticipantTests, DeleteSubscriber)
+{
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0);
+    Subscriber* subscriber = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+
+    ASSERT_TRUE(participant->delete_subscriber(subscriber) == ReturnCode_t::RETCODE_OK);
+}
+
 } // namespace dds
 } // namespace fastdds
 } // namespace eprosima
