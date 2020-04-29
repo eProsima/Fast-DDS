7686c42029021d6b7a344cc44e1cfa13b255d8ab
Miguel Company <MiguelCompany@eprosima.com>
2020-04-29 16:40:38 +0200

    Refs #7920. Added test VolatileSubBetweenPubs.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/blackbox/BlackboxTestsVolatile.cpp
+++ test/blackbox/BlackboxTestsVolatile.cpp
@@ -276,6 +276,47 @@ TEST_P(Volatile, AsyncVolatileSubBetweenPubs)
     reader.block_for_all();
 }
 
+TEST_P(Volatile, VolatileSubBetweenPubs)
+{
+    PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+
+    writer.history_kind(eprosima::fastrtps::KEEP_ALL_HISTORY_QOS).
+        reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).
+        durability_kind(eprosima::fastrtps::VOLATILE_DURABILITY_QOS).
+        resource_limits_allocated_samples(9).
+        resource_limits_max_samples(9).
+        heartbeat_period_seconds(3600).
+        init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    HelloWorld hello;
+    hello.index(1);
+    hello.message("HelloWorld 1");
+
+    writer.send_sample(hello);
+
+    reader.history_kind(eprosima::fastrtps::KEEP_ALL_HISTORY_QOS).
+        reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).
+        durability_kind(eprosima::fastrtps::VOLATILE_DURABILITY_QOS).
+        init();
+
+    ASSERT_TRUE(reader.isInitialized());
+
+    writer.wait_discovery();
+    reader.wait_discovery();
+
+    auto data = default_helloworld_data_generator(1);
+    reader.startReception(data);
+    // Send data with some interval, to let async writer thread send samples
+    writer.send(data, 300);
+    // In this test all data should be sent.
+    ASSERT_TRUE(data.empty());
+    // Block reader until reception finished or timeout.
+    reader.block_for_all();
+}
+
 TEST_P(Volatile, AsyncVolatileSubBetweenTransientPubs)
 {
     PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
