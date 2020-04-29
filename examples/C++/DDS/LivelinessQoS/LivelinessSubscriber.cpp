707068986938781695a45959d9cc5747ac09bf65
Laura Martin <laura@eprosima.com>
2020-04-29 12:58:46 +0200

    Refs #8279 Add new version of LivelinessQoS using DDS API
    Signed-off-by: Laura Martin <laura@eprosima.com>

---
+++ examples/C++/DDS/LivelinessQoS/LivelinessSubscriber.cpp
@@ -0,0 +1,196 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file LivelinessSubscriber.cpp
+ *
+ */
+
+#include "LivelinessSubscriber.h"
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+
+using namespace eprosima::fastdds::dds;
+using namespace eprosima::fastrtps::rtps;
+
+LivelinessSubscriber::LivelinessSubscriber()
+    : type_(new TopicType())
+    , participant_(nullptr)
+    , subscriber_(nullptr)
+    , topic_(nullptr)
+    , reader_(nullptr)
+{
+}
+
+bool LivelinessSubscriber::init(
+        LivelinessQosPolicyKind kind,
+        int liveliness_ms)
+{
+    //Create Participant
+    DomainParticipantQos pqos;
+    pqos.wire_protocol().builtin.discovery_config.discoveryProtocol = DiscoveryProtocol::SIMPLE;
+    pqos.wire_protocol().builtin.discovery_config.use_SIMPLE_EndpointDiscoveryProtocol = true;
+    pqos.wire_protocol().builtin.discovery_config.m_simpleEDP.use_PublicationReaderANDSubscriptionWriter = true;
+    pqos.wire_protocol().builtin.discovery_config.m_simpleEDP.use_PublicationWriterANDSubscriptionReader = true;
+    pqos.wire_protocol().builtin.use_WriterLivelinessProtocol = true;
+    pqos.name("Participant_sub");
+
+    participant_ = DomainParticipantFactory::get_instance()->create_participant(0, pqos);
+    if (participant_ == nullptr)
+    {
+        return false;
+    }
+
+    //Register Type
+    type_.register_type(participant_);
+
+    //Create Subscriber
+    subscriber_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+
+    if (subscriber_ == nullptr)
+    {
+        return false;
+    }
+
+    //Create Topic
+    topic_ = participant_->create_topic("Name", "Topic", TOPIC_QOS_DEFAULT);
+
+    if (topic_ == nullptr)
+    {
+        return false;
+    }
+
+    //Create DataReader
+    DataReaderQos rqos;
+    rqos.history().depth = 30;
+    rqos.history().kind = KEEP_LAST_HISTORY_QOS;
+    rqos.durability().kind = TRANSIENT_LOCAL_DURABILITY_QOS;
+    rqos.reliability().kind = RELIABLE_RELIABILITY_QOS;
+    rqos.liveliness().lease_duration = Duration_t(liveliness_ms * 1e-3);
+    rqos.liveliness().announcement_period = Duration_t(liveliness_ms * 1e-3 * 0.5);
+    rqos.liveliness().kind = kind;
+
+    reader_ = subscriber_->create_datareader(topic_, rqos, &listener_);
+
+    if (reader_ == nullptr)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+LivelinessSubscriber::~LivelinessSubscriber()
+{
+    if (reader_ != nullptr)
+    {
+        subscriber_->delete_datareader(reader_);
+    }
+    if (topic_ != nullptr)
+    {
+        participant_->delete_topic(topic_);
+    }
+    if (subscriber_ != nullptr)
+    {
+        participant_->delete_subscriber(subscriber_);
+    }
+    DomainParticipantFactory::get_instance()->delete_participant(participant_);
+}
+
+void LivelinessSubscriber::SubListener::on_subscription_matched(
+        DataReader*,
+        const SubscriptionMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        n_matched = info.total_count;
+        std::cout << "Subscriber matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        n_matched = info.total_count;
+        std::cout << "Subscriber unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
+    }
+}
+
+void LivelinessSubscriber::SubListener::on_data_available(
+        DataReader* reader)
+{
+    SampleInfo info;
+    if (reader->take_next_sample(&topic, &info) == ReturnCode_t::RETCODE_OK)
+    {
+        if (info.instance_state == eprosima::fastdds::dds::ALIVE)
+        {
+            this->n_samples++;
+
+            std::cout << "Message with index " << topic.index() << " RECEIVED" << std::endl;
+        }
+    }
+}
+
+void LivelinessSubscriber::SubListener::on_liveliness_changed(
+        DataReader*,
+        const LivelinessChangedStatus& status)
+{
+    if (status.alive_count_change == 1)
+    {
+        std::cout << "Publisher " << status.last_publication_handle << " recovered liveliness" << std::endl;
+    }
+    else if (status.not_alive_count_change == 1)
+    {
+        std::cout << "Publisher " << status.last_publication_handle << " lost liveliness" << std::endl;
+    }
+}
+
+void LivelinessSubscriber::run()
+{
+    std::cout << "Subscriber running. Please press enter to stop the Subscriber" << std::endl;
+    std::cin.ignore();
+}
+
+void LivelinessSubscriber::run(
+        uint32_t number)
+{
+    std::cout << "Subscriber running until " << number << "samples have been received" << std::endl;
+
+    while (number > this->listener_.n_samples)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+void LivelinessSubscriber::PartListener::on_participant_discovery(
+        DomainParticipant*,
+        ParticipantDiscoveryInfo&& info)
+{
+    if (info.status == ParticipantDiscoveryInfo::DISCOVERED_PARTICIPANT)
+    {
+        std::cout << "Participant discovered" << std::endl;
+    }
+    else if (info.status == ParticipantDiscoveryInfo::DROPPED_PARTICIPANT)
+    {
+        std::cout << "Participant dropped" << std::endl;
+    }
+    else if (info.status == ParticipantDiscoveryInfo::REMOVED_PARTICIPANT)
+    {
+        std::cout << "Participant removed" << std::endl;
+    }
+}
---
+++ examples/C++/DDS/LivelinessQoS/Liveliness_main.cpp
@@ -0,0 +1,228 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file Liveliness_main.cpp
+ *
+ */
+
+#include "LivelinessPublisher.h"
+#include "LivelinessSubscriber.h"
+
+#include <fastrtps/Domain.h>
+#include <fastrtps/log/Log.h>
+#include <fastrtps/qos/QosPolicies.h>
+
+using namespace eprosima;
+using namespace fastrtps;
+using namespace rtps;
+
+//!
+//! \brief Parses command line artuments
+//! \param argc Number of command line arguments
+//! \param argv Array of arguments
+//! \param type Publisher or subscriber
+//! \param lease_duration_ms Lease duration in ms
+//! \param kind Liveliness kind
+//! \param sleep_ms Sleep time between consecutive writes
+//! \param samples Number of samples to write
+//! \return True if command line arguments were parsed correctly
+//!
+bool parse_arguments(int argc,
+        char** argv,
+        int& type,
+        int& lease_duration_ms,
+        LivelinessQosPolicyKind &kind,
+        int& sleep_ms,
+        int& samples);
+
+int main(int argc, char** argv)
+{
+    int type = 1;
+    int lease_duration_ms = 100;
+    int sleep_ms = 1000;
+    int count = 10;
+
+    eprosima::fastrtps::LivelinessQosPolicyKind kind = eprosima::fastrtps::AUTOMATIC_LIVELINESS_QOS;
+
+    if (!parse_arguments(
+                argc,
+                argv,
+                type,
+                lease_duration_ms,
+                kind,
+                sleep_ms,
+                count))
+    {
+        std::cout << "Usage: " << std::endl;
+        std::cout << argv[0] << " publisher ";
+        std::cout << "[--lease_duration <lease_duration_ms>] ";
+        std::cout << "[--kind <AUTOMATIC|MANUAL_BY_PARTICIPANT|MANUAL_BY_TOPIC>]";
+        std::cout << "[--sleep <writer_sleep_ms>] ";
+        std::cout << "[--samples <samples>]" << std::endl;
+
+        std::cout << "OR" << std::endl;
+        std::cout << argv[0] << " subscriber ";
+        std::cout << "[--lease_duration <lease_duration_ms>]";
+        std::cout << "[--kind <AUTOMATIC|MANUAL_BY_PARTICIPANT|MANUAL_BY_TOPIC>]" << std::endl << std::endl;
+
+        std::cout << "Default values:" << std::endl;
+        std::cout << "lease_duration_ms = 100" << std::endl;
+        std::cout << "kind = AUTOMATIC" << std::endl;
+        std::cout << "writer_sleep_ms = 1000" << std::endl;
+        std::cout << "samples = 10" << std::endl;
+        return 0;
+    }
+
+    switch(type)
+    {
+    case 1:
+    {
+        LivelinessPublisher mypub;
+        if(mypub.init(kind, lease_duration_ms))
+        {
+            mypub.run(count, sleep_ms);
+        }
+        break;
+    }
+    case 2:
+    {
+        LivelinessSubscriber mysub;
+        if(mysub.init(kind, lease_duration_ms))
+        {
+            mysub.run();
+        }
+        break;
+    }
+}
+    Domain::stopAll();
+    Log::Reset();
+    return 0;
+}
+
+bool parse_arguments(
+        int argc,
+        char** argv,
+        int& type,
+        int& lease_duration_ms,
+        eprosima::fastrtps::LivelinessQosPolicyKind& kind,
+        int& sleep_ms,
+        int& samples)
+{
+    if (argc == 1)
+    {
+        // No arguments provided
+        return false;
+    }
+
+    for (int i=0; i<argc; i++)
+    {
+        if (!strcmp(argv[i], "--help"))
+        {
+            // --help command found
+            return false;
+        }
+    }
+
+    if (strcmp(argv[1], "publisher") == 0)
+    {
+        type = 1;
+
+        int count = 2;
+        while (count < argc)
+        {
+            if (!strcmp(argv[count], "--lease_duration"))
+            {
+                lease_duration_ms = atoi(argv[count + 1]);
+            }
+            else if (!strcmp(argv[count], "--kind"))
+            {
+                if (!strcmp(argv[count + 1], "AUTOMATIC"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::AUTOMATIC_LIVELINESS_QOS;
+                }
+                else if(!strcmp(argv[count + 1], "MANUAL_BY_PARTICIPANT"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::MANUAL_BY_PARTICIPANT_LIVELINESS_QOS;
+                }
+                else if(!strcmp(argv[count + 1], "MANUAL_BY_TOPIC"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::MANUAL_BY_TOPIC_LIVELINESS_QOS;
+                }
+                else
+                {
+                    std::cout << "Unknown command line value " << argv[count + 1] << " for publisher" << std::endl;
+                    return false;
+                }
+            }
+            else if (!strcmp(argv[count], "--sleep"))
+            {
+                sleep_ms = atoi(argv[count + 1]);
+            }
+            else if (!strcmp(argv[count], "--samples"))
+            {
+                samples = atoi(argv[count + 1]);
+            }
+            else
+            {
+                std::cout << "Unknown command line option " << argv[count] << " for publisher" << std::endl;
+                return false;
+            }
+            count = count + 2;
+        }
+        return true;
+    }
+    else if (strcmp(argv[1], "subscriber") == 0)
+    {
+        type = 2;
+
+        int count = 2;
+        while (count < argc)
+        {
+            if (!strcmp(argv[count], "--lease_duration"))
+            {
+                lease_duration_ms = atoi(argv[count + 1]);
+            }
+            else if (!strcmp(argv[count], "--kind"))
+            {
+                if (!strcmp(argv[count + 1], "AUTOMATIC"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::AUTOMATIC_LIVELINESS_QOS;
+                }
+                else if(!strcmp(argv[count + 1], "MANUAL_BY_PARTICIPANT"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::MANUAL_BY_PARTICIPANT_LIVELINESS_QOS;
+                }
+                else if(!strcmp(argv[count + 1], "MANUAL_BY_TOPIC"))
+                {
+                    kind = eprosima::fastrtps::LivelinessQosPolicyKind::MANUAL_BY_TOPIC_LIVELINESS_QOS;
+                }
+                else
+                {
+                    std::cout << "Unknown command line value " << argv[count + 1] << " for subscriber" << std::endl;
+                    return false;
+                }
+            }
+            else
+            {
+                std::cout << "Unknown command line option " << argv[count] << " for publisher" << std::endl;
+                return false;
+            }
+            count = count + 2;
+        }
+        return true;
+    }
+
+    return false;
+}
