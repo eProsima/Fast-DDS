cd559b0b6a357315a3bc9329d16a5ee53e785b89
Ricardo González Moreno <correoricky@gmail.com>
2020-05-06 12:40:40 +0200

    Refs #8127. Also applied in mocks.
    Signed-off-by: Ricardo González Moreno <correoricky@gmail.com>

--- test/mock/rtps/ReaderLocator/fastdds/rtps/writer/ReaderLocator.h
+++ test/mock/rtps/ReaderLocator/fastdds/rtps/writer/ReaderLocator.h
@@ -14,7 +14,7 @@
 
 /**
  * @file ReaderLocator.h
-*/
+ */
 
 
 
@@ -35,6 +35,7 @@ namespace fastrtps {
 namespace rtps {
 
 class RTPSParticipantImpl;
+class RTPSWriter;
 class RTPSReader;
 
 /**
@@ -44,152 +45,154 @@ class RTPSReader;
  */
 class ReaderLocator : public RTPSMessageSenderInterface
 {
-    public:
+public:
 
-        virtual ~ReaderLocator() = default;
+    virtual ~ReaderLocator() = default;
 
-        /**
-         * Construct a ReaderLocator.
-         *
-         * @param owner                   Pointer to the RTPSParticipantImpl creating this object.
-         * @param max_unicast_locators    Maximum number of unicast locators to hold.
-         * @param max_multicast_locators  Maximum number of multicast locators to hold.
-         */
-        ReaderLocator(
-                RTPSParticipantImpl* /*owner*/,
-                size_t /*max_unicast_locators*/,
-                size_t /*max_multicast_locators*/)
-        {
-        }
+    /**
+     * Construct a ReaderLocator.
+     *
+     * @param owner                   Pointer to the RTPSParticipantImpl creating this object.
+     * @param max_unicast_locators    Maximum number of unicast locators to hold.
+     * @param max_multicast_locators  Maximum number of multicast locators to hold.
+     */
+    ReaderLocator(
+            RTPSParticipantImpl* /*participant_owner*/,
+            RTPSWriter* /*owner*/,
+            size_t /*max_unicast_locators*/,
+            size_t /*max_multicast_locators*/)
+    {
+    }
 
-        LocatorSelectorEntry* locator_selector_entry()
-        {
-            return nullptr;
-        }
+    LocatorSelectorEntry* locator_selector_entry()
+    {
+        return nullptr;
+    }
 
-        const GUID_t& remote_guid() const
-        {
-            return remote_guid_;
-        }
+    const GUID_t& remote_guid() const
+    {
+        return remote_guid_;
+    }
 
-        /**
-         * Try to start using this object for a new matched reader.
-         *
-         * @param remote_guid         GUID of the remote reader.
-         * @param unicast_locators    Unicast locators of the remote reader.
-         * @param multicast_locators  Multicast locators of the remote reader.
-         * @param expects_inline_qos  Whether remote reader expects to receive inline QoS.
-         *
-         * @return false when this object was already started, true otherwise.
-         */
-        bool start(
-                const GUID_t& /*remote_guid*/,
-                const ResourceLimitedVector<Locator_t>& /*unicast_locators*/,
-                const ResourceLimitedVector<Locator_t>& /*multicast_locators*/,
-                bool /*expects_inline_qos*/)
-        {
-            return true;
-        }
+    /**
+     * Try to start using this object for a new matched reader.
+     *
+     * @param remote_guid         GUID of the remote reader.
+     * @param unicast_locators    Unicast locators of the remote reader.
+     * @param multicast_locators  Multicast locators of the remote reader.
+     * @param expects_inline_qos  Whether remote reader expects to receive inline QoS.
+     *
+     * @return false when this object was already started, true otherwise.
+     */
+    bool start(
+            const GUID_t& /*remote_guid*/,
+            const ResourceLimitedVector<Locator_t>& /*unicast_locators*/,
+            const ResourceLimitedVector<Locator_t>& /*multicast_locators*/,
+            bool /*expects_inline_qos*/)
+    {
+        return true;
+    }
 
-        /**
-         * Try to update information of this object.
-         *
-         * @param unicast_locators    Unicast locators of the remote reader.
-         * @param multicast_locators  Multicast locators of the remote reader.
-         * @param expects_inline_qos  Whether remote reader expects to receive inline QoS.
-         *
-         * @return true when information has changed, false otherwise.
-         */
-        bool update(
-                const ResourceLimitedVector<Locator_t>& /*unicast_locators*/,
-                const ResourceLimitedVector<Locator_t>& /*multicast_locators*/,
-                bool /*expects_inline_qos*/)
-        {
-            return true;
-        }
+    /**
+     * Try to update information of this object.
+     *
+     * @param unicast_locators    Unicast locators of the remote reader.
+     * @param multicast_locators  Multicast locators of the remote reader.
+     * @param expects_inline_qos  Whether remote reader expects to receive inline QoS.
+     *
+     * @return true when information has changed, false otherwise.
+     */
+    bool update(
+            const ResourceLimitedVector<Locator_t>& /*unicast_locators*/,
+            const ResourceLimitedVector<Locator_t>& /*multicast_locators*/,
+            bool /*expects_inline_qos*/)
+    {
+        return true;
+    }
 
-        /**
-         * Try to stop using this object for an unmatched reader.
-         *
-         * @param remote_guid  GUID of the remote reader.
-         *
-         * @return true if this object was started for remote_guid, false otherwise.
-         */
-        bool stop(const GUID_t& /*remote_guid*/)
-        {
-            return true;
-        }
+    /**
+     * Try to stop using this object for an unmatched reader.
+     *
+     * @param remote_guid  GUID of the remote reader.
+     *
+     * @return true if this object was started for remote_guid, false otherwise.
+     */
+    bool stop(
+            const GUID_t& /*remote_guid*/)
+    {
+        return true;
+    }
 
-        /**
-         * Check if the destinations managed by this sender interface have changed.
-         *
-         * @return true if destinations have changed, false otherwise.
-         */
-        bool destinations_have_changed() const override
-        {
-            return false;
-        }
+    /**
+     * Check if the destinations managed by this sender interface have changed.
+     *
+     * @return true if destinations have changed, false otherwise.
+     */
+    bool destinations_have_changed() const override
+    {
+        return false;
+    }
 
-        /**
-         * Get a GUID prefix representing all destinations.
-         *
-         * @return When all the destinations share the same prefix (i.e. belong to the same participant)
-         * that prefix is returned. When there are no destinations, or they belong to different
-         * participants, c_GuidPrefix_Unknown is returned.
-         */
-        GuidPrefix_t destination_guid_prefix() const override
-        {
-            return remote_guid_.guidPrefix;
-        }
+    /**
+     * Get a GUID prefix representing all destinations.
+     *
+     * @return When all the destinations share the same prefix (i.e. belong to the same participant)
+     * that prefix is returned. When there are no destinations, or they belong to different
+     * participants, c_GuidPrefix_Unknown is returned.
+     */
+    GuidPrefix_t destination_guid_prefix() const override
+    {
+        return remote_guid_.guidPrefix;
+    }
 
-        /**
-         * Get the GUID prefix of all the destination participants.
-         *
-         * @return a const reference to a vector with the GUID prefix of all destination participants.
-         */
-        const std::vector<GuidPrefix_t>& remote_participants() const override
-        {
-            return guid_prefix_as_vector_;
-        }
+    /**
+     * Get the GUID prefix of all the destination participants.
+     *
+     * @return a const reference to a vector with the GUID prefix of all destination participants.
+     */
+    const std::vector<GuidPrefix_t>& remote_participants() const override
+    {
+        return guid_prefix_as_vector_;
+    }
 
-        /**
-         * Get the GUID of all destinations.
-         *
-         * @return a const reference to a vector with the GUID of all destinations.
-         */
-        const std::vector<GUID_t>& remote_guids() const override
-        {
-            return guid_as_vector_;
-        }
+    /**
+     * Get the GUID of all destinations.
+     *
+     * @return a const reference to a vector with the GUID of all destinations.
+     */
+    const std::vector<GUID_t>& remote_guids() const override
+    {
+        return guid_as_vector_;
+    }
 
-        /**
-         * Send a message through this interface.
-         *
-         * @param message Pointer to the buffer with the message already serialized.
-         * @param max_blocking_time_point Future timepoint where blocking send should end.
-         */
-        bool send(
-                CDRMessage_t* /*message*/,
-                std::chrono::steady_clock::time_point& /*max_blocking_time_point*/) const override
-        {
-            return true;
-        }
+    /**
+     * Send a message through this interface.
+     *
+     * @param message Pointer to the buffer with the message already serialized.
+     * @param max_blocking_time_point Future timepoint where blocking send should end.
+     */
+    bool send(
+            CDRMessage_t* /*message*/,
+            std::chrono::steady_clock::time_point& /*max_blocking_time_point*/) const override
+    {
+        return true;
+    }
 
-        bool is_local_reader() const
-        {
-            return false;
-        }
+    bool is_local_reader() const
+    {
+        return false;
+    }
 
-        RTPSReader* local_reader()
-        {
-            return nullptr;
-        }
+    RTPSReader* local_reader()
+    {
+        return nullptr;
+    }
 
-    private:
+private:
 
-        GUID_t remote_guid_;
-        std::vector<GuidPrefix_t> guid_prefix_as_vector_;
-        std::vector<GUID_t> guid_as_vector_;
+    GUID_t remote_guid_;
+    std::vector<GuidPrefix_t> guid_prefix_as_vector_;
+    std::vector<GUID_t> guid_as_vector_;
 };
 
 } /* namespace rtps */
