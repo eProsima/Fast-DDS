ed840a23da3ff80ce3baa4605216ca7abccec5a5
Miguel Company <MiguelCompany@eprosima.com>
2020-06-05 10:48:33 +0200

    Refs #8587. Add note on foonathan size_t workaround.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/utils/collections/impl/node-sizes/foonathan/set_node_size_impl.hpp
+++ src/cpp/utils/collections/impl/node-sizes/foonathan/set_node_size_impl.hpp
@@ -19,6 +19,19 @@
 #ifndef SRC_CPP_UTILS_COLLECTIONS_IMPL_FOONATHAN_SET_NODE_SIZE_IMPL_HPP_
 #define SRC_CPP_UTILS_COLLECTIONS_IMPL_FOONATHAN_SET_NODE_SIZE_IMPL_HPP_
 
+ /* NOTE:
+  *   The way foonathan_memory_node_size_debugger tool works may lead to wrong
+  *   results on some platforms due to alignment / sizeof issues.
+  *
+  *   On MSVC in particular, it was detected that the value returned by
+  *   foonathan::memory::set_node_size<std::set<uint32_t[2]>::value_type>
+  *   was 4 bytes less than the size being asked to the allocator.
+  *
+  *   To solve this issue, we add a size_t before the value type, asking for the
+  *   node size of std::pair<size_t, value_type>. This will consume more memory
+  *   than strictly necessary but will avoid access violation exceptions.
+  */
+
 template <typename K>
 struct set_node_size : foonathan::memory::set_node_size<std::pair<size_t, typename std::set<K>::value_type> >
 {
