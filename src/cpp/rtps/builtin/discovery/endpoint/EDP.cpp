d96e9eb4585e6002d8d46150ba5388901bac5a7e
Ricardo González Moreno <correoricky@gmail.com>
2020-06-12 12:28:11 +0200

    Refs #8690. Fix error setting first QoS in EDP.
    Signed-off-by: Ricardo González Moreno <correoricky@gmail.com>

--- src/cpp/rtps/builtin/discovery/endpoint/EDP.cpp
+++ src/cpp/rtps/builtin/discovery/endpoint/EDP.cpp
@@ -116,7 +116,7 @@ bool EDP::newLocalReaderProxyData(
                 rpd->type_id(att.type_id);
                 rpd->type(att.type);
                 rpd->type_information(att.type_information);
-                rpd->m_qos = rqos;
+                rpd->m_qos.setQos(rqos, true);
                 rpd->userDefinedId(reader->getAttributes().getUserDefinedID());
 #if HAVE_SECURITY
                 if (mp_RTPSParticipant->is_secure())
@@ -230,7 +230,7 @@ bool EDP::newLocalWriterProxyData(
                 wpd->type(att.type);
                 wpd->type_information(att.type_information);
                 wpd->typeMaxSerialized(writer->getTypeMaxSerialized());
-                wpd->m_qos = wqos;
+                wpd->m_qos.setQos(wqos, true);
                 wpd->userDefinedId(writer->getAttributes().getUserDefinedID());
                 wpd->persistence_guid(writer->getAttributes().persistence_guid);
 #if HAVE_SECURITY
