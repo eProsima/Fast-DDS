9200f9a36106f6c768301c3e9ab53afcb93a56f1
Iker Luengo <ikerluengo@eprosima.com>
2020-06-26 08:19:10 +0200

    Refs 8718. Topic testing
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

---
+++ test/unittest/dds/topic/TopicTests.cpp
@@ -0,0 +1,228 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/topic/TopicListener.hpp>
+#include <fastdds/dds/topic/qos/TopicQos.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+
+#include <dds/domain/DomainParticipant.hpp>
+#include <dds/core/types.hpp>
+#include <dds/topic/Topic.hpp>
+
+#include <fastrtps/attributes/TopicAttributes.h>
+#include <fastrtps/xmlparser/XMLProfileManager.h>
+
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+
+using fastrtps::TopicAttributes;
+using fastrtps::xmlparser::XMLProfileManager;
+using fastrtps::xmlparser::XMLP_ret;
+
+
+class FooType
+{
+public:
+
+    FooType()
+    {
+    }
+
+    ~FooType()
+    {
+    }
+
+    inline std::string& message()
+    {
+        return message_;
+    }
+
+    inline void message(
+            const std::string& message)
+    {
+        message_ = message;
+    }
+
+    bool isKeyDefined()
+    {
+        return false;
+    }
+
+private:
+
+    std::string message_;
+};
+
+class TopicDataTypeMock : public TopicDataType
+{
+public:
+
+    TopicDataTypeMock()
+        : TopicDataType()
+    {
+        setName("footype");
+    }
+
+    bool serialize(
+            void* /*data*/,
+            fastrtps::rtps::SerializedPayload_t* /*payload*/) override
+    {
+        return true;
+    }
+
+    bool deserialize(
+            fastrtps::rtps::SerializedPayload_t* /*payload*/,
+            void* /*data*/) override
+    {
+        return true;
+    }
+
+    std::function<uint32_t()> getSerializedSizeProvider(
+            void* /*data*/) override
+    {
+        return std::function<uint32_t()>();
+    }
+
+    void* createData() override
+    {
+        return nullptr;
+    }
+
+    void deleteData(
+            void* /*data*/) override
+    {
+    }
+
+    bool getKey(
+            void* /*data*/,
+            fastrtps::rtps::InstanceHandle_t* /*ihandle*/,
+            bool /*force_md5*/) override
+    {
+        return true;
+    }
+
+};
+
+TEST(TopicTests, ChangeTopicQos)
+{
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0, PARTICIPANT_QOS_DEFAULT);
+    ASSERT_NE(participant, nullptr);
+
+    TypeSupport type(new TopicDataTypeMock());
+    type.register_type(participant);
+
+    Topic* topic = participant->create_topic("footopic", type.get_type_name(), TOPIC_QOS_DEFAULT);
+    ASSERT_NE(topic, nullptr);
+
+    TopicQos qos;
+    ASSERT_EQ(topic->get_qos(qos), ReturnCode_t::RETCODE_OK);
+
+    ASSERT_EQ(qos, TOPIC_QOS_DEFAULT);
+
+    qos.reliability().kind = RELIABLE_RELIABILITY_QOS;
+
+    ASSERT_EQ(topic->set_qos(qos), ReturnCode_t::RETCODE_OK);
+    TopicQos tqos;
+    ASSERT_EQ(topic->get_qos(tqos), ReturnCode_t::RETCODE_OK);
+
+    ASSERT_TRUE(qos == tqos);
+    ASSERT_EQ(tqos.reliability().kind, RELIABLE_RELIABILITY_QOS);
+
+    ASSERT_TRUE(participant->delete_topic(topic) == ReturnCode_t::RETCODE_OK);
+    ASSERT_TRUE(DomainParticipantFactory::get_instance()->delete_participant(participant) == ReturnCode_t::RETCODE_OK);
+
+}
+
+TEST(TopicTests, GetTopicParticipant)
+{
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0, PARTICIPANT_QOS_DEFAULT);
+    ASSERT_NE(participant, nullptr);
+
+    TypeSupport type(new TopicDataTypeMock());
+    type.register_type(participant);
+
+    Topic* topic = participant->create_topic("footopic", type.get_type_name(), TOPIC_QOS_DEFAULT);
+    ASSERT_NE(topic, nullptr);
+
+    ASSERT_EQ(topic->get_participant(), participant);
+
+    ASSERT_TRUE(participant->delete_topic(topic) == ReturnCode_t::RETCODE_OK);
+    ASSERT_TRUE(DomainParticipantFactory::get_instance()->delete_participant(participant) == ReturnCode_t::RETCODE_OK);
+}
+
+void set_listener_test (Topic* topic, TopicListener* listener, StatusMask mask)
+{
+    ASSERT_EQ(topic->set_listener(listener, mask), ReturnCode_t::RETCODE_OK);
+    ASSERT_EQ(topic->get_status_mask(), mask);
+}
+
+class CustomListener : public TopicListener
+{
+
+};
+
+TEST(TopicTests, SetListener)
+{
+    CustomListener listener;
+
+    DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(0, PARTICIPANT_QOS_DEFAULT);
+    ASSERT_NE(participant, nullptr);
+
+    Subscriber* subscriber = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
+    ASSERT_NE(subscriber, nullptr);
+
+    TypeSupport type(new TopicDataTypeMock());
+    type.register_type(participant);
+
+    Topic* topic = participant->create_topic("footopic", type.get_type_name(), TOPIC_QOS_DEFAULT);
+    ASSERT_NE(topic, nullptr);
+    ASSERT_EQ(topic->get_status_mask(), StatusMask::all());
+
+    std::vector<std::tuple<Topic*, TopicListener*, StatusMask> > testing_cases{
+        //statuses, one by one
+        { topic, &listener, StatusMask::data_available() },
+        //all except one
+        { topic, &listener, StatusMask::all() >> StatusMask::sample_lost() },
+        //all and none
+        { topic, &listener, StatusMask::all() },
+        { topic, &listener, StatusMask::none() }
+    };
+
+    for (auto testing_case : testing_cases)
+    {
+        set_listener_test(std::get<0>(testing_case),
+                std::get<1>(testing_case),
+                std::get<2>(testing_case));
+    }
+}
+
+} // namespace dds
+} // namespace fastdds
+} // namespace eprosima
+
+int main(
+        int argc,
+        char** argv)
+{
+    testing::InitGoogleMock(&argc, argv);
+    return RUN_ALL_TESTS();
+}
