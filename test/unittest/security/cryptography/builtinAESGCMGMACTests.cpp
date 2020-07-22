bb38ff2eadea3134cba601803551b9e0e5de2169
Luis Gasco <luisgasco@eprosima.com>
2020-07-22 15:21:59 +0200

    Avoid exporting "openssl_init" (#1305)
    * Refs #8913 Avoid exporting "openssl_init".
    
    * Uncrustify
    
    * Fixing not found header in Mac/Windows.

--- test/unittest/security/cryptography/builtinAESGCMGMACTests.cpp
+++ test/unittest/security/cryptography/builtinAESGCMGMACTests.cpp
@@ -15,18 +15,21 @@
 #include <iostream>
 #include <algorithm>
 #include "CryptographyPluginTests.hpp"
+#include "security/OpenSSLInit.hpp"
 
 TEST_F(CryptographyPluginTest, mocktest){
+    static eprosima::fastrtps::rtps::security::OpenSSLInit openssl_init;
     uint8_t mock = 7;
 
     ASSERT_TRUE(mock == 7);
 
 }
 
-int main(int argc, char **argv)
+int main(
+        int argc,
+        char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
 }
-
