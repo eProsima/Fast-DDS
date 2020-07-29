ed7d3ff984ef74a9dac2bc19c94b854d35a4ec9a
Miguel Barro <miguelbarro@eprosima.com>
2020-07-29 07:48:29 +0200

    Enable shebang hints on all platforms cli launch scripts
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- tools/fastdds/fastdds.bat
+++ tools/fastdds/fastdds.bat
@@ -13,12 +13,12 @@ goto getarg
 :continue
 
 :: Check python
-python --version > NUL 2>&1
+py --list > NUL 2>&1
 
 if not %ERRORLEVEL%==0 (
       echo python interpreter not found. Please, make sure its location is in the PATH environment variable.
       exit /B 65
    )
 )
-
-python "%dir%\..\tools\fastdds\fastdds.py" %args%
+:: Use launcher to profit from shebang hints on fastdds.py
+py "%dir%\..\tools\fastdds\fastdds.py" %args%
