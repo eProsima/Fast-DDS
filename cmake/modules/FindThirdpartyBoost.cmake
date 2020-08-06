71e519b2a2fe730d4dd099ff0dc2e36e43a6a780
Ahmed Sobhy <68698147+asobhy-qnx@users.noreply.github.com>
2020-08-06 06:29:56 -0400

    adds compiler id and removes librt for QNX (#1317)
    * adds compiler id for QNX
    
    Signed-off-by: asobhy <asobhy@blackberry.com>
    
    * removes link against librt for QNX
    
    Signed-off-by: asobhy <asobhy@blackberry.com>
    
    * cleanup
    
    Signed-off-by: asobhy <asobhy@blackberry.com>

--- cmake/modules/FindThirdpartyBoost.cmake
+++ cmake/modules/FindThirdpartyBoost.cmake
@@ -1,19 +1,19 @@
 # Test compilation of boost on thirdparty directory
-# and set 
+# and set
 
 # (This file is almost an identical copy of the original FindGTest.cmake file,
 #  feel free to use it as it is or modify it for your own needs.)
 
-set(THIRDPARTY_BOOST_INCLUDE_DIR 
-    ${PROJECT_SOURCE_DIR}/thirdparty/boost/include 
-    CACHE 
+set(THIRDPARTY_BOOST_INCLUDE_DIR
+    ${PROJECT_SOURCE_DIR}/thirdparty/boost/include
+    CACHE
     FILEPATH
     "Path to thirdparty/boost"
 )
 
 find_package(Threads REQUIRED)
 
-if(WIN32 OR APPLE)
+if(WIN32 OR APPLE OR QNX)
     set(THIRDPARTY_BOOST_LINK_LIBS ${CMAKE_THREAD_LIBS_INIT})
 else() # Posix
     set(THIRDPARTY_BOOST_LINK_LIBS ${CMAKE_THREAD_LIBS_INIT} rt)
