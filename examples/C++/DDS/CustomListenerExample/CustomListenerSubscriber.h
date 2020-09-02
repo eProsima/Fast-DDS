f25eb918cc59f4add5ef3efcef9349b1525f434b
Ignacio Montesino Valle <ignamonte@hotmail.com>
2020-09-02 14:37:38 +0200

    Added Example for CustomListener with StatusMask (#1335)
    * Refs #8755: Added custom listeners example
    
    Signed-off-by: imontesino <monte.igna@gmail.com>
    
    * Refs #8755: Changed topic name to avoid confusion
    
    Signed-off-by: imontesino <monte.igna@gmail.com>
    
    * Refs #9042: Applied suggested changes
    
    Signed-off-by: imontesino <monte.igna@gmail.com>
    
    * Refs #9042: Uncrustify
    
    Signed-off-by: imontesino <monte.igna@gmail.com>

---
+++ examples/C++/DDS/CustomListenerExample/CustomListenerSubscriber.h
@@ -0,0 +1,69 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file CustomListenerSubscriber.h
+ *
+ */
+
+#ifndef CUSTOMLISTENERSUBSCRIBER_H_
+#define CUSTOMLISTENERSUBSCRIBER_H_
+
+#include "TopicTypes.h"
+#include "CustomListeners.h"
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastrtps/subscriber/SampleInfo.h>
+#include <fastdds/dds/core/status/SubscriptionMatchedStatus.hpp>
+
+class CustomListenerSubscriber
+{
+public:
+
+    CustomListenerSubscriber();
+
+    virtual ~CustomListenerSubscriber();
+
+    //!Initialize the subscriber
+    bool init(bool use_dr);
+
+    //!RUN the subscriber
+    void run();
+
+    //!Run the subscriber until number samples have been received.
+    void run(
+            uint32_t number);
+
+private:
+
+
+    eprosima::fastdds::dds::DomainParticipant* participant_;
+
+    eprosima::fastdds::dds::Subscriber* subscriber_;
+
+    eprosima::fastdds::dds::Topic* topic_;
+
+    eprosima::fastdds::dds::DataReader* reader_;
+
+    eprosima::fastdds::dds::TypeSupport type_;
+
+    eprosima::fastdds::dds::StatusMask p_mask_;
+
+    CustomDataReaderListener dr_listener_;
+
+    CustomDomainParticipantListener p_listener_;
+};
+
+#endif /* CUSTOMLISTENERSUBSCRIBER_H_ */
---
+++ examples/C++/DDS/CustomListenerExample/CustomListeners.cpp
@@ -0,0 +1,166 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file CustomListeners.cpp
+ *
+ */
+
+#include "CustomListeners.h"
+#include <fastrtps/attributes/ParticipantAttributes.h>
+#include <fastrtps/attributes/PublisherAttributes.h>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/publisher/qos/PublisherQos.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
+
+#include <thread>
+
+using namespace eprosima::fastdds::dds;
+
+
+void CustomDataWriterListener::on_publication_matched(
+        DataWriter*,
+        const PublicationMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        matched_ = info.total_count;
+        firstConnected_ = true;
+        std::cout << "(CustomDataWriterListener) Publisher matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDataWriterListener) Publisher unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for PublicationMatchedStatus current count change" << std::endl;
+    }
+}
+
+
+void CustomDataReaderListener::on_subscription_matched(
+        DataReader*,
+        const SubscriptionMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDataReaderListener) Subscriber matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDataReaderListener) Subscriber unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
+    }
+}
+
+void CustomDataReaderListener::on_data_available(
+        DataReader* reader)
+{
+    SampleInfo info;
+    if (reader->take_next_sample(&hello_, &info) == ReturnCode_t::RETCODE_OK)
+    {
+        if (info.instance_state == ALIVE)
+        {
+            samples_++;
+            // Print your structure data here.
+            std::cout << "(CustomDataReaderListener) " << hello_.message() << " " << hello_.index() << " RECEIVED" << std::endl;
+        }
+    }
+}
+
+
+CustomDomainParticipantListener::CustomDomainParticipantListener()
+: DomainParticipantListener()
+{
+}
+
+CustomDomainParticipantListener::~CustomDomainParticipantListener()
+{
+}
+
+
+void CustomDomainParticipantListener::on_publication_matched(
+        DataWriter*,
+        const PublicationMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        matched_ = info.total_count;
+        firstConnected_ = true;
+        std::cout << "(CustomDomainParticipantListener) Publisher matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDomainParticipantListener) Publisher unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for PublicationMatchedStatus current count change" << std::endl;
+    }
+}
+
+void CustomDomainParticipantListener::on_subscription_matched(
+        DataReader*,
+        const SubscriptionMatchedStatus& info)
+{
+    if (info.current_count_change == 1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDomainParticipantListener) Subscriber matched." << std::endl;
+    }
+    else if (info.current_count_change == -1)
+    {
+        matched_ = info.total_count;
+        std::cout << "(CustomDomainParticipantListener) Subscriber unmatched." << std::endl;
+    }
+    else
+    {
+        std::cout << info.current_count_change
+                  << " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
+    }
+}
+
+void CustomDomainParticipantListener::on_data_available(
+        DataReader* reader)
+{
+    SampleInfo info;
+    if (reader->take_next_sample(&hello_, &info) == ReturnCode_t::RETCODE_OK)
+    {
+        if (info.instance_state == ALIVE)
+        {
+            samples_++;
+            // Print your structure data here.
+            std::cout << "(CustomDomainParticipantListener) " << hello_.message() << " " << hello_.index() << " RECEIVED" << std::endl;
+        }
+    }
+}
