efaa0b59204930eac113e74cde2ba817be1a0045
laura <52071480+laura-eprosima@users.noreply.github.com>
2020-09-10 07:12:40 +0200

    Fix Writer history behavior when there are no matched readers (#1331)
    * Refs #8945 Fixes wrong behavior when no readers are matched
    
    * Refs #8945 Add blackbox test to check correct behavior when there are no readers
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    * Refs #9030 Requested Changes
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    * Refs #9030 Change Blackbox Comment
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    * Refs #9030 Requested Change
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    * Refs #8945 Modify Blackbox test to check also the second problem
    
    * When there are no readers all the samples are set as acknowledged so that the old changes can be removed and replaced with new ones.
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    * Refs #9030 Fix PubReliableKeepAllSubNonReliable test
    
    Signed-off-by: Laura Martin <laura@eprosima.com>
    
    Co-authored-by: LuisGasco <luisgasco@eprosima.com>

--- src/cpp/rtps/writer/StatefulWriter.cpp
+++ src/cpp/rtps/writer/StatefulWriter.cpp
@@ -356,15 +356,12 @@ void StatefulWriter::unsent_change_added_to_history(
                     periodic_hb_event_->restart_timer(max_blocking_time);
                 }
 
-                if ( (mp_listener != nullptr) && this->is_acked_by_all(change) )
-                {
-                    mp_listener->onWriterChangeReceivedByAll(this, change);
-                }
-
                 if (disable_positive_acks_ && last_sequence_number_ == SequenceNumber_t())
                 {
                     last_sequence_number_ = change->sequenceNumber;
                 }
+
+                check_acked_status();
             }
             catch (const RTPSMessageGroup::timeout&)
             {
@@ -409,10 +406,7 @@ void StatefulWriter::unsent_change_added_to_history(
     else
     {
         logInfo(RTPS_WRITER, "No reader proxy to add change.");
-        if (mp_listener != nullptr)
-        {
-            mp_listener->onWriterChangeReceivedByAll(this, change);
-        }
+        check_acked_status();
     }
 }
 
@@ -1554,7 +1548,8 @@ void StatefulWriter::check_acked_status()
 
     bool all_acked = true;
     bool has_min_low_mark = false;
-    SequenceNumber_t min_low_mark;
+    // #8945 If no readers matched, notify all old changes.
+    SequenceNumber_t min_low_mark = mp_history->next_sequence_number() - 1;
 
     for (const ReaderProxy* it : matched_readers_)
     {
@@ -1657,14 +1652,7 @@ bool StatefulWriter::try_remove_change(
 
     {
         std::lock_guard<RecursiveTimedMutex> guard(mp_mutex);
-        for (ReaderProxy* it : matched_readers_)
-        {
-            SequenceNumber_t reader_low_mark = it->changes_low_mark();
-            if (min_low_mark == SequenceNumber_t() || reader_low_mark < min_low_mark)
-            {
-                min_low_mark = reader_low_mark;
-            }
-        }
+        min_low_mark = next_all_acked_notify_sequence_ - 1;
     }
 
     SequenceNumber_t calc = min_low_mark < get_seq_num_min() ? SequenceNumber_t() :
