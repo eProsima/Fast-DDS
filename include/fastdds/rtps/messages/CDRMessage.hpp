b63ce42aadc71dadfbab889406ca551633a82334
Miguel Company <miguelcompany@eprosima.com>
2020-09-11 07:26:49 +0200

    Protect bitmap parsing methods (#1356)
    * Refs #9132. Protect bitmap parsing methods.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #9183. Addressed review comments
    
    * Refs #9183. Fix logic inversion.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- include/fastdds/rtps/messages/CDRMessage.hpp
+++ include/fastdds/rtps/messages/CDRMessage.hpp
@@ -221,20 +221,24 @@ inline SequenceNumberSet_t CDRMessage::readSequenceNumberSet(
         CDRMessage_t* msg)
 {
     bool valid = true;
+    SequenceNumberSet_t sns(c_SequenceNumber_Unknown);
 
     SequenceNumber_t seqNum;
     valid &= CDRMessage::readSequenceNumber(msg, &seqNum);
-    SequenceNumberSet_t sns(seqNum);
     uint32_t numBits = 0;
     valid &= CDRMessage::readUInt32(msg, &numBits);
+    valid &= (numBits <= 256u);
+
     uint32_t n_longs = (numBits + 31ul) / 32ul;
     uint32_t bitmap[8];
-    for (uint32_t i = 0; i < n_longs; ++i)
+    for (uint32_t i = 0; valid && (i < n_longs); ++i)
     {
         valid &= CDRMessage::readUInt32(msg, &bitmap[i]);
     }
+
     if (valid)
     {
+        sns.base(seqNum);
         sns.bitmap_set(numBits, bitmap);
     }
 
@@ -246,21 +250,26 @@ inline bool CDRMessage::readFragmentNumberSet(
         FragmentNumberSet_t* fns)
 {
     bool valid = true;
+
     FragmentNumber_t base = 0ul;
     valid &= CDRMessage::readUInt32(msg, &base);
-    fns->base(base);
     uint32_t numBits = 0;
     valid &= CDRMessage::readUInt32(msg, &numBits);
+    valid &= (numBits <= 256u);
+
     uint32_t n_longs = (numBits + 31ul) / 32ul;
     uint32_t bitmap[8];
-    for (uint32_t i = 0; i < n_longs; ++i)
+    for (uint32_t i = 0; valid && (i < n_longs); ++i)
     {
         valid &= CDRMessage::readUInt32(msg, &bitmap[i]);
     }
+
     if (valid)
     {
+        fns->base(base);
         fns->bitmap_set(numBits, bitmap);
     }
+
     return valid;
 }
 
