5d05911e1a7795ef854f9070e2f094fecb02fe67
Miguel Barro <miguelbarro@eprosima.com>
2020-09-15 13:01:33 +0200

    skeleton framework for the new discovery-server
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- src/cpp/rtps/builtin/discovery/participant/PDPClient.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPClient.cpp
@@ -50,177 +50,6 @@ namespace eprosima {
 namespace fastrtps {
 namespace rtps {
 
-GUID_t RemoteServerAttributes::GetParticipant() const
-{
-    return GUID_t(guidPrefix, c_EntityId_RTPSParticipant);
-}
-
-GUID_t RemoteServerAttributes::GetPDPReader() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SPDPReader);
-}
-
-GUID_t RemoteServerAttributes::GetPDPWriter() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SPDPWriter);
-}
-
-GUID_t RemoteServerAttributes::GetEDPPublicationsReader() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SEDPPubReader);
-}
-
-GUID_t RemoteServerAttributes::GetEDPSubscriptionsWriter() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SEDPSubWriter);
-}
-
-GUID_t RemoteServerAttributes::GetEDPPublicationsWriter() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SEDPPubWriter);
-}
-
-GUID_t RemoteServerAttributes::GetEDPSubscriptionsReader() const
-{
-    return GUID_t(guidPrefix, c_EntityId_SEDPSubReader);
-}
-
-bool get_server_client_default_guidPrefix(
-        int id,
-        GuidPrefix_t& guid)
-{
-    if ( id >= 0
-            && id < 256
-            && std::istringstream(DEFAULT_ROS2_SERVER_GUIDPREFIX) >> guid)
-    {
-        // Last octet denotes the default server id but to ease debugging it starts on char '0' = 48
-        guid.value[11] = static_cast<octet>((48 + id) % 256);
-
-        return true;
-    }
-
-    return false;
-}
-
-bool load_environment_server_info(
-        std::string list,
-        RemoteServerList_t& attributes)
-{
-    using namespace std;
-
-    // parsing ancillary regex
-    const regex ROS2_IPV4_PATTERN(R"(^((?:[0-9]{1,3}\.){3}[0-9]{1,3})?:?(?:(\d+))?$)");
-    const regex ROS2_SERVER_LIST_PATTERN(R"(([^;]*);?)");
-
-    try
-    {
-        // Do the parsing and populate the list
-        attributes.clear();
-        RemoteServerAttributes server_att;
-        Locator_t server_locator(LOCATOR_KIND_UDPv4, DEFAULT_ROS2_SERVER_PORT);
-        int server_id = 0;
-
-        sregex_iterator server_it(
-            list.begin(),
-            list.end(),
-            ROS2_SERVER_LIST_PATTERN,
-            regex_constants::match_not_null);
-
-        while (server_it != sregex_iterator())
-        {
-            const smatch::value_type sm = *++(server_it->cbegin());
-
-            if (sm.matched)
-            {
-                // now we must parse the inner expression
-                smatch mr;
-                string locator(sm);
-                if (regex_match(locator, mr, ROS2_IPV4_PATTERN, regex_constants::match_not_null))
-                {
-                    smatch::iterator it = mr.cbegin();
-
-                    while (++it != mr.cend())
-                    {
-                        if ( !IPLocator::setIPv4(server_locator, it->str()))
-                        {
-                            stringstream ss;
-                            ss << "Wrong ipv4 address passed into the server's list " << it->str();
-                            throw std::invalid_argument(ss.str());
-                        }
-
-                        if (IPLocator::isAny(server_locator))
-                        {
-                            // A server cannot be reach in all interfaces, it's clearly a localhost call
-                            IPLocator::setIPv4(server_locator, "127.0.0.1");
-                        }
-
-                        if ( ++it != mr.cend())
-                        {
-                            // reset the locator to default
-                            IPLocator::setPhysicalPort(server_locator, DEFAULT_ROS2_SERVER_PORT);
-
-                            if ( it->matched)
-                            {
-                                // note stoi throws also an invalid_argument
-                                int port = stoi(it->str());
-
-                                if ( port > std::numeric_limits<uint16_t>::max())
-                                {
-                                    throw out_of_range("Too larget udp port passed into the server's list");
-                                }
-
-                                if ( !IPLocator::setPhysicalPort(server_locator, static_cast<uint16_t>(port)))
-                                {
-                                    stringstream ss;
-                                    ss << "Wrong udp port passed into the server's list " << it->str();
-                                    throw invalid_argument(ss.str());
-                                }
-                            }
-                        }
-                    }
-
-                    // add the server to the list
-                    if (!get_server_client_default_guidPrefix(server_id, server_att.guidPrefix))
-                    {
-                        throw std::invalid_argument("The maximum number of default discovery servers have been reached");
-                    }
-
-                    server_att.metatrafficUnicastLocatorList.clear();
-                    server_att.metatrafficUnicastLocatorList.push_back(server_locator);
-                    attributes.push_back(server_att);
-                }
-                else
-                {
-                    if (!locator.empty())
-                    {
-                        stringstream ss;
-                        ss << "Wrong locator passed into the server's list " << locator;
-                        throw std::invalid_argument(ss.str());
-                    }
-                    // else: it's intencionally empty to hint us to ignore this server
-                }
-            }
-            // advance to the next server if any
-            ++server_it;
-            ++server_id;
-        }
-
-        // Check for server info
-        if (attributes.empty())
-        {
-            throw std::invalid_argument("No default server locators were provided.");
-        }
-    }
-    catch ( std::exception& e )
-    {
-        logError(SERVER_CLIENT_DISCOVERY, e.what());
-        attributes.clear();
-        return false;
-    }
-
-    return true;
-}
-
 PDPClient::PDPClient(
         BuiltinProtocols* builtin,
         const RTPSParticipantAllocationAttributes& allocation)
@@ -373,7 +202,7 @@ bool PDPClient::createPDPEndpoints()
         //        mp_RTPSParticipant->set_endpoint_rtps_protection_supports(rout, false);
         //#endif
         // Initial peer list doesn't make sense in server scenario. Client should match its server list
-        for (const RemoteServerAttributes& it : mp_builtin->m_DiscoveryServers)
+        for (const eprosima::fastdds::rtps::RemoteServerAttributes& it : mp_builtin->m_DiscoveryServers)
         {
             std::lock_guard<std::mutex> data_guard(temp_data_lock_);
             temp_writer_data_.clear();
@@ -425,7 +254,7 @@ bool PDPClient::createPDPEndpoints()
         //#if HAVE_SECURITY
         //        mp_RTPSParticipant->set_endpoint_rtps_protection_supports(wout, false);
         //#endif
-        for (const RemoteServerAttributes& it : mp_builtin->m_DiscoveryServers)
+        for (const eprosima::fastdds::rtps::RemoteServerAttributes& it : mp_builtin->m_DiscoveryServers)
         {
             std::lock_guard<std::mutex> data_guard(temp_data_lock_);
             temp_reader_data_.clear();
---
+++ src/cpp/rtps/builtin/discovery/participant/PDPServer2.hpp
@@ -0,0 +1,139 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file PDPServer2.hpp
+ *
+ */
+
+#ifndef _FASTDDS_RTPS_PDPSERVER2_H_
+#define _FASTDDS_RTPS_PDPSERVER2_H_
+#ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
+
+#include <fastdds/rtps/builtin/discovery/participant/PDP.h>
+#include "./DServerEvent2.hpp"
+
+// To be eventually removed together with eprosima::fastrtps
+namespace aux = ::eprosima::fastrtps::rtps;
+
+namespace eprosima {
+namespace fastdds {
+namespace rtps {
+
+/**
+ * Class PDPServer2 manages server side of the discovery server mechanism
+ *@ingroup DISCOVERY_MODULE
+ */
+class PDPServer2 : public aux::PDP
+{
+    friend class DServerEvent2;
+    friend class PDPServerListener2;
+
+    public:
+
+    /**
+     * Constructor
+     * @param builtin Pointer to the BuiltinProcols object.
+     * @param allocation participant's allocation settings
+     * @param durability_kind the kind of persistence we want for the discovery data
+     */
+    PDPServer2(
+            aux::BuiltinProtocols* builtin,
+            const aux::RTPSParticipantAllocationAttributes& allocation);
+    ~PDPServer2();
+
+    void initializeParticipantProxyData(
+            aux::ParticipantProxyData* participant_data) override;
+
+    /**
+     * Initialize the PDP.
+     * @param part Pointer to the RTPSParticipant.
+     * @return True on success
+     */
+    bool init(
+            aux::RTPSParticipantImpl* part) override;
+
+    /**
+     * Creates an initializes a new participant proxy from a DATA(p) raw info
+     * @param p ParticipantProxyData from DATA msg deserialization
+     * @param writer_guid GUID of originating writer
+     * @return new ParticipantProxyData * or nullptr on failure
+     */
+    aux::ParticipantProxyData* createParticipantProxyData(
+            const aux::ParticipantProxyData& p,
+            const aux::GUID_t& writer_guid) override;
+
+    /**
+     * Create the SPDP Writer and Reader
+     * @return True if correct.
+     */
+    bool createPDPEndpoints() override;
+
+    /**
+     * This method removes a remote RTPSParticipant and all its writers and readers.
+     * @param participant_guid GUID_t of the remote RTPSParticipant.
+     * @param reason Why the participant is being removed (dropped vs removed)
+     * @return true if correct.
+     */
+    bool remove_remote_participant(
+            const aux::GUID_t& participant_guid,
+            aux::ParticipantDiscoveryInfo::DISCOVERY_STATUS reason) override;
+
+    /**
+     * Force the sending of our local PDP to all servers
+     * @param new_change If true a new change (with new seqNum) is created and sent; if false the last change is re-sent
+     * @param dispose Sets change kind to NOT_ALIVE_DISPOSED_UNREGISTERED
+     * @param wparams allows to identify the change
+     */
+    void announceParticipantState(
+            bool new_change,
+            bool dispose = false,
+            aux::WriteParams& wparams = aux::WriteParams::WRITE_PARAM_DEFAULT) override;
+
+    /**
+     * These methods wouldn't be needed under perfect server operation (no need of dynamic endpoint allocation)
+     * but must be implemented to solve server shutdown situations.
+     * @param pdata Pointer to the RTPSParticipantProxyData object.
+     */
+    void assignRemoteEndpoints(
+            aux::ParticipantProxyData* pdata) override;
+    void removeRemoteEndpoints(
+            aux::ParticipantProxyData* pdata) override;
+    void notifyAboveRemoteEndpoints(
+            const aux::ParticipantProxyData& pdata) override;
+
+#if HAVE_SQLITE3
+    //! Get filename for persistence database file
+    std::string GetPersistenceFileName();
+#endif // if HAVE_SQLITE3
+
+    //! Wakes up the DServerEvent2 for new matching or trimming
+    void awakeServerThread()
+    {
+        mp_sync->restart_timer();
+    }
+
+private:
+
+    /**
+     * TimedEvent for server synchronization
+     */
+    DServerEvent2* mp_sync;
+};
+
+} // namespace rtps
+} // namespace fastdds
+} // namespace eprosima
+#endif // ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
+#endif /* _FASTDDS_RTPS_PDPSERVER2_H_ */
