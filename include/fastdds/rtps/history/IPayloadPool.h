27293bcdd5a577a44acf8d17749c1ac9765d0e43
Miguel Company <MiguelCompany@eprosima.com>
2020-09-17 10:38:05 +0200

    Refs 9306. Uncrustify.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- include/fastdds/rtps/history/IPayloadPool.h
+++ include/fastdds/rtps/history/IPayloadPool.h
@@ -39,18 +39,18 @@ public:
 
     /**
      * @brief Get a serialized payload for a new sample.
-     * 
+     *
      * This method will usually be called in one of the following situations:
      * @li When a writer creates a new cache change
      * @li When a reader receives the first fragment of a cache change
-     * 
+     *
      * In both cases, the received @c size will be for the whole serialized payload.
-     * 
+     *
      * @param [in]     size          Number of bytes required for the serialized payload
      * @param [in,out] cache_change  Cache change to assign the payload to
-     * 
+     *
      * @returns whether the operation succeeded or not
-     * 
+     *
      * @post @li Field @c payload_owner of @c cache_change equals this
      * @post @li Field @c serializedPayload.data points to a buffer of at least @c size bytes
      * @post @li Field @c serializedPayload.max_size is greater than or equal to @c size
@@ -61,7 +61,7 @@ public:
 
     /**
      * @brief Assign a serialized payload to a new sample.
-     * 
+     *
      * This method will usually be called when a reader receives a whole cache change.
      *
      * @param [in]     data          Serialized payload received
@@ -81,16 +81,16 @@ public:
 
     /**
      * @brief Release a serialized payload from a sample.
-     * 
+     *
      * This method will be called when a cache change is removed from a history.
-     * 
+     *
      * @param [in,out] cache_change  Cache change to assign the payload to
      *
      * @returns whether the operation succeeded or not
      *
      * @pre @li Field @c payload_owner of @c cache_change equals this
-     * 
-     * @post @li Field @c payload_owner of @c cache_change is 
+     *
+     * @post @li Field @c payload_owner of @c cache_change is
      */
     virtual bool release_payload(
             CacheChange_t& cache_change) = 0;
