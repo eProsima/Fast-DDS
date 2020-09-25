adbd8d359862655919a7df35cc21a21695392bec
jparisu <javierparis@eprosima.com>
2020-09-25 22:49:55 +0200

    Refs #9330: uncrustify manually
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.hpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.hpp
@@ -58,7 +58,7 @@ public:
 
     class AckedFunctor
     {
-        using argument_type = eprosima::fastrtps::rtps::ReaderProxy *;
+        using argument_type = eprosima::fastrtps::rtps::ReaderProxy*;
         using result_type = void;
 
     public:
