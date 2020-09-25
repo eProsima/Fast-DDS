bacb8ca5fe385999a8c292a6ccfb555d9b0514e3
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2020-09-25 20:04:27 +0200

    Server routine: Process to-send lists sub-routine (#1415)
    * Refs #9410: Process to-send lists sub-routine
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #9410: Apply suggestions
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- src/cpp/rtps/builtin/discovery/participant/PDPServer2.hpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPServer2.hpp
@@ -158,10 +158,20 @@ protected:
             fastrtps::rtps::WriterHistory* writer_history,
             const fastrtps::rtps::GuidPrefix_t& entity_guid_prefix);
 
-
     bool announcement_from_same_participant_in_disposals(
             const std::vector<fastrtps::rtps::CacheChange_t*>& disposals,
             const fastrtps::rtps::GuidPrefix_t& participant);
+
+    bool process_to_send_lists();
+
+    bool process_to_send_list(
+            const std::vector<eprosima::fastrtps::rtps::CacheChange_t*>& send_list,
+            fastrtps::rtps::RTPSWriter* writer,
+            fastrtps::rtps::WriterHistory* history);
+
+    bool remove_change_from_history_nts(
+            fastrtps::rtps::WriterHistory* history,
+            fastrtps::rtps::CacheChange_t* change);
 
     bool process_dirty_topics();
 
