5b597911e273c4c349ef391f55dd10f8a9cc4a9b
Miguel Company <MiguelCompany@eprosima.com>
2020-09-30 08:11:22 +0200

    Refs 9437. Added constructors to StatelessPersistentReader.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- include/fastdds/rtps/reader/StatelessPersistentReader.h
+++ include/fastdds/rtps/reader/StatelessPersistentReader.h
@@ -38,9 +38,28 @@ class StatelessPersistentReader : public StatelessReader
     friend class RTPSParticipantImpl;
 
     StatelessPersistentReader(
-            RTPSParticipantImpl*,
-            GUID_t& guid,
-            ReaderAttributes& att,
+            RTPSParticipantImpl* pimpl,
+            const GUID_t& guid,
+            const ReaderAttributes& att,
+            ReaderHistory* hist,
+            ReaderListener* listen,
+            IPersistenceService* persistence);
+
+    StatelessPersistentReader(
+            RTPSParticipantImpl* pimpl,
+            const GUID_t& guid,
+            const ReaderAttributes& att,
+            const std::shared_ptr<IPayloadPool>& payload_pool,
+            ReaderHistory* hist,
+            ReaderListener* listen,
+            IPersistenceService* persistence);
+
+    StatelessPersistentReader(
+            RTPSParticipantImpl* pimpl,
+            const GUID_t& guid,
+            const ReaderAttributes& att,
+            const std::shared_ptr<IPayloadPool>& payload_pool,
+            const std::shared_ptr<IChangePool>& change_pool,
             ReaderHistory* hist,
             ReaderListener* listen,
             IPersistenceService* persistence);
@@ -57,12 +76,17 @@ protected:
 
 private:
 
+    void init(
+            const GUID_t& guid,
+            const ReaderAttributes& att);
+
     IPersistenceService* persistence_;
     std::string persistence_guid_;
 };
+
 } // namespace rtps
-} /* namespace rtps */
-} /* namespace eprosima */
+} // namespace fastrtps
+} // namespace eprosima
 
 #endif // ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
 #endif /* _FASTDDS_RTPS_STATELESSPERSISTENTREADER_H_ */
