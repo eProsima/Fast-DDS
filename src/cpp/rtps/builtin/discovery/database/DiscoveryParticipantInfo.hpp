8f63aec13ed47f8fd6a80dc81168486e173d3434
jparisu <69341543+jparisu@users.noreply.github.com>
2020-10-05 11:55:52 +0200

    Server is acked by default in DDB [9470] (#1431)
    * Refs #9446: change sharedInfo constructor to do default acked by server
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9446: apply changes of pr
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9446: uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryParticipantInfo.hpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryParticipantInfo.hpp
@@ -42,8 +42,9 @@ class DiscoveryParticipantInfo : public DiscoverySharedInfo
 public:
 
     DiscoveryParticipantInfo(
-            eprosima::fastrtps::rtps::CacheChange_t* change_)
-        : DiscoverySharedInfo(change_)
+            eprosima::fastrtps::rtps::CacheChange_t* change,
+            const eprosima::fastrtps::rtps::GuidPrefix_t& known_participant)
+        : DiscoverySharedInfo(change, known_participant)
     {
     }
 
