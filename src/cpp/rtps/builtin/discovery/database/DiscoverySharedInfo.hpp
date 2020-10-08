72db8e452b2ea4ff81c64fec76c06af4788fe7f2
jparisu <69341543+jparisu@users.noreply.github.com>
2020-10-08 18:39:32 +0200

    Discovery Server: New design for process disposals [9499] (#1449)
    * Refs #9445: functions signature
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: functions signature updated
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: disposals and erase functions implemented, not working properly
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: Disposals tested and working
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: rebase to prototype
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: Fix incorrect access to writer history
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #9445: skip unmatch when disposal w|r
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: apply changes and uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9445: uncrustify ammend
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    Co-authored-by: EduPonz <eduardoponz@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoverySharedInfo.hpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoverySharedInfo.hpp
@@ -59,7 +59,7 @@ public:
             bool status = false)
     {
         logInfo(DISCOVERY_DATABASE, "Adding relevant participant " << guid_p << " with status " << status << " to " <<
-                fastrtps::rtps::iHandle2GUID(change_->instanceHandle).guidPrefix);
+                fastrtps::rtps::iHandle2GUID(change_->instanceHandle));
         relevant_participants_builtin_ack_status_.add_or_update_participant(guid_p, status);
     }
 
@@ -86,7 +86,12 @@ public:
         return change_;
     }
 
-protected:
+    std::vector<eprosima::fastrtps::rtps::GuidPrefix_t> relevant_participants() const
+    {
+        return relevant_participants_builtin_ack_status_.relevant_participants();
+    }
+
+private:
 
     eprosima::fastrtps::rtps::CacheChange_t* change_;
 
