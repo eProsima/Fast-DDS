ee3b24b3e70efcfb98ced6743f76fa7f150f8a1a
IkerLuengo <57146230+IkerLuengo@users.noreply.github.com>
2020-10-09 07:24:30 +0200

    Topic payload pool implementation (#1424)
    * Refs 9310. Definition of TopicPayloadPool
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9310. TopicPayloadPool tests
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9310. Uncrustify
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9310. Correct tests
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Adapt tests to the implementation
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. implementation of TopicPayloadPool for every memory policy
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Take IPayloadPool interface outside
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Convert template to class hierarchy
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Uncrustify
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Use metadata inserted in the header of the payload
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Testing reuse of payloads and refcounter
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Reorganize code to reduce repetition
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Thread safety
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Update data owner argument on get_payload
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Correct compilation warnings and test leaks
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Linter errors
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. suggested changes
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Suggested changes
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393 Make ref counter atomic
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Simplify offset calculation
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Delegate get_payload+resize
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Remove unused methods
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Handle bad alloc
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Suggested improvements
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>
    
    * Refs 9393. Uncrustify
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

---
+++ src/cpp/rtps/history/TopicPayloadPool_impl/Dynamic.hpp
@@ -0,0 +1,88 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file Dynamic.hpp
+ */
+
+#ifndef RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_DYNAMIC_HPP
+#define RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_DYNAMIC_HPP
+
+#include <rtps/history/TopicPayloadPool.hpp>
+
+namespace eprosima {
+namespace fastrtps {
+namespace rtps {
+
+class DynamicTopicPayloadPool : public TopicPayloadPool
+{
+public:
+
+    virtual bool release_payload(
+            CacheChange_t& cache_change) override
+    {
+        assert(cache_change.payload_owner() == this);
+
+        {
+            if (PayloadNode::dereference(cache_change.serializedPayload.data))
+            {
+                //First remove it from all_payloads
+                std::unique_lock<std::mutex> lock(mutex_);
+                uint32_t data_index = PayloadNode::data_index(cache_change.serializedPayload.data);
+                PayloadNode* payload = all_payloads_.at(data_index);
+                all_payloads_.at(data_index) = all_payloads_.back();
+                all_payloads_.back()->data_index(data_index);
+                all_payloads_.pop_back();
+                lock.unlock();
+
+                // Now delete the data
+                delete(payload);
+            }
+        }
+
+        cache_change.serializedPayload.length = 0;
+        cache_change.serializedPayload.pos = 0;
+        cache_change.serializedPayload.max_size = 0;
+        cache_change.serializedPayload.data = nullptr;
+        cache_change.payload_owner(nullptr);
+
+        return true;
+    }
+
+    bool release_history(
+            const PoolConfig& config,
+            bool /*is_reader*/) override
+    {
+        assert(config.memory_policy == memory_policy());
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        update_maximum_size(config, false);
+
+        return true;
+    }
+
+protected:
+
+    MemoryManagementPolicy_t memory_policy() const override
+    {
+        return DYNAMIC_RESERVE_MEMORY_MODE;
+    }
+
+};
+
+}  // namespace rtps
+}  // namespace fastrtps
+}  // namespace eprosima
+
+#endif  // RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_DYNAMIC_HPP
---
+++ src/cpp/rtps/history/TopicPayloadPool_impl/PreallocatedWithRealloc.hpp
@@ -0,0 +1,89 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file Preallocated.hpp
+ */
+
+#ifndef RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_PREALLOCATED_REALLOC_HPP
+#define RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_PREALLOCATED_REALLOC_HPP
+
+#include <rtps/history/TopicPayloadPool.hpp>
+
+namespace eprosima {
+namespace fastrtps {
+namespace rtps {
+
+class PreallocatedReallocTopicPayloadPool : public TopicPayloadPool
+{
+public:
+
+    explicit PreallocatedReallocTopicPayloadPool(
+            uint32_t payload_size)
+        : min_payload_size_(payload_size)
+        , minimum_pool_size_for_writers(0)
+        , minimum_pool_size_for_readers(0)
+    {
+    }
+
+    bool get_payload(
+            uint32_t size,
+            CacheChange_t& cache_change) override
+    {
+        return do_get_payload(std::max(size, min_payload_size_), cache_change, true);
+    }
+
+    bool reserve_history(
+            const PoolConfig& config,
+            bool is_reader) override
+    {
+        if (!TopicPayloadPool::reserve_history(config, is_reader))
+        {
+            return false;
+        }
+
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (is_reader)
+        {
+            minimum_pool_size_for_readers =
+                    std::max(minimum_pool_size_for_readers, config.initial_size);
+        }
+        else
+        {
+            minimum_pool_size_for_writers += config.initial_size;
+        }
+
+        reserve(minimum_pool_size_for_writers + minimum_pool_size_for_readers, min_payload_size_);
+        return true;
+    }
+
+protected:
+
+    MemoryManagementPolicy_t memory_policy() const override
+    {
+        return PREALLOCATED_WITH_REALLOC_MEMORY_MODE;
+    }
+
+private:
+
+    uint32_t min_payload_size_;
+    uint32_t minimum_pool_size_for_writers;    //< Initial pool size due to writers (sum of all writers)
+    uint32_t minimum_pool_size_for_readers;    //< Initial pool size due to readers (max of all readers)
+};
+
+}  // namespace rtps
+}  // namespace fastrtps
+}  // namespace eprosima
+
+#endif  // RTPS_HISTORY_TOPICPAYLOADPOOLIMPL_PREALLOCATED_REALLOC_HPP
