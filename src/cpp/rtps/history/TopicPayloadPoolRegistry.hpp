92e352eced2a39bdc45b813bdb9695416312337a
Miguel Company <MiguelCompany@eprosima.com>
2020-10-09 11:51:58 +0200

    Refs 9595. Added registry header.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---
+++ src/cpp/rtps/history/TopicPayloadPoolRegistry.hpp
@@ -0,0 +1,50 @@
+// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file TopicPayloadPoolRegistry.hpp
+ */
+
+#ifndef RTPS_HISTORY_TOPICPAYLOADPOOLREGISTRY_HPP
+#define RTPS_HISTORY_TOPICPAYLOADPOOLREGISTRY_HPP
+
+#include <fastdds/rtps/history/IPayloadPool.h>
+
+#include <rtps/history/PoolConfig.h>
+
+#include <memory>
+#include <string>
+
+namespace eprosima {
+namespace fastrtps {
+namespace rtps {
+
+class TopicPayloadPoolRegistry
+{
+
+public:
+
+    static std::shared_ptr<IPayloadPool> get(
+            const std::string& topic_name,
+            const PoolConfig& config);
+
+    static void release(
+            std::shared_ptr<IPayloadPool>& pool);
+};
+
+}  // namespace rtps
+}  // namespace fastrtps
+}  // namespace eprosima
+
+#endif  // RTPS_HISTORY_TOPICPAYLOADPOOLREGISTRY_HPP
