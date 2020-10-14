19f2209a8a07af4c930bdd767cba0968252f07f6
Miguel Company <miguelcompany@eprosima.com>
2020-10-14 14:59:58 +0200

    Topic pool registry (#1481)
    * Refs 9595. Fixed TopicPayloadPool header
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added registry header.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added empty implementation.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. TopicPayloadPool factory receives const ref.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added unit test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added TopicPayloadPoolProxy.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added TopicPayloadPoolRegistryEntry.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added basic implementation of TopicPayloadPoolRegistry.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added unordered_map to registry.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Added reference count checks to test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Keep policy on TopicPayloadPoolProxy.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Reference counting on TopicPayloadPoolProxy.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Full implementation.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Uncrustify.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. TopicPayloadPoolRegistry API uses ITopicPayloadPool.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Refactor PoolConfig with a base BasicPoolConfig.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Update API on BasicPayloadPool.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Update API on TopicPayloadPool.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Update API on TopicPayloadPoolRegistry.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Update TopicPayloadPoolRegistryTests.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Improve TopicPayloadPoolTests.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. TopicPayloadPoolTests account for new requirements.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Fixes on preallocated versions TopicPayloadPool.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 9595. Warnings on Mac.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/history/TopicPayloadPool.cpp
+++ src/cpp/rtps/history/TopicPayloadPool.cpp
@@ -278,7 +278,7 @@ bool TopicPayloadPool::shrink (
 }
 
 std::shared_ptr<ITopicPayloadPool> TopicPayloadPool::get(
-        PoolConfig config)
+        const BasicPoolConfig& config)
 {
     switch (config.memory_policy)
     {
