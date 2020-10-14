e3a351219891e084a70853634d5dbb3f5182bb0f
Miguel Company <MiguelCompany@eprosima.com>
2020-10-14 15:43:31 +0200

    Refs 9613. Uncrustify EDPSimple.h.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- include/fastdds/rtps/builtin/discovery/endpoint/EDPSimple.h
+++ include/fastdds/rtps/builtin/discovery/endpoint/EDPSimple.h
@@ -81,7 +81,7 @@ public:
     t_p_StatefulWriter subscriptions_secure_writer_;
 
     t_p_StatefulReader subscriptions_secure_reader_;
-#endif
+#endif // if HAVE_SECURITY
 
     //!Pointer to the listener associated with PubReader and PubWriter.
     EDPListener* publications_listener_;
@@ -137,14 +137,14 @@ public:
      * @return True if correct.
      */
     bool removeLocalReader(
-            RTPSReader*R) override;
+            RTPSReader* R) override;
     /**
      * This methods generates the change disposing of the local Writer and calls the unpairing and removal methods of the base class.
      * @param W Pointer to the RTPSWriter object.
      * @return True if correct.
      */
     bool removeLocalWriter(
-            RTPSWriter*W) override;
+            RTPSWriter* W) override;
 
 protected:
 
@@ -215,9 +215,12 @@ protected:
             CacheChange_t** created_change);
 
     //! Process the info recorded in the persistence database
-    static void processPersistentData(t_p_StatefulReader & reader, t_p_StatefulWriter & writer);
+    static void processPersistentData(
+            t_p_StatefulReader& reader,
+            t_p_StatefulWriter& writer);
 
 private:
+
     /**
      * Create a cache change on a builtin writer and serialize a ProxyData on it.
      * @param [in] data The ProxyData object to be serialized.
@@ -243,7 +246,7 @@ private:
     bool pairing_remote_reader_with_local_builtin_writer_after_security(
             const GUID_t& local_writer,
             const ReaderProxyData& remote_reader_data) override;
-#endif
+#endif // if HAVE_SECURITY
 
     std::mutex temp_data_lock_;
     ReaderProxyData temp_reader_proxy_data_;
@@ -254,5 +257,5 @@ private:
 } /* namespace fastrtps */
 } /* namespace eprosima */
 
-#endif
+#endif // ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
 #endif /* _FASTDDS_RTPS_EDPSIMPLE_H_ */
