6a59e88b74ac48904e600fe72d02653df61060b5
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2020-10-16 14:27:41 +0200

    [Discovery Server] Propagation to other servers using virtual topics [9582] (#1473)
    * Refs #9492: create_participant_from_change_ accounts for local SERVERs
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #9492: create_writers|readers__from_change accounts for local, external, and virtual entities
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #9492: Delete virtual changes
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #9492: encapsulate functionality
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: correction over rebase
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: virtuals compiling and working
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: fix related_sample_identity and fix dirty_topics
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: changes requested for pr
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Refs #9492: uncrustify ammend
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    Co-authored-by: jparisu <javierparis@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.hpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.hpp
@@ -303,6 +303,11 @@ protected:
             eprosima::fastrtps::rtps::CacheChange_t* ch,
             const std::string& topic_name);
 
+    // change ack relevants and matched between entities participants and endpoints
+    void match_writer_reader_(
+            const eprosima::fastrtps::rtps::GUID_t& writer_guid,
+            const eprosima::fastrtps::rtps::GUID_t& reader_guid);
+
     void process_dispose_participant_(
             eprosima::fastrtps::rtps::CacheChange_t* ch);
 
@@ -366,6 +371,10 @@ protected:
             const eprosima::fastrtps::rtps::GUID_t& reader_guid,
             const std::string& topic_name);
 
+    // create a new topic in writers and readers and fill it with virtuals
+    void create_topic_(
+            const std::string& topic_name);
+
     void add_writer_to_topic_(
             const eprosima::fastrtps::rtps::GUID_t& writer_guid,
             const std::string& topic_name);
@@ -373,6 +382,34 @@ protected:
     void add_reader_to_topic_(
             const eprosima::fastrtps::rtps::GUID_t& reader_guid,
             const std::string& topic_name);
+
+    //! Add a topic to the list of dirty topics, unless it's already present
+    // Return true if added, false if already there
+    bool set_dirty_topic_(
+            std::string topic);
+
+    // Add data in pdp_to_send if not already in it
+    bool add_pdp_to_send_(
+            eprosima::fastrtps::rtps::CacheChange_t* change);
+
+    // Add data in edp_to_send if not already in it
+    bool add_edp_publications_to_send_(
+            eprosima::fastrtps::rtps::CacheChange_t* change);
+
+    // Add data in edp_to_send if not already in it
+    bool add_edp_subscriptions_to_send_(
+            eprosima::fastrtps::rtps::CacheChange_t* change);
+
+    // Get all the writers in given topic and in virtual topic
+    std::vector<eprosima::fastrtps::rtps::GUID_t> get_writers_in_topic(
+            const std::string& topic_name);
+
+    // Get all the readers in given topic and in virtual topic
+    std::vector<eprosima::fastrtps::rtps::GUID_t> get_readers_in_topic(
+            const std::string& topic_name);
+
+    ////////////////
+    // Variables
 
     fastrtps::DBQueue<eprosima::fastdds::rtps::ddb::DiscoveryPDPDataQueueInfo> pdp_data_queue_;
 
@@ -420,6 +457,12 @@ protected:
 
     //! List of GUID prefixes of the remote servers
     std::vector<fastrtps::rtps::GuidPrefix_t> servers_;
+
+    // The number of local servers known by the database
+    uint16_t local_servers_count_ = 0;
+
+    // The virtual topic associated with virtual writers and readers
+    const std::string virtual_topic_ = "eprosima_server_virtual_topic";
 
     // Whether the database is enabled
     std::atomic<bool> enabled_;
--- src/cpp/rtps/builtin/discovery/participant/PDPServerListener2.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPServerListener2.cpp
@@ -94,6 +94,14 @@ void PDPServerListener2::onNewCacheChangeAdded(
         return;
     }
 
+    // Related_sample_identity could be lost in message delivered, so we set as sample_identity
+    // An empty related_sample_identity could lead into an empty sample_identity when resending this msg
+    if (change->write_params.related_sample_identity() == SampleIdentity::unknown())
+    {
+        change->write_params.related_sample_identity(change->write_params.sample_identity());
+    }
+
+
     // DATA(p) case
     if (change->kind == ALIVE)
     {
@@ -136,34 +144,14 @@ void PDPServerListener2::onNewCacheChangeAdded(
                 }
             }
 
-            // Check whether the participant is a client of this server, or it has been discovered through another
-            // server
-            bool is_my_client = true;
-            bool is_my_server = false;
-            // If the instance handle is different from the writer GUID, then the change has been relayed. That means,
-            // that the participants is somebody else's client
+            // Check whether the participant is a client/server of this server or if it has been forwarded from
+            //  another entity (server).
+            // is_local means that the server is connected (or will be) with this entity directly
+            bool is_local = true;
+            // If the instance handle is different from the writer GUID, then the change has been relayed
             if (iHandle2GUID(change->instanceHandle).guidPrefix != change->writerGUID.guidPrefix)
             {
-                is_my_client = false;
-            }
-            // If the change has NOT been relayed, then look for it in this server's list of remote servers (servers
-            // for which this sever is a client).
-            //    1. If the participant is there, it means that this server is a CLIENT of the remote one, thus the
-            //       remote is not my client.
-            //    2. If the participant is not there, it means that the remote server is a CLIENT of this server
-            else
-            {
-                // Iterate over the servers for which I'm a CLIENT
-                for (auto server : pdp_server()->servers())
-                {
-                    // If the change's participant is in the list, then it means I'm a CLIENT to it.
-                    if (iHandle2GUID(change->instanceHandle).guidPrefix == server.guidPrefix)
-                    {
-                        is_my_client = false;
-                        is_my_server = true;
-                        break;
-                    }
-                }
+                is_local = false;
             }
 
             // Notify the DiscoveryDataBase
@@ -172,8 +160,7 @@ void PDPServerListener2::onNewCacheChangeAdded(
                         ddb::DiscoveryParticipantChangeData(
                             temp_participant_data_.metatraffic_locators,
                             is_client,
-                            is_my_client,
-                            is_my_server)))
+                            is_local)))
             {
                 // Remove change from PDP reader history, but do not return it to the pool. From here on, the discovery
                 // database takes ownership of the CacheChange_t. Henceforth there are no references to the change.
@@ -184,7 +171,6 @@ void PDPServerListener2::onNewCacheChangeAdded(
                 // The server does not have to postpone the execution of the routine if a change is received, i.e.
                 // the server routine is triggered instantly as the default value of the interval that the server has
                 // to wait is 0.
-                pdp_server()->awake_server_thread();
                 pdp_server()->awake_routine_thread();
 
                 // TODO: when the DiscoveryDataBase allows updating capabilities we can dismissed old PDP processing
