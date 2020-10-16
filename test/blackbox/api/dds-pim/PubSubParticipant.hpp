2a1406d43dc51f13ecdc898893e81b7f18d083bb
Iker Luengo <ikerluengo@eprosima.com>
2020-10-16 10:40:36 +0200

    Refs 9627. Wait for partial matching on PubSubParticipant
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- test/blackbox/api/dds-pim/PubSubParticipant.hpp
+++ test/blackbox/api/dds-pim/PubSubParticipant.hpp
@@ -342,6 +342,30 @@ public:
         std::cout << "Publisher discovery finished " << std::endl;
     }
 
+    void pub_wait_discovery(
+            unsigned int expected_match,
+            std::chrono::seconds timeout = std::chrono::seconds::zero())
+    {
+        std::unique_lock<std::mutex> lock(pub_mutex_);
+
+        std::cout << "Publisher is waiting discovery..." << std::endl;
+
+        if (timeout == std::chrono::seconds::zero())
+        {
+            pub_cv_.wait(lock, [&](){
+                return pub_matched_ == expected_match;
+            });
+        }
+        else
+        {
+            pub_cv_.wait_for(lock, timeout, [&](){
+                return pub_matched_ == expected_match;
+            });
+        }
+
+        std::cout << "Publisher discovery finished " << std::endl;
+    }
+
     void sub_wait_discovery(
             std::chrono::seconds timeout = std::chrono::seconds::zero())
     {
@@ -362,6 +386,30 @@ public:
                     {
                         return sub_matched_ == num_expected_subscribers_;
                     });
+        }
+
+        std::cout << "Subscriber discovery finished " << std::endl;
+    }
+
+    void sub_wait_discovery(
+            unsigned int expected_match,
+            std::chrono::seconds timeout = std::chrono::seconds::zero())
+    {
+        std::unique_lock<std::mutex> lock(sub_mutex_);
+
+        std::cout << "Subscriber is waiting discovery..." << std::endl;
+
+        if (timeout == std::chrono::seconds::zero())
+        {
+            sub_cv_.wait(lock, [&](){
+                return sub_matched_ == expected_match;
+            });
+        }
+        else
+        {
+            sub_cv_.wait_for(lock, timeout, [&](){
+                return sub_matched_ == expected_match;
+            });
         }
 
         std::cout << "Subscriber discovery finished " << std::endl;
