1d884fbfa0047ad58ecf6663a11d0681cd1faeb6
jparisu <69341543+jparisu@users.noreply.github.com>
2020-10-21 12:07:35 +0200

    Routine is restarted till server is acked by all (#1509)
    * Fix pending routine to server be acked
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Revision changes and uncrustify
    
    Signed-off-by: jparisu <javierparis@eprosima.com>
    
    * Apply changes
    
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
@@ -477,9 +477,6 @@ bool DiscoveryDataBase::process_edp_data_queue()
                                                                 << change->instanceHandle);
                 create_readers_from_change_(change, topic_name);
             }
-
-            // Update set of dirty_topics
-            set_dirty_topic_(topic_name);
         }
         // If the change is a DATA(Uw|Ur)
         else
@@ -732,6 +729,8 @@ void DiscoveryDataBase::create_writers_from_change_(
                 match_writer_reader_(writer_guid, reader);
             }
         }
+        // Update set of dirty_topics
+        set_dirty_topic_(topic_name);
     }
 }
 
@@ -836,6 +835,8 @@ void DiscoveryDataBase::create_readers_from_change_(
                 match_writer_reader_(writer, reader_guid);
             }
         }
+        // Update set of dirty_topics
+        set_dirty_topic_(topic_name);
     }
 }
 
@@ -992,6 +993,8 @@ void DiscoveryDataBase::match_writer_reader_(
 bool DiscoveryDataBase::set_dirty_topic_(
         std::string topic)
 {
+    logInfo(DISCOVERY_DATABASE, "Setting topic " << topic << " as dirty");
+
     // If topic is virtual, we need to set as dirty all the other (non-virtual) topics
     if (topic == virtual_topic_)
     {
@@ -1016,7 +1019,6 @@ bool DiscoveryDataBase::set_dirty_topic_(
                     dirty_topics_.end(),
                     topic) == dirty_topics_.end())
         {
-            logInfo(DISCOVERY_DATABASE, "Setting topic " << topic << " as dirty");
             dirty_topics_.push_back(topic);
             return true;
         }
@@ -1776,15 +1778,19 @@ void DiscoveryDataBase::add_writer_to_topic_(
                 it_topics != writers_by_topic_.end();
                 ++it_topics)
         {
+            // This find should be useless because right now we only call this function from
+            // create_writer_from_change, so the entity must be always new
             std::vector<eprosima::fastrtps::rtps::GUID_t>::iterator writer_by_topic_it =
                     std::find(it_topics->second.begin(), it_topics->second.end(), writer_guid);
             if (writer_by_topic_it == it_topics->second.end())
             {
-                logInfo(DISCOVERY_DATABASE, "New writer " << writer_guid << " in writers_by_topic: " << topic_name);
+                logInfo(DISCOVERY_DATABASE,
+                        "New virtual writer " << writer_guid << " in writers_by_topic: " << it_topics->first);
                 it_topics->second.push_back(writer_guid);
             }
         }
-        return; // the writer has been added already, avoid try to add it again
+        // The writer has been already added to every topic, avoid try to add it again in virtual topic
+        return;
     }
 
     // add the writer in the topic
@@ -1817,15 +1823,19 @@ void DiscoveryDataBase::add_reader_to_topic_(
                 it_topics != readers_by_topic_.end();
                 ++it_topics)
         {
+            // This find should be useless because right now we only call this function from
+            // create_reader_from_change, so the entity must be always new
             std::vector<eprosima::fastrtps::rtps::GUID_t>::iterator reader_by_topic_it =
                     std::find(it_topics->second.begin(), it_topics->second.end(), reader_guid);
             if (reader_by_topic_it == it_topics->second.end())
             {
-                logInfo(DISCOVERY_DATABASE, "New reader " << reader_guid << " in readers_by_topic: " << topic_name);
+                logInfo(DISCOVERY_DATABASE,
+                        "New virtual reader " << reader_guid << " in readers_by_topic: " << it_topics->first);
                 it_topics->second.push_back(reader_guid);
             }
         }
-        return; // the reader has been added already, avoid try to add it again
+        // The reader has been already added to every topic, avoid try to add it again in virtual topic
+        return;
     }
 
     // add the reader in the topic
