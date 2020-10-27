29c7b587f243f0ea71d4c2736341fd889eb3d927
Miguel Company <MiguelCompany@eprosima.com>
2020-10-27 10:21:20 +0100

    Refs 9741. Allow twenty participants test to configure each participant.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/blackbox/BlackboxTestsDiscovery.cpp
+++ test/blackbox/BlackboxTestsDiscovery.cpp
@@ -576,11 +576,13 @@ TEST_P(Discovery, PubSubAsReliableHelloworldUserData)
 }
 
 //! Auxiliar method for discovering participants tests
+template <typename ParticipantConfigurator>
 static void discoverParticipantsTest(
         bool avoid_multicast,
         size_t n_participants,
         uint32_t wait_ms,
-        const std::string& topic_name)
+        const std::string& topic_name,
+        ParticipantConfigurator participant_configurator)
 {
     std::vector<std::shared_ptr<PubSubWriterReader<HelloWorldType> > > pubsub;
     pubsub.reserve(n_participants);
@@ -596,6 +598,7 @@ static void discoverParticipantsTest(
     for (auto& ps : pubsub)
     {
         std::cout << "\rParticipant " << idx++ << " of " << n_participants << std::flush;
+        participant_configurator(ps);
         ps->init(avoid_multicast);
         ASSERT_EQ(ps->isInitialized(), true);
     }
@@ -632,6 +635,16 @@ static void discoverParticipantsTest(
     {
         ps->destroy();
     }
+}
+
+static void discoverParticipantsTest(
+        bool avoid_multicast,
+        size_t n_participants,
+        uint32_t wait_ms,
+        const std::string& topic_name)
+{
+    auto no_op = [](const std::shared_ptr<PubSubWriterReader<HelloWorldType> >&) {};
+    discoverParticipantsTest(avoid_multicast, n_participants, wait_ms, topic_name, no_op);
 }
 
 //! Tests discovery of 20 participants, having one publisher and one subscriber each, using multicast
