e64f1366f65f3f67066b1f290b6db2aa3305a37d
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2020-10-29 12:11:22 +0100

    Refs #9767: fix Windows errors and warnings
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/blackbox/BlackboxTestsDiscovery.cpp
+++ test/blackbox/BlackboxTestsDiscovery.cpp
@@ -869,7 +869,7 @@ TEST_P(Discovery, EndpointCreationMultithreaded)
 #ifdef INSTANTIATE_TEST_SUITE_P
 #define GTEST_INSTANTIATE_TEST_MACRO(x, y, z, w) INSTANTIATE_TEST_SUITE_P(x, y, z, w)
 #else
-#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z, w) INSTANTIATE_TEST_CASE_P(x, y, z, w, )
+#define GTEST_INSTANTIATE_TEST_MACRO(x, y, z, w) INSTANTIATE_TEST_CASE_P(x, y, z, w)
 #endif // ifdef INSTANTIATE_TEST_SUITE_P
 
 GTEST_INSTANTIATE_TEST_MACRO(Discovery,
