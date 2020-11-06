069b7873edc5430b3d5b8d1a518ca8c4f7ed8aa7
Stephen Brawner <brawner@gmail.com>
2020-11-06 15:25:08 -0800

    Cast potentially unused variable to void
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
@@ -2322,7 +2322,9 @@ bool DiscoveryDataBase::from_json(
             }
 
             // Add Participant
-            writers_.insert(std::make_pair(guid_aux, dei));
+            auto wit = writers_.insert(std::make_pair(guid_aux, dei));
+            // wit is only used in log message below, so it's potentially unused. 
+            static_cast<void>(wit);
 
             // Extra configurations for writers
             // Add writer to writers_by_topic. This will create the topic if necessary
