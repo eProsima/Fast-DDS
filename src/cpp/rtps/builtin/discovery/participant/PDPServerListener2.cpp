eb5e69dfc82ee2e5dabac7817ec0a298d0eb6749
RaulSanchez <raul@eprosima.com>
2020-11-06 13:05:54 +0100

    Fixed issue uncrustify.
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- src/cpp/rtps/builtin/discovery/participant/PDPServerListener2.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPServerListener2.cpp
@@ -100,7 +100,9 @@ void PDPServerListener2::onNewCacheChangeAdded(
     // Related_sample_identity could be lost in message delivered, so we set as sample_identity
     // An empty related_sample_identity could lead into an empty sample_identity when resending this msg
     if (change->write_params.related_sample_identity() == SampleIdentity::unknown())
-    {:
+    {
+        change->write_params.related_sample_identity(change->write_params.sample_identity());
+    }
 
 
     // DATA(p) case
