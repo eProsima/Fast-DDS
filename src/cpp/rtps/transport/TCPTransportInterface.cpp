1b97e965e6502b803363dd544a5aa95904e4f5c4
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2020-11-23 15:09:31 +0100

    Refs #9957: fix uncrustify
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- src/cpp/rtps/transport/TCPTransportInterface.cpp
+++ src/cpp/rtps/transport/TCPTransportInterface.cpp
@@ -1231,7 +1231,7 @@ void TCPTransportInterface::SecureSocketAccepted(
             logInfo(RTCP, " Accepted connection (local: " << IPLocator::to_string(locator)
                                                           << ", remote: " << socket->lowest_layer().remote_endpoint().address()
                                                           << ":" << socket->lowest_layer().remote_endpoint().port() <<
-                                ")");
+                    ")");
         }
         else
         {
