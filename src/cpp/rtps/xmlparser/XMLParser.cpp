3e95167e403ce0b929d259ca3cc475c6749adc18
Ignacio Montesino <ignaciomontesino@eprosima.com>
2020-12-03 16:24:10 +0100

    Refs #9965: Revert copyright update
    Signed-off-by: Ignacio Montesino <ignaciomontesino@eprosima.com>

--- src/cpp/rtps/xmlparser/XMLParser.cpp
+++ src/cpp/rtps/xmlparser/XMLParser.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+// Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
