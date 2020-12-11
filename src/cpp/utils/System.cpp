ededff5a01984be7e7eea36e2e61b9b00f06e40a
Miguel Company <MiguelCompany@eprosima.com>
2020-12-11 10:35:40 +0100

    Refs 10123. Using SystemInfo on System.cpp.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/utils/System.cpp
+++ src/cpp/utils/System.cpp
@@ -1,24 +1,32 @@
+// Copyright 2018 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file System.cpp
+ */
+
 #include <fastrtps/utils/System.h>
 
-#if defined(_WIN32)
-#include <process.h>
-#else
-#include <unistd.h>
-#endif
+#include <utils/SystemInfo.hpp>
 
 namespace eprosima {
 namespace fastrtps {
 
 int System::GetPID()
 {
-#if defined(__cplusplus_winrt)
-    return (int)GetCurrentProcessId();
-#elif defined(_WIN32)
-    return (int)_getpid();
-#else
-    return (int)getpid();
-#endif
+    return SystemInfo::get().process_id();
 }
 
-}
-}
+} // namespace fastrtps
+} // namespace eprosima
