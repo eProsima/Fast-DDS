86257f446c8fa09db92188ae6c1de38731c28971
Iker Luengo <ikerluengo@eprosima.com>
2020-12-17 16:59:40 +0100

    Refs 9919. Check sequence numbers for gaps
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/rtps/DataSharing/ReaderPool.hpp
+++ src/cpp/rtps/DataSharing/ReaderPool.hpp
@@ -38,6 +38,7 @@ public:
     ReaderPool (
             bool is_volatile)
         : is_volatile_(is_volatile)
+        , last_sn_(c_SequenceNumber_Unknown)
     {
     }
 
@@ -131,9 +132,12 @@ public:
         return true;
     }
 
-bool get_next_unread_payload(
-        CacheChange_t& cache_change)
+void get_next_unread_payload(
+        CacheChange_t& cache_change,
+        SequenceNumber_t& last_sequence_number)
 {
+    last_sequence_number = last_sn_;
+
     while (next_payload_ != end())
     {
         // history_[next_payload_] contains the offset to the payload
@@ -145,7 +149,22 @@ bool get_next_unread_payload(
         {
             // Reset by the writer. Discard and continue
             next_payload_ = advance(next_payload_);
+            if (last_sn_ != c_SequenceNumber_Unknown)
+            {
+                ++last_sn_;
+            }
+            logWarning(RTPS_READER, "Dirty data detected on datasharing writer " << writer());
             continue;
+        }
+
+        if (last_sn_ != c_SequenceNumber_Unknown && cache_change.sequenceNumber != last_sn_ + 1)
+        {
+            ++last_sn_;
+            logWarning(RTPS_READER, "GAP for SN " << last_sn_ << " detected on datasharing writer " << writer());
+        }
+        else
+        {
+            last_sn_ = cache_change.sequenceNumber;
         }
 
         cache_change.serializedPayload.data = payload->data();
@@ -164,21 +183,28 @@ bool get_next_unread_payload(
         {
             // Overriden while retrieving. Discard and continue
             next_payload_ = advance(next_payload_);
+            ++last_sn_;
+            logWarning(RTPS_READER, "Dirty data detected on datasharing writer " << writer());
             continue;
         }
 
         next_payload_ = advance(next_payload_);
-        return true;
+        return;
     }
 
-    return false;
+    cache_change.sequenceNumber = c_SequenceNumber_Unknown;
 }
 
+const SequenceNumber_t& get_last_read_sequence_number()
+{
+    return last_sn_;
+}
 
 private:
 
-    bool is_volatile_;       //< Whether the reader is volatile or not
-    uint32_t next_payload_;  //< Index of the next history position to read
+    bool is_volatile_;          //< Whether the reader is volatile or not
+    uint32_t next_payload_;     //< Index of the next history position to read
+    SequenceNumber_t last_sn_;  //< Sequence number of the last read payload
 };
 
 }  // namespace rtps
