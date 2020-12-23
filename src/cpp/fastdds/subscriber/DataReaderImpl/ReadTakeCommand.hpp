7eea7d2cc7fe01f6caf34a6f20ac3ce21918c2fd
Miguel Company <MiguelCompany@eprosima.com>
2020-12-23 11:49:12 +0100

    Refs 10180. Basic implementation of ReadTakeCommand.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/fastdds/subscriber/DataReaderImpl/ReadTakeCommand.hpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl/ReadTakeCommand.hpp
@@ -19,16 +19,21 @@
 #ifndef _FASTDDS_SUBSCRIBER_DATAREADERIMPL_READTAKECOMMAND_HPP_
 #define _FASTDDS_SUBSCRIBER_DATAREADERIMPL_READTAKECOMMAND_HPP_
 
+#include <cassert>
 #include <cstdint>
 
 #include <fastdds/dds/core/LoanableCollection.hpp>
+#include <fastdds/dds/core/LoanableSequence.hpp>
 #include <fastdds/dds/topic/TypeSupport.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
 
 #include <fastrtps/subscriber/SubscriberHistory.h>
 #include <fastrtps/types/TypesBase.h>
 
 #include <fastdds/subscriber/DataReaderImpl/StateFilter.hpp>
 #include <fastdds/subscriber/DataReaderImpl/SampleInfoPool.hpp>
+
+#include <fastdds/rtps/common/CacheChange.h>
 
 namespace eprosima {
 namespace fastdds {
@@ -39,22 +44,84 @@ struct ReadTakeCommand
 {
     using ReturnCode_t = eprosima::fastrtps::types::ReturnCode_t;
     using history_type = eprosima::fastrtps::SubscriberHistory;
+    using CacheChange_t = eprosima::fastrtps::rtps::CacheChange_t;
+    using SampleInfoSeq = LoanableSequence<SampleInfo>;
 
     ReadTakeCommand(
             history_type& history,
             SampleInfoPool& info_pool,
             LoanableCollection& data_values,
-            LoanableCollection& sample_infos,
+            SampleInfoSeq& sample_infos,
             int32_t max_samples,
             const StateFilter& states,
             history_type::instance_iterator instance,
             bool single_instance = false)
+        : history_(history)
+        , info_pool_(info_pool)
+        , data_values_(data_values)
+        , sample_infos_(sample_infos)
+        , remaining_samples_(max_samples)
+        , states_(states)
+        , instance_(instance)
+        , handle_(instance->first)
+        , single_instance_(single_instance)
     {
+        assert(0 <= remaining_samples_);
+
+        current_slot_ = data_values_.length();
     }
 
     void add_instance(
             bool take_samples)
     {
+        // Advance to the first instance with a valid state
+        if (!go_to_first_valid_instance())
+        {
+            return;
+        }
+
+        // Traverse changes on current instance
+        LoanableCollection::size_type first_slot = current_slot_;
+        auto it = instance_->second.cache_changes.begin();
+        while (!finished_ && it != instance_->second.cache_changes.end())
+        {
+            CacheChange_t* change = *it;
+            SampleStateKind check;
+            check = change->isRead ? SampleStateKind::READ_SAMPLE_STATE : SampleStateKind::NOT_READ_SAMPLE_STATE;
+            if ((check & states_.sample_states) != 0)
+            {
+                // Add sample and info to collections
+                if (add_sample(*it))
+                {
+                    if (take_samples)
+                    {
+                        // Remove from history
+                        history_.remove_change_sub(change);
+
+                        // Current iterator will point to change next to the one removed. Avoid incrementing.
+                        continue;
+                    }
+                }
+
+                // Go to next sample on instance
+                ++it;
+            }
+        }
+
+        if (current_slot_ > first_slot)
+        {
+            // complete sample infos
+            LoanableCollection::size_type slot = current_slot_;
+            LoanableCollection::size_type n = 0;
+            while (slot > first_slot)
+            {
+                --slot;
+                sample_infos_[slot].sample_rank = n;
+                ++n;
+            }
+        }
+
+        next_instance();
     }
 
     inline bool is_finished() const
@@ -69,8 +136,151 @@ struct ReadTakeCommand
 
 private:
 
+    history_type& history_;
+    SampleInfoPool& info_pool_;
+    LoanableCollection& data_values_;
+    SampleInfoSeq& sample_infos_;
+    int32_t remaining_samples_;
+    StateFilter states_;
+    history_type::instance_iterator instance_;
+    InstanceHandle_t handle_;
+    bool single_instance_;
+
     bool finished_ = false;
-    ReturnCode_t return_value_ = ReturnCode_t::RETCODE_OK;
+    ReturnCode_t return_value_ = ReturnCode_t::RETCODE_NO_DATA;
+
+    LoanableCollection::size_type current_slot_ = 0;
+
+    bool go_to_first_valid_instance()
+    {
+        while (!is_current_instance_valid())
+        {
+            if (!next_instance())
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool is_current_instance_valid()
+    {
+        // We are not implementing instance_state or view_state yet, so all instances will be considered to have
+        // a valid state. In the future this should check instance_state against states_.instance_states and
+        // view_state against states_.view_states
+        return true;
+    }
+
+    bool next_instance()
+    {
+        if (single_instance_)
+        {
+            finished_ = true;
+            return false;
+        }
+
+        auto result = history_.lookup_instance(handle_, false);
+        if (!result.first)
+        {
+            finished_ = true;
+            return false;
+        }
+
+        instance_ = result.second;
+        handle_ = instance_->first;
+        return true;
+    }
+
+    bool add_sample(
+            CacheChange_t* change)
+    {
+        // Mark that some data is available
+        return_value_ = ReturnCode_t::RETCODE_OK;
+        bool ret_val = false;
+
+        if (remaining_samples_ > 0)
+        {
+            // Increment length of collections
+            auto new_len = current_slot_ + 1;
+            data_values_.length(new_len);
+            sample_infos_.length(new_len);
+
+            // Add information
+            generate_info(change);
+            if (sample_infos_[current_slot_].valid_data)
+            {
+                deserialize_sample(change);
+            }
+
+            ++current_slot_;
+            --remaining_samples_;
+            ret_val = true;
+        }
+
+        // Finish when there are no remaining samples
+        finished_ = (remaining_samples_ == 0);
+        return ret_val;
+    }
+
+    void deserialize_sample(
+            CacheChange_t* change)
+    {
+        if (data_values_.has_ownership())
+        {
+            // perform deserialization
+        }
+        else
+        {
+            // loan
+            auto ptr = change->serializedPayload.data;
+            ptr += change->serializedPayload.representation_header_size;
+            const_cast<void**>(data_values_.buffer())[current_slot_] = ptr;
+        }
+    }
+
+    void generate_info(
+            CacheChange_t* change)
+    {
+        // Loan when necessary
+        if (!sample_infos_.has_ownership())
+        {
+            SampleInfo* item = info_pool_.get_item();
+            assert(item != nullptr);
+            const_cast<void**>(sample_infos_.buffer())[current_slot_] = item;
+        }
+
+        SampleInfo& info = sample_infos_[current_slot_];
+        info.sample_state = change->isRead ? READ_SAMPLE_STATE : NOT_READ_SAMPLE_STATE;
+        info.view_state = NOT_NEW_VIEW_STATE;
+        info.disposed_generation_count = 0;
+        info.no_writers_generation_count = 1;
+        info.sample_rank = 0;
+        info.generation_rank = 0;
+        info.absoulte_generation_rank = 0;
+        info.source_timestamp = change->sourceTimestamp;
+        info.instance_handle = handle_;
+        info.publication_handle = InstanceHandle_t(change->writerGUID);
+        info.sample_identity.writer_guid(change->writerGUID);
+        info.sample_identity.sequence_number(change->sequenceNumber);
+        info.related_sample_identity = change->write_params.sample_identity();
+        info.valid_data = true;
+
+        switch (change->kind)
+        {
+        case eprosima::fastrtps::rtps::ALIVE:
+            info.instance_state = ALIVE_INSTANCE_STATE;
+            break;
+        case eprosima::fastrtps::rtps::NOT_ALIVE_DISPOSED:
+        case eprosima::fastrtps::rtps::NOT_ALIVE_DISPOSED_UNREGISTERED:
+            info.instance_state = NOT_ALIVE_DISPOSED_INSTANCE_STATE;
+            info.valid_data = false;
+            break;
+        default:
+            //TODO [ILG] change this if the other kinds ever get implemented
+            info.instance_state = ALIVE_INSTANCE_STATE;
+            break;
+        }
+    }
 };
 
 } /* namespace detail */
