27e917b1fcfcf84f2b7339360a248bad39bcb708
Iker Luengo <ikerluengo@eprosima.com>
2021-01-18 09:32:46 +0100

    Refs 9919. PayloadPool data structures alignment
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/rtps/DataSharing/DataSharingPayloadPool.hpp
+++ src/cpp/rtps/DataSharing/DataSharingPayloadPool.hpp
@@ -121,7 +121,7 @@ protected:
 
 #pragma warning(push)
 #pragma warning(disable:4324)
-    class alignas (8) PayloadNode
+    class alignas (void*) PayloadNode
     {
 
         struct PayloadNodeMetaData
@@ -304,7 +304,7 @@ protected:
 
     };
 
-    struct alignas (8) PoolDescriptor
+    struct alignas (void*) PoolDescriptor
     {
         uint32_t history_size;          //< Number of payloads in the history
         uint64_t notified_begin;        //< The index of the oldest history entry already notified (ready to read)
