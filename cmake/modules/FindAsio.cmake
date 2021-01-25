217cb632974747be121455a2613e8fb9c086ee92
RaulSanchez <raul@eprosima.com>
2021-01-25 08:07:21 +0100

    Refs #10306: Manual setting of Asio_FOUND if find_path is used
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- cmake/modules/FindAsio.cmake
+++ cmake/modules/FindAsio.cmake
@@ -8,6 +8,8 @@ else()
     # If Asio was not found, search for the header file asio.hpp in the common CMake directories.
     if(NOT Asio_FOUND)
         find_path(Asio_INCLUDE_DIR NAMES asio.hpp)
+    else()
+        set(Asio_FOUND_PACKAGE ON)
     endif()
 
     # Not found a local version of Asio
@@ -34,6 +36,7 @@ else()
             # If THIRDPARTY_Asio=ON the Asio version from thirdparty is used.
             unset(Asio_INCLUDE_DIR)
             unset(Asio_FOUND)
+            set(Asio_FOUND_PACKAGE OFF)
             if(THIRDPARTY_Asio STREQUAL "ON")
                 find_path(Asio_INCLUDE_DIR NAMES asio.hpp NO_CMAKE_FIND_ROOT_PATH)
             # If THIRDPARTY_Asio=OFF the search is stopped and an error is shown
@@ -50,8 +53,9 @@ endif()
 # The fact that at this point Asio has not yet been found means that the package is not among the Fast DDS
 # submodules.
 # It could be updated later and retry the search for the package among the local dependencies.
-if(Asio_INCLUDE_DIR)
+if(Asio_INCLUDE_DIR AND (NOT Asio_FOUND_PACKAGE))
     include(FindPackageHandleStandardArgs)
+    # Asio is considered to be found if Asio_INCLUDE_DIR is valid.
     find_package_handle_standard_args(Asio DEFAULT_MSG Asio_INCLUDE_DIR)
     mark_as_advanced(Asio_INCLUDE_DIR)
     message(STATUS "Found Asio ${ASIO_VERSION}: ${Asio_INCLUDE_DIR}")
