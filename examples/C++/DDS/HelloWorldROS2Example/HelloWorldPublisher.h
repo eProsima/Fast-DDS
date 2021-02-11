6c12f1c4ab2166d0c4784217944bee70bf30ec72
laura-eprosima <laura@eprosima.com>
2021-02-11 12:31:44 +0100

    Add HelloWorldExample compatible with ROS2

---
+++ examples/C++/DDS/HelloWorldROS2Example/HelloWorldPublisher.h
@@ -0,0 +1,95 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldPublisher.h
+ *
+ */
+
+#ifndef HELLOWORLDPUBLISHER_H_
+#define HELLOWORLDPUBLISHER_H_
+
+#include "HelloWorldPubSubTypes.h"
+
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+
+class HelloWorldPublisher
+{
+public:
+
+    HelloWorldPublisher();
+
+    virtual ~HelloWorldPublisher();
+
+    //!Initialize
+    bool init();
+
+    //!Publish a sample
+    bool publish(
+            bool waitForListener = true);
+
+    //!Run for number samples
+    void run(
+            uint32_t number,
+            uint32_t sleep);
+
+private:
+
+    HelloWorld hello_;
+
+    eprosima::fastdds::dds::DomainParticipant* participant_;
+
+    eprosima::fastdds::dds::Publisher* publisher_;
+
+    eprosima::fastdds::dds::Topic* topic_;
+
+    eprosima::fastdds::dds::DataWriter* writer_;
+
+    bool stop_;
+
+    class PubListener : public eprosima::fastdds::dds::DataWriterListener
+    {
+public:
+
+        PubListener()
+            : matched_(0)
+            , firstConnected_(false)
+        {
+        }
+
+        ~PubListener() override
+        {
+        }
+
+        void on_publication_matched(
+                eprosima::fastdds::dds::DataWriter* writer,
+                const eprosima::fastdds::dds::PublicationMatchedStatus& info) override;
+
+        int matched_;
+
+        bool firstConnected_;
+    } listener_;
+
+    void runThread(
+            uint32_t number,
+            uint32_t sleep);
+
+    eprosima::fastdds::dds::TypeSupport type_;
+};
+
+
+
+#endif /* HELLOWORLDPUBLISHER_H_ */
---
+++ examples/C++/DDS/HelloWorldROS2Example/HelloWorldSubscriber.h
@@ -0,0 +1,89 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file HelloWorldSubscriber.h
+ *
+ */
+
+#ifndef HELLOWORLDSUBSCRIBER_H_
+#define HELLOWORLDSUBSCRIBER_H_
+
+#include "HelloWorldPubSubTypes.h"
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastrtps/subscriber/SampleInfo.h>
+#include <fastdds/dds/core/status/SubscriptionMatchedStatus.hpp>
+
+class HelloWorldSubscriber
+{
+public:
+
+    HelloWorldSubscriber();
+
+    virtual ~HelloWorldSubscriber();
+
+    //!Initialize the subscriber
+    bool init();
+
+    //!RUN the subscriber
+    void run();
+
+    //!Run the subscriber until number samples have been recevied.
+    void run(
+            uint32_t number);
+
+private:
+
+    eprosima::fastdds::dds::DomainParticipant* participant_;
+
+    eprosima::fastdds::dds::Subscriber* subscriber_;
+
+    eprosima::fastdds::dds::Topic* topic_;
+
+    eprosima::fastdds::dds::DataReader* reader_;
+
+    eprosima::fastdds::dds::TypeSupport type_;
+
+    class SubListener : public eprosima::fastdds::dds::DataReaderListener
+    {
+public:
+
+        SubListener()
+            : matched_(0)
+            , samples_(0)
+        {
+        }
+
+        ~SubListener() override
+        {
+        }
+
+        void on_data_available(
+                eprosima::fastdds::dds::DataReader* reader) override;
+
+        void on_subscription_matched(
+                eprosima::fastdds::dds::DataReader* reader,
+                const eprosima::fastdds::dds::SubscriptionMatchedStatus& info) override;
+
+        HelloWorld hello_;
+
+        int matched_;
+
+        uint32_t samples_;
+    } listener_;
+};
+
+#endif /* HELLOWORLDSUBSCRIBER_H_ */
