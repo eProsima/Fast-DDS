c78d3ffaa7190cc43e92d12e64bc0dd6f865e57b
Miguel Company <MiguelCompany@eprosima.com>
2021-02-22 17:03:38 +0100

    Refs 10657. Added NetworkBuffer struct.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---
+++ include/fastdds/rtps/network/NetworkBuffer.hpp
@@ -0,0 +1,44 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file NetworkBuffer.hpp
+ */
+
+#ifndef FASTDDS_RTPS_NETWORK_NETWORKBUFFER_HPP
+#define FASTDDS_RTPS_NETWORK_NETWORKBUFFER_HPP
+
+#include <cstdint>  // uint32_t
+#include <cstdlib>  // size_t
+
+namespace eprosima {
+namespace fastdds {
+namespace rtps {
+
+/**
+ * Mimicks C struct iovec
+ */
+struct NetworkBuffer final
+{
+    //! Pointer to the buffer where the data is stored.
+    const void* buffer = nullptr;
+    //! Number of bytes
+    uint32_t length = 0;
+};
+
+}  // namespace rtps
+}  // namespace fastdds
+}  // namespace eprosima
+
+#endif  // FASTDDS_RTPS_NETWORK_NETWORKBUFFER_HPP
