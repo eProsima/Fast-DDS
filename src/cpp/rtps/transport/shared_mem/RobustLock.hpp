f1cd8aae7e2fb0e85d4d58556da209d27c1e4147
Ahmed Sobhy <68698147+asobhy-qnx@users.noreply.github.com>
2021-02-23 01:59:59 -0500

    provide an alternative path for robustlock files under QNX (#1776)
    * provide an alternative path for robustlock files under QNX
    
    Signed-off-by: Ahmed Sobhy <asobhy@blackberry.com>
    
    * Uncrustify
    
    * Uncrustify
    
    * Uncrustify
    
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>

--- src/cpp/rtps/transport/shared_mem/RobustLock.hpp
+++ src/cpp/rtps/transport/shared_mem/RobustLock.hpp
@@ -18,6 +18,12 @@
 #include <boostconfig.hpp>
 #include <boost/interprocess/detail/shared_dir_helpers.hpp>
 
+#ifdef __QNXNTO__
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#endif  // __QNXNTO__
+
 namespace eprosima {
 namespace fastdds {
 namespace rtps {
@@ -41,20 +47,49 @@ public:
 
 #else
 
+
     static std::string get_file_path(
             const std::string& filename)
     {
+
+#ifdef __QNXNTO__
+        static const char defaultdir[] = "/var/lock";
+        struct stat buf;
+        // check directory status
+        if (stat(defaultdir, &buf) != 0)
+        {
+            // directory not found, create it
+            if (errno == ENOENT)
+            {
+                mkdir(defaultdir, 0777);
+            }
+            // if another error then throw exception
+            else
+            {
+                std::string err("get_file_path() ");
+                err = err + strerror(errno);
+                throw std::runtime_error(err);
+            }
+        }
+        else
+        {
+            // directory exists do nothing
+        }
+#else
         // Default value from: glibc-2.29/sysdeps/unix/sysv/linux/shm-directory.c
         static const char defaultdir[] = "/dev/shm/";
+#endif  // __QNXNTO__
 
         std::string filepath;
+
         #if defined(BOOST_INTERPROCESS_FILESYSTEM_BASED_POSIX_SHARED_MEMORY)
         const bool add_leading_slash = false;
         #elif defined(BOOST_INTERPROCESS_RUNTIME_FILESYSTEM_BASED_POSIX_SHARED_MEMORY)
         const bool add_leading_slash = !boost::interprocess::shared_memory_object_detail::use_filesystem_based_posix();
         #else
         const bool add_leading_slash = true;
-        #endif
+        #endif  // Platform dependent leading slash
+
         if (add_leading_slash)
         {
             boost::interprocess::ipcdetail::add_leading_slash(filename.c_str(), filepath);
@@ -63,11 +98,11 @@ public:
         {
             boost::interprocess::ipcdetail::shared_filepath(filename.c_str(), filepath);
         }
-
         return defaultdir + filepath;
     }
 
-#endif
+#endif  // !defined(BOOST_INTERPROCESS_POSIX_SHARED_MEMORY_OBJECTS)
+
 };
 
 } // namespace rtps
