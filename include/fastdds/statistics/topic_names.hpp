5a672028fdeeea4ea622c9f20f9c8e5e6b741f78
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2021-03-18 15:47:18 +0100

    Refs #10712: apply review suggestion concerning topic_names namespace
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- include/fastdds/statistics/topic_names.hpp
+++ include/fastdds/statistics/topic_names.hpp
@@ -22,7 +22,6 @@
 namespace eprosima {
 namespace fastdds {
 namespace statistics {
-namespace dds {
 
 //! Statistic topic that reports the write-to-notification latency between any two pairs of matched
 //! DataWriter-DataReader histories
@@ -61,7 +60,6 @@ constexpr const char* SAMPLE_DATAS_TOPIC = "_fastdds_statistics_sample_datas";
 //! Statistics topic that reports the host, user and process where the module is running
 constexpr const char* PHYSICAL_DATA_TOPIC = "_fastdds_statistics_physical_data";
 
-} // dds
 } // statistics
 } // fastdds
 } // eprosima
