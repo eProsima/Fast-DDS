ed64c00b57ca2f13aba08e34e4ae64596762b88c
Miguel Company <MiguelCompany@eprosima.com>
2021-03-23 09:13:17 +0100

    Refs 10955. Fix issue.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/fastdds/core/policy/QosPoliciesSerializer.hpp
+++ src/cpp/fastdds/core/policy/QosPoliciesSerializer.hpp
@@ -930,17 +930,17 @@ inline bool QosPoliciesSerializer<TypeIdV1>::read_content_from_cdr_message(
     eprosima::fastcdr::Cdr deser(fastbuffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN,
             eprosima::fastcdr::Cdr::DDS_CDR); // Object that deserializes the data.
 
-    // Deserialize encapsulation.
-    deser.read_encapsulation();
-    payload.encapsulation = deser.endianness() == eprosima::fastcdr::Cdr::BIG_ENDIANNESS ? CDR_BE : CDR_LE;
-
     try
     {
+        // Deserialize encapsulation.
+        deser.read_encapsulation();
+        payload.encapsulation = deser.endianness() == eprosima::fastcdr::Cdr::BIG_ENDIANNESS ? CDR_BE : CDR_LE;
+
         qos_policy.m_type_identifier.deserialize(deser);
     }
-    catch (eprosima::fastcdr::exception::NotEnoughMemoryException& /*exception*/)
+    catch (eprosima::fastcdr::exception::Exception& /*exception*/)
     {
-        return false;
+        qos_policy.clear();
     }
 
     return true;
@@ -992,17 +992,17 @@ inline bool QosPoliciesSerializer<TypeObjectV1>::read_content_from_cdr_message(
     eprosima::fastcdr::Cdr deser(fastbuffer, eprosima::fastcdr::Cdr::DEFAULT_ENDIAN,
             eprosima::fastcdr::Cdr::DDS_CDR); // Object that deserializes the data.
 
-    // Deserialize encapsulation.
-    deser.read_encapsulation();
-    payload.encapsulation = deser.endianness() == eprosima::fastcdr::Cdr::BIG_ENDIANNESS ? CDR_BE : CDR_LE;
-
     try
     {
+        // Deserialize encapsulation.
+        deser.read_encapsulation();
+        payload.encapsulation = deser.endianness() == eprosima::fastcdr::Cdr::BIG_ENDIANNESS ? CDR_BE : CDR_LE;
+
         qos_policy.m_type_object.deserialize(deser);
     }
-    catch (eprosima::fastcdr::exception::NotEnoughMemoryException& /*exception*/)
+    catch (eprosima::fastcdr::exception::Exception& /*exception*/)
     {
-        return false;
+        qos_policy.clear();
     }
 
     return true;
