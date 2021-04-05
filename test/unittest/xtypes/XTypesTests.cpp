93c046d313b650637ccba3a666909b0b3ae7d511
Yangbo Long <yangbo.long.mav@gmail.com>
2021-04-05 14:20:55 -0400

    Add some unit tests for type descriptor names
    Signed-off-by: Yangbo Long <yangbo.long.mav@gmail.com>

--- test/unittest/xtypes/XTypesTests.cpp
+++ test/unittest/xtypes/XTypesTests.cpp
@@ -18,6 +18,8 @@
 #include "idl/TypesTypeObject.h"
 #include "idl/WideEnumTypeObject.h"
 #include <gtest/gtest.h>
+#include <fastrtps/types/DynamicTypeBuilderFactory.h>
+#include <fastrtps/types/TypeDescriptor.h>
 
 using namespace eprosima::fastrtps;
 using namespace eprosima::fastrtps::types;
@@ -729,6 +731,55 @@ TEST_F(XTypesTests, SimpleUnionCompleteCoercion)
     consistencyQos.m_kind = DISALLOW_TYPE_COERCION;
     ASSERT_FALSE(basic_union->consistent(*basic_wide_union, consistencyQos));
     ASSERT_FALSE(basic_wide_union->consistent(*basic_union, consistencyQos));
+}
+
+TEST_F(XTypesTests, TypeDescriptorFullyQualifiedName)
+{
+    DynamicTypeBuilder_ptr my_builder(DynamicTypeBuilderFactory::get_instance()->create_struct_builder());
+    my_builder->add_member(0, "x", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+    my_builder->add_member(0, "y", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+    my_builder->add_member(0, "z", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+    const TypeDescriptor* my_descriptor = my_builder->get_type_descriptor();
+
+    my_builder->set_name("Position");
+    ASSERT_TRUE(my_descriptor->is_consistent());
+    my_builder->set_name("Position_");
+    ASSERT_TRUE(my_descriptor->is_consistent());
+    my_builder->set_name("Position123");
+    ASSERT_TRUE(my_descriptor->is_consistent());
+    my_builder->set_name("position_123");
+    ASSERT_TRUE(my_descriptor->is_consistent());
+    my_builder->set_name("_Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("123Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("Position&");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+
+    my_builder->set_name("my_interface::action::dds_::Position");
+    ASSERT_TRUE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface:action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface:::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("_my_interface::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("1my_interface::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name(":my_interface::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("::my_interface::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("$my_interface::action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface::2action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface::_action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface::*action::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
+    my_builder->set_name("my_interface::action*::dds_::Position");
+    ASSERT_FALSE(my_descriptor->is_consistent());
 }
 
 int main(int argc, char **argv)
