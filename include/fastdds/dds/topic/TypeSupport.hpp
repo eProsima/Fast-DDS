d05d0cf2a78b8a1588e9f2d712e200fde62554dd
Miguel Company <miguelcompany@eprosima.com>
2021-04-13 15:02:54 +0200

    Add missing operators to TypeSupport (#1912)
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- include/fastdds/dds/topic/TypeSupport.hpp
+++ include/fastdds/dds/topic/TypeSupport.hpp
@@ -57,25 +57,39 @@ public:
     using Base::operator ->;
     using Base::operator *;
     using Base::operator bool;
-    using Base::operator =;
 
     /**
      * @brief Constructor
      */
-    RTPS_DllAPI TypeSupport()
-        : std::shared_ptr<fastdds::dds::TopicDataType>(nullptr)
-    {
-    }
+    RTPS_DllAPI TypeSupport() noexcept = default;
 
     /**
      * @brief Copy Constructor
      * @param type Another instance of TypeSupport
      */
     RTPS_DllAPI TypeSupport(
-            const TypeSupport& type)
-        : std::shared_ptr<fastdds::dds::TopicDataType>(type)
-    {
-    }
+            const TypeSupport& type) noexcept = default;
+
+    /**
+     * @brief Move Constructor
+     * @param type Another instance of TypeSupport
+     */
+    RTPS_DllAPI TypeSupport(
+            TypeSupport&& type) noexcept = default;
+
+    /**
+     * @brief Copy Assignment
+     * @param type Another instance of TypeSupport
+     */
+    RTPS_DllAPI TypeSupport& operator = (
+            const TypeSupport& type) noexcept = default;
+
+    /**
+     * @brief Move Assignment
+     * @param type Another instance of TypeSupport
+     */
+    RTPS_DllAPI TypeSupport& operator = (
+            TypeSupport&& type) noexcept = default;
 
     /*!
      * \brief TypeSupport constructor that receives a TopicDataType pointer.
