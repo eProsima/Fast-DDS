1711b41dfa030137c955f704d1a6750a8d07721e
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2021-04-15 09:21:41 +0200

    Refs #10807: modify Statistics Blackbox to use all aliases (coverage transform_topic_name_alias)
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/blackbox/common/DDSBlackboxTestsStatisticsDomainParticipant.cpp
+++ test/blackbox/common/DDSBlackboxTestsStatisticsDomainParticipant.cpp
@@ -285,13 +285,21 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
         eprosima::fastdds::statistics::NETWORK_LATENCY_TOPIC, true, true);
     PubSubReader<Entity2LocatorTrafficPubSubTypeTest> rtps_lost_reader(
         eprosima::fastdds::statistics::RTPS_LOST_TOPIC, true, true);
+    PubSubReader<Entity2LocatorTrafficPubSubTypeTest> rtps_sent_reader(
+        eprosima::fastdds::statistics::RTPS_SENT_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> gap_count_reader(
+        eprosima::fastdds::statistics::GAP_COUNT_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> pdp_reader(
+        eprosima::fastdds::statistics::PDP_PACKETS_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> edp_reader(
+        eprosima::fastdds::statistics::EDP_PACKETS_TOPIC, true, true);
     PubSubReader<DiscoveryTimePubSubTypeTest> discovery_reader(
         eprosima::fastdds::statistics::DISCOVERY_TOPIC, true, true);
     PubSubReader<PhysicalDataPubSubTypeTest> physical_data_reader(
         eprosima::fastdds::statistics::PHYSICAL_DATA_TOPIC, true, true);
 
     // 1. Set environment variable and create participant using Qos set by code
-    const char* value = "HISTORY_LATENCY_TOPIC;NETWORK_LATENCY_TOPIC;RTPS_LOST_TOPIC";
+    const char* value = "HISTORY_LATENCY_TOPIC;NETWORK_LATENCY_TOPIC;RTPS_LOST_TOPIC;RTPS_SENT_TOPIC;GAP_COUNT_TOPIC";
 #ifdef _WIN32
     ASSERT_EQ(0, _putenv_s(eprosima::fastdds::statistics::dds::FASTDDS_STATISTICS_ENVIRONMENT_VARIABLE, value));
 #else
@@ -301,7 +309,7 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
     // There is no problem if some topic name is repeated.
     eprosima::fastdds::dds::DomainParticipantQos pqos;
     pqos.properties().properties().emplace_back("fastdds.statistics",
-            "HISTORY_LATENCY_TOPIC;DISCOVERY_TOPIC;PHYSICAL_DATA_TOPIC");
+            "HISTORY_LATENCY_TOPIC;DISCOVERY_TOPIC;PHYSICAL_DATA_TOPIC;PDP_PACKETS_TOPIC;EDP_PACKETS_TOPIC");
 
     eprosima::fastdds::dds::DomainParticipant* participant =
             eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->
@@ -319,6 +327,12 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
     EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RTPS_LOST_TOPIC));
     EXPECT_TRUE(rtps_traffic_type == participant->find_type(rtps_traffic_type.get_type_name()));
 
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RTPS_SENT_TOPIC));
+    EXPECT_TRUE(rtps_traffic_type == participant->find_type(rtps_traffic_type.get_type_name()));
+
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::GAP_COUNT_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
+
     // Check that the statistics topics and types related to the statistics DataWriters set with the PropertyPolicyQos
     // have been created correctly.
     EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::DISCOVERY_TOPIC));
@@ -326,6 +340,12 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
 
     EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::PHYSICAL_DATA_TOPIC));
     EXPECT_TRUE(physical_data_type == participant->find_type(physical_data_type.get_type_name()));
+
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::PDP_PACKETS_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
+
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::EDP_PACKETS_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
 
     // Before initializing the DataReaders the environment variable should be unset.
     // Otherwise each domainParticipant (each DataReader is launched in its own domainParticipant) will also enable
@@ -340,12 +360,20 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
     history_latency_reader.init();
     network_latency_reader.init();
     rtps_lost_reader.init();
+    rtps_sent_reader.init();
+    gap_count_reader.init();
+    pdp_reader.init();
+    edp_reader.init();
     discovery_reader.init();
     physical_data_reader.init();
 
     ASSERT_TRUE(history_latency_reader.isInitialized());
     ASSERT_TRUE(network_latency_reader.isInitialized());
     ASSERT_TRUE(rtps_lost_reader.isInitialized());
+    ASSERT_TRUE(rtps_sent_reader.isInitialized());
+    ASSERT_TRUE(gap_count_reader.isInitialized());
+    ASSERT_TRUE(pdp_reader.isInitialized());
+    ASSERT_TRUE(edp_reader.isInitialized());
     ASSERT_TRUE(discovery_reader.isInitialized());
     ASSERT_TRUE(physical_data_reader.isInitialized());
 
@@ -353,33 +381,36 @@ TEST(StatisticsDomainParticipant, CreateParticipant)
     history_latency_reader.wait_discovery();
     network_latency_reader.wait_discovery();
     rtps_lost_reader.wait_discovery();
+    rtps_sent_reader.wait_discovery();
+    gap_count_reader.wait_discovery();
+    pdp_reader.wait_discovery();
+    edp_reader.wait_discovery();
     discovery_reader.wait_discovery();
     physical_data_reader.wait_discovery();
 
     // 3. Check that those topics and types related to the statistics DataWriters not enabled does not exist.
     EXPECT_EQ(null_type, participant->find_type(throughput_type.get_type_name()));
-    EXPECT_EQ(null_type, participant->find_type(count_type.get_type_name()));
     EXPECT_EQ(null_type, participant->find_type(sample_identity_count_type.get_type_name()));
 
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(
                 eprosima::fastdds::statistics::PUBLICATION_THROUGHPUT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(
                 eprosima::fastdds::statistics::SUBSCRIPTION_THROUGHPUT_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RTPS_SENT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RESENT_DATAS_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::HEARTBEAT_COUNT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::ACKNACK_COUNT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::NACKFRAG_COUNT_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::GAP_COUNT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::DATA_COUNT_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::PDP_PACKETS_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::EDP_PACKETS_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::SAMPLE_DATAS_TOPIC));
 
     // 4. Remove DDS entities
     history_latency_reader.destroy();
     network_latency_reader.destroy();
     rtps_lost_reader.destroy();
+    rtps_sent_reader.destroy();
+    gap_count_reader.destroy();
+    pdp_reader.destroy();
+    edp_reader.destroy();
     discovery_reader.destroy();
     physical_data_reader.destroy();
 
@@ -428,15 +459,21 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
         eprosima::fastdds::statistics::PUBLICATION_THROUGHPUT_TOPIC, true, true);
     PubSubReader<EntityDataPubSubTypeTest> subscription_throughput_reader(
         eprosima::fastdds::statistics::SUBSCRIPTION_THROUGHPUT_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> resent_reader(
+        eprosima::fastdds::statistics::RESENT_DATAS_TOPIC, true, true);
     PubSubReader<EntityCountPubSubTypeTest> heartbeat_count_reader(
         eprosima::fastdds::statistics::HEARTBEAT_COUNT_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> acknack_reader(
+        eprosima::fastdds::statistics::ACKNACK_COUNT_TOPIC, true, true);
+    PubSubReader<EntityCountPubSubTypeTest> data_count_reader(
+        eprosima::fastdds::statistics::DATA_COUNT_TOPIC, true, true);
     PubSubReader<EntityCountPubSubTypeTest> nackfrag_count_reader(
         eprosima::fastdds::statistics::NACKFRAG_COUNT_TOPIC, true, true);
     PubSubReader<SampleIdentityCountPubSubTypeTest> sample_datas_reader(
         eprosima::fastdds::statistics::SAMPLE_DATAS_TOPIC, true, true);
 
     // 1. Set environment variable and create participant using Qos set by code
-    const char* value = "PUBLICATION_THROUGHPUT_TOPIC;HEARTBEAT_COUNT_TOPIC";
+    const char* value = "PUBLICATION_THROUGHPUT_TOPIC;HEARTBEAT_COUNT_TOPIC;RESENT_DATAS_TOPIC;ACKNACK_COUNT_TOPIC";
 #ifdef _WIN32
     ASSERT_EQ(0, _putenv_s(eprosima::fastdds::statistics::dds::FASTDDS_STATISTICS_ENVIRONMENT_VARIABLE, value));
 #else
@@ -460,6 +497,12 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
     EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::HEARTBEAT_COUNT_TOPIC));
     EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
 
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RESENT_DATAS_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
+
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::ACKNACK_COUNT_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
+
     // Check that the statistics topics and types related to the statistics DataWriters set with the PropertyPolicyQos
     // have been created correctly.
     EXPECT_NE(nullptr, participant->lookup_topicdescription(
@@ -471,6 +514,9 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
 
     EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::SAMPLE_DATAS_TOPIC));
     EXPECT_TRUE(sample_identity_count_type == participant->find_type(sample_identity_count_type.get_type_name()));
+
+    EXPECT_NE(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::DATA_COUNT_TOPIC));
+    EXPECT_TRUE(count_type == participant->find_type(count_type.get_type_name()));
 
     // Before initializing the DataReaders the environment variable should be unset.
     // Otherwise each domainParticipant (each DataReader is launched in its own domainParticipant) will also enable
@@ -484,20 +530,29 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
     // Check that the statistics DataWriters has been created by matching a corresponding DataReader on those topics
     publication_throughput_reader.init();
     subscription_throughput_reader.init();
+    resent_reader.init();
     heartbeat_count_reader.init();
+    acknack_reader.init();
+    data_count_reader.init();
     nackfrag_count_reader.init();
     sample_datas_reader.init();
 
     ASSERT_TRUE(publication_throughput_reader.isInitialized());
     ASSERT_TRUE(subscription_throughput_reader.isInitialized());
+    ASSERT_TRUE(resent_reader.isInitialized());
     ASSERT_TRUE(heartbeat_count_reader.isInitialized());
+    ASSERT_TRUE(acknack_reader.isInitialized());
+    ASSERT_TRUE(data_count_reader.isInitialized());
     ASSERT_TRUE(nackfrag_count_reader.isInitialized());
     ASSERT_TRUE(sample_datas_reader.isInitialized());
 
     // Wait for discovery. If there is discovery the test will pass. Otherwise, the test will timeout.
     publication_throughput_reader.wait_discovery();
     subscription_throughput_reader.wait_discovery();
+    resent_reader.wait_discovery();
     heartbeat_count_reader.wait_discovery();
+    acknack_reader.wait_discovery();
+    data_count_reader.wait_discovery();
     nackfrag_count_reader.wait_discovery();
     sample_datas_reader.wait_discovery();
 
@@ -512,10 +567,7 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::NETWORK_LATENCY_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RTPS_SENT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RTPS_LOST_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::RESENT_DATAS_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::ACKNACK_COUNT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::GAP_COUNT_TOPIC));
-    EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::DATA_COUNT_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::PDP_PACKETS_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::EDP_PACKETS_TOPIC));
     EXPECT_EQ(nullptr, participant->lookup_topicdescription(eprosima::fastdds::statistics::DISCOVERY_TOPIC));
@@ -524,7 +576,10 @@ TEST(StatisticsDomainParticipant, CreateParticipantUsingXML)
     // 4. Remove DDS entities
     publication_throughput_reader.destroy();
     subscription_throughput_reader.destroy();
+    resent_reader.destroy();
     heartbeat_count_reader.destroy();
+    acknack_reader.destroy();
+    data_count_reader.destroy();
     nackfrag_count_reader.destroy();
     sample_datas_reader.destroy();
 
