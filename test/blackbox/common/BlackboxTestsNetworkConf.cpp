ffcd9bdf25c57dae4b220926a49dd8efbeecad12
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2021-05-17 15:43:39 +0200

    Refs #11278: add Whitelist IPv6 tests to Blackbox Network Configuration Tests
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/blackbox/common/BlackboxTestsNetworkConf.cpp
+++ test/blackbox/common/BlackboxTestsNetworkConf.cpp
@@ -12,22 +12,27 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <memory>
+#include <string>
+
+#include <gtest/gtest.h>
 #include "BlackboxTests.hpp"
 #include "PubSubReader.hpp"
 #include "PubSubWriter.hpp"
 #include "PubSubParticipant.hpp"
 
+#include <fastdds/dds/log/Log.hpp>
+#include <fastdds/rtps/transport/UDPv6TransportDescriptor.h>
 #include <fastrtps/rtps/common/Locator.h>
 #include <fastrtps/utils/IPFinder.h>
 #include <rtps/transport/UDPv4Transport.h>
 
-#include <gtest/gtest.h>
 
-#include <memory>
 
 using namespace eprosima::fastrtps;
 using namespace eprosima::fastrtps::rtps;
 using UDPv4Transport = eprosima::fastdds::rtps::UDPv4Transport;
+using UDPv6TransportDescriptor = eprosima::fastdds::rtps::UDPv6TransportDescriptor;
 
 static void GetIP4s(
         std::vector<IPFinder::info_IP>& interfaces)
@@ -43,6 +48,23 @@ static void GetIP4s(
     std::for_each(interfaces.begin(), interfaces.end(), [](IPFinder::info_IP& loc)
             {
                 loc.locator.kind = LOCATOR_KIND_UDPv4;
+            });
+}
+
+static void GetIP6s(
+        std::vector<IPFinder::info_IP>& interfaces)
+{
+    IPFinder::getIPs(&interfaces, false);
+    auto new_end = remove_if(interfaces.begin(),
+                    interfaces.end(),
+                    [](IPFinder::info_IP ip)
+                    {
+                        return ip.type != IPFinder::IP6 && ip.type != IPFinder::IP6_LOCAL;
+                    });
+    interfaces.erase(new_end, interfaces.end());
+    std::for_each(interfaces.begin(), interfaces.end(), [](IPFinder::info_IP& loc)
+            {
+                loc.locator.kind = LOCATOR_KIND_UDPv6;
             });
 }
 
@@ -184,6 +206,42 @@ TEST(BlackBox, PubSubInterfaceWhitelistLocalhost)
     reader.block_for_all();
 }
 
+TEST(BlackBox, PubSubInterfaceWhitelistLocalhostIPv6)
+{
+    PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+
+    std::shared_ptr<UDPv6TransportDescriptor> descriptor = std::make_shared<UDPv6TransportDescriptor>();
+    descriptor->interfaceWhiteList.push_back("::1");
+
+    reader.reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).history_depth(10).
+            disable_multicast_ipv6(0).
+            disable_builtin_transport().
+            add_user_transport_to_pparams(descriptor).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+
+    writer.reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).history_depth(10).
+            disable_multicast_ipv6(1).
+            disable_builtin_transport().
+            add_user_transport_to_pparams(descriptor).init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    // Because its volatile the durability
+    // Wait for discovery.
+    writer.wait_discovery();
+    reader.wait_discovery();
+
+    auto data = default_helloworld_data_generator();
+
+    reader.startReception(data);
+
+    writer.send(data);
+    ASSERT_TRUE(data.empty());
+    reader.block_for_all();
+}
+
 TEST(BlackBox, PubSubInterfaceWhitelistUnicast)
 {
     PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
@@ -193,6 +251,46 @@ TEST(BlackBox, PubSubInterfaceWhitelistUnicast)
     GetIP4s(interfaces);
 
     std::shared_ptr<UDPv4TransportDescriptor> descriptor = std::make_shared<UDPv4TransportDescriptor>();
+    for (const auto& interface : interfaces)
+    {
+        descriptor->interfaceWhiteList.push_back(interface.name);
+    }
+
+    reader.reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).history_depth(10).
+            disable_builtin_transport().
+            add_user_transport_to_pparams(descriptor).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+
+    writer.reliability(eprosima::fastrtps::RELIABLE_RELIABILITY_QOS).history_depth(10).
+            disable_builtin_transport().
+            add_user_transport_to_pparams(descriptor).init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    // Because its volatile the durability
+    // Wait for discovery.
+    writer.wait_discovery();
+    reader.wait_discovery();
+
+    auto data = default_helloworld_data_generator();
+
+    reader.startReception(data);
+
+    writer.send(data);
+    ASSERT_TRUE(data.empty());
+    reader.block_for_all();
+}
+
+TEST(BlackBox, PubSubInterfaceWhitelistUnicastIPv6)
+{
+    PubSubReader<HelloWorldType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+
+    std::vector<IPFinder::info_IP> interfaces;
+    GetIP6s(interfaces);
+
+    std::shared_ptr<UDPv6TransportDescriptor> descriptor = std::make_shared<UDPv6TransportDescriptor>();
     for (const auto& interface : interfaces)
     {
         descriptor->interfaceWhiteList.push_back(interface.name);
@@ -316,6 +414,72 @@ TEST(BlackBox, PubGetSendingLocatorsWhitelist)
         for (size_t idx = 0; idx < interfaces.size(); ++idx)
         {
             if (interfaces[idx].name == locator_ip)
+            {
+                std::cout << "Found " << locator_ip << std::endl;
+                interfaces_found[idx] = true;
+                break;
+            }
+        }
+    }
+    EXPECT_TRUE(std::all_of(interfaces_found.cbegin(), interfaces_found.cend(),
+            [](const bool& v)
+            {
+                return v;
+            }));
+
+    for (size_t i = 0; i < interfaces.size(); ++i)
+    {
+        if (!interfaces_found[i])
+        {
+            std::cout << "Interface '" << interfaces[i].name << "' not found" << std::endl;
+        }
+    }
+}
+
+TEST(BlackBox, PubGetSendingLocatorsWhitelistIPv6)
+{
+    PubSubWriter<HelloWorldType> writer(TEST_TOPIC_NAME);
+
+    std::vector<IPFinder::info_IP> interfaces;
+    GetIP6s(interfaces);
+
+    constexpr uint32_t port = 31337u;
+
+    std::shared_ptr<UDPv6TransportDescriptor> descriptor = std::make_shared<UDPv6TransportDescriptor>();
+    descriptor->m_output_udp_socket = static_cast<uint16_t>(port);
+    for (const auto& interface : interfaces)
+    {
+        std::cout << "Adding interface '" << interface.name << "' (" << interface.name.size() << ")" << std::endl;
+        descriptor->interfaceWhiteList.push_back(interface.name);
+    }
+
+    writer.disable_builtin_transport().
+            add_user_transport_to_pparams(descriptor).
+            init();
+
+    ASSERT_TRUE(writer.isInitialized());
+
+    LocatorList_t locators;
+    writer.get_native_writer().get_sending_locators(locators);
+
+    std::vector<bool> interfaces_found(interfaces.size(), false);
+
+    EXPECT_EQ(interfaces.size(), locators.size());
+    for (const Locator_t& locator : locators)
+    {
+        std::cout << "Checking locator " << locator << std::endl;
+
+        EXPECT_EQ(locator.port, port);
+        EXPECT_EQ(locator.kind, LOCATOR_KIND_UDPv6);
+
+        auto locator_ip = IPLocator::ip_to_string(locator);
+        std::cout << "Checking '" << locator_ip << "' (" << locator_ip.size() << ")" << std::endl;
+        for (size_t idx = 0; idx < interfaces.size(); ++idx)
+        {
+            // Remove ipv6 scope address information to compare
+            std::string substr1 = interfaces[idx].name.substr(0, interfaces[idx].name.find('%'));
+            std::cout << substr1 << "\t" << locator_ip << std::endl;
+            if (substr1 == locator_ip)
             {
                 std::cout << "Found " << locator_ip << std::endl;
                 interfaces_found[idx] = true;
