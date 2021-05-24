1070c40c0154d78754fc1bf151ed2fc1375b9152
Federico Maggi <fede@maggi.cc>
2021-05-24 15:27:26 +0200

    Using MAX_SIZE as a #define in fuzz_processCDRMsg

--- src/cpp/rtps/messages/fuzz_processCDRMsg.cpp
+++ src/cpp/rtps/messages/fuzz_processCDRMsg.cpp
@@ -7,13 +7,19 @@
 #include <fastrtps/rtps/messages/MessageReceiver.h>
 #include <fastdds/rtps/attributes/RTPSParticipantAttributes.h>
 
+#define MIN_SIZE 256
+#define MAX_SIZE 64000
+
 extern "C" int LLVMFuzzerTestOneInput(
         const uint8_t* data,
         size_t size)
 {
+    if (size < MIN_SIZE || size > MAX_SIZE)
+        return 0;
+
     const eprosima::fastrtps::rtps::Locator_t remoteLocator;
     const eprosima::fastrtps::rtps::Locator_t recvLocator;
-    eprosima::fastrtps::rtps::MessageReceiver* rcv = new eprosima::fastrtps::rtps::MessageReceiver(NULL, 4096);
+    eprosima::fastrtps::rtps::MessageReceiver* rcv = new eprosima::fastrtps::rtps::MessageReceiver(NULL, MAX_SIZE);
 
     eprosima::fastrtps::rtps::CDRMessage_t msg(0);
     msg.wraps = true;
