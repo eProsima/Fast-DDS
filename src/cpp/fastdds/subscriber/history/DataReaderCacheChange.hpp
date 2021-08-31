9d2f2d69dfac6c5f47c2ce61e5a5c395ac1310e0
Miguel Company <MiguelCompany@eprosima.com>
2021-08-31 13:06:06 +0200

    Refs 12400. Added DataReaderCacheChange.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---
+++ src/cpp/fastdds/subscriber/history/DataReaderCacheChange.hpp
@@ -0,0 +1,47 @@
+// Copyright 2021 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file DataReaderCacheChange.hpp
+ */
+
+#ifndef _FASTDDS_SUBSCRIBER_HISTORY_DATAREADERCACHECHANGE_HPP_
+#define _FASTDDS_SUBSCRIBER_HISTORY_DATAREADERCACHECHANGE_HPP_
+
+#include <cstdint>
+
+#include <fastdds/rtps/common/CacheChange.h>
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+namespace detail {
+
+/// A DataReader cache entry
+struct DataReaderCacheChange
+{
+    //! Low level cache entry
+    fastrtps::rtps::CacheChange_t* change;
+    //! Disposed generation of the instance when this entry was added to it
+    int32_t disposed_generation_count;
+    //! No-writers generation of the instance when this entry was added to it
+    int32_t no_writers_generation_count;
+};
+
+} /* namespace detail */
+} /* namespace dds */
+} /* namespace fastdds */
+} /* namespace eprosima */
+
+#endif  // _FASTDDS_SUBSCRIBER_HISTORY_DATAREADERCACHECHANGE_HPP_
