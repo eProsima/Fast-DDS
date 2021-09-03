1d0343db94155ebecf57a1275f5931c7047a3bb5
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2021-09-03 12:09:48 +0200

    Refs #12199: avoid waiting and instead block until callback is executed
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/unittest/utils/SystemInfoTests.cpp
+++ test/unittest/utils/SystemInfoTests.cpp
@@ -12,9 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <atomic>
 #include <chrono>
+#include <condition_variable>
 #include <fstream>
 #include <functional>
+#include <mutex>
 #include <stdlib.h>
 #include <thread>
 
@@ -35,6 +38,39 @@
 
 using ReturnCode_t = eprosima::fastrtps::types::ReturnCode_t;
 
+class SystemInfoTests : public ::testing::Test
+{
+public:
+
+    void block_for_at_least_N_callbacks(
+            uint32_t amount)
+    {
+        std::unique_lock<std::mutex> lck(*mutex_);
+        cv_.wait(lck, [this, amount]
+                {
+                    return times_called_ >= amount;
+                });
+    }
+
+    mutable std::mutex* mutex_;
+    std::condition_variable cv_;
+    std::atomic_uint32_t times_called_;
+
+protected:
+
+    void SetUp() override
+    {
+        mutex_ = new std::mutex();
+        times_called_ = 0;
+    }
+
+    void TearDown() override
+    {
+        delete mutex_;
+        mutex_ = nullptr;
+    }
+};
+
 /*
  * This test checks the get_env static method of the SystemInfo class
  * 1. Set an environment variable
@@ -43,7 +79,7 @@ using ReturnCode_t = eprosima::fastrtps::types::ReturnCode_t;
  * 4. Call get_env with empty environment name
  * 5. Call get_env with an invalid environment variable
  */
-TEST(SystemInfoTests, GetEnvTest)
+TEST_F(SystemInfoTests, GetEnvTest)
 {
     const char* env_var_name = "TEST_ENVIRONMENT_VARIABLE";
     const char* value = "TESTING";
@@ -75,7 +111,7 @@ TEST(SystemInfoTests, GetEnvTest)
  * This test checks the get_username static method of the SystemInfo class
  * The test only checks that the method returns RETCODE_OK and that the username is not an empty string
  */
-TEST(SystemInfoTests, GetUsernameTest)
+TEST_F(SystemInfoTests, GetUsernameTest)
 {
     std::string username;
     EXPECT_EQ(ReturnCode_t::RETCODE_OK, eprosima::SystemInfo::get_username(username));
@@ -85,7 +121,7 @@ TEST(SystemInfoTests, GetUsernameTest)
 /**
  * This test checks the file_exists static method of the SystemInfo class
  */
-TEST(SystemInfoTests, FileExistsTest)
+TEST_F(SystemInfoTests, FileExistsTest)
 {
     // 1. Check that a valid file in the current directory exists
     std::string filename = "environment_test_file.json";
@@ -123,7 +159,7 @@ TEST(SystemInfoTests, FileExistsTest)
 /**
  * This test checks the load_environment_file method of the SystemInfo class
  */
-TEST(SystemInfoTests, LoadEnvironmentFileTest)
+TEST_F(SystemInfoTests, LoadEnvironmentFileTest)
 {
     // 1. Check that reading the environment variable ROS_DISCOVERY_SERVER returns the correct information
     std::string filename = "environment_test_file.json";
@@ -158,30 +194,21 @@ TEST(SystemInfoTests, LoadEnvironmentFileTest)
     EXPECT_TRUE(environment_value.empty());
 }
 
-struct FileWatchTestCallback
-{
-    void callback(
-            const std::string& /*path*/)
-    {
-        ++times_called;
-    }
-
-    int times_called = 0;
-};
-
 /**
  * This test checks the file watchers
  */
-TEST(SystemInfoTests, FileWatchTest)
+TEST_F(SystemInfoTests, FileWatchTest)
 {
     std::string filename = "environment_test_file.json";
     nlohmann::json file_content;
 
     // Create filewatch
-    FileWatchTestCallback callback;
-    using std::placeholders::_1;
     eprosima::FileWatchHandle watch =
-            eprosima::SystemInfo::watch_file(filename, std::bind(&FileWatchTestCallback::callback, &callback, _1));
+            eprosima::SystemInfo::watch_file(filename, [&](const std::string&)
+                    {
+                        ++times_called_;
+                        cv_.notify_one();
+                    });
 
     // Read contents
     {
@@ -198,8 +225,8 @@ TEST(SystemInfoTests, FileWatchTest)
     }
 
     // Check modifications.
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    int times_called = callback.times_called;
+    block_for_at_least_N_callbacks(1);
+    int times_called = times_called_;
 #if defined(_WIN32) || defined(__unix__)
     EXPECT_EQ(1, times_called);
 
@@ -220,7 +247,7 @@ TEST(SystemInfoTests, FileWatchTest)
 
     // Check no modifications were notified
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    EXPECT_EQ(times_called, callback.times_called);
+    EXPECT_EQ(times_called, times_called_);
 }
 
 int main(
