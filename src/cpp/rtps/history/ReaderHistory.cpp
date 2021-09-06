ddcd193f0e002777ffc9b5fcda0788d672730509
Miguel Company <miguelcompany@eprosima.com>
2021-09-06 14:49:02 +0200

    Keep changes inside instances sorted by source timestamp (#2182)
    * Refs 12419. Regression test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12419. Added sorted_vector_insert utility method.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12419. ReaderHistory uses sorted_vector_insert.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12419. SubscriberHistory uses sorted_vector_insert.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12421. Uncrustify.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12421. Apply suggestions
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 12421. Moved new template method to correct namespace.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    (cherry picked from commit 38e8d0fb7e1fdf5e90d03d3be33369dec87af8a2)
    
    # Conflicts:
    #	src/cpp/fastrtps_deprecated/subscriber/SubscriberHistory.cpp
    #	src/cpp/rtps/history/ReaderHistory.cpp

--- src/cpp/rtps/history/ReaderHistory.cpp
+++ src/cpp/rtps/history/ReaderHistory.cpp
@@ -24,6 +24,8 @@
 #include <fastdds/rtps/reader/RTPSReader.h>
 #include <fastdds/rtps/reader/ReaderListener.h>
 
+#include <utils/collections/sorted_vector_insert.hpp>
+
 #include <mutex>
 
 namespace eprosima {
@@ -71,6 +73,7 @@ bool ReaderHistory::add_change(
         logError(RTPS_READER_HISTORY, "The Writer GUID_t must be defined");
     }
 
+<<<<<<< HEAD
     if (!m_changes.empty() && a_change->sourceTimestamp < (*m_changes.rbegin())->sourceTimestamp)
     {
         auto it = std::lower_bound(m_changes.begin(), m_changes.end(), a_change,
@@ -85,6 +88,13 @@ bool ReaderHistory::add_change(
         m_changes.push_back(a_change);
     }
 
+=======
+    eprosima::utilities::collections::sorted_vector_insert(m_changes, a_change,
+            [](const CacheChange_t* lhs, const CacheChange_t* rhs)
+            {
+                return lhs->sourceTimestamp < rhs->sourceTimestamp;
+            });
+>>>>>>> 38e8d0fb7 (Keep changes inside instances sorted by source timestamp (#2182))
     logInfo(RTPS_READER_HISTORY,
             "Change " << a_change->sequenceNumber << " added with " << a_change->serializedPayload.length << " bytes");
 
