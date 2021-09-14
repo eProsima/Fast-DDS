87a4216513651ee20138fc770f699ff6def7f5aa
Miguel Company <miguelcompany@eprosima.com>
2021-09-14 08:05:24 +0200

    Correctly give priority to intraprocess over data sharing (#2200)
    Signed-off-by: Samuel Wilhelmsson <samuel@halodi.com>

--- src/cpp/rtps/reader/StatelessReader.cpp
+++ src/cpp/rtps/reader/StatelessReader.cpp
@@ -101,8 +101,8 @@ bool StatelessReader::matched_writer_add(
         }
     }
 
-    bool is_datasharing = is_datasharing_compatible_with(wdata);
     bool is_same_process = RTPSDomainImpl::should_intraprocess_between(m_guid, wdata.guid());
+    bool is_datasharing = !is_same_process && is_datasharing_compatible_with(wdata);
 
     RemoteWriterInfo_t info;
     info.guid = wdata.guid();
