e651c90d89022fd3474edf7b5b616ad98b20fd0a
Ricardo González Moreno <ricardo@richiware.dev>
2021-09-16 07:18:38 +0200

    Refs #12518. New statistics API for external participant
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/cpp/statistics/fastdds/domain/DomainParticipant.cpp
+++ src/cpp/statistics/fastdds/domain/DomainParticipant.cpp
@@ -60,6 +60,34 @@ fastdds::dds::ReturnCode_t DomainParticipant::enable_statistics_datawriter_with_
 #endif // FASTDDS_STATISTICS
 }
 
+fastdds::dds::ReturnCode_t DomainParticipant::enable_statistics_datawriter(
+        const std::string& topic_name,
+        const eprosima::fastdds::dds::DataWriterQos& dwqos,
+        eprosima::fastdds::dds::DataWriter*& writer)
+{
+#ifndef FASTDDS_STATISTICS
+    (void) topic_name;
+    (void) dwqos;
+    (void) writer;
+
+    return fastdds::dds::RETCODE_UNSUPPORTED;
+#else
+    return static_cast<DomainParticipantImpl*>(impl_)->enable_statistics_datawriter(topic_name, dwqos, writer);
+#endif // FASTDDS_STATISTICS
+}
+
+fastdds::dds::ReturnCode_t DomainParticipant::enable_statistics_datawriter(
+        eprosima::fastdds::dds::DataWriter* writer)
+{
+#ifndef FASTDDS_STATISTICS
+    (void) writer;
+
+    return fastdds::dds::RETCODE_UNSUPPORTED;
+#else
+    return static_cast<DomainParticipantImpl*>(impl_)->enable_statistics_datawriter(writer);
+#endif // FASTDDS_STATISTICS
+}
+
 fastdds::dds::ReturnCode_t DomainParticipant::disable_statistics_datawriter(
         const std::string& topic_name)
 {
--- src/cpp/statistics/fastdds/domain/DomainParticipantImpl.hpp
+++ src/cpp/statistics/fastdds/domain/DomainParticipantImpl.hpp
@@ -72,6 +72,15 @@ public:
             const std::string& topic_name,
             const efd::DataWriterQos& dwqos);
 
+    efd::ReturnCode_t enable_statistics_datawriter(
+            const std::string& topic_name,
+            const eprosima::fastdds::dds::DataWriterQos& dwqos,
+            efd::DataWriter*& writer,
+            bool specific_statistics_participant = true);
+
+    efd::ReturnCode_t enable_statistics_datawriter(
+            efd::DataWriter* writer);
+
     /**
      * @brief This operation enables a Statistics DataWriter from a provided XML defined profile
      *
