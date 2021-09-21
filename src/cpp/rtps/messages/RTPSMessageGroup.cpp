ff3b8c0b032ca74bc136691322b28636683b40ea
Ricardo González <ricardo@richiware.dev>
2021-09-21 10:13:22 +0200

    Refs #12161. Remove sending special RTPS submessage for statistics
    Signed-off-by: Ricardo González <ricardo@richiware.dev>

--- src/cpp/rtps/messages/RTPSMessageGroup.cpp
+++ src/cpp/rtps/messages/RTPSMessageGroup.cpp
@@ -308,7 +308,7 @@ void RTPSMessageGroup::send()
             }
 #endif // if HAVE_SECURITY
 
-            eprosima::fastdds::statistics::rtps::add_statistics_submessage(msgToSend);
+            //eprosima::fastdds::statistics::rtps::add_statistics_submessage(msgToSend);
 
             if (!sender_->send(msgToSend,
                     max_blocking_time_point_))
