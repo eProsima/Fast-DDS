7a1fe368bbe3145898936af1706f86f8301ff85b
Iker Luengo <ikerluengo@eprosima.com>
2021-09-22 14:58:55 +0200

    Refs 11914. Create a fake UI method for PKCS11 provider
    Otherwise, if no PIN is given on environment nor URI,
    the default behavior of the wrapper library is to prompt
    the user on the console... And we do not want that
    
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/security/artifact_providers/Pkcs11Provider.cpp
+++ src/cpp/security/artifact_providers/Pkcs11Provider.cpp
@@ -22,6 +22,7 @@
 
 #include <libp11.h>
 
+#include <fastdds/dds/log/Log.hpp>
 #include <utils/SystemInfo.hpp>
 
 #define S1(x) #x
@@ -39,11 +40,50 @@ namespace detail {
 constexpr const char* FASTDDS_PKCS11_PIN = "FASTDDS_PKCS11_PIN";
 constexpr const char* PKCS11_ENGINE_ID = "pkcs11";
 
+static int ui_open(UI *ui)
+{
+    return UI_method_get_opener(UI_OpenSSL())(ui);
+}
+
+static int ui_read(UI *ui, UI_STRING *uis)
+{
+    switch(UI_get_string_type(uis))
+    {
+        case UIT_PROMPT:
+        case UIT_VERIFY:
+        {
+                logWarning(PKCS11_PROVIDER, "PKCS#11 engine is asking: " << UI_get0_output_string(uis));
+                // Return an empty password without asking the user
+                UI_set_result(ui, uis, "");
+                return 1;
+        }
+        default:
+            break;
+    }
+
+    // Call the default method of the engine provider
+    return UI_method_get_reader(UI_OpenSSL())(ui, uis);
+}
+
+
+static int ui_close(UI *ui)
+{
+    return UI_method_get_closer(UI_OpenSSL())(ui);
+}
+
 Pkcs11Provider::Pkcs11Provider()
 {
     SSL_load_error_strings();                /* readable error messages */
     SSL_library_init();                      /* initialize library */
 
+    // Create an UI method to use with the engine
+    // This will be used to retrieve the PIN if none was given in the ENV nor in the URI
+    ui_method_ = UI_create_method("OpenSSL application user interface");
+    UI_method_set_opener(ui_method_, ui_open);
+    UI_method_set_reader(ui_method_, ui_read);
+    UI_method_set_closer(ui_method_, ui_close);
+
+    // Load the engine
     ENGINE_load_builtin_engines();
     pkcs11_ = ENGINE_by_id(PKCS11_ENGINE_ID);
     if (!pkcs11_)
@@ -52,10 +92,11 @@ Pkcs11Provider::Pkcs11Provider()
         initialization_exception_ = _SecurityException_(std::string("Error retrieving 'pkcs11' engine"));
     }
 
-    const char* pin;
-    if (ReturnCode_t::RETCODE_OK == SystemInfo::get_env(FASTDDS_PKCS11_PIN, &pin))
+    // Load the PIN from the environment
+    std::string pin;
+    if (ReturnCode_t::RETCODE_OK == SystemInfo::get_env(FASTDDS_PKCS11_PIN, pin))
     {
-        if (!ENGINE_ctrl_cmd_string( pkcs11_, "PIN", pin, 0))
+        if (!ENGINE_ctrl_cmd_string( pkcs11_, "PIN", pin.c_str(), 0))
         {
             has_initialization_error_ = true;
             initialization_exception_ = _SecurityException_(std::string("Error setting the PIN in the 'pkcs11' engine"));
@@ -63,6 +104,7 @@ Pkcs11Provider::Pkcs11Provider()
         }
     }
 
+    // Init the engine with the PIN (if any)
     if(!ENGINE_init(pkcs11_))
     {
         has_initialization_error_ = true;
@@ -75,6 +117,11 @@ Pkcs11Provider::~Pkcs11Provider()
 {
     ENGINE_finish(pkcs11_);
     ENGINE_free(pkcs11_);
+
+    if(ui_method_)
+    {
+        UI_destroy_method(ui_method_);
+    }
 }
 
 EVP_PKEY* Pkcs11Provider::load_private_key(
@@ -92,15 +139,13 @@ EVP_PKEY* Pkcs11Provider::load_private_key_impl(
         const std::string& /*password*/,
         SecurityException& exception)
 {
-    std::cerr << "We have PKCS11 key: " << pkey << std::endl;
-
     if (has_initialization_error_)
     {
         exception = initialization_exception_;
         return nullptr;
     }
 
-    EVP_PKEY* returnedValue = ENGINE_load_private_key(pkcs11_, pkey.c_str(), NULL, NULL);
+    EVP_PKEY* returnedValue = ENGINE_load_private_key(pkcs11_, pkey.c_str(), ui_method_, nullptr);
     if (!returnedValue)
     {
         exception = _SecurityException_(std::string("Error opening the private key ") + pkey.substr(7));
