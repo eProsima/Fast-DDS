42aafb7e407f40e77bfa681513e725a042fba693
Miguel Company <MiguelCompany@eprosima.com>
2021-11-10 13:27:59 +0100

    Refs 12540. Using can_change_be_added on StatefulReader::processDataMsg.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/reader/StatefulReader.cpp
+++ src/cpp/rtps/reader/StatefulReader.cpp
@@ -446,58 +446,63 @@ bool StatefulReader::processDataMsg(
             logInfo(RTPS_MSG_IN,
                     IDSTRING "Trying to add change " << change->sequenceNumber << " TO reader: " << getGuid().entityId);
 
-            // Ask the pool for a cache change
-            CacheChange_t* change_to_add = nullptr;
-            if (!change_pool_->reserve_cache(change_to_add))
+            size_t unknown_missing_changes_up_to = pWP ? pWP->unknown_missing_changes_up_to(change->sequenceNumber) : 0;
+            if (mp_history->can_change_be_added(change, unknown_missing_changes_up_to))
             {
-                logError(RTPS_MSG_IN, IDSTRING "Problem reserving CacheChange in reader: " << m_guid);
-                return false;
-            }
-
-            // Copy metadata to reserved change
-            change_to_add->copy_not_memcpy(change);
-
-            // Ask payload pool to copy the payload
-            IPayloadPool* payload_owner = change->payload_owner();
-
-            if (is_datasharing_compatible_ && datasharing_listener_->writer_is_matched(change->writerGUID))
-            {
-                // We may receive the change from the listener (with owner a ReaderPool) or intraprocess (with owner a WriterPool)
-                ReaderPool* datasharing_pool = dynamic_cast<ReaderPool*>(payload_owner);
-                if (!datasharing_pool)
+                // Ask the pool for a cache change
+                CacheChange_t* change_to_add = nullptr;
+                if (!change_pool_->reserve_cache(change_to_add))
                 {
-                    datasharing_pool = datasharing_listener_->get_pool_for_writer(change->writerGUID).get();
+                    logError(RTPS_MSG_IN, IDSTRING "Problem reserving CacheChange in reader: " << m_guid);
+                    return false;
                 }
-                if (!datasharing_pool)
+
+                // Copy metadata to reserved change
+                change_to_add->copy_not_memcpy(change);
+
+                // Ask payload pool to copy the payload
+                IPayloadPool* payload_owner = change->payload_owner();
+
+                if (is_datasharing_compatible_ && datasharing_listener_->writer_is_matched(change->writerGUID))
                 {
-                    logWarning(RTPS_MSG_IN, IDSTRING "Problem copying DataSharing CacheChange from writer "
-                            << change->writerGUID);
+                    // We may receive the change from the listener (with owner a ReaderPool) or intraprocess (with owner a WriterPool)
+                    ReaderPool* datasharing_pool = dynamic_cast<ReaderPool*>(payload_owner);
+                    if (!datasharing_pool)
+                    {
+                        datasharing_pool = datasharing_listener_->get_pool_for_writer(change->writerGUID).get();
+                    }
+                    if (!datasharing_pool)
+                    {
+                        logWarning(RTPS_MSG_IN, IDSTRING "Problem copying DataSharing CacheChange from writer "
+                                << change->writerGUID);
+                        change_pool_->release_cache(change_to_add);
+                        return false;
+                    }
+                    datasharing_pool->get_payload(change->serializedPayload, payload_owner, *change_to_add);
+                }
+                else if (payload_pool_->get_payload(change->serializedPayload, payload_owner, *change_to_add))
+                {
+                    change->payload_owner(payload_owner);
+                }
+                else
+                {
+                    logWarning(RTPS_MSG_IN, IDSTRING "Problem copying CacheChange, received data is: "
+                            << change->serializedPayload.length << " bytes and max size in reader "
+                            << m_guid << " is "
+                            << (fixed_payload_size_ > 0 ? fixed_payload_size_ : std::numeric_limits<uint32_t>::max()));
                     change_pool_->release_cache(change_to_add);
                     return false;
                 }
-                datasharing_pool->get_payload(change->serializedPayload, payload_owner, *change_to_add);
-            }
-            else if (payload_pool_->get_payload(change->serializedPayload, payload_owner, *change_to_add))
-            {
-                change->payload_owner(payload_owner);
-            }
-            else
-            {
-                logWarning(RTPS_MSG_IN, IDSTRING "Problem copying CacheChange, received data is: "
-                        << change->serializedPayload.length << " bytes and max size in reader "
-                        << m_guid << " is "
-                        << (fixed_payload_size_ > 0 ? fixed_payload_size_ : std::numeric_limits<uint32_t>::max()));
-                change_pool_->release_cache(change_to_add);
-                return false;
-            }
 
-            // Perform reception of cache change
-            if (!change_received(change_to_add, pWP))
-            {
-                logInfo(RTPS_MSG_IN, IDSTRING "Change " << change_to_add->sequenceNumber << " not added to history");
-                change_to_add->payload_owner()->release_payload(*change_to_add);
-                change_pool_->release_cache(change_to_add);
-                return false;
+                // Perform reception of cache change
+                if (!change_received(change_to_add, pWP))
+                {
+                    logInfo(RTPS_MSG_IN,
+                            IDSTRING "Change " << change_to_add->sequenceNumber << " not added to history");
+                    change_to_add->payload_owner()->release_payload(*change_to_add);
+                    change_pool_->release_cache(change_to_add);
+                    return false;
+                }
             }
         }
 
