03f329dad07ad7455a9c53e2d7e3de921aa47da0
jparisu <javierparis@eprosima.com>
2021-12-15 14:59:33 +0100

    uncrustify
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- examples/C++/DDS/HelloWorldExampleTCP/HelloWorld_main.cpp
+++ examples/C++/DDS/HelloWorldExampleTCP/HelloWorld_main.cpp
@@ -27,31 +27,48 @@
 
 #include <optionparser.h>
 
-struct Arg: public option::Arg
+struct Arg : public option::Arg
 {
-    static void print_error(const char* msg1, const option::Option& opt, const char* msg2)
+    static void print_error(
+            const char* msg1,
+            const option::Option& opt,
+            const char* msg2)
     {
         fprintf(stderr, "%s", msg1);
         fwrite(opt.name, opt.namelen, 1, stderr);
         fprintf(stderr, "%s", msg2);
     }
 
-    static option::ArgStatus Unknown(const option::Option& option, bool msg)
+    static option::ArgStatus Unknown(
+            const option::Option& option,
+            bool msg)
     {
-        if (msg) print_error("Unknown option '", option, "'\n");
+        if (msg)
+        {
+            print_error("Unknown option '", option, "'\n");
+        }
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus Required(const option::Option& option, bool msg)
+    static option::ArgStatus Required(
+            const option::Option& option,
+            bool msg)
     {
         if (option.arg != 0 && option.arg[0] != 0)
-        return option::ARG_OK;
+        {
+            return option::ARG_OK;
+        }
 
-        if (msg) print_error("Option '", option, "' requires an argument\n");
+        if (msg)
+        {
+            print_error("Option '", option, "' requires an argument\n");
+        }
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus Numeric(const option::Option& option, bool msg)
+    static option::ArgStatus Numeric(
+            const option::Option& option,
+            bool msg)
     {
         char* endptr = 0;
         if (option.arg != 0 && strtol(option.arg, &endptr, 10))
@@ -69,7 +86,9 @@ struct Arg: public option::Arg
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus String(const option::Option& option, bool msg)
+    static option::ArgStatus String(
+            const option::Option& option,
+            bool msg)
     {
         if (option.arg != 0)
         {
@@ -81,11 +100,13 @@ struct Arg: public option::Arg
         }
         return option::ARG_ILLEGAL;
     }
+
 };
 
 
 
-enum  optionIndex {
+enum  optionIndex
+{
     UNKNOWN_OPT,
     HELP,
     SAMPLES,
@@ -118,30 +139,30 @@ enum  optionIndex {
         std::cout << "The optional arguments are: subscriber [server_ip] [port] " << std::endl;
         std::cout << "\t- server_ip: IP Address of the publisher. " << std::endl;
         std::cout << "\t- port: Physical Port where the publisher is listening for connections." << std::endl << std::endl;
-*/
+ */
 
 const option::Descriptor usage[] = {
-    { UNKNOWN_OPT, 0,"", "",                Arg::None,
-        "Usage: HelloWorldExampleTCP <publisher|subscriber>\n\nGeneral options:" },
-    { HELP,    0,"h", "help",               Arg::None,      "  -h \t--help  \tProduce help message." },
+    { UNKNOWN_OPT, 0, "", "",                Arg::None,
+      "Usage: HelloWorldExampleTCP <publisher|subscriber>\n\nGeneral options:" },
+    { HELP,    0, "h", "help",               Arg::None,      "  -h \t--help  \tProduce help message." },
     { TLS, 0, "t", "tls",          Arg::None,      "  -t \t--tls \tUse TLS." },
     { WHITELIST, 0, "w", "whitelist",       Arg::String,    "  -w \t--whitelist \tUse Whitelist." },
 
-    { UNKNOWN_OPT, 0,"", "",                Arg::None,      "\nPublisher options:"},
-    { SAMPLES,0,"s","samples",              Arg::Numeric,
-        "  -s <num>, \t--samples=<num>  \tNumber of samples (0, default, infinite)." },
-    { INTERVAL,0,"i","interval",            Arg::Numeric,
-        "  -i <num>, \t--interval=<num>  \tTime between samples in milliseconds (Default: 100)." },
-    { IP,0,"a","address",                   Arg::String,
-        "  -a <address>, \t--address=<address> \tPublic IP Address of the publisher (Default: None)." },
+    { UNKNOWN_OPT, 0, "", "",                Arg::None,      "\nPublisher options:"},
+    { SAMPLES, 0, "s", "samples",              Arg::Numeric,
+      "  -s <num>, \t--samples=<num>  \tNumber of samples (0, default, infinite)." },
+    { INTERVAL, 0, "i", "interval",            Arg::Numeric,
+      "  -i <num>, \t--interval=<num>  \tTime between samples in milliseconds (Default: 100)." },
+    { IP, 0, "a", "address",                   Arg::String,
+      "  -a <address>, \t--address=<address> \tPublic IP Address of the publisher (Default: None)." },
     { PORT, 0, "p", "port",                 Arg::Numeric,
-        "  -p <num>, \t--port=<num>  \tPhysical Port to listening incoming connections (Default: 5100)." },
+      "  -p <num>, \t--port=<num>  \tPhysical Port to listening incoming connections (Default: 5100)." },
 
-    { UNKNOWN_OPT, 0,"", "",                Arg::None,      "\nSubscriber options:"},
-    { IP,0,"a","address",                   Arg::String,
-        "  -a <address>, \t--address=<address> \tIP Address of the publisher (Default: 127.0.0.1)." },
+    { UNKNOWN_OPT, 0, "", "",                Arg::None,      "\nSubscriber options:"},
+    { IP, 0, "a", "address",                   Arg::String,
+      "  -a <address>, \t--address=<address> \tIP Address of the publisher (Default: 127.0.0.1)." },
     { PORT, 0, "p", "port",                 Arg::Numeric,
-        "  -p <num>, \t--port=<num>  \tPhysical Port where the publisher is listening for connections (Default: 5100)." },
+      "  -p <num>, \t--port=<num>  \tPhysical Port where the publisher is listening for connections (Default: 5100)." },
 
     { 0, 0, 0, 0, 0, 0 }
 };
@@ -149,7 +170,9 @@ const option::Descriptor usage[] = {
 using namespace eprosima;
 using namespace fastrtps;
 using namespace rtps;
-int main(int argc, char** argv)
+int main(
+        int argc,
+        char** argv)
 {
     int columns;
 
@@ -167,7 +190,7 @@ int main(int argc, char** argv)
     }
 #else
     columns = getenv("COLUMNS") ? atoi(getenv("COLUMNS")) : 80;
-#endif
+#endif // if defined(_WIN32)
 
     std::cout << "Starting " << std::endl;
     int type = 1;
@@ -268,23 +291,23 @@ int main(int argc, char** argv)
     switch (type)
     {
         case 1:
+        {
+            HelloWorldPublisher mypub;
+            if (mypub.init(wan_ip, static_cast<uint16_t>(port), use_tls, whitelist))
             {
-                HelloWorldPublisher mypub;
-                if (mypub.init(wan_ip, static_cast<uint16_t>(port), use_tls, whitelist))
-                {
-                    mypub.run(count, sleep);
-                }
-                break;
+                mypub.run(count, sleep);
             }
+            break;
+        }
         case 2:
+        {
+            HelloWorldSubscriber mysub;
+            if (mysub.init(wan_ip, static_cast<uint16_t>(port), use_tls, whitelist))
             {
-                HelloWorldSubscriber mysub;
-                if (mysub.init(wan_ip, static_cast<uint16_t>(port), use_tls, whitelist))
-                {
-                    mysub.run();
-                }
-                break;
+                mysub.run();
             }
+            break;
+        }
     }
     Domain::stopAll();
     return 0;
--- test/performance/video/main_VideoTest.cpp
+++ test/performance/video/main_VideoTest.cpp
@@ -33,7 +33,7 @@
 #if defined(_MSC_VER)
 #pragma warning (push)
 #pragma warning (disable:4512)
-#endif
+#endif // if defined(_MSC_VER)
 
 using namespace eprosima::fastrtps;
 using namespace eprosima::fastrtps::rtps;
@@ -45,19 +45,24 @@ using std::endl;
 #define COPYSTR strcpy_s
 #else
 #define COPYSTR strcpy
-#endif
+#endif // if defined(_WIN32)
 
 
-struct Arg: public option::Arg
+struct Arg : public option::Arg
 {
-    static void printError(const char* msg1, const option::Option& opt, const char* msg2)
+    static void printError(
+            const char* msg1,
+            const option::Option& opt,
+            const char* msg2)
     {
         fprintf(stderr, "%s", msg1);
         fwrite(opt.name, opt.namelen, 1, stderr);
         fprintf(stderr, "%s", msg2);
     }
 
-    static option::ArgStatus Unknown(const option::Option& option, bool msg)
+    static option::ArgStatus Unknown(
+            const option::Option& option,
+            bool msg)
     {
         if (msg)
         {
@@ -66,7 +71,9 @@ struct Arg: public option::Arg
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus Required(const option::Option& option, bool msg)
+    static option::ArgStatus Required(
+            const option::Option& option,
+            bool msg)
     {
         if (option.arg != 0 && option.arg[0] != 0)
         {
@@ -80,7 +87,9 @@ struct Arg: public option::Arg
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus Numeric(const option::Option& option, bool msg)
+    static option::ArgStatus Numeric(
+            const option::Option& option,
+            bool msg)
     {
         char* endptr = 0;
         if (option.arg != 0 && strtol(option.arg, &endptr, 10))
@@ -98,7 +107,9 @@ struct Arg: public option::Arg
         return option::ARG_ILLEGAL;
     }
 
-    static option::ArgStatus String(const option::Option& option, bool msg)
+    static option::ArgStatus String(
+            const option::Option& option,
+            bool msg)
     {
         if (option.arg != 0)
         {
@@ -110,9 +121,11 @@ struct Arg: public option::Arg
         }
         return option::ARG_ILLEGAL;
     }
+
 };
 
-enum  optionIndex {
+enum  optionIndex
+{
     UNKNOWN_OPT,
     HELP,
     RELIABILITY,
@@ -136,12 +149,16 @@ enum  optionIndex {
 };
 
 const option::Descriptor usage[] = {
-    { UNKNOWN_OPT, 0,"", "",                    Arg::None,      "Usage: VideoTest <publisher|subscriber>\n\nGeneral options:" },
-    { HELP,    0,"h", "help",                   Arg::None,      "  -h \t--help  \tProduce help message." },
-    { RELIABILITY,0,"r","reliability",          Arg::Required,  "  -r <arg>, \t--reliability=<arg>  \tSet reliability (\"reliable\"/\"besteffort\")."},
-    { SAMPLES,0,"s","samples",                  Arg::Numeric,   "  -s <num>, \t--samples=<num>  \tNumber of samples." },
-    { SEED,0,"","seed",                         Arg::Numeric,   "  \t--seed=<num>  \tSeed to calculate domain and topic, to isolate test." },
-    { HOSTNAME,0,"","hostname",                 Arg::None,      "\t--hostname \tAppend hostname to the topic." },
+    { UNKNOWN_OPT, 0, "", "",                    Arg::None,
+      "Usage: VideoTest <publisher|subscriber>\n\nGeneral options:" },
+    { HELP,    0, "h", "help",                   Arg::None,      "  -h \t--help  \tProduce help message." },
+    { RELIABILITY, 0, "r", "reliability",          Arg::Required,
+      "  -r <arg>, \t--reliability=<arg>  \tSet reliability (\"reliable\"/\"besteffort\")."},
+    { SAMPLES, 0, "s", "samples",                  Arg::Numeric,
+      "  -s <num>, \t--samples=<num>  \tNumber of samples." },
+    { SEED, 0, "", "seed",                         Arg::Numeric,
+      "  \t--seed=<num>  \tSeed to calculate domain and topic, to isolate test." },
+    { HOSTNAME, 0, "", "hostname",                 Arg::None,      "\t--hostname \tAppend hostname to the topic." },
     { LARGE_DATA, 0, "l", "large",              Arg::None,      "  -l \t--large\tTest large data." },
     { XML_FILE, 0, "", "xml",                   Arg::String,    "\t--xml \tXML Configuration file." },
     { FORCED_DOMAIN, 0, "", "domain",           Arg::Numeric,   "\t--domain \tRTPS Domain." },
@@ -149,16 +166,24 @@ const option::Descriptor usage[] = {
     { HEIGHT, 0, "", "height",                  Arg::Numeric,   "\t--height \tHeight of the video." },
     { FRAMERATE, 0, "", "rate",                 Arg::Numeric,   "\t--rate \tFrame rate of the video." },
 #if HAVE_SECURITY
-    { USE_SECURITY, 0, "", "security",          Arg::Required,  "  --security <arg>  \tEcho mode (\"true\"/\"false\")." },
+    {
+        USE_SECURITY, 0, "", "security",          Arg::Required,
+        "  --security <arg>  \tEcho mode (\"true\"/\"false\")."
+    },
     { CERTS_PATH, 0, "", "certs",               Arg::Required,  "  --certs <arg>  \tPath where located certificates." },
-#endif
-    { UNKNOWN_OPT, 0,"", "",                    Arg::None,      "\nPublisher options:"},
-    { SUBSCRIBERS,0,"n","subscribers",          Arg::Numeric,   "  -n <num>,   \t--subscribers=<arg>  \tNumber of subscribers." },
+#endif // if HAVE_SECURITY
+    {
+        UNKNOWN_OPT, 0, "", "",                    Arg::None,      "\nPublisher options:"
+    },
+    { SUBSCRIBERS, 0, "n", "subscribers",          Arg::Numeric,
+      "  -n <num>,   \t--subscribers=<arg>  \tNumber of subscribers." },
     { TEST_TIME, 0, "", "testtime",             Arg::Numeric,   "\t--testtime \tTest duration time in seconds." },
     { DROP_RATE, 0, "", "droprate",             Arg::Numeric,   "\t--droprate \tSending drop percentage ( 0 - 100 )." },
-    { SEND_SLEEP_TIME, 0, "", "sleeptime",      Arg::Numeric,   "\t--sleeptime \tMaximum sleep time before shipments (milliseconds)." },
-    { EXPORT_CSV,0,"","export_csv",             Arg::None,      "\t--export_csv \tFlag to export a CSV file." },
-    { EXPORT_PREFIX,0,"","export_prefix",       Arg::String,    "\t--export_prefix \tFile prefix for the CSV file." },
+    { SEND_SLEEP_TIME, 0, "", "sleeptime",      Arg::Numeric,
+      "\t--sleeptime \tMaximum sleep time before shipments (milliseconds)." },
+    { EXPORT_CSV, 0, "", "export_csv",             Arg::None,      "\t--export_csv \tFlag to export a CSV file." },
+    { EXPORT_PREFIX, 0, "", "export_prefix",       Arg::String,
+      "\t--export_prefix \tFile prefix for the CSV file." },
     //{ UNKNOWN_OPT, 0,"", "",                    Arg::None,      "\nSubscriber options:"},
 
 
@@ -167,7 +192,9 @@ const option::Descriptor usage[] = {
 
 const int c_n_samples = 10000;
 
-int main(int argc, char** argv)
+int main(
+        int argc,
+        char** argv)
 {
     int columns;
 
@@ -187,7 +214,7 @@ int main(int argc, char** argv)
     }
 #else
     columns = getenv("COLUMNS") ? atoi(getenv("COLUMNS")) : 80;
-#endif
+#endif // if defined(_WIN32)
 
     bool pub_sub = false;
     int sub_number = 1;
@@ -202,7 +229,7 @@ int main(int argc, char** argv)
 #if HAVE_SECURITY
     bool use_security = false;
     std::string certs_path;
-#endif
+#endif // if HAVE_SECURITY
     bool reliable = false;
     uint32_t seed = 80;
     bool hostname = false;
@@ -364,7 +391,7 @@ int main(int argc, char** argv)
             case CERTS_PATH:
                 certs_path = opt.arg;
                 break;
-#endif
+#endif // if HAVE_SECURITY
 
             case UNKNOWN_OPT:
                 option::printUsage(fwrite, stdout, usage, columns);
@@ -374,7 +401,7 @@ int main(int argc, char** argv)
     }
 
     PropertyPolicy pub_part_property_policy, sub_part_property_policy,
-        pub_property_policy, sub_property_policy;
+            pub_property_policy, sub_property_policy;
 
 #if HAVE_SECURITY
     if (use_security)
@@ -386,34 +413,34 @@ int main(int argc, char** argv)
         }
 
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.plugin",
-            "builtin.PKI-DH"));
+                "builtin.PKI-DH"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_ca",
-            "file://" + certs_path + "/maincacert.pem"));
+                "file://" + certs_path + "/maincacert.pem"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_certificate",
-            "file://" + certs_path + "/mainsubcert.pem"));
+                "file://" + certs_path + "/mainsubcert.pem"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.private_key",
-            "file://" + certs_path + "/mainsubkey.pem"));
+                "file://" + certs_path + "/mainsubkey.pem"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.crypto.plugin",
-            "builtin.AES-GCM-GMAC"));
+                "builtin.AES-GCM-GMAC"));
         sub_part_property_policy.properties().emplace_back("rtps.participant.rtps_protection_kind", "ENCRYPT");
         sub_property_policy.properties().emplace_back("rtps.endpoint.submessage_protection_kind", "ENCRYPT");
         sub_property_policy.properties().emplace_back("rtps.endpoint.payload_protection_kind", "ENCRYPT");
 
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.plugin",
-            "builtin.PKI-DH"));
+                "builtin.PKI-DH"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_ca",
-            "file://" + certs_path + "/maincacert.pem"));
+                "file://" + certs_path + "/maincacert.pem"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_certificate",
-            "file://" + certs_path + "/mainpubcert.pem"));
+                "file://" + certs_path + "/mainpubcert.pem"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.private_key",
-            "file://" + certs_path + "/mainpubkey.pem"));
+                "file://" + certs_path + "/mainpubkey.pem"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.crypto.plugin",
-            "builtin.AES-GCM-GMAC"));
+                "builtin.AES-GCM-GMAC"));
         pub_part_property_policy.properties().emplace_back("rtps.participant.rtps_protection_kind", "ENCRYPT");
         pub_property_policy.properties().emplace_back("rtps.endpoint.submessage_protection_kind", "ENCRYPT");
         pub_property_policy.properties().emplace_back("rtps.endpoint.payload_protection_kind", "ENCRYPT");
     }
-#endif
+#endif // if HAVE_SECURITY
 
     // Load an XML file with predefined profiles for publisher and subscriber
     if (sXMLConfigFile.length() > 0)
@@ -429,14 +456,16 @@ int main(int argc, char** argv)
         cout << "Performing video test" << endl;
         VideoTestPublisher pub;
         pub.init(sub_number, n_samples, reliable, seed, hostname, pub_part_property_policy,
-            pub_property_policy, large_data, sXMLConfigFile, test_time, drop_rate, max_sleep_time, forced_domain, video_width, video_height, frame_rate);
+                pub_property_policy, large_data, sXMLConfigFile, test_time, drop_rate, max_sleep_time, forced_domain,
+                video_width, video_height, frame_rate);
         pub.run();
     }
     else
     {
         VideoTestSubscriber sub;
         sub.init(n_samples, reliable, seed, hostname, sub_part_property_policy, sub_property_policy,
-            large_data, sXMLConfigFile, export_csv, export_prefix, forced_domain, video_width, video_height, frame_rate);
+                large_data, sXMLConfigFile, export_csv, export_prefix, forced_domain, video_width, video_height,
+                frame_rate);
         sub.run();
     }
 
@@ -449,4 +478,4 @@ int main(int argc, char** argv)
 
 #if defined(_MSC_VER)
 #pragma warning (pop)
-#endif
+#endif // if defined(_MSC_VER)
