e70b41191e576655956646ec82443fc6283f0f58
Miguel Company <MiguelCompany@eprosima.com>
2022-01-04 16:08:04 +0100

    Refs 13364. Test with different lengths.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/blackbox/common/DDSBlackboxTestsSecurity.cpp
+++ test/blackbox/common/DDSBlackboxTestsSecurity.cpp
@@ -19,6 +19,7 @@
 #include <fastdds/dds/domain/DomainParticipant.hpp>
 #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
 #include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
 #include <fastdds/dds/publisher/Publisher.hpp>
 #include <fastdds/dds/subscriber/DataReader.hpp>
 #include <fastdds/dds/subscriber/Subscriber.hpp>
@@ -62,8 +63,36 @@ void set_participant_crypto_config(
     props.emplace_back(kRtpsProtectionKind, kProtectionKindValue);
 }
 
-TEST(DDSSecurity, big_message_corner_case)
+void test_big_message_corner_case(
+        const std::string& name,
+        uint32_t array_length)
 {
+    std::cout << "==== Checking with length = " << array_length << " ====" << std::endl;
+
+    struct WriterListener : public fastdds::DataWriterListener
+    {
+        WriterListener(
+                std::atomic_bool& cond_ref)
+            : cond_(cond_ref)
+        {
+            cond_ = false;
+        }
+
+        void on_publication_matched(
+                fastdds::DataWriter* writer,
+                const fastdds::PublicationMatchedStatus& info)
+        {
+            static_cast<void>(writer);
+            static_cast<void>(info);
+
+            cond_ = true;
+        }
+
+    private:
+
+        std::atomic_bool& cond_;
+    };
+
     auto qos{ fastdds::PARTICIPANT_QOS_DEFAULT };
     set_authentication_config(qos.properties().properties());
     set_participant_crypto_config(qos.properties().properties());
@@ -74,11 +103,11 @@ TEST(DDSSecurity, big_message_corner_case)
     auto participant = fastdds::DomainParticipantFactory::get_instance()->create_participant(0, qos);
     ASSERT_NE(nullptr, participant);
 
-    std::vector<uint32_t> lengths = { 65252 };
+    std::vector<uint32_t> lengths = { array_length };
     DynamicType_ptr base_type = DynamicTypeBuilderFactory::get_instance()->create_char8_type();
     DynamicTypeBuilder_ptr builder =
-        DynamicTypeBuilderFactory::get_instance()->create_array_builder(base_type, lengths);
-    builder->set_name(TEST_TOPIC_NAME);
+            DynamicTypeBuilderFactory::get_instance()->create_array_builder(base_type, lengths);
+    builder->set_name(name);
     DynamicType_ptr array_type = builder->build();
 
     fastdds::TypeSupport type_support(new eprosima::fastrtps::types::DynamicPubSubType(array_type));
@@ -86,7 +115,7 @@ TEST(DDSSecurity, big_message_corner_case)
     type_support.get()->auto_fill_type_object(false);
     EXPECT_EQ(ReturnCode_t::RETCODE_OK, participant->register_type(type_support));
 
-    auto topic = participant->create_topic(TEST_TOPIC_NAME, TEST_TOPIC_NAME, fastdds::TOPIC_QOS_DEFAULT);
+    auto topic = participant->create_topic(name, name, fastdds::TOPIC_QOS_DEFAULT);
     ASSERT_NE(nullptr, topic);
     auto subscriber = participant->create_subscriber(fastdds::SUBSCRIBER_QOS_DEFAULT);
     ASSERT_NE(nullptr, subscriber);
@@ -94,23 +123,57 @@ TEST(DDSSecurity, big_message_corner_case)
     ASSERT_NE(nullptr, reader);
     auto publisher = participant->create_publisher(fastdds::PUBLISHER_QOS_DEFAULT);
     ASSERT_NE(nullptr, publisher);
-    auto writer = publisher->create_datawriter(topic, fastdds::DATAWRITER_QOS_DEFAULT);
+
+    std::atomic_bool writer_matched{ false };
+    WriterListener wlistener(writer_matched);
+    auto writer = publisher->create_datawriter(topic, fastdds::DATAWRITER_QOS_DEFAULT, &wlistener,
+                    fastdds::StatusMask::publication_matched());
     ASSERT_NE(nullptr, writer);
 
-    // TODO: wait for discovery
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // Wait for discovery to occur
+    while (!writer_matched)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
     auto data = eprosima::fastrtps::types::DynamicDataFactory::get_instance()->create_data(array_type);
     EXPECT_EQ(ReturnCode_t::RETCODE_OK, writer->write(data, fastdds::HANDLE_NIL));
 
     fastdds::SampleInfo info{};
-    do
+    bool taken = false;
+    for (size_t n_tries = 0; n_tries < 6u; ++n_tries)
     {
+        if (ReturnCode_t::RETCODE_OK == reader->take_next_sample(data, &info))
+        {
+            taken = true;
+            break;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    } while (ReturnCode_t::RETCODE_OK != reader->take_next_sample(data, &info));
+    }
 
+    EXPECT_TRUE(taken);
     EXPECT_EQ(ReturnCode_t::RETCODE_OK, participant->delete_contained_entities());
     fastdds::DomainParticipantFactory::get_instance()->delete_participant(participant);
+}
+
+TEST(DDSSecurity, big_message_corner_case)
+{
+    const uint32_t array_lengths[] =
+    {
+        // Working case
+        65200,
+        // Failing cases
+        65240, 65252, 65260, 65300,
+        // Working case
+        65400
+    };
+
+    std::string topic_name = TEST_TOPIC_NAME;
+    for (uint32_t len : array_lengths)
+    {
+        topic_name += "_";
+        test_big_message_corner_case(topic_name, len);
+    }
 }
 
 #endif  // HAVE_SECURITY
