6f7bb41a3b1fbf066187927fec16574c46af3f9e
Miguel Company <MiguelCompany@eprosima.com>
2022-01-18 11:19:10 +0100

    Refs 12986. Added test case execution infrastructure.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/unittest/dds/topic/DDSSQLFilter/DDSSQLFilterTests.cpp
+++ test/unittest/dds/topic/DDSSQLFilter/DDSSQLFilterTests.cpp
@@ -12,18 +12,69 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <vector>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include "fastdds/topic/DDSSQLFilter/DDSFilterFactory.hpp"
 
+#include "fastdds/dds/core/StackAllocatedSequence.hpp"
+
+#include "data_types/ContentFilterTestTypePubSubTypes.h"
+
 namespace eprosima {
 namespace fastdds {
 namespace dds {
 
+using DDSFilterFactory = DDSSQLFilter::DDSFilterFactory;
+using ReturnCode_t = DDSFilterFactory::ReturnCode_t;
+
 class DDSSQLFilterTests : public testing::Test
 {
-    DDSSQLFilter::DDSFilterFactory uut;
+    DDSFilterFactory uut;
+    ContentFilterTestTypePubSubType type_support;
+
+protected:
+
+    struct TestCase
+    {
+        const char* expression = nullptr;
+        std::vector<const char*> parameters{};
+        ReturnCode_t result{};
+    };
+
+    void run(
+            const TestCase& test)
+    {
+        IContentFilter* filter_instance = nullptr;
+
+        StackAllocatedSequence<const char*, 10> params;
+        LoanableCollection::size_type n_params = static_cast<LoanableCollection::size_type>(test.parameters.size());
+        params.length(n_params);
+        for (LoanableCollection::size_type n = 0; n < n_params; ++n)
+        {
+            params[n] = test.parameters[n];
+        }
+
+        auto ret = uut.create_content_filter("DDSSQL", "ContentFilterTestType", &type_support,
+                        test.expression, params, filter_instance);
+        EXPECT_EQ(ret, test.result) << " failed for expression '" << test.expression << "'";
+        if (ret == ReturnCode_t::RETCODE_OK)
+        {
+            uut.delete_content_filter("DDSSQL", filter_instance);
+        }
+    }
+
+    void run(
+            const std::vector<TestCase>& test_cases)
+    {
+        for (const TestCase& tc : test_cases)
+        {
+            run(tc);
+        }
+    }
+
 };
 
 } // namespace dds
