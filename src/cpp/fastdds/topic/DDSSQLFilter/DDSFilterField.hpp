163f24e2fe07005a4d9c9ee6136aadad3f709d15
Miguel Company <MiguelCompany@eprosima.com>
2022-02-08 12:36:44 +0100

    Refs 13763. Direct members deserialization.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/fastdds/topic/DDSSQLFilter/DDSFilterField.hpp
+++ src/cpp/fastdds/topic/DDSSQLFilter/DDSFilterField.hpp
@@ -105,16 +105,31 @@ struct DDSFilterField final : public DDSFilterValue
         // TODO: Set value from payload
         static_cast<void>(data);
 
-        has_value_ = true;
-        value_has_changed();
-
-        // Inform parent predicates
-        for (DDSFilterPredicate* parent : parents_)
+        size_t num_steps = access_path_.size() - 1;
+        size_t n;
+        for (n = 0; n < num_steps; ++n)
         {
-            parent->value_has_changed(*this);
+            // TODO: Advance
         }
 
-        return true;
+        uint32_t index = static_cast<uint32_t>(access_path_[n].member_index);
+        auto member_id = data.get_member_id_at_index(index);
+        auto& member_type_id = type_object_->complete().struct_type().member_seq()[index].common().member_type_id();
+        
+        bool ret = set_value(&data, member_id, member_type_id);
+        if (ret)
+        {
+            has_value_ = true;
+            value_has_changed();
+
+            // Inform parent predicates
+            for (DDSFilterPredicate* parent : parents_)
+            {
+                parent->value_has_changed(*this);
+            }
+        }
+
+        return ret;
     }
 
 protected:
@@ -127,6 +142,85 @@ protected:
     }
 
 private:
+
+    bool set_value(
+        const eprosima::fastrtps::types::DynamicData* data,
+        eprosima::fastrtps::types::MemberId member_id,
+        const eprosima::fastrtps::types::TypeIdentifier& type_id)
+    {
+        using namespace eprosima::fastrtps::types;
+
+        bool ret = true;
+        try
+        {
+            switch (type_id._d())
+            {
+                case TK_BOOLEAN:
+                    boolean_value = data->get_bool_value(member_id);
+                    break;
+
+                case TK_CHAR8:
+                    char_value = data->get_char8_value(member_id);
+                    break;
+
+                case TK_STRING8:
+                case TI_STRING8_SMALL:
+                case TI_STRING8_LARGE:
+                    string_value = data->get_string_value(member_id);
+                    break;
+
+                case TK_INT16:
+                    signed_integer_value = data->get_int16_value(member_id);
+                    break;
+
+                case TK_INT32:
+                    signed_integer_value = data->get_int32_value(member_id);
+                    break;
+
+                case TK_INT64:
+                    signed_integer_value = data->get_int64_value(member_id);
+                    break;
+
+                case TK_BYTE:
+                    unsigned_integer_value = data->get_uint8_value(member_id);
+                    break;
+
+                case TK_UINT16:
+                    unsigned_integer_value = data->get_uint16_value(member_id);
+                    break;
+
+                case TK_UINT32:
+                    unsigned_integer_value = data->get_uint32_value(member_id);
+                    break;
+
+                case TK_UINT64:
+                    unsigned_integer_value = data->get_uint64_value(member_id);
+                    break;
+
+                case TK_FLOAT32:
+                    float_value = data->get_float32_value(member_id);
+                    break;
+
+                case TK_FLOAT64:
+                    float_value = data->get_float64_value(member_id);
+                    break;
+
+                case TK_FLOAT128:
+                    float_value = data->get_float128_value(member_id);
+                    break;
+
+                default:
+                    ret = false;
+                    break;
+            }
+        }
+        catch (...)
+        {
+            ret = false;
+        }
+
+        return ret;
+    }
 
     bool has_value_ = false;
     std::vector<FieldAccessor> access_path_;
