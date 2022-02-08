3ea8d7a0f089488abd9dcfc4a05bdf8ea883b834
Miguel Company <miguelcompany@eprosima.com>
2022-02-08 11:33:19 +0100

    Enable memory protection on DataSharing readers (#2405)
    * Refs 13457. DataSharing reader map memory as read only.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13457. Allow disabling non-const accessor on LoanableSequence.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13457. New macro FASTDDS_CONST_SEQUENCE.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13457. Use FASTDDS_CONST_SEQUENCE on zero copy example.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13457. Linters.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    (cherry picked from commit 6505a51ff5134498e8fcbfef34ceeda791290d34)

--- include/fastdds/dds/core/LoanableSequence.hpp
+++ include/fastdds/dds/core/LoanableSequence.hpp
@@ -22,6 +22,7 @@
 #include <cassert>
 #include <cstdint>
 #include <vector>
+#include <type_traits>
 
 #include <fastdds/dds/core/LoanableTypedCollection.hpp>
 #include <fastdds/dds/log/Log.hpp>
@@ -57,8 +58,8 @@ namespace dds {
  *         the sequence is loaning the buffer. The sequence should not be destroyed until the loan is returned.
  *     @li A sequence with a zero maximum always has has_ownership == true
  */
-template<typename T>
-class LoanableSequence : public LoanableTypedCollection<T>
+template<typename T, typename _NonConstEnabler = std::true_type>
+class LoanableSequence : public LoanableTypedCollection<T, _NonConstEnabler>
 {
 public:
 
@@ -226,5 +227,7 @@ private:
 
 // Macro to easily declare a LoanableSequence for a data type
 #define FASTDDS_SEQUENCE(FooSeq, Foo) using FooSeq = eprosima::fastdds::dds::LoanableSequence<Foo>
+#define FASTDDS_CONST_SEQUENCE(FooSeq, Foo) using FooSeq = eprosima::fastdds::dds::LoanableSequence<Foo, \
+                    std::false_type>
 
 #endif // _FASTDDS_DDS_CORE_LOANABLESEQUENCE_HPP_
