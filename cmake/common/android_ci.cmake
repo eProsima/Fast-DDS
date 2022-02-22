df607737d4f22d8e259d384d2bd04b46edd64c03
Javier Santiago <javiersantiago@eprosima.com>
2022-02-22 14:22:32 +0000

    Refs #13911: Initial Android testing commit
    Signed-off-by: Javier Santiago <javiersantiago@eprosima.com>

---
+++ cmake/common/android_ci.cmake
@@ -0,0 +1,18 @@
+###############################################################################
+# Android testing post-installation steps
+###############################################################################
+message(STATUS "Fixing CI Paths")
+message(STATUS "Vars:" ${ANDROID} " " ${ANDROID_TESTING_ROOT})
+if(ANDROID)
+    if(ANDROID_TESTING_ROOT)
+        file(GLOB_RECURSE CTEST_GENERATED_FILES RELATIVE ${CMAKE_BINARY_DIR} "CTestTestfile.cmake")
+        message(STATUS "Vars:" ${location} " " ${CTEST_GENERATED_FILES})
+        foreach(CTEST_FILE ${CTEST_GENERATED_FILES})
+            file(READ ${CTEST_FILE} contents)
+            message(STATUS "Vars:" ${CTEST_FILE} " " ${CMAKE_BINARY_DIR} " " ${ANDROID_TESTING_ROOT})
+            string(REGEX REPLACE "${CMAKE_BINARY_DIR}" "${ANDROID_TESTING_ROOT}" contents "${contents}")
+            file(REMOVE ${CTEST_FILE})
+            file(WRITE  ${CTEST_FILE} "${contents}")
+        endforeach()
+    endif()
+endif()
--- thirdparty/boost/include/boostconfig.hpp
+++ thirdparty/boost/include/boostconfig.hpp
@@ -26,6 +26,10 @@
 // it is more performant
 #define BOOST_INTERPROCESS_FORCE_NATIVE_EMULATION
 
+#ifdef ANDROID
+#define BOOST_INTERPROCESS_SHARED_DIR_PATH "/data/local/tmp"  
+#endif
+
 #ifdef _MSC_VER
 
 #include <stdlib.h>
