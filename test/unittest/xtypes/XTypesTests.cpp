681799592aa5701768630dfa0bea78b235c493e7
Miguel Company <miguelcompany@eprosima.com>
2022-02-22 14:28:35 +0100

    Allow fully qualified name on MemberDescriptor (#2518)
    * Refs 13938. Regression test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13938. Uncrustify.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13938. Fix issue.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    (cherry picked from commit 4a3f9194f3393721b1452284de325e0c4339933e)

--- test/unittest/xtypes/XTypesTests.cpp
+++ test/unittest/xtypes/XTypesTests.cpp
@@ -784,6 +784,55 @@ TEST_F(XTypesTests, TypeDescriptorFullyQualifiedName)
     ASSERT_FALSE(my_descriptor->is_consistent());
 }
 
+TEST_F(XTypesTests, MemberDescriptorFullyQualifiedName)
+{
+    MemberId member_id = 0;
+    DynamicTypeBuilder_ptr my_builder(DynamicTypeBuilderFactory::get_instance()->create_struct_builder());
+    my_builder->add_member(member_id++, "x", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+    my_builder->add_member(member_id++, "y", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+    my_builder->add_member(member_id, "z", DynamicTypeBuilderFactory::get_instance()->create_float32_type());
+
+    my_builder->set_name("Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK, my_builder->add_member(member_id++, "t1", my_builder->build()));
+    my_builder->set_name("Position_");
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK, my_builder->add_member(member_id++, "t2", my_builder->build()));
+    my_builder->set_name("Position123");
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK, my_builder->add_member(member_id++, "t3", my_builder->build()));
+    my_builder->set_name("position_123");
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK, my_builder->add_member(member_id++, "t4", my_builder->build()));
+    my_builder->set_name("_Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "t5", my_builder->build()));
+    my_builder->set_name("123Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "t6", my_builder->build()));
+    my_builder->set_name("Position&");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "t7", my_builder->build()));
+
+    my_builder->set_name("my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK, my_builder->add_member(member_id++, "t8", my_builder->build()));
+    my_builder->set_name("my_interface:action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "t9", my_builder->build()));
+    my_builder->set_name("my_interface:::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tA", my_builder->build()));
+    my_builder->set_name("_my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tB", my_builder->build()));
+    my_builder->set_name("1my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tC", my_builder->build()));
+    my_builder->set_name(":my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tD", my_builder->build()));
+    my_builder->set_name("::my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tE", my_builder->build()));
+    my_builder->set_name("$my_interface::action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tF", my_builder->build()));
+    my_builder->set_name("my_interface::2action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tG", my_builder->build()));
+    my_builder->set_name("my_interface::_action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tH", my_builder->build()));
+    my_builder->set_name("my_interface::*action::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tI", my_builder->build()));
+    my_builder->set_name("my_interface::action*::dds_::Position");
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER, my_builder->add_member(member_id++, "tJ", my_builder->build()));
+}
+
 int main(
         int argc,
         char** argv)
