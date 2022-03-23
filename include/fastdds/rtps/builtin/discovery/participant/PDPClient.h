7e3029856ea1da1d38c885795395699ead44fd87
RaulSanchez <raul@eprosima.com>
2022-03-23 14:41:41 +0100

    Refs #14221: Fix conficts backport #2246 to 2.1.x
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- include/fastdds/rtps/builtin/discovery/participant/PDPClient.h
+++ include/fastdds/rtps/builtin/discovery/participant/PDPClient.h
@@ -135,6 +135,29 @@ public:
      */
     bool match_servers_EDP_endpoints();
 
+    /*
+     * Update the list of remote servers
+     */
+    void update_remote_servers_list();
+
+protected:
+
+    /**
+     * Manually match the local PDP reader with the PDP writer of a given server. The function is
+     * not thread safe (nts) in the sense that it does not take the PDP mutex. It does however take
+     * temp_data_lock_
+     */
+    void match_pdp_writer_nts_(
+            const eprosima::fastdds::rtps::RemoteServerAttributes& server_att);
+
+    /**
+     * Manually match the local PDP writer with the PDP reader of a given server. The function is
+     * not thread safe (nts) in the sense that it does not take the PDP mutex. It does however take
+     * temp_data_lock_
+     */
+    void match_pdp_reader_nts_(
+            const eprosima::fastdds::rtps::RemoteServerAttributes& server_att);
+
 private:
 
     /**
