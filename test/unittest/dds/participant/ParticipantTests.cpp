a6a625807a4a9e9d1f5b114fd0f071c39a8e7ea0
José Luis Bueno López <69244257+JLBuenoLopez-eProsima@users.noreply.github.com>
2022-04-13 10:52:38 +0200

    Improve ParticipantTests with ContentFilteredTopic  (#2634)
    * Refs #14361: fix DeleteTopicInUse test
    
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
    
    * Refs #14361: fix DeleteContainedEntities test
    
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
    
    * Refs #14361: linters
    
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/unittest/dds/participant/ParticipantTests.cpp
+++ test/unittest/dds/participant/ParticipantTests.cpp
@@ -1894,6 +1894,10 @@ TEST(ParticipantTests, DeleteTopicInUse)
 
     Topic* topic = participant->create_topic("footopic", "footype", TOPIC_QOS_DEFAULT);
 
+    ContentFilteredTopic* content_filtered_topic = participant->create_contentfilteredtopic("contentfilteredtopic",
+                    topic, "", {});
+    ASSERT_NE(content_filtered_topic, nullptr);
+
     Subscriber* subscriber = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT);
     ASSERT_NE(subscriber, nullptr);
 
@@ -1903,6 +1907,8 @@ TEST(ParticipantTests, DeleteTopicInUse)
     ASSERT_EQ(participant->delete_topic(topic), ReturnCode_t::RETCODE_PRECONDITION_NOT_MET);
 
     ASSERT_EQ(subscriber->delete_datareader(data_reader), ReturnCode_t::RETCODE_OK);
+    ASSERT_EQ(participant->delete_topic(topic), ReturnCode_t::RETCODE_PRECONDITION_NOT_MET);
+    ASSERT_EQ(participant->delete_contentfilteredtopic(content_filtered_topic), ReturnCode_t::RETCODE_OK);
     ASSERT_EQ(participant->delete_topic(topic), ReturnCode_t::RETCODE_OK);
     ASSERT_EQ(participant->delete_subscriber(subscriber), ReturnCode_t::RETCODE_OK);
 
@@ -3015,6 +3021,10 @@ TEST(ParticipantTests, DeleteContainedEntities)
     Topic* topic_bar = participant->create_topic("bartopic", type.get_type_name(), TOPIC_QOS_DEFAULT);
     ASSERT_NE(topic_bar, nullptr);
 
+    ContentFilteredTopic* content_filtered_topic_bar = participant->create_contentfilteredtopic("contentfilteredtopic",
+                    topic_bar, "", {});
+    ASSERT_NE(content_filtered_topic_bar, nullptr);
+
     DataReader* data_reader_bar = subscriber->create_datareader(topic_bar, DATAREADER_QOS_DEFAULT);
     ASSERT_NE(data_reader_bar, nullptr);
 
@@ -3121,6 +3131,8 @@ TEST(ParticipantTests, DeleteContainedEntities)
     EXPECT_FALSE(participant->contains_entity(subscriber_handle));
     EXPECT_FALSE(participant->contains_entity(topic_foo_handle));
     EXPECT_FALSE(participant->contains_entity(topic_bar_handle));
+    // ContentFilteredTopic is not considered an entity
+    EXPECT_EQ(participant->lookup_topicdescription("contentfilteredtopic"), nullptr);
 
     ASSERT_EQ(retcode, ReturnCode_t::RETCODE_OK);
 
