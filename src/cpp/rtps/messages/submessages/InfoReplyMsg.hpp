21d5945e07427b8a9baadc3791159bc9cc590a1d
Miguel Company <MiguelCompany@eprosima.com>
2022-05-12 08:08:57 +0200

    Refs #14527. Implementation of addSubmessageInfoReplyV4.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/messages/submessages/InfoReplyMsg.hpp
+++ src/cpp/rtps/messages/submessages/InfoReplyMsg.hpp
@@ -21,12 +21,67 @@ namespace eprosima {
 namespace fastrtps {
 namespace rtps {
 
+static void add_locator_udp4(
+        CDRMessage_t* msg,
+        const Locator_t& locator)
+{
+    uint32_t address =
+        static_cast<uint32_t>(locator.address[3]) << 24 |
+        static_cast<uint32_t>(locator.address[2]) << 16 |
+        static_cast<uint32_t>(locator.address[1]) << 8 |
+        static_cast<uint32_t>(locator.address[0]);
+
+    CDRMessage::addUInt32(msg, address);
+    CDRMessage::addUInt32(msg, locator.port);
+}
+
 static bool addSubmessageInfoReplyV4(
         CDRMessage_t* msg,
         const Locator_t& unicast_locator,
         const Locator_t& multicast_locator)
 {
-    return false;
+    // Calculate total submessage size
+    bool has_multicast = false;
+    uint16_t submessage_size = 2 * sizeof(uint32_t);
+    if (LOCATOR_KIND_UDPv4 == multicast_locator.kind)
+    {
+        has_multicast = true;
+        submessage_size *= 2;
+    }
+
+    // Check if submessage fits into message
+    if (msg->pos + submessage_size + 4 > msg->max_size)
+    {
+        return false;
+    }
+
+    octet flags = 0x0;
+    Endianness_t old_endianess = msg->msg_endian;
+#if FASTDDS_IS_BIG_ENDIAN_TARGET
+    msg->msg_endian = BIGEND;
+#else
+    flags = flags | BIT(0);
+    msg->msg_endian = LITTLEEND;
+#endif // if FASTDDS_IS_BIG_ENDIAN_TARGET
+
+    if (has_multicast)
+    {
+        flags = flags | BIT(1);
+    }
+
+    // Submessage header.
+    CDRMessage::addOctet(msg, INFO_REPLY_IP4);
+    CDRMessage::addOctet(msg, flags);
+    CDRMessage::addUInt16(msg, submessage_size);
+
+    add_locator_udp4(msg, unicast_locator);
+    if (has_multicast)
+    {
+        add_locator_udp4(msg, multicast_locator);
+    }
+
+    msg->msg_endian = old_endianess;
+    return true;
 }
 
 bool RTPSMessageCreator::addSubmessageInfoReply(
