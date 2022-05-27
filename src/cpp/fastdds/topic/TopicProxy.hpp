099881914a165cc1eb8288c1d38a94bb21a35602
Miguel Company <MiguelCompany@eprosima.com>
2022-05-27 12:14:06 +0200

    Refs #14615. Added basic TopicProxy class.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---
+++ src/cpp/fastdds/topic/TopicProxy.hpp
@@ -0,0 +1,108 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ * TopicProxy.hpp
+ */
+
+#ifndef _FASTDDS_TOPICPROXY_HPP_
+#define _FASTDDS_TOPICPROXY_HPP_
+
+#include <fastdds/dds/core/status/StatusMask.hpp>
+#include <fastdds/dds/topic/Topic.hpp>
+#include <fastdds/dds/topic/TopicListener.hpp>
+#include <fastdds/dds/topic/qos/TopicQos.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+#include <fastrtps/types/TypesBase.h>
+
+#include <fastdds/topic/TopicDescriptionImpl.hpp>
+#include <fastdds/topic/TopicImpl.hpp>
+
+using eprosima::fastrtps::types::ReturnCode_t;
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+
+class DomainParticipant;
+
+class TopicProxy : public TopicDescriptionImpl
+{
+public:
+
+    explicit TopicProxy(
+            TopicImpl* impl) noexcept
+        : impl_(impl)
+    {
+    }
+
+    const TopicQos& get_qos() const
+    {
+        return impl_->get_qos();
+    }
+
+    ReturnCode_t set_qos(
+            const TopicQos& qos)
+    {
+        return impl_->set_qos(qos);
+    }
+
+    const TopicListener* get_listener() const
+    {
+        return impl_->get_listener();
+    }
+
+    ReturnCode_t set_listener(
+            TopicListener* listener)
+    {
+        return impl_->set_listener(listener);
+    }
+
+    DomainParticipant* get_participant() const
+    {
+        return impl_->get_participant();
+    }
+
+    const TypeSupport& get_type() const
+    {
+        return impl_->get_type();
+    }
+
+    TopicListener* get_listener_for(
+            const StatusMask& status)
+    {
+        return impl_->get_listener_for(status);
+    }
+
+    Topic* get_topic() const
+    {
+        return const_cast<Topic*>(impl_->get_topic());
+    }
+
+    const std::string& get_rtps_topic_name() const override
+    {
+        return get_topic()->get_name();
+    }
+
+private:
+
+    TopicImpl* impl_ = nullptr;
+};
+
+}  // namespace dds
+}  // namespace fastdds
+}  // namespace eprosima
+
+#endif /* _FASTDDS_TOPICPROXY_HPP_ */
