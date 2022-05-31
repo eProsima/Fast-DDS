d6dde52c07da3b37b6442226c9c4718e5941d3ec
Miguel Company <MiguelCompany@eprosima.com>
2022-05-31 10:12:50 +0200

    Refs #14615. TopicProxyFactory empty implementation.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---
+++ src/cpp/fastdds/topic/TopicProxyFactory.cpp
@@ -0,0 +1,47 @@
+// Copyright 2022 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+ * TopicProxyFactory.cpp
+ */
+
+#include <fastdds/topic/TopicProxyFactory.hpp>
+
+#include <fastdds/topic/TopicProxy.hpp>
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+
+TopicProxy* TopicProxyFactory::create_topic()
+{
+	return nullptr;
+}
+
+ReturnCode_t TopicProxyFactory::delete_topic(
+		TopicProxy* proxy)
+{
+	static_cast<void>(proxy);
+
+	return ReturnCode_t::RETCODE_UNSUPPORTED;
+}
+
+bool TopicProxyFactory::can_be_deleted()
+{
+	return true;
+}
+
+}  // namespace dds
+}  // namespace fastdds
+}  // namespace eprosima
