46c146a22c6968e151abdf15a1cca71dce09bac2
MiguelBarro <45819833+MiguelBarro@users.noreply.github.com>
2022-06-03 13:05:38 +0200

    [14467] Fixing event thread shutdown <feature/tsan/fixes> (#2660)
    * Refs #14285: Trigger workflow
    
    * Refs #14354: Trigger workflows
    
    Signed-off-by: Javier Santiago <javiersantiago@eprosima.com>
    
    * Refs #14069: Trigger workflow
    
    Signed-off-by: Javier Santiago <javiersantiago@eprosima.com>
    
    * Refs #14069: Workflow trigger
    
    Signed-off-by: Javier Santiago <javiersantiago@eprosima.com>
    
    * Refs #13983. Stop event thread before deleting builtin protocols.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 14467. Linter pass
    
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>
    
    Co-authored-by: Javier Santiago <javiersantiago@eprosima.com>
    Co-authored-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/resources/ResourceEvent.cpp
+++ src/cpp/rtps/resources/ResourceEvent.cpp
@@ -41,11 +41,16 @@ ResourceEvent::~ResourceEvent()
     assert(pending_timers_.empty());
     assert(timers_count_ == 0);
 
+    stop_thread();
+}
+
+void ResourceEvent::stop_thread()
+{
     logInfo(RTPS_PARTICIPANT, "Removing event thread");
     if (thread_.joinable())
     {
         {
-            std::unique_lock<TimedMutex> lock(mutex_);
+            std::lock_guard<TimedMutex> guard(mutex_);
             stop_.store(true);
             cv_.notify_one();
         }
@@ -56,11 +61,10 @@ ResourceEvent::~ResourceEvent()
 void ResourceEvent::register_timer(
         TimedEventImpl* /*event*/)
 {
-    assert(!stop_.load());
-
-    std::lock_guard<TimedMutex> lock(mutex_);
-
-    ++timers_count_;
+    {
+        std::lock_guard<TimedMutex> lock(mutex_);
+        ++timers_count_;
+    }
 
     // Notify the execution thread that something changed
     cv_.notify_one();
@@ -69,8 +73,6 @@ void ResourceEvent::register_timer(
 void ResourceEvent::unregister_timer(
         TimedEventImpl* event)
 {
-    assert(!stop_.load());
-
     std::unique_lock<TimedMutex> lock(mutex_);
 
     cv_manipulation_.wait(lock, [&]()
@@ -191,6 +193,13 @@ void ResourceEvent::event_service()
         allow_vector_manipulation_ = false;
         resize_collections();
     }
+
+    // Thread being stopped. Allow other threads to manipulate the timer collections.
+    {
+        std::lock_guard<TimedMutex> guard(mutex_);
+        allow_vector_manipulation_ = true;
+    }
+    cv_manipulation_.notify_all();
 }
 
 void ResourceEvent::sort_timers()
@@ -274,6 +283,7 @@ void ResourceEvent::init_thread()
     std::lock_guard<TimedMutex> lock(mutex_);
 
     allow_vector_manipulation_ = false;
+    stop_.store(false);
     resize_collections();
 
     thread_ = std::thread(&ResourceEvent::event_service, this);
