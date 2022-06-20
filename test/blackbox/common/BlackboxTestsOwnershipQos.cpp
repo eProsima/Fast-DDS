fee827eedab450b717f7af68b15226ad1873aa49
Ricardo González Moreno <ricardo@richiware.dev>
2022-06-20 16:28:30 +0200

    Refs #14570. Add besteffort blackbox tests
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/blackbox/common/BlackboxTestsOwnershipQos.cpp
+++ test/blackbox/common/BlackboxTestsOwnershipQos.cpp
@@ -74,10 +74,10 @@ public:
 };
 
 /*!
- * \test DDS-OWN-SAMPLE-01 Tests samples reception works successfully with Non-Keyed types, Ownership QoS EXCLUSIVE and
- * dynamic change of strength.
+ * \test DDS-OWN-SAMPLE-01 Tests samples reception works successfully with Non-Keyed types, Reliable,  Ownership QoS
+ * EXCLUSIVE and dynamic change of strength.
  */
-TEST_P(OwnershipQos, exclusive_kind_non_keyed_sample_reception)
+TEST_P(OwnershipQos, exclusive_kind_non_keyed_reliable_sample_reception)
 {
 
     PubSubReader<HelloWorldPubSubType> reader(TEST_TOPIC_NAME);
@@ -191,10 +191,10 @@ TEST_P(OwnershipQos, exclusive_kind_non_keyed_sample_reception)
 }
 
 /*!
- * \test DDS-OWN-SAMPLE-02 Tests samples reception works successfully with Keyed types, Ownership QoS EXCLUSIVE and
- * dynamic change of strength.
+ * \test DDS-OWN-SAMPLE-02 Tests samples reception works successfully with Keyed types, Reliable,  Ownership QoS
+ * EXCLUSIVE and dynamic change of strength.
  */
-TEST_P(OwnershipQos, exclusive_kind_keyed_sample_reception)
+TEST_P(OwnershipQos, exclusive_kind_keyed_reliable_sample_reception)
 {
 
     PubSubReader<KeyedHelloWorldPubSubType> reader(TEST_TOPIC_NAME);
@@ -368,6 +368,301 @@ TEST_P(OwnershipQos, exclusive_kind_keyed_sample_reception)
     ASSERT_EQ(denied_samples, reader.data_not_received());
 }
 
+/*!
+ * \test DDS-OWN-SAMPLE-03 Tests samples reception works successfully with Non-Keyed types, BestEffort, Ownership QoS
+ * EXCLUSIVE and dynamic change of strength.
+ */
+TEST_P(OwnershipQos, exclusive_kind_non_keyed_besteffort_sample_reception)
+{
+
+    PubSubReader<HelloWorldPubSubType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer1(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer2(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer3(TEST_TOPIC_NAME);
+
+    reader.ownership_exclusive().init();
+    writer1.ownership_strength(1).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+    writer2.ownership_strength(2).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+    writer3.ownership_strength(3).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+    ASSERT_TRUE(writer1.isInitialized());
+    ASSERT_TRUE(writer2.isInitialized());
+    ASSERT_TRUE(writer3.isInitialized());
+
+    // Wait for discovery.
+    writer1.wait_discovery();
+    writer2.wait_discovery();
+    writer3.wait_discovery();
+    reader.wait_discovery(std::chrono::seconds(1), 3);
+
+    auto data = default_helloworld_data_generator(13);
+    reader.startReception(data);
+
+    decltype(data) denied_samples;
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 changes its strength to 4.
+    ASSERT_TRUE(writer2.ownership_strength(4).set_qos());
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait the reader receives the update
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 changes its strength to 1.
+    ASSERT_TRUE(writer3.ownership_strength(1).set_qos());
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait the reader receives the update
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    reader.block_for_at_least(7);
+    ASSERT_EQ(denied_samples.size(), reader.data_not_received().size());
+    ASSERT_EQ(denied_samples, reader.data_not_received());
+}
+
+/*!
+ * \test DDS-OWN-SAMPLE-04 Tests samples reception works successfully with Keyed types, BestEffort, Ownership QoS
+ * EXCLUSIVE and dynamic change of strength.
+ */
+TEST_P(OwnershipQos, exclusive_kind_keyed_besteffort_sample_reception)
+{
+
+    PubSubReader<KeyedHelloWorldPubSubType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer1(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer2(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer3(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer4(TEST_TOPIC_NAME);
+
+    reader.ownership_exclusive().init();
+    writer1.ownership_strength(1).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+    writer2.ownership_strength(2).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+    writer3.ownership_strength(3).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+    writer4.ownership_strength(4).reliability(BEST_EFFORT_RELIABILITY_QOS).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+    ASSERT_TRUE(writer1.isInitialized());
+    ASSERT_TRUE(writer2.isInitialized());
+    ASSERT_TRUE(writer3.isInitialized());
+    ASSERT_TRUE(writer4.isInitialized());
+
+    // Wait for discovery.
+    writer1.wait_discovery();
+    writer2.wait_discovery();
+    writer3.wait_discovery();
+    writer4.wait_discovery();
+    reader.wait_discovery(std::chrono::seconds(1), 4);
+
+    auto data = default_keyedhelloworld_data_generator(23);
+    reader.startReception(data);
+
+    decltype(data) denied_samples;
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 2.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW4 sends a sample of instance 1.
+    writer4.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 2.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 changes its strength to 4.
+    ASSERT_TRUE(writer2.ownership_strength(4).set_qos());
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait the reader receives the update
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW4 sends a sample of instance 1.
+    writer4.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 changes its strength to 1.
+    ASSERT_TRUE(writer3.ownership_strength(1).set_qos());
+    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait the reader receives the update
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW2 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+    // DW4 sends a sample of instance 1.
+    writer4.send_sample(data.front());
+    data.pop_front();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+
+    reader.block_for_at_least(10);
+    ASSERT_EQ(denied_samples.size(), reader.data_not_received().size());
+    ASSERT_EQ(denied_samples, reader.data_not_received());
+}
 
 #ifdef INSTANTIATE_TEST_SUITE_P
 #define GTEST_INSTANTIATE_TEST_MACRO(x, y, z, w) INSTANTIATE_TEST_SUITE_P(x, y, z, w)
