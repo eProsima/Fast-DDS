95edfbc6618bc2f89d462c46e981e3c2bcf1f3e7
Miguel Company <MiguelCompany@eprosima.com>
2022-07-04 07:46:08 +0200

    Refs #15048. Fix duration cast on FileWatch.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- thirdparty/filewatch/FileWatch.hpp
+++ thirdparty/filewatch/FileWatch.hpp
@@ -506,9 +506,11 @@ namespace filewatch {
                 struct stat result;
                 stat(_path.c_str(), &result);
 
-                std::chrono::time_point<std::chrono::system_clock> current_time;
-                current_time += std::chrono::seconds(result.st_mtim.tv_sec);
-                current_time += std::chrono::nanoseconds(result.st_mtim.tv_nsec);
+                using clock = std::chrono::system_clock;
+                using duration = clock::duration;
+                std::chrono::time_point<clock> current_time;
+                current_time += std::chrono::duration_cast<duration>(std::chrono::seconds(result.st_mtim.tv_sec));
+                current_time += std::chrono::duration_cast<duration>(std::chrono::nanoseconds(result.st_mtim.tv_nsec));
 
                 unsigned long current_size = result.st_size;
 
@@ -604,8 +606,9 @@ namespace filewatch {
             struct stat result;
             stat(_path.c_str(), &result);
 
-            last_write_time_ += std::chrono::seconds(result.st_mtim.tv_sec);
-            last_write_time_ += std::chrono::nanoseconds(result.st_mtim.tv_nsec);
+            using duration = std::chrono::system_clock::duration;
+            last_write_time_ += std::chrono::duration_cast<duration>(std::chrono::seconds(result.st_mtim.tv_sec));
+            last_write_time_ += std::chrono::duration_cast<duration>(std::chrono::nanoseconds(result.st_mtim.tv_nsec));
 
             // Initialize filesize
             last_size_ = result.st_size;
