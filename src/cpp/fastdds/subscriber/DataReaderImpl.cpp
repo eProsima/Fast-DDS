7ed40195e0274c4ed14526438efff4ae31d7f30d
Mikel Rico <mikelrico@eprosima.com>
2022-07-27 21:03:29 +0200

    Refs #14958: Remove allocation consistency checks within former check_qos() method from DataReaderImpl.
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>

--- src/cpp/fastdds/subscriber/DataReaderImpl.cpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -1444,20 +1444,6 @@ ReturnCode_t DataReaderImpl::check_qos(
         logError(DDS_QOS_CHECK, "unique_network_request cannot be set along specific locators");
         return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
     }
-    if ((qos.resource_limits().max_samples > 0) &&
-            (qos.resource_limits().max_samples <
-            (qos.resource_limits().max_instances * qos.resource_limits().max_samples_per_instance)))
-    {
-        logError(DDS_QOS_CHECK, "max_samples should be greater than max_instances * max_samples_per_instance");
-        return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
-    }
-    if ((qos.resource_limits().max_instances <= 0 || qos.resource_limits().max_samples_per_instance <= 0) &&
-            (qos.resource_limits().max_samples > 0))
-    {
-        logError(DDS_QOS_CHECK,
-                "max_samples should be infinite when max_instances or max_samples_per_instance are infinite");
-        return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
-    }
     return ReturnCode_t::RETCODE_OK;
 }
 
