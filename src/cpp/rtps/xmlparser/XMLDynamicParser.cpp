7065d65f3f704488f1ef0aa14f6b5aee8c16e85d
Miguel Company <MiguelCompany@eprosima.com>
2022-07-29 07:36:04 +0200

    Refs #14456. Fix issue.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/xmlparser/XMLDynamicParser.cpp
+++ src/cpp/rtps/xmlparser/XMLDynamicParser.cpp
@@ -394,6 +394,11 @@ XMLP_ret XMLParser::parseXMLBitsetDynamicType(
     uint32_t mId = 0;
 
     const char* name = p_root->Attribute(NAME);
+    if (nullptr == name)
+    {
+        logError(XMLPARSER, "Error parsing 'bitsetDcl' type. No name attribute given.");
+        return XMLP_ret::XML_ERROR;
+    }
 
     const char* baseType = p_root->Attribute(BASE_TYPE);
     if (baseType != nullptr)
