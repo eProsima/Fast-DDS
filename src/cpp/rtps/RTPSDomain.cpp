053d769f57a4cb8387cfdfd874e2d8bb74e28d81
jparisu <javierparis@eprosima.com>
2022-07-29 10:01:57 +0200

    extend API to entity Id
    Signed-off-by: jparisu <javierparis@eprosima.com>

--- src/cpp/rtps/RTPSDomain.cpp
+++ src/cpp/rtps/RTPSDomain.cpp
@@ -340,6 +340,28 @@ RTPSWriter* RTPSDomain::createRTPSWriter(
         const EntityId_t& entity_id,
         WriterAttributes& watt,
         const std::shared_ptr<IPayloadPool>& payload_pool,
+        const std::shared_ptr<IChangePool>& change_pool,
+        WriterHistory* hist,
+        WriterListener* listen)
+{
+    RTPSParticipantImpl* impl = RTPSDomainImpl::find_local_participant(p->getGuid());
+    if (impl)
+    {
+        RTPSWriter* ret_val = nullptr;
+        if (impl->create_writer(&ret_val, watt, payload_pool, change_pool, hist, listen, entity_id))
+        {
+            return ret_val;
+        }
+    }
+
+    return nullptr;
+}
+
+RTPSWriter* RTPSDomain::createRTPSWriter(
+        RTPSParticipant* p,
+        const EntityId_t& entity_id,
+        WriterAttributes& watt,
+        const std::shared_ptr<IPayloadPool>& payload_pool,
         WriterHistory* hist,
         WriterListener* listen)
 {
