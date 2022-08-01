985053a72905f3027b9078ff4340466e9564596e
RaulSanchez <raul@eprosima.com>
2022-08-01 09:17:48 +0200

    Use thread-safe localtime function if possible
    Signed-off-by: RaulSanchez <raul@eprosima.com>

--- src/cpp/fastdds/log/Log.cpp
+++ src/cpp/fastdds/log/Log.cpp
@@ -331,13 +331,16 @@ void Log::get_timestamp(
 #if defined(_WIN32)
     struct tm timeinfo;
     localtime_s(&timeinfo, &now_c);
-    stream << std::put_time(&timeinfo, "%F %T") << "." << std::setw(3) << std::setfill('0') << ms << " ";
     //#elif defined(__clang__) && !defined(std::put_time) // TODO arm64 doesn't seem to support std::put_time
     //    (void)now_c;
     //    (void)ms;
+#elif defined(__unix__)
+    std::tm timeinfo;
+    localtime_r(&now_c, &timeinfo);
 #else
-    stream << std::put_time(localtime(&now_c), "%F %T") << "." << std::setw(3) << std::setfill('0') << ms << " ";
+    std::tm timeinfo = *localtime(&now_c);
 #endif // if defined(_WIN32)
+    stream << std::put_time(&timeinfo, "%F %T") << "." << std::setw(3) << std::setfill('0') << ms << " ";
     timestamp = stream.str();
 }
 
