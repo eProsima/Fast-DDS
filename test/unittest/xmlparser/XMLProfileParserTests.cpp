b5e5cfc61379533339562202b3934792fb3a7ab7
Miguel Company <MiguelCompany@eprosima.com>
2022-08-03 13:04:13 +0200

    Refs #14631. Adding data_writer positive cases.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/unittest/xmlparser/XMLProfileParserTests.cpp
+++ test/unittest/xmlparser/XMLProfileParserTests.cpp
@@ -2058,6 +2058,78 @@ TEST_F(XMLProfileParserTests, external_locators_feature)
             </rtps></participant></profiles>)",
             xmlparser::XMLP_ret::XML_ERROR
         },
+        // ------------------ data_writer positive cases
+        {
+            "writer_no_external",
+            R"(<profiles><data_writer profile_name="w1"></data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
+        {
+            "writer_external_empty",
+            R"(<profiles><data_writer profile_name="w1">
+                <external_unicast_locators>
+                </external_unicast_locators>
+            </data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
+        {
+            "writer_external_udp4",
+            R"(<profiles><data_writer profile_name="w1">
+                <external_unicast_locators>
+                    <udpv4 externality="1" cost="0" mask="24">
+                        <address>192.168.1.100</address>
+                        <port>1234</port>
+                    </udpv4>
+                </external_unicast_locators>
+            </data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
+        {
+            "writer_external_udp6",
+            R"(<profiles><data_writer profile_name="w1">
+                <external_unicast_locators>
+                    <udpv6 externality="1" cost="0" mask="24">
+                        <address>::1:2</address>
+                        <port>1234</port>
+                    </udpv6>
+                </external_unicast_locators>
+            </data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
+        {
+            "writer_external_complete_ignore_true",
+            R"(<profiles><data_writer profile_name="w1">
+                <ignore_non_matching_locators>true</ignore_non_matching_locators>
+                <external_unicast_locators>
+                    <udpv4 externality="1" cost="0" mask="24">
+                        <address>192.168.1.100</address>
+                        <port>1234</port>
+                    </udpv4>
+                    <udpv6 externality="1" cost="0" mask="24">
+                        <address>::1:2</address>
+                        <port>1235</port>
+                    </udpv6>
+                </external_unicast_locators>
+            </data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
+        {
+            "writer_external_complete_ignore_false",
+            R"(<profiles><data_writer profile_name="w1">
+                <ignore_non_matching_locators>true</ignore_non_matching_locators>
+                <external_unicast_locators>
+                    <udpv4 externality="1" cost="0" mask="24">
+                        <address>192.168.1.100</address>
+                        <port>1234</port>
+                    </udpv4>
+                    <udpv6 externality="1" cost="0" mask="24">
+                        <address>::1:2</address>
+                        <port>1235</port>
+                    </udpv6>
+                </external_unicast_locators>
+            </data_writer></profiles>)",
+            xmlparser::XMLP_ret::XML_OK
+        },
     };
 
     for (const TestCase& test : test_cases)
