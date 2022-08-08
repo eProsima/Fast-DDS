26d0517f80009256fbedb56ad0947a7c89951fe9
MRicoIE2C <43696963+MRicoIE2C@users.noreply.github.com>
2022-08-08 15:06:59 +0200

    Refs #14958: Simplification of check_qos_including_resource_limits()
    Co-authored-by: Ricardo González <ricardo@richiware.dev>
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>

--- src/cpp/fastdds/topic/TopicImpl.cpp
+++ src/cpp/fastdds/topic/TopicImpl.cpp
@@ -54,21 +54,13 @@ ReturnCode_t TopicImpl::check_qos_including_resource_limits(
         const TopicQos& qos,
         const TypeSupport& type)
 {
-    ReturnCode_t check_qos_return;
-    check_qos_return = check_qos(qos);
-    if (!check_qos_return)
-    {
-        return check_qos_return;
-    }
-    if (type->m_isGetKeyDefined)
+    ReturnCode_t check_qos_return = check_qos(qos);
+    if (ReturnCode_t::RETCODE_OK == check_qos_return &&
+            type->m_isGetKeyDefined)
     {
         check_qos_return = check_allocation_consistency(qos);
-        if (!check_qos_return)
-        {
-            return check_qos_return;
-        }
     }
-    return ReturnCode_t::RETCODE_OK;
+    return check_qos_return;
 }
 
 ReturnCode_t TopicImpl::check_qos(
