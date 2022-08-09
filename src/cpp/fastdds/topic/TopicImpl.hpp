9a26a098bf59cc2d53ed3163d92f6d5c5d694792
Mikel Rico <mikelrico@eprosima.com>
2022-08-09 09:29:29 +0200

    Refs 14958: TopicImpl: Documentation over new methods.
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>

--- src/cpp/fastdds/topic/TopicImpl.hpp
+++ src/cpp/fastdds/topic/TopicImpl.hpp
@@ -53,16 +53,26 @@ public:
     /**
      * Extends the check_qos() call, including the check for
      * resource limits policy.
+     * @param qos Pointer to the qos to be checked.
+     * @param type Pointer to the associated TypeSupport object.
+     * @return True if correct.
      */
     static ReturnCode_t check_qos_including_resource_limits(
             const TopicQos& qos,
             const TypeSupport& type);
 
+    /**
+     * Checks the consistency of the qos configuration.
+     * @param qos Pointer to the qos to be checked.
+     * @return True if correct.
+     */
     static ReturnCode_t check_qos(
             const TopicQos& qos);
 
     /**
      * Checks resource limits policy: Instance allocation consistency
+     * @param qos Pointer to the qos to be checked.
+     * @return True if correct.
      */
     static ReturnCode_t check_allocation_consistency(
             const TopicQos& qos);
