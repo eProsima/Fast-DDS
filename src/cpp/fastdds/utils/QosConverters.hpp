3d09b097e2d7ff59767815855facf2684f7ca9c1
Mikel Rico <mikelrico@eprosima.com>
2022-08-10 10:28:48 +0200

    Refs #15417: New QosConverters.hpp and .cpp in src/cpp/fastdds/utils folder.
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>

---
+++ src/cpp/fastdds/utils/QosConverters.hpp
@@ -0,0 +1,35 @@
+// Copyright 2019 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file QosConverters.h
+ */
+
+
+
+#ifndef _QOS_CONVERTERS_HPP_
+#define _QOS_CONVERTERS_HPP_
+#ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+
+using fastrtps::PublisherAttributes;
+
+} /* namespace dds */
+} /* namespace fastdds */
+} /* namespace eprosima */
+#endif // ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
+#endif /* _QOS_CONVERTERS_HPP_ */
