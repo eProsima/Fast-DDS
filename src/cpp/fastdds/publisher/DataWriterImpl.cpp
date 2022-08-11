aef67d2e9ff49bd3d94aaf43e174ae1aeb73044b
Mikel Rico <mikelrico@eprosima.com>
2022-08-11 11:29:31 +0200

    Refs #14958: Fix over case in DataWriterImpl when max_samples = ( max_instances * max_samples_per_instance ) should not return errorCode.
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>

--- src/cpp/fastdds/publisher/DataWriterImpl.cpp
+++ src/cpp/fastdds/publisher/DataWriterImpl.cpp
@@ -1764,7 +1764,7 @@ ReturnCode_t DataWriterImpl::check_allocation_consistency(
         const DataWriterQos& qos)
 {
     if ((qos.resource_limits().max_samples > 0) &&
-            (qos.resource_limits().max_samples <=
+            (qos.resource_limits().max_samples <
             (qos.resource_limits().max_instances * qos.resource_limits().max_samples_per_instance)))
     {
         logError(DDS_QOS_CHECK,
