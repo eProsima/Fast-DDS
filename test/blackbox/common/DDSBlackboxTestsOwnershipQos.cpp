c4796be5da014fb39b21ee57c07999a8c8d962d9
Ricardo González Moreno <ricardo@richiware.dev>
2022-08-12 14:20:12 +0200

    Refs #14570. Add blackbox test
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/blackbox/common/DDSBlackboxTestsOwnershipQos.cpp
+++ test/blackbox/common/DDSBlackboxTestsOwnershipQos.cpp
@@ -437,6 +437,246 @@ TEST_P(OwnershipQos, exclusive_kind_keyed_besteffort_sample_reception)
     exclusive_kind_keyed_sample_reception(false);
 }
 
+void exclusive_kind_non_keyed_writers_same_guid(
+        bool reliable)
+{
+    PubSubReader<HelloWorldPubSubType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer1(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer2(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer3(TEST_TOPIC_NAME);
+
+    reader.ownership_exclusive().reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer1.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer2.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer3.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+    ASSERT_TRUE(writer1.isInitialized());
+    ASSERT_TRUE(writer2.isInitialized());
+    ASSERT_TRUE(writer3.isInitialized());
+
+    // Wait for discovery.
+    writer1.wait_discovery();
+    writer2.wait_discovery();
+    writer3.wait_discovery();
+    reader.wait_discovery(std::chrono::seconds(1), 3);
+
+    auto wait_functor = reliable ?
+            [] (PubSubWriter<HelloWorldPubSubType>& writer)
+    {
+        writer.waitForAllAcked(std::chrono::milliseconds(100));
+    }
+    :
+    [] (PubSubWriter<HelloWorldPubSubType>&)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    };
+
+    auto data = default_helloworld_data_generator(7);
+    reader.startReception(data);
+
+    decltype(data) denied_samples;
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer3);
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer1);
+
+    // DW3 sends a sample.
+    writer3.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer3);
+
+    // DW2 sends a sample.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW1 sends a sample.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer1);
+
+    reader.block_for_at_least(4);
+    ASSERT_EQ(denied_samples.size(), reader.data_not_received().size());
+    ASSERT_EQ(denied_samples, reader.data_not_received());
+}
+
+/*!
+ * @test DDS-OWN-SAMPLE-05 Tests samples reception works successfully with Non-Keyed types, Reliable,  Ownership QoS
+ * EXCLUSIVE and several writers with same GUID.
+ */
+TEST_P(OwnershipQos, exclusive_kind_non_keyed_reliable_writers_same_guid)
+{
+    exclusive_kind_non_keyed_writers_same_guid(true);
+}
+
+void exclusive_kind_keyed_writers_same_guid(
+        bool reliable)
+{
+    PubSubReader<KeyedHelloWorldPubSubType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer1(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer2(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer3(TEST_TOPIC_NAME);
+    PubSubWriter<KeyedHelloWorldPubSubType> writer4(TEST_TOPIC_NAME);
+
+    reader.ownership_exclusive().reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer1.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer2.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer3.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+    writer4.ownership_strength(10).reliability(
+        reliable ? RELIABLE_RELIABILITY_QOS : BEST_EFFORT_RELIABILITY_QOS
+        ).init();
+
+    ASSERT_TRUE(reader.isInitialized());
+    ASSERT_TRUE(writer1.isInitialized());
+    ASSERT_TRUE(writer2.isInitialized());
+    ASSERT_TRUE(writer3.isInitialized());
+    ASSERT_TRUE(writer4.isInitialized());
+
+    // Wait for discovery.
+    writer1.wait_discovery();
+    writer2.wait_discovery();
+    writer3.wait_discovery();
+    writer4.wait_discovery();
+    reader.wait_discovery(std::chrono::seconds(1), 4);
+
+    auto wait_functor = reliable ?
+            [] (PubSubWriter<KeyedHelloWorldPubSubType>& writer)
+    {
+        writer.waitForAllAcked(std::chrono::milliseconds(100));
+    }
+    :
+    [] (PubSubWriter<KeyedHelloWorldPubSubType>&)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    };
+
+    auto data = default_keyedhelloworld_data_generator(10);
+    reader.startReception(data);
+
+    decltype(data) denied_samples;
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer3);
+
+    // DW4 sends a sample of instance 1.
+    writer4.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer4);
+
+    // DW4 sends a sample of instance 2.
+    writer4.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer4);
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW3 sends a sample of instance 2.
+    writer3.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer3);
+
+    // DW1 sends a sample of instance 1.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer1);
+
+    // DW1 sends a sample of instance 2.
+    writer1.send_sample(data.front());
+    data.pop_front();
+    wait_functor(writer1);
+
+    // DW2 sends a sample of instance 1.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    // DW3 sends a sample of instance 2.
+    writer2.send_sample(data.front());
+    denied_samples.push_back(data.front());
+    data.pop_front();
+    wait_functor(writer2);
+
+    reader.block_for_at_least(6);
+    ASSERT_EQ(denied_samples.size(), reader.data_not_received().size());
+    ASSERT_EQ(denied_samples, reader.data_not_received());
+}
+
+/*!
+ * @test DDS-OWN-SAMPLE-06 Tests samples reception works successfully with Keyed types, Reliable,  Ownership QoS
+ * EXCLUSIVE and several writers with same GUID.
+ */
+TEST_P(OwnershipQos, exclusive_kind_keyed_reliable_writers_same_guid)
+{
+    exclusive_kind_keyed_writers_same_guid(true);
+}
+
+/*!
+ * @test DDS-OWN-SAMPLE-07 Tests samples reception works successfully with Keyed types, BestEffort,  Ownership QoS
+ * EXCLUSIVE and several writers with same GUID.
+ */
+TEST_P(OwnershipQos, exclusive_kind_non_keyed_besteffort_writers_same_guid)
+{
+    exclusive_kind_non_keyed_writers_same_guid(false);
+}
+
+/*!
+ * @test DDS-OWN-SAMPLE-08 Tests samples reception works successfully with Keyed types, BestEffort,  Ownership QoS
+ * EXCLUSIVE and several writers with same GUID.
+ */
+TEST_P(OwnershipQos, exclusive_kind_keyed_besteffort_writers_same_guid)
+{
+    exclusive_kind_keyed_writers_same_guid(false);
+}
+
 /*!
  * @test DDS-OWN-DEADLINE-01 Tests Ownership changes when the current owner doesn't comply with deadline QoS, in a
  * Reliable communication with Non-Keyed types.
