67797b987d89f4476b0b2376a5fbfa65c8cbe91d
Ricardo González Moreno <ricardo@richiware.dev>
2022-09-15 08:55:52 +0200

    Refs #15483. Apply suggestions
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- utils/scripts/update_generated_code_from_idl.sh
+++ utils/scripts/update_generated_code_from_idl.sh
@@ -2,17 +2,23 @@
 
 files_to_exclude=(
     './include/fastrtps/types/*'
-    './include/fastdds/statistics/types.idl'
     )
 
 files_needing_typeobject=(
     './test/unittest/dynamic_types/idl/Basic.idl'
     './test/unittest/dynamic_types/idl/new_features_4_2.idl'
     './test/unittest/dynamic_types/idl/Test.idl'
+    './test/unittest/xtypes/idl/Types.idl'
+    './test/unittest/xtypes/idl/WideEnum.idl'
+    './test/xtypes/idl/Types.idl'
     )
 
 files_needing_case_sensitive=(
     './test/unittest/dynamic_types/idl/new_features_4_2.idl'
+    )
+
+files_needing_output_dir=(
+    './include/fastdds/statistics/types.idl|../../../src/cpp/statistics/types'
     )
 
 
@@ -54,9 +60,19 @@ for idl_file in "${idl_files[@]}"; do
     [[ ${files_needing_typeobject[*]} =~ $idl_file ]] && to_arg='-typeobject' || to_arg=''
 
     # Detect if needs case sensitive.
-    [[ ${files_needing_case_sensitive[*]} =~ $idl_file ]] && to_arg='-cs' || to_arg=''
+    [[ ${files_needing_case_sensitive[*]} =~ $idl_file ]] && cs_arg='-cs' || cs_arg=''
 
-    fastddsgen -replace $to_arg "$file_from_gen"
+    # Detect if needs output directory.
+    od_arg=""
+    for od_entry in ${files_needing_output_dir[@]}; do
+        if [[ $od_entry = $idl_file\|* ]]; then
+            od_entry_split=(${od_entry//\|/ })
+            od_arg="-d ${od_entry_split[1]}"
+            break
+        fi
+    done
+
+    fastddsgen -replace $to_arg $cs_arg $od_arg "$file_from_gen"
 
     if [[ $? != 0 ]]; then
         ret_value=-1
