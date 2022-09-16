08ffe7c2fed86a11c60c2808e8c0019cdbe51cc4
Ricardo González <ricardo@richiware.dev>
2022-09-16 07:41:06 +0200

    Update script for generating idl files (#2940)
    * Refs #15294. Fix
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #15294. Fix unit test
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    * Refs #15483. Apply suggestions
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>
    
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- utils/scripts/update_generated_code_from_idl.sh
+++ utils/scripts/update_generated_code_from_idl.sh
@@ -2,9 +2,25 @@
 
 files_to_exclude=(
     './include/fastrtps/types/*'
-    './include/fastdds/statistics/types.idl'
+    )
+
+files_needing_typeobject=(
+    './test/unittest/dynamic_types/idl/Basic.idl'
+    './test/unittest/dynamic_types/idl/new_features_4_2.idl'
+    './test/unittest/dynamic_types/idl/Test.idl'
+    './test/unittest/xtypes/idl/Types.idl'
+    './test/unittest/xtypes/idl/WideEnum.idl'
+    './test/xtypes/idl/Types.idl'
+    )
+
+files_needing_case_sensitive=(
     './test/unittest/dynamic_types/idl/new_features_4_2.idl'
     )
+
+files_needing_output_dir=(
+    './include/fastdds/statistics/types.idl|../../../src/cpp/statistics/types'
+    )
+
 
 yellow='\E[1;33m'
 textreset='\E[1;0m'
@@ -25,7 +41,7 @@ readarray -d '' idl_files < <(find . -iname \*.idl -print0)
 
 for del in ${files_to_exclude[@]}
 do
-idl_files=("${idl_files[@]/$del}")
+    idl_files=("${idl_files[@]/$del}")
 done
 
 idl_files=(${idl_files[@]/$files_to_exclude})
@@ -40,7 +56,23 @@ for idl_file in "${idl_files[@]}"; do
 
     cd "${idl_dir}"
 
-    fastddsgen -replace "$file_from_gen"
+    # Detect if needs type_object.
+    [[ ${files_needing_typeobject[*]} =~ $idl_file ]] && to_arg='-typeobject' || to_arg=''
+
+    # Detect if needs case sensitive.
+    [[ ${files_needing_case_sensitive[*]} =~ $idl_file ]] && cs_arg='-cs' || cs_arg=''
+
+    # Detect if needs output directory.
+    od_arg=""
+    for od_entry in ${files_needing_output_dir[@]}; do
+        if [[ $od_entry = $idl_file\|* ]]; then
+            od_entry_split=(${od_entry//\|/ })
+            od_arg="-d ${od_entry_split[1]}"
+            break
+        fi
+    done
+
+    fastddsgen -replace $to_arg $cs_arg $od_arg "$file_from_gen"
 
     if [[ $? != 0 ]]; then
         ret_value=-1
