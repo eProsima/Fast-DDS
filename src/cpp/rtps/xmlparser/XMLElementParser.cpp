98ee0b0e35f767fd6aa0076ce24ab64396f206ab
Mikel Rico <mikelrico@eprosima.com>
2022-09-22 12:03:31 +0200

    Refs #15628: Uncomment and prepare execution for getXMLOwnershipQos call
    Signed-off-by: Mikel Rico <mikelrico@eprosima.com>
    Signed-off-by: Eduardo Ponz <eduardoponz@eprosima.com>

--- src/cpp/rtps/xmlparser/XMLElementParser.cpp
+++ src/cpp/rtps/xmlparser/XMLElementParser.cpp
@@ -1178,14 +1178,12 @@ XMLP_ret XMLParser::getXMLWriterQosPolicies(
             }
         }
         else if (strcmp(name, DURABILITY_SRV) == 0 ||
-                strcmp(name, TIME_FILTER) == 0 || strcmp(name, OWNERSHIP) == 0 ||
-                strcmp(name, OWNERSHIP_STRENGTH) == 0 || strcmp(name, DEST_ORDER) == 0 ||
-                strcmp(name, PRESENTATION) == 0)
+                strcmp(name, TIME_FILTER) == 0 || strcmp(name, OWNERSHIP_STRENGTH) == 0 ||
+                strcmp(name, DEST_ORDER) == 0 || strcmp(name, PRESENTATION) == 0)
         {
             // TODO: Do not supported for now
             //if (nullptr != (p_aux = elem->FirstChildElement(    DURABILITY_SRV))) getXMLDurabilityServiceQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(       TIME_FILTER))) getXMLTimeBasedFilterQos(p_aux, ident);
-            //if (nullptr != (p_aux = elem->FirstChildElement(         OWNERSHIP))) getXMLOwnershipQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(OWNERSHIP_STRENGTH))) getXMLOwnershipStrengthQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(        DEST_ORDER))) getXMLDestinationOrderQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(      PRESENTATION))) getXMLPresentationQos(p_aux, ident);
@@ -1195,6 +1193,14 @@ XMLP_ret XMLParser::getXMLWriterQosPolicies(
         {
             //data sharing
             if (XMLP_ret::XML_OK != getXMLDataSharingQos(p_aux0, qos.data_sharing, ident))
+            {
+                return XMLP_ret::XML_ERROR;
+            }
+        }
+        else if (strcmp(name, OWNERSHIP) == 0)
+        {
+            //ownership
+            if (XMLP_ret::XML_OK != getXMLOwnershipQos(p_aux0, qos.m_ownership, ident))
             {
                 return XMLP_ret::XML_ERROR;
             }
@@ -1338,14 +1344,12 @@ XMLP_ret XMLParser::getXMLReaderQosPolicies(
             }
         }
         else if (strcmp(name, DURABILITY_SRV) == 0 ||
-                strcmp(name, TIME_FILTER) == 0 || strcmp(name, OWNERSHIP) == 0 ||
-                strcmp(name, OWNERSHIP_STRENGTH) == 0 || strcmp(name, DEST_ORDER) == 0 ||
-                strcmp(name, PRESENTATION) == 0)
+                strcmp(name, TIME_FILTER) == 0 || strcmp(name, OWNERSHIP_STRENGTH) == 0 ||
+                strcmp(name, DEST_ORDER) == 0 || strcmp(name, PRESENTATION) == 0)
         {
             // TODO: Do not supported for now
             //if (nullptr != (p_aux = elem->FirstChildElement(    DURABILITY_SRV))) getXMLDurabilityServiceQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(       TIME_FILTER))) getXMLTimeBasedFilterQos(p_aux, ident);
-            //if (nullptr != (p_aux = elem->FirstChildElement(         OWNERSHIP))) getXMLOwnershipQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(        DEST_ORDER))) getXMLDestinationOrderQos(p_aux, ident);
             //if (nullptr != (p_aux = elem->FirstChildElement(      PRESENTATION))) getXMLPresentationQos(p_aux, ident);
             logError(XMLPARSER, "Quality of Service '" << p_aux0->Value() << "' do not supported for now");
@@ -1354,6 +1358,14 @@ XMLP_ret XMLParser::getXMLReaderQosPolicies(
         {
             //data sharing
             if (XMLP_ret::XML_OK != getXMLDataSharingQos(p_aux0, qos.data_sharing, ident))
+            {
+                return XMLP_ret::XML_ERROR;
+            }
+        }
+        else if (strcmp(name, OWNERSHIP) == 0)
+        {
+            //ownership
+            if (XMLP_ret::XML_OK != getXMLOwnershipQos(p_aux0, qos.m_ownership, ident))
             {
                 return XMLP_ret::XML_ERROR;
             }
@@ -2103,13 +2115,12 @@ XMLP_ret XMLParser::getXMLDataSharingQos(
    }
  */
 
-// TODO Implement OwnershipQos
-/*
    XMLP_ret XMLParser::getXMLOwnershipQos(
         tinyxml2::XMLElement* elem,
         OwnershipQosPolicy& ownership,
         uint8_t ident)
    {
+    (void)ident;
 
     //    <xs:complexType name="ownershipQosPolicyType">
     //        <xs:all>
@@ -2170,7 +2181,6 @@ XMLP_ret XMLParser::getXMLDataSharingQos(
     return XMLP_ret::
     XML_OK;
    }
- */
 
 // TODO Implement OwnershipStrengthQos
 /*
