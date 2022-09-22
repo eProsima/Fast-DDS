cbfae2048efd7bb30dc245ef6edd94ab8a336192
Miguel Barro <miguelbarro@eprosima.com>
2022-09-22 16:22:34 +0200

    Refs 15477. Fixing gcc warnings.
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- examples/cpp/dds/BasicConfigurationExample/arg_configuration.h
+++ examples/cpp/dds/BasicConfigurationExample/arg_configuration.h
@@ -22,6 +22,7 @@
 
 #include <iostream>
 #include <limits>
+#include <sstream>
 #include <string>
 
 #include <optionparser.hpp>
--- examples/cpp/dds/HelloWorldExample/HelloWorld_main.cpp
+++ examples/cpp/dds/HelloWorldExample/HelloWorld_main.cpp
@@ -18,6 +18,7 @@
  */
 
 #include <limits>
+#include <sstream>
 
 #include "HelloWorldPublisher.h"
 #include "HelloWorldSubscriber.h"
