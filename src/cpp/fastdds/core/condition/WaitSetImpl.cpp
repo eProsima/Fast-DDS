6e3a78a49d519f4d1a9736ca6630176a6ec5e141
Chen Lihui <lihui.chen@sony.com>
2022-11-14 15:43:50 +0800

    fix notification lost
    Signed-off-by: Chen Lihui <lihui.chen@sony.com>

--- src/cpp/fastdds/core/condition/WaitSetImpl.cpp
+++ src/cpp/fastdds/core/condition/WaitSetImpl.cpp
@@ -68,7 +68,7 @@ ReturnCode_t WaitSetImpl::attach_condition(
             // Should wake_up when adding a new triggered condition
             if (is_waiting_ && condition.get_trigger_value())
             {
-                wake_up();
+                cond_.notify_one();
             }
         }
     }
@@ -156,6 +156,7 @@ ReturnCode_t WaitSetImpl::get_conditions(
 
 void WaitSetImpl::wake_up()
 {
+    std::lock_guard<std::mutex> guard(mutex_);
     cond_.notify_one();
 }
 
