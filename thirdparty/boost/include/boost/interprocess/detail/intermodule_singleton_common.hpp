a9e96a0f4649483ac3fe05caf1273c00d537d942
Miguel Company <MiguelCompany@eprosima.com>
2023-01-05 09:15:30 +0100

    Refs #16647. Allow injection of custom exit work registration.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- thirdparty/boost/include/boost/interprocess/detail/intermodule_singleton_common.hpp
+++ thirdparty/boost/include/boost/interprocess/detail/intermodule_singleton_common.hpp
@@ -418,7 +418,7 @@ class intermodule_singleton_impl
             }
          }
          //if(Phoenix){
-            std::atexit(&atexit_work);
+            BOOST_INTERPROCESS_ATEXIT(&atexit_work);
          //}
          atomic_inc32(&rcount->singleton_ref_count);
          ret_ptr = rcount->ptr;
