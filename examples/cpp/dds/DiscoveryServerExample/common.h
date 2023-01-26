f2c071039b7748bd8d3c6e7d92f19b84522db96e
Irene Bandera <irenebandera@eprosima.com>
2023-01-26 15:36:56 +0100

    Uncrustify
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- examples/cpp/dds/DiscoveryServerExample/common.h
+++ examples/cpp/dds/DiscoveryServerExample/common.h
@@ -25,63 +25,67 @@
 
 enum class TransportKind
 {
-	UDPv4,
-	UDPv6,
-	SHM,
+    UDPv4,
+    UDPv6,
+    SHM,
 };
 
-inline eprosima::fastrtps::rtps::GuidPrefix_t get_discovery_server_guid_from_id(unsigned short id)
+inline eprosima::fastrtps::rtps::GuidPrefix_t get_discovery_server_guid_from_id(
+        unsigned short id)
 {
-	eprosima::fastrtps::rtps::GuidPrefix_t result;
+    eprosima::fastrtps::rtps::GuidPrefix_t result;
 
-	// Get default DS guid and modify the one value expected to be changed
-	std::istringstream(eprosima::fastdds::rtps::DEFAULT_ROS2_SERVER_GUIDPREFIX) >> result;
-	result.value[2] =
-		static_cast<eprosima::fastrtps::rtps::octet>(id); // This is done like this in Fast
+    // Get default DS guid and modify the one value expected to be changed
+    std::istringstream(eprosima::fastdds::rtps::DEFAULT_ROS2_SERVER_GUIDPREFIX) >> result;
+    result.value[2] =
+            static_cast<eprosima::fastrtps::rtps::octet>(id); // This is done like this in Fast
 
-	return result;
+    return result;
 }
 
-inline bool is_ip(const std::string ip_str)
+inline bool is_ip(
+        const std::string ip_str)
 {
-	return eprosima::fastrtps::rtps::IPLocator::isIPv4(ip_str) || eprosima::fastrtps::rtps::IPLocator::isIPv6(ip_str);
+    return eprosima::fastrtps::rtps::IPLocator::isIPv4(ip_str) || eprosima::fastrtps::rtps::IPLocator::isIPv6(ip_str);
 }
 
-inline std::string get_ip_from_dns(const std::string& domain_name, TransportKind kind)
+inline std::string get_ip_from_dns(
+        const std::string& domain_name,
+        TransportKind kind)
 {
-	std::pair<std::set<std::string>, std::set<std::string> > dns_response =
-		eprosima::fastrtps::rtps::IPLocator::resolveNameDNS(domain_name);
+    std::pair<std::set<std::string>, std::set<std::string>> dns_response =
+            eprosima::fastrtps::rtps::IPLocator::resolveNameDNS(domain_name);
 
-	if (kind == TransportKind::UDPv4)
-	{
-		if (dns_response.first.empty())
-		{
-			std::cout << "Not DNS found for IPv4 for " << domain_name << std::endl;
-			return "";
-		}
-		else
-		{
-			std::string solution(*dns_response.first.begin());
-			std::cout << "DNS found for " << domain_name << " => " << solution << std::endl;
-			return solution;
-		}
-	}
-	else if (kind == TransportKind::UDPv6)
-	{
-		if (dns_response.second.empty())
-		{
-			std::cout << "Not DNS found for IPv6 for " << domain_name << std::endl;
-			return "";
-		}
-		else
-		{
-			std::string solution(*dns_response.second.begin());
-			std::cout << "DNS found for " << domain_name << " => " << solution << std::endl;
-			return solution;
-		}
-	}
+    if (kind == TransportKind::UDPv4)
+    {
+        if (dns_response.first.empty())
+        {
+            std::cout << "Not DNS found for IPv4 for " << domain_name << std::endl;
+            return "";
+        }
+        else
+        {
+            std::string solution(*dns_response.first.begin());
+            std::cout << "DNS found for " << domain_name << " => " << solution << std::endl;
+            return solution;
+        }
+    }
+    else if (kind == TransportKind::UDPv6)
+    {
+        if (dns_response.second.empty())
+        {
+            std::cout << "Not DNS found for IPv6 for " << domain_name << std::endl;
+            return "";
+        }
+        else
+        {
+            std::string solution(*dns_response.second.begin());
+            std::cout << "DNS found for " << domain_name << " => " << solution << std::endl;
+            return solution;
+        }
+    }
 
-	return domain_name;
+    return domain_name;
 }
 
 #endif /* _EPROSIMA_FASTDDS_EXAMPLES_CPP_DDS_DISCOVERYSERVEREXAMPLE_COMMON_H_ */
