5a59587a820409cd42c127061eb8e9598b53bd53
Ricardo González Moreno <ricardo@richiware.dev>
2023-03-08 10:59:05 +0100

    Refs #17552. Check support of dlopen
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- src/cpp/utils/dlopen.cpp
+++ src/cpp/utils/dlopen.cpp
@@ -8,14 +8,22 @@ namespace fastrtps {
 void* dlopen::open_library(
         const std::string& library)
 {
+#if HAVE_DLOPEN
     return ::dlopen(library.c_str(), RTLD_LAZY);
+#else
+    return nullptr
+#endif
 }
 
 void* dlopen::open_symbol(
         void* library,
         const std::string& symbol)
 {
+#if HAVE_DLOPEN
     return dlsym(library, symbol.c_str());
+#else
+    return nullptr
+#endif
 }
 
 } // namespace fastrtps
