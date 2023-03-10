79ba117957656884aeb90b2350d2ac360d6c309e
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-03-10 08:47:08 +0100

    Refs #17509: RTPSParticipantImpl empty ignore API
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- src/cpp/rtps/participant/RTPSParticipantImpl.cpp
+++ src/cpp/rtps/participant/RTPSParticipantImpl.cpp
@@ -2509,6 +2509,39 @@ void RTPSParticipantImpl::get_default_unicast_locators()
     m_network_Factory.NormalizeLocators(m_att.defaultUnicastLocatorList);
 }
 
+bool RTPSParticipantImpl::is_participant_ignored(
+        const GuidPrefix_t& /*participant_guid*/)
+{
+    return false;
+}
+
+bool RTPSParticipantImpl::is_writer_ignored(
+        const GUID_t& /*writer_guid*/)
+{
+    return false;
+}
+
+bool RTPSParticipantImpl::is_reader_ignored(
+        const GUID_t& /*reader_guid*/)
+{
+    return false;
+}
+
+void RTPSParticipantImpl::ignore_participant(
+        const GuidPrefix_t& /*participant_guid*/)
+{
+}
+
+void RTPSParticipantImpl::ignore_writer(
+        const GUID_t& /*writer_guid*/)
+{
+}
+
+void RTPSParticipantImpl::ignore_reader(
+        const GUID_t& /*reader_guid*/)
+{
+}
+
 #ifdef FASTDDS_STATISTICS
 
 bool RTPSParticipantImpl::register_in_writer(
