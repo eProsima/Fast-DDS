bf797d1985311763d936367def2b35eb158a9d78
Miguel Barro <miguelbarro@eprosima.com>
2023-03-23 17:09:02 +0100

    Refs 17138. Fixing fastdds cli to work on python venv on windows
    Signed-off-by: Miguel Barro <miguelbarro@eprosima.com>

--- tools/fastdds/fastdds.bat
+++ tools/fastdds/fastdds.bat
@@ -1,24 +1,16 @@
 @echo off
-setlocal
 
-set dir=%~dp0
-set args=%1
+rem Check python
+set _interpreter=
+python --version | findstr /C:"Python 3" >nul
+if %ERRORLEVEL% equ 0 set _interpreter=python
+if %ERRORLEVEL% equ 1 py -0 2>&1 | findstr 3. >nul && set _interpreter=py -3
 
-:getarg
-shift
-if "%~1"=="" goto continue
-set args=%args% %1
-goto getarg
-
-:continue
-
-:: Check python
-py --list > NUL 2>&1
-
-if not %ERRORLEVEL%==0 (
-      echo python interpreter not found. Please, make sure its location is in the PATH environment variable.
-      exit /B 65
-   )
+if not defined _interpreter (
+  echo Suitable python interpreter not found.
+  echo Please, make sure its location is in the PATH environment variable.
+  exit /B 42 
 )
-:: Use launcher to profit from shebang hints on fastdds.py
-py "%dir%\..\tools\fastdds\fastdds.py" %args%
+
+rem Use launcher to profit from shebang hints on fastdds.py
+%_interpreter% %~dp0..\tools\fastdds\fastdds.py %*
