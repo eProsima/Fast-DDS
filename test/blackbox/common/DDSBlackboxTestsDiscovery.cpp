eProsima/Fast-DDS

commit 8a5a9160482b1543495c1ba49f3100fcceda12d9

Fix repeated matched event notification (#3396)
* Fix repeated matched event notification

Signed-off-by: Barry Xu <barry.xu@sony.com>

* Fix coding style

Signed-off-by: Barry Xu <barry.xu@sony.com>

---------

Signed-off-by: Barry Xu <barry.xu@sony.com>

src/cpp/fastdds/publisher/DataWriterImpl.cpp
@@ -1192,6 +1192,21 @@ void DataWriterImpl::InnerDataWriterListener::onWriterMatched(
         const PublicationMatchedStatus& info)
 {
     data_writer_->update_publication_matched_status(info);
+
+    StatusMask notify_status = StatusMask::publication_matched();
+    DataWriterListener* listener = data_writer_->get_listener_for(notify_status);
+    if (listener != nullptr)
+    {
+        PublicationMatchedStatus callback_status;
+        if (ReturnCode_t::RETCODE_OK == data_writer_->get_publication_matched_status(callback_status))
+        {
+            listener->on_publication_matched(data_writer_->user_datawriter_, callback_status);
+        }
+    }
+    else
+    {
+        data_writer_->user_datawriter_->get_statuscondition().get_impl()->set_status(notify_status, true);
+    }
 }
 
 void DataWriterImpl::InnerDataWriterListener::on_offered_incompatible_qos(
@@ -1337,16 +1352,6 @@ void DataWriterImpl::update_publication_matched_status(
         publication_matched_status_.total_count_change += count_change;
     }
     publication_matched_status_.last_subscription_handle = status.last_subscription_handle;
-
-    StatusMask notify_status = StatusMask::publication_matched();
-    DataWriterListener* listener = get_listener_for(notify_status);
-    if (listener != nullptr)
-    {
-        listener->on_publication_matched(user_datawriter_, publication_matched_status_);
-        publication_matched_status_.current_count_change = 0;
-        publication_matched_status_.total_count_change = 0;
-    }
-    user_datawriter_->get_statuscondition().get_impl()->set_status(notify_status, true);
 }
 
 ReturnCode_t DataWriterImpl::get_publication_matched_status(

src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -913,6 +913,21 @@ void DataReaderImpl::InnerDataReaderListener::onReaderMatched(
         const SubscriptionMatchedStatus& info)
 {
     data_reader_->update_subscription_matched_status(info);
+
+    StatusMask notify_status = StatusMask::subscription_matched();
+    DataReaderListener* listener = data_reader_->get_listener_for(notify_status);
+    if (listener != nullptr)
+    {
+        SubscriptionMatchedStatus callback_status;
+        if (ReturnCode_t::RETCODE_OK == data_reader_->get_subscription_matched_status(callback_status))
+        {
+            listener->on_subscription_matched(data_reader_->user_datareader_, callback_status);
+        }
+    }
+    else
+    {
+        data_reader_->user_datareader_->get_statuscondition().get_impl()->set_status(notify_status, true);
+    }
 }
 
 void DataReaderImpl::InnerDataReaderListener::on_liveliness_changed(
@@ -1100,16 +1115,6 @@ void DataReaderImpl::update_subscription_matched_status(
         history_.writer_not_alive(iHandle2GUID(status.last_publication_handle));
         try_notify_read_conditions();
     }
-
-    StatusMask notify_status = StatusMask::subscription_matched();
-    DataReaderListener* listener = get_listener_for(notify_status);
-    if (listener != nullptr)
-    {
-        listener->on_subscription_matched(user_datareader_, subscription_matched_status_);
-        subscription_matched_status_.current_count_change = 0;
-        subscription_matched_status_.total_count_change = 0;
-    }
-    user_datareader_->get_statuscondition().get_impl()->set_status(notify_status, true);
 }
 
 ReturnCode_t DataReaderImpl::get_subscription_matched_status(

commit 7123d353f4e630cfa55932ecf74b5eceb0670713

Fix regression introduced by #3396 (#3418)
* Refs #17919. Added regression test.

Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

* Refs #17919. Fix DataReaderImpl.

Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

* Refs #17919. Fix DataWriterImpl.

Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

---------

Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

src/cpp/fastdds/publisher/DataWriterImpl.cpp
@@ -1203,10 +1203,7 @@ void DataWriterImpl::InnerDataWriterListener::onWriterMatched(
             listener->on_publication_matched(data_writer_->user_datawriter_, callback_status);
         }
     }
-    else
-    {
-        data_writer_->user_datawriter_->get_statuscondition().get_impl()->set_status(notify_status, true);
-    }
+    data_writer_->user_datawriter_->get_statuscondition().get_impl()->set_status(notify_status, true);
 }
 
 void DataWriterImpl::InnerDataWriterListener::on_offered_incompatible_qos(

src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -924,10 +924,7 @@ void DataReaderImpl::InnerDataReaderListener::onReaderMatched(
             listener->on_subscription_matched(data_reader_->user_datareader_, callback_status);
         }
     }
-    else
-    {
-        data_reader_->user_datareader_->get_statuscondition().get_impl()->set_status(notify_status, true);
-    }
+    data_reader_->user_datareader_->get_statuscondition().get_impl()->set_status(notify_status, true);
 }
 
 void DataReaderImpl::InnerDataReaderListener::on_liveliness_changed(

test/blackbox/common/DDSBlackboxTestsDiscovery.cpp
@@ -1511,3 +1511,95 @@ TEST(DDSDiscovery, CheckIncorrectWriterDurabilityQosXmlStaticDiscoveryFile)
     DomainParticipantFactory* factory = DomainParticipantFactory::get_instance();
     ASSERT_EQ(factory->check_xml_static_discovery(file), ReturnCode_t::RETCODE_ERROR);
 }
+
+/**
+ * Regression test for redmine issue #17919
+ */
+static void test_DDSDiscovery_WaitSetMatchedStatus(
+        bool with_listener)
+{
+    using namespace eprosima;
+
+    auto factory = fastdds::dds::DomainParticipantFactory::get_instance();
+    fastdds::dds::DomainParticipantQos pqos;
+    factory->get_default_participant_qos(pqos);
+    uint32_t ignore_flags =
+            fastrtps::rtps::ParticipantFilteringFlags::FILTER_DIFFERENT_HOST |
+            fastrtps::rtps::ParticipantFilteringFlags::FILTER_DIFFERENT_PROCESS;
+    pqos.wire_protocol().builtin.discovery_config.ignoreParticipantFlags =
+            static_cast<fastrtps::rtps::ParticipantFilteringFlags>(ignore_flags);
+
+    fastdds::dds::DomainParticipantListener listener;
+    auto listener_to_use = with_listener ? &listener : nullptr;
+
+    fastdds::dds::DomainParticipant* participant = factory->create_participant(0, pqos, listener_to_use);
+    ASSERT_NE(participant, nullptr);
+
+    fastdds::dds::TypeSupport type(new HelloWorldPubSubType());
+    EXPECT_EQ(type.register_type(participant), ReturnCode_t::RETCODE_OK);
+
+    auto topic = participant->create_topic(TEST_TOPIC_NAME, type->getName(), fastdds::dds::TOPIC_QOS_DEFAULT);
+    ASSERT_NE(topic, nullptr);
+
+    auto publisher = participant->create_publisher(fastdds::dds::PUBLISHER_QOS_DEFAULT);
+    ASSERT_NE(publisher, nullptr);
+
+    auto data_writer = publisher->create_datawriter(topic, fastdds::dds::DATAWRITER_QOS_DEFAULT);
+    ASSERT_NE(data_writer, nullptr);
+
+    auto thread_run = [data_writer]()
+            {
+                fastdds::dds::WaitSet wait_set;
+                fastdds::dds::ConditionSeq triggered_conditions;
+
+                fastdds::dds::StatusCondition& condition = data_writer->get_statuscondition();
+                condition.set_enabled_statuses(fastdds::dds::StatusMask::publication_matched());
+                wait_set.attach_condition(condition);
+
+                fastdds::dds::PublicationMatchedStatus matched_status{};
+                while (1 > matched_status.current_count)
+                {
+                    ReturnCode_t ret_code = wait_set.wait(triggered_conditions, fastrtps::c_TimeInfinite);
+                    if (ReturnCode_t::RETCODE_OK != ret_code)
+                    {
+                        continue;
+                    }
+
+                    for (fastdds::dds::Condition* cond : triggered_conditions)
+                    {
+                        fastdds::dds::StatusCondition* status_cond = dynamic_cast<fastdds::dds::StatusCondition*>(cond);
+                        if (nullptr != status_cond)
+                        {
+                            EXPECT_EQ(status_cond, &condition);
+                            fastdds::dds::Entity* entity = status_cond->get_entity();
+                            EXPECT_EQ(entity, data_writer);
+                            fastdds::dds::StatusMask changed_statuses = entity->get_status_changes();
+
+                            if (changed_statuses.is_active(fastdds::dds::StatusMask::publication_matched()))
+                            {
+                                data_writer->get_publication_matched_status(matched_status);
+                            }
+                        }
+                    }
+                }
+            };
+
+    std::thread waiting_thread(thread_run);
+
+    auto subscriber = participant->create_subscriber(fastdds::dds::SUBSCRIBER_QOS_DEFAULT);
+    ASSERT_NE(subscriber, nullptr);
+
+    auto data_reader = subscriber->create_datareader(topic, fastdds::dds::DATAREADER_QOS_DEFAULT);
+    ASSERT_NE(data_reader, nullptr);
+
+    waiting_thread.join();
+
+    participant->delete_contained_entities();
+    factory->delete_participant(participant);
+}
+
+TEST(DDSDiscovery, WaitSetMatchedStatus)
+{
+    test_DDSDiscovery_WaitSetMatchedStatus(false);
+    test_DDSDiscovery_WaitSetMatchedStatus(true);
+}