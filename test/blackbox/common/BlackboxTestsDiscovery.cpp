49baf5154cd1c9c6185babbf7532fd842ec61cc6
Miguel Company <miguelcompany@eprosima.com>
2023-04-03 18:58:30 +0200

    Correctly assign multicast port to multicast initial peers (#3425)
    * Refs #10674. Add regression test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #10674. Correctly configure multicast initial peers.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- test/blackbox/common/BlackboxTestsDiscovery.cpp
+++ test/blackbox/common/BlackboxTestsDiscovery.cpp
@@ -1861,3 +1861,26 @@ TEST(Discovery, RemoteBuiltinEndpointHonoring)
     ASSERT_EQ(num_writer_heartbeat, 3u);
     ASSERT_EQ(num_writer_acknack, 3u);
 }
+
+//! Regression test for redmine issue 10674
+TEST(Discovery, MulticastInitialPeer)
+{
+    PubSubReader<HelloWorldPubSubType> reader(TEST_TOPIC_NAME);
+    PubSubWriter<HelloWorldPubSubType> writer(TEST_TOPIC_NAME);
+
+    eprosima::fastdds::rtps::LocatorList peers;
+    eprosima::fastdds::rtps::Locator loc{};
+    loc.kind = LOCATOR_KIND_UDPv4;
+    IPLocator::setIPv4(loc, "239.255.0.1");
+    peers.push_back(loc);
+
+    reader.participant_id(100).initial_peers(peers).init();
+    ASSERT_TRUE(reader.isInitialized());
+
+    writer.participant_id(101).initial_peers(peers).init();
+    ASSERT_TRUE(writer.isInitialized());
+
+    // Wait for discovery (times out before the fix).
+    writer.wait_discovery();
+    reader.wait_discovery();
+}
