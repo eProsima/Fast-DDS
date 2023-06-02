71afa9ab33a3078d3cf055d2b46813bd38a8a9a1
juanlofer-eprosima <88179026+juanlofer-eprosima@users.noreply.github.com>
2023-06-02 07:55:27 +0200

    Examples fix: delete topic after endpoint (#3558)
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>
    (cherry picked from commit 5f2621cebca3d9ad0c14e146eb660b9c78878c61)

--- examples/cpp/dds/BasicConfigurationExample/BasicConfigurationSubscriber.cpp
+++ examples/cpp/dds/BasicConfigurationExample/BasicConfigurationSubscriber.cpp
@@ -207,10 +207,6 @@ HelloWorldSubscriber::~HelloWorldSubscriber()
 {
     if (participant_ != nullptr)
     {
-        if (topic_ != nullptr)
-        {
-            participant_->delete_topic(topic_);
-        }
         if (subscriber_ != nullptr)
         {
             if (reader_ != nullptr)
@@ -218,6 +214,10 @@ HelloWorldSubscriber::~HelloWorldSubscriber()
                 subscriber_->delete_datareader(reader_);
             }
             participant_->delete_subscriber(subscriber_);
+        }
+        if (topic_ != nullptr)
+        {
+            participant_->delete_topic(topic_);
         }
         DomainParticipantFactory::get_instance()->delete_participant(participant_);
     }
