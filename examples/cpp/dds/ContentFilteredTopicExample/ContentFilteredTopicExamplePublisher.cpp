ea9ff1e011df97a04a523284054d0f72d28e1df8
Miguel Company <MiguelCompany@eprosima.com>
2023-06-15 09:31:28 +0200

    Refs #18825. Explicitly register type object in ContentFilteredTopicExample.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- examples/cpp/dds/ContentFilteredTopicExample/ContentFilteredTopicExamplePublisher.cpp
+++ examples/cpp/dds/ContentFilteredTopicExample/ContentFilteredTopicExamplePublisher.cpp
@@ -29,10 +29,16 @@
 #include <fastdds/dds/publisher/qos/DataWriterQos.hpp>
 #include <fastdds/dds/publisher/qos/PublisherQos.hpp>
 
+#include "HelloWorldTypeObject.h"
+
 using namespace eprosima::fastdds::dds;
 
 bool ContentFilteredTopicExamplePublisher::init()
 {
+    // The default filter class requires the TypeObject to be registered
+    // (see https://fast-dds.docs.eprosima.com/en/latest/fastdds/dds_layer/topic/contentFilteredTopic/createContentFilteredTopic.html)
+    registerHelloWorldTypes();
+
     // Initialize internal variables
     matched_ = 0;
 
