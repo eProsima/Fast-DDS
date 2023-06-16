5c9b31717581d023770ee19410a0d2626d3e0a82
Mario Dominguez <mariodominguez@eprosima.com>
2023-06-16 10:16:54 +0200

    Refs #18825: Fix for DDSQL filter test mac segfault
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- test/blackbox/common/DDSBlackboxTestsContentFilter.cpp
+++ test/blackbox/common/DDSBlackboxTestsContentFilter.cpp
@@ -597,12 +597,6 @@ TEST(DDSContentFilter, CorrectlyHandleAliasOtherHeader)
         throw std::runtime_error("Failed to register type");
     }
 
-    auto sub = participant->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
-    if (sub == nullptr)
-    {
-        throw std::runtime_error("Failed to create subscriber");
-    }
-
     auto topic = participant->create_topic("TestTopic", type->getName(), TOPIC_QOS_DEFAULT);
     if (topic == nullptr)
     {
@@ -616,6 +610,10 @@ TEST(DDSContentFilter, CorrectlyHandleAliasOtherHeader)
         "FilteredTestTopic", topic, expression, parameters);
 
     EXPECT_NE(nullptr, filtered_topic);
+
+    participant->delete_contentfilteredtopic(filtered_topic);
+    participant->delete_topic(topic);
+    dpf->delete_participant(participant);
 }
 
 #ifdef INSTANTIATE_TEST_SUITE_P
