440a51212d1795dc785da6a9841da76bed6ebeb5
Eduardo Ponz <eduardoponz@eprosima.com>
2023-06-26 12:06:58 +0200

    Refs #19054: Clean before running the test
    Signed-off-by: Eduardo Ponz <eduardoponz@eprosima.com>

--- cmake/common/test_wrapper.cmake
+++ cmake/common/test_wrapper.cmake
@@ -1,5 +1,5 @@
 find_package(PythonInterp 3)
 
-execute_process(COMMAND ${ACTUAL_TEST} ${ACTUAL_ARGS})
-
+# Clean first so the exit code comes from the second process
 execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/../../tools/fastdds/fastdds.py shm clean)
+execute_process(COMMAND ${ACTUAL_TEST} ${ACTUAL_ARGS})
