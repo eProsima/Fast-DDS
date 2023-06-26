89285f6bc43854558154fcc73dafc589529fbba4
Eduardo Ponz <eduardoponz@eprosima.com>
2023-06-26 14:56:24 +0200

    Refs #19054: Make test_wrapper.cmake a configurable file
    Signed-off-by: Eduardo Ponz <eduardoponz@eprosima.com>

--- cmake/common/test_wrapper.cmake
+++
@@ -1,5 +0,0 @@
-find_package(PythonInterp 3)
-
-# Clean first so the exit code comes from the second process
-execute_process(COMMAND ${PYTHON_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/../../tools/fastdds/fastdds.py shm clean)
-execute_process(COMMAND ${ACTUAL_TEST} ${ACTUAL_ARGS})
