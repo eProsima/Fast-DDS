e5acf4b3ef075fb6a39d8601d18b3a60fee61438
Miguel Company <MiguelCompany@eprosima.com>
2023-06-28 12:49:50 +0200

    Refs #18966. Retry try_push after regenerating output port.
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/rtps/transport/shared_mem/SharedMemTransport.cpp
+++ src/cpp/rtps/transport/shared_mem/SharedMemTransport.cpp
@@ -503,16 +503,20 @@ bool SharedMemTransport::push_discard(
     try
     {
         bool is_port_ok = false;
-        if (!find_port(remote_locator.port)->try_push(buffer, is_port_ok))
+        const size_t num_retries = 2;
+        for (size_t i = 0; i < num_retries && !is_port_ok; ++i)
         {
-            if (is_port_ok)
+            if (!find_port(remote_locator.port)->try_push(buffer, is_port_ok))
             {
-                EPROSIMA_LOG_INFO(RTPS_MSG_OUT, "Port " << remote_locator.port << " full. Buffer dropped");
-            }
-            else
-            {
-                EPROSIMA_LOG_WARNING(RTPS_MSG_OUT, "Port " << remote_locator.port << " inconsistent. Port dropped");
-                opened_ports_.erase(remote_locator.port);
+                if (is_port_ok)
+                {
+                    EPROSIMA_LOG_INFO(RTPS_MSG_OUT, "Port " << remote_locator.port << " full. Buffer dropped");
+                }
+                else
+                {
+                    EPROSIMA_LOG_WARNING(RTPS_MSG_OUT, "Port " << remote_locator.port << " inconsistent. Port dropped");
+                    opened_ports_.erase(remote_locator.port);
+                }
             }
         }
     }
