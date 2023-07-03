8af09414852942ec36c795883650de046782d6c0
AdrianCampo <adriancampo@eprosima.com>
2023-07-03 15:10:38 +0200

    Added operators for fixed_string<N>.
    Signed-off-by: AdrianCampo <adriancampo@eprosima.com>

--- include/fastrtps/utils/fixed_size_string.hpp
+++ include/fastrtps/utils/fixed_size_string.hpp
@@ -161,6 +161,18 @@ public:
         return strncmp(string_data, rhs.c_str(), MAX_CHARS) != 0;
     }
 
+    template<size_t N>  bool operator < (
+            const fixed_string<N>& rhs) const noexcept
+    {
+        return 0 > compare(rhs);
+    }
+
+    template<size_t N>  bool operator > (
+            const fixed_string<N>& rhs) const noexcept
+    {
+        return 0 < compare(rhs);
+    }
+
     bool operator < (
             const std::string& rhs) const noexcept
     {
