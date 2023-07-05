61dd20d8d9f58c04736cb50f18362078f650e121
Juan López Fernández <juanlopez@eprosima.com>
2023-07-05 15:00:27 +0200

    Add fix
    Signed-off-by: Juan López Fernández <juanlopez@eprosima.com>

--- src/cpp/rtps/participant/RTPSParticipantImpl.h
+++ src/cpp/rtps/participant/RTPSParticipantImpl.h
@@ -642,6 +642,12 @@ private:
     bool createSendResources(
             Endpoint* pend);
 
+    /** Add participant's external locators to endpoint's when none available
+        @param pend - Pointer to the endpoint whose external locators are to be set
+     */
+    void setupExternalLocators(
+            Endpoint* pend);
+
     /** When we want to create a new Resource but the physical channel specified by the Locator
         can not be opened, we want to mutate the Locator to open a more or less equivalent channel.
         @param loc -  Locator we want to change
