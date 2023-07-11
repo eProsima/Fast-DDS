8bb9ca493778dabb9a028d266b96e8708e9e73b7
Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
2023-07-11 09:23:15 +0200

    Add Monitor Service RTPS interfaces (#3662)
    * Refs #18069: Add monitor service RTPS interfaces
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #18069: Linter
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * REfs #18069: fix on interfaces include
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

---
+++ include/fastdds/statistics/rtps/monitor_service/interfaces/IConnectionsQueryable.hpp
@@ -0,0 +1,58 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file IConnectionsQueryable.hpp
+ *
+ */
+
+#ifndef _FASTDDS_STATISTICS_MONITOR_SERVICE_INTERFACES_ICONNECTIONSQUERYABLE_HPP_
+#define _FASTDDS_STATISTICS_MONITOR_SERVICE_INTERFACES_ICONNECTIONSQUERYABLE_HPP_
+
+#include <vector>
+
+#include <fastdds/rtps/common/Guid.h>
+
+namespace eprosima {
+namespace fastdds {
+namespace statistics {
+namespace rtps {
+
+using namespace eprosima::fastdds::statistics;
+
+class Connection;
+
+struct IConnectionsQueryable
+{
+    using ConnectionList = std::vector<Connection>;
+
+    /**
+     * @brief Interface for retrieving a list of Connections
+     * (announced locators, used_locators and mode)
+     * of a given entity, by guid
+     *
+     * @param guid The GUID_t identifying the target entity
+     * @return ConnectionList The Connections collection
+     */
+    virtual ConnectionList get_entity_connections(
+            const fastrtps::rtps::GUID_t& guid) = 0;
+};
+
+} // rtps
+} // statistics
+} // fastdds
+} // eprosima
+
+#endif // _FASTDDS_STATISTICS_MONITOR_SERVICE_INTERFACES_ICONNECTIONSQUERYABLE_HPP_
+
