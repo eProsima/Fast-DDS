65f9cc177173dd4a42500876a194a7f84cf73546
Miguel Company <miguelcompany@eprosima.com>
2023-09-04 07:33:02 +0200

    Platform implementations for set_name_to_current_thread (#3823)
    * Refs #17492. Added pthread implementation for set_name_to_current_thread
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #17492. Added implementation for Windows.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #17492. Added implementation for Mac.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #17492. Added doxygen to threading.hpp
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #17492. Using pthread implementation for Android.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #17492. Fix build error on snprintf.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>

--- src/cpp/utils/threading.hpp
+++ src/cpp/utils/threading.hpp
@@ -17,13 +17,39 @@
 
 namespace eprosima {
 
+/**
+ * @brief Give a name to the thread calling this function.
+ *
+ * @param[in]  name  A null-terminated string with the name to give to the calling thread.
+ *                   The implementation for certain platforms may truncate the final thread
+ *                   name if there is a limit on the length of the name of a thread.
+ */
 void set_name_to_current_thread(
         const char* name);
 
+/**
+ * @brief Give a name to the thread calling this function.
+ *
+ * @param[in]  fmt   A null-terminated string to be used as the format argument of
+ *                   a `snprintf` like function, in order to accomodate the restrictions of
+ *                   the OS. Those restrictions may truncate the final thread name if there
+ *                   is a limit on the length of the name of a thread.
+ * @param[in]  arg   Single variadic argument passed to the formatting function.
+ */
 void set_name_to_current_thread(
         const char* fmt,
         uint32_t arg);
 
+/**
+ * @brief Give a name to the thread calling this function.
+ *
+ * @param[in]  fmt   A null-terminated string to be used as the format argument of
+ *                   a `snprintf` like function, in order to accomodate the restrictions of
+ *                   the OS. Those restrictions may truncate the final thread name if there
+ *                   is a limit on the length of the name of a thread.
+ * @param[in]  arg1  First variadic argument passed to the formatting function.
+ * @param[in]  arg2  Second variadic argument passed to the formatting function.
+ */
 void set_name_to_current_thread(
         const char* fmt,
         uint32_t arg1,
