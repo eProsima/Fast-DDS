14debd769af5d4d4399861ffb244b250c62fc711
Mario Dominguez <mariodominguez@eprosima.com>
2023-09-07 15:24:33 +0200

    Refs #19402: MonitorServiceListener implementation
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- src/cpp/statistics/rtps/monitor-service/MonitorServiceListener.cpp
+++ src/cpp/statistics/rtps/monitor-service/MonitorServiceListener.cpp
@@ -18,6 +18,8 @@
 
 #include <statistics/rtps/monitor-service/MonitorServiceListener.hpp>
 
+#include <statistics/rtps/monitor-service/MonitorService.hpp>
+
 namespace eprosima {
 namespace fastdds {
 namespace statistics {
@@ -27,36 +29,58 @@ MonitorServiceListener::MonitorServiceListener(
         MonitorService* ms)
     : monitor_srv_(ms)
 {
-    //! TODO
-    static_cast<void>(monitor_srv_);
+
 }
 
 bool MonitorServiceListener::on_local_entity_status_change(
         const fastrtps::rtps::GUID_t& guid,
-        const uint32_t& id)
+        const uint32_t& id) const
 {
-    //! TODO
-    static_cast<void>(guid);
-    static_cast<void>(id);
-    return false;
+    bool ret = false;
+    ret = monitor_srv_->push_entity_update(guid.entityId, id);
+    return ret;
 }
 
 bool MonitorServiceListener::on_local_entity_change(
         const fastrtps::rtps::GUID_t& guid,
-        bool is_alive)
+        bool is_alive) const
 {
-    //! TODO
-    static_cast<void>(guid);
-    static_cast<void>(is_alive);
-    return false;
+    bool ret = false;
+
+    if (is_alive)
+    {
+        ret = monitor_srv_->push_entity_update(guid.entityId, PROXY);
+    }
+    else
+    {
+        ret = monitor_srv_->remove_local_entity(guid.entityId);
+    }
+
+    return ret;
 }
 
 bool MonitorServiceListener::on_local_entity_connections_change(
-        const fastrtps::rtps::GUID_t& guid)
+        const fastrtps::rtps::GUID_t& guid) const
 {
-    //! TODO
-    static_cast<void>(guid);
-    return false;
+    bool ret = false;
+    ret = monitor_srv_->push_entity_update(guid.entityId, CONNECTION_LIST);
+    return ret;
+}
+
+void MonitorServiceListener::onWriterMatched(
+        fastrtps::rtps::RTPSWriter* writer,
+        fastrtps::rtps::MatchingInfo& info)
+{
+    static_cast<void>(writer);
+    static_cast<void>(info);
+}
+
+void MonitorServiceListener::onWriterChangeReceivedByAll(
+        fastrtps::rtps::RTPSWriter* writer,
+        fastrtps::rtps::CacheChange_t* change)
+{
+    static_cast<void>(writer);
+    static_cast<void>(change);
 }
 
 } // namespace rtps
