1136245e4dd577e0f0a7d1c1310be8c04a4ba3c1
JesusPoderoso <jesuspoderoso@eprosima.com>
2023-09-28 08:56:12 +0200

    Refs #19089: Fix conflicts (regenerate types with proper fastddsgen & fastcdr versions)
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- examples/cpp/dds/HelloWorldExampleDataSharing/HelloWorld.h
+++ examples/cpp/dds/HelloWorldExampleDataSharing/HelloWorld.h
@@ -147,33 +147,33 @@ public:
      * @param _message New value to be copied in member message
      */
     eProsima_user_DllExport void message(
-            const eprosima::fastcdr::fixed_string<20>& _message);
+            const eprosima::fastrtps::fixed_string<20>& _message);
 
     /*!
      * @brief This function moves the value in member message
      * @param _message New value to be moved in member message
      */
     eProsima_user_DllExport void message(
-            eprosima::fastcdr::fixed_string<20>&& _message);
+            eprosima::fastrtps::fixed_string<20>&& _message);
 
     /*!
      * @brief This function returns a constant reference to member message
      * @return Constant reference to member message
      */
-    eProsima_user_DllExport const eprosima::fastcdr::fixed_string<20>& message() const;
+    eProsima_user_DllExport const eprosima::fastrtps::fixed_string<20>& message() const;
 
     /*!
      * @brief This function returns a reference to member message
      * @return Reference to member message
      */
-    eProsima_user_DllExport eprosima::fastcdr::fixed_string<20>& message();
+    eProsima_user_DllExport eprosima::fastrtps::fixed_string<20>& message();
 
     /*!
-    * @brief This function returns the maximum serialized size of an object
-    * depending on the buffer alignment.
-    * @param current_alignment Buffer alignment.
-    * @return Maximum serialized size.
-    */
+     * @brief This function returns the maximum serialized size of an object
+     * depending on the buffer alignment.
+     * @param current_alignment Buffer alignment.
+     * @return Maximum serialized size.
+     */
     eProsima_user_DllExport static size_t getMaxCdrSerializedSize(
             size_t current_alignment = 0);
 
@@ -227,14 +227,9 @@ public:
 
 private:
 
-<<<<<<< HEAD
     uint32_t m_index;
-    std::string m_message;
+    eprosima::fastrtps::fixed_string<20> m_message;
 
-=======
-    uint32_t m_index{0};
-    eprosima::fastcdr::fixed_string<20> m_message;
->>>>>>> 4fed0bd5d (Fix HelloWorld DataSharing example idl (#3714))
 };
 
 #endif // _FAST_DDS_GENERATED_HELLOWORLD_H_
