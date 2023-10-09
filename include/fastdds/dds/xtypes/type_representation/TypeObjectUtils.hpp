7f8e77a89b181445ea70b3a3072bddbadae7ced2
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-10-09 15:18:15 +0200

    Refs #19648: apply design suggestions
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

---
+++ include/fastdds/dds/xtypes/type_representation/TypeObjectUtils.hpp
@@ -0,0 +1,57 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*!
+ * @file
+ * This file contains static functions to help build a TypeObject.
+ */
+
+#ifndef _FASTDDS_DDS_XTYPES_TYPE_REPRESENTATION_TYPEOBJECTUTILS_HPP_
+#define _FASTDDS_DDS_XTYPES_TYPE_REPRESENTATION_TYPEOBJECTUTILS_HPP_
+
+#include <string>
+
+#include <fastdds/dds/xtypes/type_representation/TypeObject.h>
+#include <fastrtps/fastrtps_dll.h>
+#include <fastrtps/types/TypesBase.h>
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+namespace xtypes1_3 {
+
+using ReturnCode_t = eprosima::fastrtps::types::ReturnCode_t;
+
+class TypeObjectUtils
+{
+public:
+
+    /**
+     * @brief Setters: PENDING
+     * 
+     */
+
+private:
+
+    // Class with only static methods
+    TypeObjectUtils() = delete;
+    ~TypeObjectUtils() = delete;
+};
+
+} // xtypes1_3
+} // dds
+} // fastdds
+} // eprosima
+
+#endif // _FASTDDS_DDS_XTYPES_TYPE_REPRESENTATION_TYPEOBJECTUTILS_HPP_
