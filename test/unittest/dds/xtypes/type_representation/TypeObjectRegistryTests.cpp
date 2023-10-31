1f7cb6f065ccbf18a7622ad0409e3be321bb8403
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2023-10-31 12:58:25 +0100

    Refs #19837: add TypeObjectRegistry::register_type_identifier test
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

---
+++ test/unittest/dds/xtypes/type_representation/TypeObjectRegistryTests.cpp
@@ -0,0 +1,60 @@
+// Copyright 2023 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @file
+ * This file contains unit tests related to the TypeObjectRegistry API.
+ */
+
+#include <gtest/gtest.h>
+
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/xtypes/type_representation/TypeObject.hpp>
+
+namespace eprosima {
+namespace fastdds {
+namespace dds {
+namespace xtypes1_3 {
+
+TEST(TypeObjectRegistryTests, register_type_identifier)
+{
+    TypeIdentifier type_id;
+    type_id.equivalence_hash(EquivalenceHash());
+    EXPECT_EQ(ReturnCode_t::RETCODE_PRECONDITION_NOT_MET,
+        DomainParticipantFactory::get_instance()->type_object_registry().register_type_identifier("hash_type_id",
+            type_id));
+    StringSTypeDefn small_string;
+    small_string.bound(10);
+    type_id.string_sdefn(small_string);
+    EXPECT_EQ(ReturnCode_t::RETCODE_OK,
+        DomainParticipantFactory::get_instance()->type_object_registry().register_type_identifier("string_type_id",
+            type_id));
+    type_id.string_sdefn().bound(5);
+    EXPECT_EQ(ReturnCode_t::RETCODE_BAD_PARAMETER,
+        DomainParticipantFactory::get_instance()->type_object_registry().register_type_identifier("string_type_id",
+            type_id));
+}
+
+} // xtypes1_3
+} // dds
+} // fastdds
+} // eprosima
+
+int main(
+        int argc,
+        char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
