1e441b9a1079a340eff9d116a1c36ac27e420935
elianalf <62831776+elianalf@users.noreply.github.com>
2023-11-03 11:29:42 +0100

    Refs #19780: Uncrustify
    Signed-off-by: elianalf <62831776+elianalf@users.noreply.github.com>

--- src/cpp/rtps/transport/TCPv4Transport.cpp
+++ src/cpp/rtps/transport/TCPv4Transport.cpp
@@ -88,7 +88,7 @@ TCPv4Transport::TCPv4Transport(
             if (std::find_if(white_begin, white_end, [infoIP](const std::string& white_list_element)
                     {
                         return white_list_element == infoIP.dev || white_list_element == infoIP.name;
-                    }) != white_end )  
+                    }) != white_end )
             {
                 interface_whitelist_.emplace_back(ip::address_v4::from_string(infoIP.name));
             }
