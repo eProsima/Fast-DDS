488f74da01078470cd7644bfe6a90fdc1b6032dd
Ricardo González Moreno <ricardo@richiware.dev>
2023-12-14 16:15:04 +0100

    Refs 17138. New DynamicType API
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- include/fastdds/dds/xtypes/dynamic_types/DynamicTypeMember.hpp
+++ include/fastdds/dds/xtypes/dynamic_types/DynamicTypeMember.hpp
@@ -15,6 +15,7 @@
 #ifndef FASTDDS_DDS_XTYPES_DYNAMIC_TYPES_DYNAMIC_TYPE_MEMBER_HPP
 #define FASTDDS_DDS_XTYPES_DYNAMIC_TYPES_DYNAMIC_TYPE_MEMBER_HPP
 
+#include <map>
 #include <memory>
 
 #include <fastdds/dds/core/ReturnCode.hpp>
@@ -27,14 +28,6 @@
 namespace eprosima {
 namespace fastdds {
 namespace dds {
-
-class DynamicTypeMember;
-
-template<>
-struct traits<DynamicTypeMember> : public object_traits<DynamicTypeMember>
-{
-    using base_type = DynamicTypeMember;
-};
 
 /*!
  * Represents a "member" of a type. A "member" in this sense may be a member of an aggregated type, a constant within an
@@ -134,6 +127,10 @@ private:
     DynamicTypeMember& operator =(
             DynamicTypeMember&&) = delete;
 };
+
+typedef std::map<ObjectName, traits<DynamicTypeMember>::ref_type> DynamicTypeMembersByName;
+
+typedef std::map<MemberId, traits<DynamicTypeMember>::ref_type> DynamicTypeMembersById;
 
 } // namespace dds
 } // namespace fastdds
