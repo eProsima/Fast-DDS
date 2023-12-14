37cb6eb33d898c1e4ad9e31c2a1de93b126fe37d
Ricardo González Moreno <ricardo@richiware.dev>
2023-12-14 12:28:49 +0100

    Refs 17138. Apply suggestion
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- include/fastdds/dds/xtypes/dynamic_types/DynamicTypeMember.hpp
+++ include/fastdds/dds/xtypes/dynamic_types/DynamicTypeMember.hpp
@@ -48,7 +48,7 @@ public:
 
     /*!
      * Provides a summary of the state of this type overwriting a provided object (see [standard] 7.5.2.6.2)
-     * @param[inout] descriptor @ref MemberDescriptor reference where copied the information.
+     * @param[inout] descriptor @ref MemberDescriptor reference where  the information is copied.
      * @return standard @ref ReturnCode_t
      * @retval RETCODE_OK when the copy was successful.
      * @retval RETCODE_BAD_PARAMETER when descriptor reference is nil.
@@ -82,7 +82,7 @@ public:
 
     /*!
      * Returns an applied verbatim text by index.
-     * @param[inout] descriptor @ref VerbatimTextDescriptor reference where copied the information.
+     * @param[inout] descriptor @ref VerbatimTextDescriptor reference where the information is copied.
      * @param[in] idx Index.
      * @return standard @ref ReturnCode_t
      * @retval RETCODE_OK when the copy was successful.
