6647698fc876e9b1cdc605276c75506f7675d4fd
José Luis Bueno López <69244257+JLBuenoLopez-eProsima@users.noreply.github.com>
2023-12-16 17:34:16 +0100

    Fix memory problem when ciphering payload (#2485)
    * Refs 13364. Regression test.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13364. fix build when TLS_FOUND
    
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
    
    * Refs 13364. Test with different lengths.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13364. Apply review suggestions.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs 13364. Fix #2379.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Include what you use in DDSBlackboxTestsSecurity.cpp
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #13364. Fix warning.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    Co-authored-by: Miguel Company <MiguelCompany@eprosima.com>
    (cherry picked from commit 25dee0530d515b2903d1e026effcc1c6f1db8c70)

--- test/blackbox/common/TCPReqRepHelloWorldReplier.hpp
+++ test/blackbox/common/TCPReqRepHelloWorldReplier.hpp
@@ -138,7 +138,7 @@ public:
             int domainId,
             uint16_t listeningPort,
             uint32_t maxInitialPeer = 0,
-            const char* certs_path = nullptr,
+            const char* certs_folder = nullptr,
             bool use_busy_listener = false);
     bool isInitialized() const
     {
