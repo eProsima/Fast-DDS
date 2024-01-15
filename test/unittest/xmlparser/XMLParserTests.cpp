147a168f591975ded1381bb3acd46c1ff19fa89d
Miguel Company <miguelcompany@eprosima.com>
2024-01-15 16:54:24 +0100

    Improve environment variable substitution algorithm (#4236)
    * Refs #20186. Added regression test.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #20186. Silently ignore `${_}` expansion.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #20186. Improve substitution algorithm.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #20236. Fix linters.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- test/unittest/xmlparser/XMLParserTests.cpp
+++ test/unittest/xmlparser/XMLParserTests.cpp
@@ -70,6 +70,7 @@ TEST_F(XMLParserTests, regressions)
     EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/19851_profile_bin.xml", root));
     EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/simple_participant_profiles_nok.xml", root));
     EXPECT_EQ(XMLP_ret::XML_OK, XMLParser::loadXML("regressions/simple_participant_profiles_ok.xml", root));
+    EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/20186_profile_bin.xml", root));
 }
 
 TEST_F(XMLParserTests, NoFile)
