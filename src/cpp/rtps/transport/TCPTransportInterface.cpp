6738dd8565e1533c8779e856fe5753bbf86073c0
Jesús Pérez <78275223+jepemi@users.noreply.github.com>
2024-01-22 15:36:34 +0100

    Fix wrong log info messages on TCP (#4258)
    Signed-off-by: Jesus Perez <jesusperez@eprosima.com>
    (cherry picked from commit 36f7f57b039d9f762c9d04797c5ee5b34ac0ca1d)
    
    # Conflicts:
    #	src/cpp/rtps/transport/TCPTransportInterface.cpp

--- src/cpp/rtps/transport/TCPTransportInterface.cpp
+++ src/cpp/rtps/transport/TCPTransportInterface.cpp
@@ -805,6 +805,7 @@ void TCPTransportInterface::perform_listen_operation(
     if (rtcp_message_manager)
     {
         channel = channel_weak.lock();
+        endpoint_to_locator(channel->remote_endpoint(), remote_locator);
 
         if (channel)
         {
@@ -1288,9 +1289,17 @@ void TCPTransportInterface::SocketAccepted(
             channel->thread(std::thread(&TCPTransportInterface::perform_listen_operation, this,
                     channel_weak_ptr, rtcp_manager_weak_ptr));
 
+<<<<<<< HEAD
             logInfo(RTCP, " Accepted connection (local: " << IPLocator::to_string(locator)
                                                           << ", remote: " << channel->remote_endpoint().address()
                                                           << ":" << channel->remote_endpoint().port() << ")");
+=======
+            EPROSIMA_LOG_INFO(RTCP, "Accepted connection (local: "
+                    << channel->local_endpoint().address() << ":"
+                    << channel->local_endpoint().port() << "), remote: "
+                    << channel->remote_endpoint().address() << ":"
+                    << channel->remote_endpoint().port() << ")");
+>>>>>>> 36f7f57b0 (Fix wrong log info messages on TCP (#4258))
         }
         else
         {
@@ -1334,10 +1343,18 @@ void TCPTransportInterface::SecureSocketAccepted(
             secure_channel->thread(std::thread(&TCPTransportInterface::perform_listen_operation, this,
                     channel_weak_ptr, rtcp_manager_weak_ptr));
 
+<<<<<<< HEAD
             logInfo(RTCP, " Accepted connection (local: " << IPLocator::to_string(locator)
                                                           << ", remote: " << socket->lowest_layer().remote_endpoint().address()
                                                           << ":" << socket->lowest_layer().remote_endpoint().port() <<
                     ")");
+=======
+            EPROSIMA_LOG_INFO(RTCP, " Accepted connection (local: "
+                    << socket->lowest_layer().local_endpoint().address() << ":"
+                    << socket->lowest_layer().local_endpoint().port() << "), remote: "
+                    << socket->lowest_layer().remote_endpoint().address() << ":"
+                    << socket->lowest_layer().remote_endpoint().port() << ")");
+>>>>>>> 36f7f57b0 (Fix wrong log info messages on TCP (#4258))
         }
         else
         {
