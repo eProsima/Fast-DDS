9558ce436628bb0ce0bd76d44400efce1eed8378
Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
2024-01-27 09:16:07 +0100

    Add a keyed fragmented change to the reader data instance only when its completed (#4261)
    * Refs #20257: Add regression test
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * #Refs #20257: Fix
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #20239: Second rev suggestions
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #20257: Linter
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #20257: Retrieve instance handle condition before for avoid being nullptr
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- src/cpp/rtps/reader/StatelessReader.cpp
+++ src/cpp/rtps/reader/StatelessReader.cpp
@@ -714,6 +714,7 @@ bool StatelessReader::processDataFragMsg(
                             // Sample fits inside pending change. Reuse it.
                             work_change->copy_not_memcpy(change_to_add);
                             work_change->serializedPayload.length = sampleSize;
+                            work_change->instanceHandle.clear();
                             work_change->setFragmentSize(change_to_add->getFragmentSize(), true);
                         }
                         else
@@ -739,6 +740,7 @@ bool StatelessReader::processDataFragMsg(
                         {
                             work_change->copy_not_memcpy(change_to_add);
                             work_change->serializedPayload.length = sampleSize;
+                            work_change->instanceHandle.clear();
                             work_change->setFragmentSize(change_to_add->getFragmentSize(), true);
                         }
                     }
@@ -748,6 +750,12 @@ bool StatelessReader::processDataFragMsg(
                 CacheChange_t* change_completed = nullptr;
                 if (work_change != nullptr)
                 {
+                    // Set the instanceHandle only when fragment number 1 is received
+                    if (!work_change->instanceHandle.isDefined() && fragmentStartingNum == 1)
+                    {
+                        work_change->instanceHandle = change_to_add->instanceHandle;
+                    }
+
                     if (work_change->add_fragments(change_to_add->serializedPayload, fragmentStartingNum,
                             fragmentsInSubmessage))
                     {
