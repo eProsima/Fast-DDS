3be79af1cd88bc227d00bd79638cd3aa5c873c34
Irene Bandera <irenebandera@eprosima.com>
2024-01-30 15:28:34 +0100

    Add listener classes to handle cache change acknowledgments
    Signed-off-by: Irene Bandera <irenebandera@eprosima.com>

--- include/fastdds/dds/builtin/typelookup/TypeLookupRequestListener.hpp
+++ include/fastdds/dds/builtin/typelookup/TypeLookupRequestListener.hpp
@@ -21,6 +21,8 @@
 #define TYPELOOKUP_REQUEST_LISTENER_HPP_
 #ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
 #include <fastrtps/rtps/reader/ReaderListener.h>
+#include <fastrtps/rtps/writer/WriterListener.h>
+
 
 namespace eprosima {
 namespace fastrtps {
@@ -70,7 +72,37 @@ public:
      */
     void onNewCacheChangeAdded(
             fastrtps::rtps::RTPSReader* reader,
-            const fastrtps::rtps::CacheChange_t* const  change) override;
+            const fastrtps::rtps::CacheChange_t* const change) override;
+
+private:
+
+    //! A pointer to the typelookup manager
+    TypeLookupManager* tlm_;
+
+    //! A pointer to the TypeObject factory.
+    fastrtps::types::TypeObjectFactory* factory_;
+
+};
+
+class TypeLookupRequestWListener : public fastrtps::rtps::WriterListener
+{
+public:
+
+    /**
+     * @brief Constructor
+     * @param pwlp Pointer to the writer liveliness protocol
+     */
+    TypeLookupRequestWListener(
+            TypeLookupManager* pwlp);
+
+    /**
+     * @brief Destructor
+     */
+    virtual ~TypeLookupRequestWListener() override;
+
+    void onWriterChangeReceivedByAll(
+            fastrtps::rtps::RTPSWriter*,
+            fastrtps::rtps::CacheChange_t* change) override;
 
 private:
 
@@ -86,5 +118,5 @@ private:
 } /* namespace dds */
 } /* namespace fastdds */
 } /* namespace eprosima */
-#endif
+#endif // ifndef DOXYGEN_SHOULD_SKIP_THIS_PUBLIC
 #endif /* TYPELOOKUP_REQUEST_LISTENER_HPP_*/
