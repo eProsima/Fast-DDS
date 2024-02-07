435bdf5a32b0f73121fe8fc28a16411ae5bad2f8
EduPonz <eduardoponz@eprosima.com>
2024-02-07 07:42:41 +0100

    Refs #20318: Try to pass full runtime_list correctly
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- cmake/testing/GoogleTest.cmake
+++ cmake/testing/GoogleTest.cmake
@@ -19,8 +19,7 @@ function(gtest_discover_tests TARGET)
     if (WIN32)
         add_custom_command(
             TARGET ${TARGET} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> -DRUNTIME_LIST=$<TARGET_RUNTIME_DLLS:${TARGET}> -P ${CMAKE_SOURCE_DIR}/cmake/testing/generate_google_test_win_wrapper.cmake
-            COMMAND_EXPAND_LISTS
+            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> "-DRUNTIME_LIST=$<TARGET_RUNTIME_DLLS:${TARGET}>" -P ${CMAKE_SOURCE_DIR}/cmake/testing/generate_google_test_win_wrapper.cmake
             VERBATIM
             )
 
