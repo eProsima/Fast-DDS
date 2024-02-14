d5e6ee3ad2c66f8dbbf64a22189d0ddd80b701b7
Juan Lopez Fernandez <juanlopez@eprosima.com>
2024-02-14 09:56:37 +0100

    Fix data race on PDP
    Signed-off-by: Juan Lopez Fernandez <juanlopez@eprosima.com>

--- src/cpp/rtps/builtin/discovery/participant/PDPClient.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPClient.cpp
@@ -472,7 +472,6 @@ bool PDPClient::create_ds_pdp_reliable_endpoints(
     return true;
 }
 
-// the ParticipantProxyData* pdata must be the one kept in PDP database
 void PDPClient::assignRemoteEndpoints(
         ParticipantProxyData* pdata)
 {
@@ -488,8 +487,7 @@ void PDPClient::assignRemoteEndpoints(
             {
                 if (data_matches_with_prefix(svr.guidPrefix, *pdata))
                 {
-                    std::unique_lock<std::recursive_mutex> lock(*getMutex());
-                    svr.proxy = pdata;
+                    svr.is_connected = true;
                 }
             }
         }
@@ -517,11 +515,11 @@ void PDPClient::notifyAboveRemoteEndpoints(
         {
             if (data_matches_with_prefix(svr.guidPrefix, pdata))
             {
-                if (nullptr == svr.proxy)
+                if (!svr.is_connected && nullptr != get_participant_proxy_data(svr.guidPrefix))
                 {
-                    //! try to retrieve the participant proxy data from an unmangled prefix in case
-                    //! we could not fill svr.proxy in assignRemoteEndpoints()
-                    svr.proxy = get_participant_proxy_data(svr.guidPrefix);
+                    //! mark proxy as connected from an unmangled prefix in case
+                    //! it could not be done in assignRemoteEndpoints()
+                    svr.is_connected = true;
                 }
 
                 match_pdp_reader_nts_(svr, pdata.m_guid.guidPrefix);
@@ -596,7 +594,7 @@ void PDPClient::removeRemoteEndpoints(
             if (svr.guidPrefix == pdata->m_guid.guidPrefix)
             {
                 std::unique_lock<std::recursive_mutex> lock(*getMutex());
-                svr.proxy = nullptr; // reasign when we receive again server DATA(p)
+                svr.is_connected = false;
                 is_server = true;
                 mp_sync->restart_timer(); // enable announcement and sync mechanism till this server reappears
             }
@@ -768,11 +766,11 @@ void PDPClient::announceParticipantState(
                     for (auto& svr : mp_builtin->m_DiscoveryServers)
                     {
                         // if we are matched to a server report demise
-                        if (svr.proxy != nullptr)
+                        if (svr.is_connected)
                         {
                             //locators.push_back(svr.metatrafficMulticastLocatorList);
                             locators.push_back(svr.metatrafficUnicastLocatorList);
-                            remote_readers.emplace_back(svr.proxy->m_guid.guidPrefix,
+                            remote_readers.emplace_back(svr.guidPrefix,
                                     endpoints->reader.reader_->getGuid().entityId);
                         }
                     }
@@ -805,7 +803,7 @@ void PDPClient::announceParticipantState(
                     {
                         // non-pinging announcements like lease duration ones must be
                         // broadcast to all servers
-                        if (svr.proxy == nullptr || !_serverPing)
+                        if (!svr.is_connected || !_serverPing)
                         {
                             locators.push_back(svr.metatrafficMulticastLocatorList);
                             locators.push_back(svr.metatrafficUnicastLocatorList);
