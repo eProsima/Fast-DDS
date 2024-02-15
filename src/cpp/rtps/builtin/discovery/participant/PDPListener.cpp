c501c471f74e62c5537bde538fe2081a463d5f1e
juanlofer-eprosima <88179026+juanlofer-eprosima@users.noreply.github.com>
2024-02-15 11:12:58 +0100

    Fix data race on PDP (#4382)
    Signed-off-by: Juan Lopez Fernandez <juanlopez@eprosima.com>

--- src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDPListener.cpp
@@ -177,11 +177,14 @@ void PDPListener::process_alive_data(
         // Create a new one when not found
         old_data = parent_pdp_->createParticipantProxyData(new_data, writer_guid);
 
-        reader->getMutex().unlock();
-        lock.unlock();
-
         if (old_data != nullptr)
         {
+            // Copy proxy to be passed forward before releasing PDP mutex
+            ParticipantProxyData old_data_copy(*old_data);
+
+            reader->getMutex().unlock();
+            lock.unlock();
+
             // Assigning remote endpoints implies sending a DATA(p) to all matched and fixed readers, since
             // StatelessWriter::matched_reader_add marks the entire history as unsent if the added reader's
             // durability is bigger or equal to TRANSIENT_LOCAL_DURABILITY_QOS (TRANSIENT_LOCAL or TRANSIENT),
@@ -192,7 +195,12 @@ void PDPListener::process_alive_data(
             // participant is discovered in the middle of BuiltinProtocols::initBuiltinProtocols, which will
             // create the first DATA(p) upon finishing, thus triggering the sent to all fixed and matched
             // readers anyways.
-            parent_pdp_->assignRemoteEndpoints(old_data);
+            parent_pdp_->assignRemoteEndpoints(&old_data_copy);
+        }
+        else
+        {
+            reader->getMutex().unlock();
+            lock.unlock();
         }
     }
     else
@@ -211,6 +219,9 @@ void PDPListener::process_alive_data(
             parent_pdp_->mp_EDP->assignRemoteEndpoints(*old_data, true);
         }
 
+        // Copy proxy to be passed forward before releasing PDP mutex
+        ParticipantProxyData old_data_copy(*old_data);
+
         lock.unlock();
 
         RTPSParticipantListener* listener = parent_pdp_->getRTPSParticipant()->getListener();
@@ -220,7 +231,7 @@ void PDPListener::process_alive_data(
 
             {
                 std::lock_guard<std::mutex> cb_lock(parent_pdp_->callback_mtx_);
-                ParticipantDiscoveryInfo info(*old_data);
+                ParticipantDiscoveryInfo info(old_data_copy);
                 info.status = ParticipantDiscoveryInfo::CHANGED_QOS_PARTICIPANT;
 
                 listener->onParticipantDiscovery(
