d48969f61c722af8b25b1309521ca4a41f0025e5
Eduardo Ponz Segrelles <eduardoponz@eprosima.com>
2024-02-16 09:35:57 +0100

    Restore Blackbox tests names (#4235)
    * Refs #20234: Restore Blackbox tests names
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    * Refs #20234: Correctly add xfail tests
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>
    
    ---------
    
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- test/blackbox/xfail_tests.cmake
+++ test/blackbox/xfail_tests.cmake
@@ -1,27 +1,55 @@
 set(XFAIL_DDS_PIM_TESTS
-    Discovery.ParticipantLivelinessAssertion
-    LivelinessQos/LivelinessQos.LongLiveliness_ManualByParticipant_BestEffort/Transport
-    LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_Automatic_BestEffort/Intraprocess
-    LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_BestEffort/Intraprocess
-    LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_BestEffort/Transport
-    LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_Reliable/Transport
-    LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Intraprocess
-    LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Transport
-    LivelinessQos/LivelinessQos.TwoWriters_OneReader_ManualByParticipant/Intraprocess
-    PersistenceLargeData/PersistenceLargeData.PubSubAsReliablePubPersistentWithFrag/Transport
+    BlackboxTests_DDS_PIM.Discovery.ParticipantLivelinessAssertion
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.LongLiveliness_ManualByParticipant_BestEffort/Transport
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_Automatic_BestEffort/Intraprocess
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_BestEffort/Intraprocess
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_BestEffort/Transport
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_Reliable/Transport
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Intraprocess
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Transport
+    BlackboxTests_DDS_PIM.LivelinessQos/LivelinessQos.TwoWriters_OneReader_ManualByParticipant/Intraprocess
+    BlackboxTests_DDS_PIM.PersistenceLargeData/PersistenceLargeData.PubSubAsReliablePubPersistentWithFrag/Transport
 
     ##########################################################
     # These test are here because of TCP instabilities
     ##########################################################
-    ChainingTransportTests.builtin_transports_api_large_data
-    ChainingTransportTests.builtin_transports_api_large_datav6
-    ChainingTransportTests.builtin_transports_env_large_data
-    ChainingTransportTests.builtin_transports_env_large_datav6
-    ChainingTransportTests.builtin_transports_xml_large_data
-    ChainingTransportTests.builtin_transports_xml_large_datav6
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_api_large_data
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_api_large_datav6
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_env_large_data
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_env_large_datav6
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_xml_large_data
+    BlackboxTests_DDS_PIM.ChainingTransportTests.builtin_transports_xml_large_datav6
+    ##########################################################
+    )
+
+set(XFAIL_FASTRTPS_TESTS
+    BlackboxTests_FastRTPS.Discovery.ParticipantLivelinessAssertion
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.LongLiveliness_ManualByParticipant_BestEffort/Transport
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_Automatic_BestEffort/Intraprocess
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_BestEffort/Intraprocess
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_BestEffort/Transport
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ShortLiveliness_ManualByTopic_Automatic_Reliable/Transport
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Intraprocess
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.ThreeWriters_ThreeReaders/Transport
+    BlackboxTests_FastRTPS.LivelinessQos/LivelinessQos.TwoWriters_OneReader_ManualByParticipant/Intraprocess
+    BlackboxTests_FastRTPS.PersistenceLargeData/PersistenceLargeData.PubSubAsReliablePubPersistentWithFrag/Transport
+
+    ##########################################################
+    # These test are here because of TCP instabilities
+    ##########################################################
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_api_large_data
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_api_large_datav6
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_env_large_data
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_env_large_datav6
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_xml_large_data
+    BlackboxTests_FastRTPS.ChainingTransportTests.builtin_transports_xml_large_datav6
     ##########################################################
     )
 
 set_tests_properties(${XFAIL_DDS_PIM_TESTS}
+    PROPERTIES LABELS xfail
+    )
+
+set_tests_properties(${XFAIL_FASTRTPS_TESTS}
     PROPERTIES LABELS xfail
     )
