abe4c568bdf9479e289af891e581dc87f4fe4d13
Ricardo González Moreno <ricardo@richiware.dev>
2024-02-19 16:11:05 +0100

    Refs #17138. Remove unused header
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- include/fastdds/dds/xtypes/dynamic_types/DynamicDataFactory.hpp
+++ include/fastdds/dds/xtypes/dynamic_types/DynamicDataFactory.hpp
@@ -19,7 +19,6 @@
 
 #include <fastdds/dds/core/ReturnCode.hpp>
 #include <fastdds/dds/xtypes/dynamic_types/DynamicData.hpp>
-#include <fastdds/dds/xtypes/Types.hpp>
 #include <fastdds/fastdds_dll.hpp>
 
 namespace eprosima {
--- src/cpp/xmlparser/XMLDynamicParser.cpp
+++ src/cpp/xmlparser/XMLDynamicParser.cpp
@@ -1031,7 +1031,7 @@ traits<DynamicType>::ref_type XMLParser:: parseXMLMemberDynamicType(
         }
 
         const char* lengthStr = p_root->Attribute(SEQ_MAXLENGTH);
-        uint32_t length = MAX_ELEMENTS_COUNT;
+        uint32_t length = static_cast<uint32_t>(LENGTH_UNLIMITED);
         if (lengthStr != nullptr)
         {
             try
@@ -1114,7 +1114,7 @@ traits<DynamicType>::ref_type XMLParser:: parseXMLMemberDynamicType(
         }
 
         const char* lengthStr = p_root->Attribute(MAP_MAXLENGTH);
-        uint32_t length = MAX_ELEMENTS_COUNT;
+        uint32_t length = static_cast<uint32_t>(LENGTH_UNLIMITED);
         if (lengthStr != nullptr)
         {
             try
