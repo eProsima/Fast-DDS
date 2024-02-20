68acb5a6cc9b45010f555af932b2f4a475362ab3
Jesús Poderoso <120394830+JesusPoderoso@users.noreply.github.com>
2024-02-20 14:33:22 +0100

    Check History QoS inconsistencies (#4375)
    * Refs #20401: Add regression test
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    * Refs #20401: Check History QoS inconsistencies
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    * Refs #20401: Check also the history depth vs resource limits max_sample_per_instance bounds
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    * Refs #20401: Update HeartbeatWhileDestruction test
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    * Refs #20401: Update unit test DDS profiles XML tests profile
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>
    
    ---------
    
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- src/cpp/fastdds/subscriber/DataReaderImpl.cpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -1501,6 +1501,19 @@ ReturnCode_t DataReaderImpl::check_qos(
         EPROSIMA_LOG_ERROR(DDS_QOS_CHECK, "unique_network_request cannot be set along specific locators");
         return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
     }
+    if (qos.history().kind == KEEP_LAST_HISTORY_QOS && qos.history().depth <= 0)
+    {
+        EPROSIMA_LOG_ERROR(RTPS_QOS_CHECK, "HISTORY DEPTH must be higher than 0 if HISTORY KIND is KEEP_LAST.");
+        return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
+    }
+    if (qos.history().kind == KEEP_LAST_HISTORY_QOS && qos.history().depth > 0 &&
+            qos.resource_limits().max_samples_per_instance > 0 &&
+            qos.history().depth > qos.resource_limits().max_samples_per_instance)
+    {
+        EPROSIMA_LOG_ERROR(RTPS_QOS_CHECK,
+                "HISTORY DEPTH must be lower or equal to the max_samples_per_instance value.");
+        return ReturnCode_t::RETCODE_INCONSISTENT_POLICY;
+    }
     return ReturnCode_t::RETCODE_OK;
 }
 
