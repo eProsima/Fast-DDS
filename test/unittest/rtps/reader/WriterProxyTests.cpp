796daeb3db3dd873454ed618b33eeed7a5871a4f
EduPonz <eduardoponz@eprosima.com>
2024-02-24 16:00:35 +0100

    Refs #20511: Support GTest v1.14.0
    Signed-off-by: EduPonz <eduardoponz@eprosima.com>

--- test/unittest/rtps/reader/WriterProxyTests.cpp
+++ test/unittest/rtps/reader/WriterProxyTests.cpp
@@ -30,65 +30,7 @@
 
 #include <rtps/reader/WriterProxy.cpp>
 
-// Make SequenceNumberSet_t compatible with GMock macros
-
-namespace testing {
-namespace internal {
-using namespace eprosima::fastrtps::rtps;
-
-template<>
-bool AnyEq::operator ()(
-        const SequenceNumberSet_t& a,
-        const SequenceNumberSet_t& b) const
-{
-    // remember that using SequenceNumberSet_t = BitmapRange<SequenceNumber_t, SequenceNumberDiff, 256>;
-    // see test\unittest\utils\BitmapRangeTests.cpp method TestResult::Check
-
-    if (a.empty() && b.empty())
-    {
-        return true;
-    }
-
-    if (a.base() == b.base())
-    {
-        uint32_t num_bits[2];
-        uint32_t num_longs[2];
-        SequenceNumberSet_t::bitmap_type bitmap[2];
-
-        a.bitmap_get(num_bits[0], bitmap[0], num_longs[0]);
-        b.bitmap_get(num_bits[1], bitmap[1], num_longs[1]);
-
-        if (num_bits[0] != num_bits[1] || num_longs[0] != num_longs[1])
-        {
-            return false;
-        }
-        return std::equal(bitmap[0].cbegin(), bitmap[0].cbegin() + num_longs[0], bitmap[1].cbegin());
-    }
-    else
-    {
-        bool equal = true;
-
-        a.for_each([&b, &equal](const SequenceNumber_t& e)
-                {
-                    equal &= b.is_set(e);
-                });
-
-        if (!equal)
-        {
-            return false;
-        }
-
-        b.for_each([&a, &equal](const SequenceNumber_t& e)
-                {
-                    equal &= a.is_set(e);
-                });
-
-        return equal;
-    }
-}
-
-} // namespace internal
-} // namespace testing
+#include "../../common/operators.hpp"
 
 namespace eprosima {
 namespace fastrtps {
