251cca7130e43721e6d29d5153759a35200db174
JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>
2024-02-27 15:42:18 +0100

    Refs #20424: sequences
    Signed-off-by: JLBuenoLopez-eProsima <joseluisbueno@eprosima.com>

--- test/feature/dynamic_types/DynamicTypesDDSTypesTest.hpp
+++ test/feature/dynamic_types/DynamicTypesDDSTypesTest.hpp
@@ -60,6 +60,8 @@ constexpr const char* bitfield_d = "d";
 constexpr const char* bounded_string_alias = "Inner_alias_bounded_string_helper";
 constexpr const char* bounded_wstring_alias = "Inner_alias_bounded_wstring_helper";
 constexpr const char* array_alias = "Inner_alias_array_helper";
+constexpr const char* seq_alias = "Inner_alias_sequence_helper";
+constexpr const char* map_alias = "Inner_alias_map_helper";
 
 constexpr const char* var_short_name = "var_short";
 constexpr const char* var_ushort_name = "var_ushort";
@@ -129,12 +131,14 @@ public:
     static DynamicType::_ref_type create_inner_enum_helper();
     static DynamicType::_ref_type create_inner_bitmask_helper();
     static DynamicType::_ref_type create_inner_alias_helper();
-    static DynamicType::_ref_type create_inner_union_helper();
     static DynamicType::_ref_type create_inner_struct_helper();
+    static DynamicType::_ref_type create_inner_union_helper();
     static DynamicType::_ref_type create_inner_bitset_helper();
     static DynamicType::_ref_type create_inner_alias_bounded_string_helper();
     static DynamicType::_ref_type create_inner_alias_bounded_wstring_helper();
     static DynamicType::_ref_type create_inner_alias_array_helper();
+    static DynamicType::_ref_type create_inner_alias_sequence_helper();
+    static DynamicType::_ref_type create_inner_alias_map_helper();
 
 };
 
