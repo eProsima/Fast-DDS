d551272396135c20bc753c42ccd6fdf5d35a3255
Miguel Company <miguelcompany@eprosima.com>
2024-03-04 17:42:55 +0100

    Allow persistent
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- src/cpp/fastdds/subscriber/DataReaderImpl.cpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -1499,11 +1499,6 @@ ReturnCode_t DataReaderImpl::check_qos_including_resource_limits(
 ReturnCode_t DataReaderImpl::check_qos(
         const DataReaderQos& qos)
 {
-    if (qos.durability().kind == PERSISTENT_DURABILITY_QOS)
-    {
-        EPROSIMA_LOG_ERROR(DDS_QOS_CHECK, "PERSISTENT Durability not supported");
-        return ReturnCode_t::RETCODE_UNSUPPORTED;
-    }
     if (qos.destination_order().kind == BY_SOURCE_TIMESTAMP_DESTINATIONORDER_QOS)
     {
         EPROSIMA_LOG_ERROR(DDS_QOS_CHECK, "BY SOURCE TIMESTAMP DestinationOrder not supported");
