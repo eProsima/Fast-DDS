ac42325acb84047b02330fc67f60c77336068bca
Carlos Ferreira González <carlosferreira@eprosima.com>
2024-03-08 09:39:54 +0100

    Protect asio exception hotfix (#4527)
    * Refs #20599: Handle error code before function call
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    
    * Apply suggestion
    
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    Co-authored-by: Miguel Company <miguelcompany@eprosima.com>
    (cherry picked from commit 08193d5f7eb4dcf3de8ecee3725f57bee28d35cd)
    
    # Conflicts:
    #	src/cpp/rtps/transport/TCPTransportInterface.cpp

--- src/cpp/rtps/transport/TCPTransportInterface.cpp
+++ src/cpp/rtps/transport/TCPTransportInterface.cpp
@@ -221,6 +221,43 @@ void TCPTransportInterface::clean()
     }
 }
 
+<<<<<<< HEAD
+=======
+Locator TCPTransportInterface::remote_endpoint_to_locator(
+        const std::shared_ptr<TCPChannelResource>& channel) const
+{
+    Locator locator;
+    asio::error_code ec;
+    auto endpoint = channel->remote_endpoint(ec);
+    if (ec)
+    {
+        LOCATOR_INVALID(locator);
+    }
+    else
+    {
+        endpoint_to_locator(endpoint, locator);
+    }
+    return locator;
+}
+
+Locator TCPTransportInterface::local_endpoint_to_locator(
+        const std::shared_ptr<TCPChannelResource>& channel) const
+{
+    Locator locator;
+    asio::error_code ec;
+    auto endpoint = channel->local_endpoint(ec);
+    if (ec)
+    {
+        LOCATOR_INVALID(locator);
+    }
+    else
+    {
+        endpoint_to_locator(endpoint, locator);
+    }
+    return locator;
+}
+
+>>>>>>> 08193d5f7 (Protect asio exception hotfix (#4527))
 void TCPTransportInterface::bind_socket(
         std::shared_ptr<TCPChannelResource>& channel)
 {
