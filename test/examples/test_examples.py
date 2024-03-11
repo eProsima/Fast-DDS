61e12c4c00e7286f3716263b9c98c50435691bd3
JesusPoderoso <jesuspoderoso@eprosima.com>
2024-03-11 10:07:02 +0100

    Refs #20543: Include example in test automotation
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- test/examples/test_examples.py
+++ test/examples/test_examples.py
@@ -25,6 +25,46 @@ def test_basic_configuration():
                 received += 1
                 continue
 
+            print(line)
+
+        if sent * 2 == received:
+            ret = True
+        else:
+            raise subprocess.CalledProcessError(1, '')
+
+    except subprocess.CalledProcessError:
+        for l in out:
+            print(l)
+    except subprocess.TimeoutExpired:
+        print('TIMEOUT')
+
+    assert(ret)
+
+def test_hello_world():
+    """."""
+    ret = False
+    out = ''
+    try:
+        out = subprocess.check_output(
+            '@DOCKER_EXECUTABLE@ compose -f hello_world.compose.yml up',
+            stderr=subprocess.STDOUT,
+            shell=True,
+            timeout=10
+        ).decode().split('\n')
+
+        sent = 0
+        received = 0
+        for line in out:
+            if 'SENT' in line:
+                sent += 1
+                continue
+
+            if 'RECEIVED' in line:
+                received += 1
+                continue
+
+            print(line)
+
         if sent * 2 == received:
             ret = True
         else:
