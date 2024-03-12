4d6539b4f6c702910b2b47d4e9a24fe84ea8052d
Des glaneurs <146199020+Desglaneurs@users.noreply.github.com>
2024-03-12 17:34:01 +0900

    Merge pull request from GHSA-9m2j-qw67-ph4w
    * Refs #20549: Add BB test
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Fix:Fixed integer overflow causing heap overflow
    
    When a sub node receives a manipulated DATA sub-message, an Integer Overflow occurs in uint32_t payload_size. This causes a heap buffer overflow error. A comparison statement was inserted before the line that calculates the variable, which fixes the error.
    
    Signed-off-by: Desglaneurs <cveissacgleaning@gmail.com>
    
    * Refs #20549: Fix review
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #201549: Reset the change data fields before exiting
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    Signed-off-by: Desglaneurs <cveissacgleaning@gmail.com>
    Co-authored-by: Mario Dominguez <mariodominguez@eprosima.com>

---
+++ test/blackbox/common/UDPMessageSender.hpp
@@ -0,0 +1,34 @@
+#include <asio/io_service.hpp>
+#include <asio/ip/udp.hpp>
+
+#include <fastdds/rtps/common/CDRMessage_t.h>
+#include <fastrtps/utils/IPLocator.h>
+
+using namespace eprosima::fastrtps;
+using namespace eprosima::fastrtps::rtps;
+
+struct UDPMessageSender
+{
+    asio::io_service service;
+    asio::ip::udp::socket socket;
+
+    UDPMessageSender()
+        : service()
+        , socket(service)
+    {
+        socket.open(asio::ip::udp::v4());
+    }
+
+    void send(
+            const CDRMessage_t& msg,
+            const Locator_t& destination)
+    {
+        std::string addr = IPLocator::toIPv4string(destination);
+        unsigned short port = static_cast<unsigned short>(destination.port);
+        auto remote = asio::ip::udp::endpoint(asio::ip::address::from_string(addr), port);
+        asio::error_code ec;
+
+        socket.send_to(asio::buffer(msg.buffer, msg.length), remote, 0, ec);
+    }
+
+};
--- test/blackbox/utils/data_generators.cpp
+++ test/blackbox/utils/data_generators.cpp
@@ -277,3 +277,24 @@ std::list<KeyedData1mb> default_keyeddata300kb_data_generator(
 
     return returnedValue;
 }
+
+std::list<UnboundedHelloWorld> default_unbounded_helloworld_data_generator(
+        size_t max)
+{
+    uint16_t index = 1;
+    size_t maximum = max ? max : 10;
+    std::list<UnboundedHelloWorld> returnedValue(maximum);
+
+    std::generate(returnedValue.begin(), returnedValue.end(), [&index]
+            {
+                UnboundedHelloWorld hello;
+                hello.index(index);
+                std::stringstream ss;
+                ss << "HelloWorld " << index;
+                hello.message(ss.str());
+                ++index;
+                return hello;
+            });
+
+    return returnedValue;
+}
