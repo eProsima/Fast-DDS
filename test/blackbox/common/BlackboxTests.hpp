9956bb0ea05016ecda8062c7f6a20da8510ad699
Des glaneurs <146199020+Desglaneurs@users.noreply.github.com>
2024-03-12 17:34:01 +0900

    Merge pull request from GHSA-9m2j-qw67-ph4w
    * Refs #20549: Add BB test
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Fix:Fixed integer overflow causing heap overflow
    
    When a sub node receives a manipulated DATA sub-message, an Integer Overflow occurs in uint32_t payload_size. This causes a heap buffer overflow error. A comparison statement was inserted before the line that calculates the variable, which fixes the error.
    
    Signed-off-by: Desglaneurs <cveissacgleaning@gmail.com>
    
    * Refs #20549: Fix review
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #201549: Reset the change data fields before exiting
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    Signed-off-by: Desglaneurs <cveissacgleaning@gmail.com>
    Co-authored-by: Mario Dominguez <mariodominguez@eprosima.com>

--- test/blackbox/common/BlackboxTests.hpp
+++ test/blackbox/common/BlackboxTests.hpp
@@ -31,13 +31,14 @@
 #include <unistd.h>
 #endif // if defined(_WIN32)
 
-#include "../types/HelloWorldPubSubTypes.h"
+#include "../types/Data1mbPubSubTypes.h"
+#include "../types/Data64kbPubSubTypes.h"
 #include "../types/FixedSizedPubSubTypes.h"
+#include "../types/HelloWorldPubSubTypes.h"
+#include "../types/KeyedData1mbPubSubTypes.h"
 #include "../types/KeyedHelloWorldPubSubTypes.h"
 #include "../types/StringTestPubSubTypes.h"
-#include "../types/Data64kbPubSubTypes.h"
-#include "../types/Data1mbPubSubTypes.h"
-#include "../types/KeyedData1mbPubSubTypes.h"
+#include "../types/UnboundedHelloWorldPubSubTypes.h"
 
 #include <algorithm>
 #include <cstddef>
@@ -168,6 +169,9 @@ std::list<Data1mb> default_data96kb_data300kb_data_generator(
         size_t max = 0);
 
 std::list<KeyedData1mb> default_keyeddata300kb_data_generator(
+        size_t max = 0);
+
+std::list<UnboundedHelloWorld> default_unbounded_helloworld_data_generator(
         size_t max = 0);
 
 /****** Auxiliary lambda functions  ******/
