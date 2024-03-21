1c9c7befc7863dcd7381bdfcd3d1fe6c162632ea
cferreiragonz <carlosferreira@eprosima.com>
2024-03-21 16:36:39 +0100

    Refs #20629: Update CLI for GUID_LESS DS when no server_id is provided
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- test/system/tools/fds/tests.py
+++ test/system/tools/fds/tests.py
@@ -499,6 +499,17 @@ def test_fast_discovery_backup(fast_discovery_tool):
     sys.exit(exit_code)
 
 
+def test_fast_discovery_no_prefix(fast_discovery_tool):
+    """Test to set a random GUID when no server ID is provided"""
+
+    XML_file_path = "test_xml_discovery_server_profile.xml"
+    command = [fast_discovery_tool, '-x', 'UDP_no_prefix@' + XML_file_path]
+    output, err, exit_code = send_command(command)
+    msg = "Using random GUID because no server ID was specified (-i option) or defined in the XML file."
+    exit_code = check_output(output, err, msg, False)
+    sys.exit(exit_code)
+
+
 def test_fast_discovery_no_XML(fast_discovery_tool):
     """Test that checks output when the XML file provided does not exist"""
 
@@ -525,16 +536,6 @@ def test_fast_discovery_incorrect_participant(fast_discovery_tool):
     output, err, exit_code = send_command(command)
 
     exit_code = check_output(output, err, "The provided configuration is not valid", True)
-    sys.exit(exit_code)
-
-
-def test_fast_discovery_no_prefix(fast_discovery_tool):
-    """Test failure when no server ID is provided"""
-
-    XML_file_path = "test_wrong_xml_discovery_server_profile.xml"
-    command = [fast_discovery_tool, '-x', 'UDP_no_prefix@' + XML_file_path]
-    output, err, exit_code = send_command(command)
-    exit_code = check_output(output, err, "Server id is mandatory if not defined in the XML file", True)
     sys.exit(exit_code)
 
 
