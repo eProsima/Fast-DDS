1d82d1f635c17edd72a44e7f8af9f88d6b734e9d
JesusPoderoso <jesuspoderoso@eprosima.com>
2024-04-02 23:28:23 +0200

    Refs #20722: Apply rev suggestions
    Signed-off-by: JesusPoderoso <jesuspoderoso@eprosima.com>

--- src/cpp/fastrtps_deprecated/publisher/PublisherHistory.cpp
+++ src/cpp/fastrtps_deprecated/publisher/PublisherHistory.cpp
@@ -66,6 +66,11 @@ PublisherHistory::PublisherHistory(
     , resource_limited_qos_(topic_att.resourceLimitsQos)
     , topic_att_(topic_att)
 {
+    if (resource_limited_qos_.max_samples <= 0)
+    {
+        resource_limited_qos_.max_samples = std::numeric_limits<int32_t>::max();
+    }
+
     if (resource_limited_qos_.max_instances <= 0)
     {
         resource_limited_qos_.max_instances = std::numeric_limits<int32_t>::max();
