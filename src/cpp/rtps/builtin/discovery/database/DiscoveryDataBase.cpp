9fe3e9b2f2bcf960bf8059cab090ae014187bc83
cferreiragonz <carlosferreira@eprosima.com>
2024-04-12 13:06:00 +0200

    Refs #20629: Add server pdp to queue on client match
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
@@ -2344,6 +2344,14 @@ bool DiscoveryDataBase::add_pdp_to_send_(
     return false;
 }
 
+bool DiscoveryDataBase::add_own_pdp_to_send_()
+{
+    auto our_data_it = participants_.find(server_guid_prefix_);
+    assert(our_data_it != participants_.end());
+
+    return add_pdp_to_send_(our_data_it->second.change());
+}
+
 bool DiscoveryDataBase::add_edp_publications_to_send_(
         eprosima::fastdds::rtps::CacheChange_t* change)
 {
