06b2e81fb95b8839fe1c31b1fc16e37c7ea6d79f
Miguel Company <miguelcompany@eprosima.com>
2024-04-17 16:33:29 +0200

    Internal refactor on port handling (#3440)
    * Refs #18002. Avoid mutation of multicast ports.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Move security initalization up.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Prepare for refactor of locators setup.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Refactor metatraffic related code.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Refactor initial peers related code.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Refactor user traffic related code.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Refactor output traffic related code.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Safe metatraffic unicast port on a new attribute.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. User unicast port calculated on participant instead of NetworkFactory.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Refs #18002. Update metatraffic_unicast_port_ inside applyLocatorAdaptRule.
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    
    * Apply suggestion
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #18002. Always create unicast receiver resources first.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #18002. Improve warning message.
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #18002. Refactor createReceiverResources
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    * Refs #18002. Improve SHM.SamePortUnicastMulticast blackbox test
    
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    
    ---------
    
    Signed-off-by: Miguel Company <MiguelCompany@eprosima.com>
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    (cherry picked from commit ea5af48d0d6f4c4e71e4ca3ad421b3b78fc1fffe)
    
    # Conflicts:
    #	src/cpp/rtps/network/NetworkFactory.h
    #	src/cpp/rtps/participant/RTPSParticipantImpl.cpp

--- src/cpp/rtps/network/NetworkFactory.h
+++ src/cpp/rtps/network/NetworkFactory.h
@@ -195,18 +195,15 @@ public:
      * Add locators to the default unicast configuration.
      * */
     bool getDefaultUnicastLocators(
-            uint32_t domain_id,
             LocatorList_t& locators,
-            const RTPSParticipantAttributes& m_att) const;
+            uint32_t port) const;
 
     /**
      * Fill the locator with the default unicast configuration.
      * */
     bool fill_default_locator_port(
-            uint32_t domain_id,
             Locator_t& locator,
-            const RTPSParticipantAttributes& m_att,
-            bool is_multicast) const;
+            uint32_t port) const;
 
     /**
      * Shutdown method to close the connections of the transports.
@@ -230,6 +227,22 @@ public:
             const LocatorList_t& remote_participant_locators,
             const LocatorList_t& participant_initial_peers) const;
 
+<<<<<<< HEAD
+=======
+    /**
+     * Returns transports' netmask filter information (transport's netmask filter kind and allowlist).
+     */
+    std::vector<fastdds::rtps::TransportNetmaskFilterInfo> netmask_filter_info() const;
+
+    /**
+     * Calculate well-known ports.
+     */
+    uint16_t calculate_well_known_port(
+            uint32_t domain_id,
+            const RTPSParticipantAttributes& att,
+            bool is_multicast) const;
+
+>>>>>>> ea5af48d0 (Internal refactor on port handling (#3440))
 private:
 
     std::vector<std::unique_ptr<fastdds::rtps::TransportInterface>> mRegisteredTransports;
@@ -244,6 +257,7 @@ private:
     // Whether multicast metatraffic on SHM transport should always be used
     bool enforce_shm_multicast_metatraffic_ = false;
 
+<<<<<<< HEAD
     /**
      * Calculate well-known ports.
      */
@@ -251,6 +265,10 @@ private:
             uint32_t domain_id,
             const RTPSParticipantAttributes& att,
             bool is_multicast) const;
+=======
+    // Mask using transport kinds to indicate whether the transports allows localhost
+    NetworkConfigSet_t network_configuration_;
+>>>>>>> ea5af48d0 (Internal refactor on port handling (#3440))
 };
 
 } // namespace rtps
--- src/cpp/rtps/participant/RTPSParticipantImpl.h
+++ src/cpp/rtps/participant/RTPSParticipantImpl.h
@@ -523,6 +523,8 @@ private:
     uint32_t domain_id_;
     //!Attributes of the RTPSParticipant
     RTPSParticipantAttributes m_att;
+    //! Metatraffic unicast port used by default on this participant
+    uint32_t metatraffic_unicast_port_ = 0;
     //!Guid of the RTPSParticipant.
     GUID_t m_guid;
     //! String containing the RTPSParticipant Guid.
@@ -594,6 +596,11 @@ private:
     std::set<GUID_t> ignored_readers_;
     //! Protect ignored entities collection concurrent access
     mutable shared_mutex ignored_mtx_;
+
+    void setup_meta_traffic();
+    void setup_user_traffic();
+    void setup_initial_peers();
+    void setup_output_traffic();
 
     RTPSParticipantImpl& operator =(
             const RTPSParticipantImpl&) = delete;
