fa790fe19648a953cacae14116c076d2dc7d336e
Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
2024-04-19 13:49:51 +0200

    Make `get_first_untaken_info()` coherent with `read()/take()` (#4696)
    * Refs #20706: Add regression BB test
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #20706: Fix
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #20706: Apply review suggestions
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    (cherry picked from commit 5fbd88ed2bcaebccf6b1d1132bb9710fd5d5aca2)

--- src/cpp/fastdds/subscriber/DataReaderImpl.hpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl.hpp
@@ -188,7 +188,8 @@ public:
             SampleInfoSeq& sample_infos);
 
     /**
-     * @brief Returns information about the first untaken sample.
+     * @brief Returns information about the first untaken sample. This method is meant to be called prior to
+     * a read() or take() operation as it does not modify the status condition of the entity.
      * @param [out] info Pointer to a SampleInfo structure to store first untaken sample information.
      * @return true if sample info was returned. false if there is no sample to take.
      */
