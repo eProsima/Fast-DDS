1f80c012b2a62fc5b8d001a300068ffb8fd7adff
eduponz <eduardoponz@eprosima.com>
2024-06-18 15:25:29 +0200

    Refs #21188: Update header guards in other examples
    Signed-off-by: eduponz <eduardoponz@eprosima.com>

--- examples/cpp/content_filter/CLIParser.hpp
+++ examples/cpp/content_filter/CLIParser.hpp
@@ -12,15 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef FASTDDS_EXAMPLES_CPP_CONTENT_FILTER__CLIPARSER_HPP
+#define FASTDDS_EXAMPLES_CPP_CONTENT_FILTER__CLIPARSER_HPP
+
 #include <csignal>
 #include <cstdlib>
 #include <iostream>
 #include <thread>
 
 #include <fastdds/dds/log/Log.hpp>
-
-#ifndef FASTDDS_EXAMPLES_CPP_CONTENT_FILTER__CLIPARSER_HPP
-#define FASTDDS_EXAMPLES_CPP_CONTENT_FILTER__CLIPARSER_HPP
 
 namespace eprosima {
 namespace fastdds {
