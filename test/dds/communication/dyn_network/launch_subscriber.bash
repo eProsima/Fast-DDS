0ac94ca10baac1c22298731614ad6105e1c46529
Miguel Company <miguelcompany@eprosima.com>
2024-09-30 10:53:48 +0200

    Refs #21690. Add docker infrastructure.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

---
+++ test/dds/communication/dyn_network/launch_subscriber.bash
@@ -0,0 +1,9 @@
+echo "Putting down eth0 interface..."
+ifconfig eth0 down
+echo "Launching subscriber..."
+${EXAMPLE_DIR}/DDSCommunicationSubscriber --xmlfile ${EXAMPLE_DIR}/simple_reliable_profile.xml --samples 10 --rescan 2 &
+echo "Subscriber launched. Waiting 2 seconds and bring up eth0 interface..."
+sleep 2s
+ifconfig eth0 up
+echo "eth0 interface is up. Waiting 3s for the matching to occur..."
+sleep 3s
