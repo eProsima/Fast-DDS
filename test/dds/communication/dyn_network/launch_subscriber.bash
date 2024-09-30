ea60f90d9a2029c91e5b3d92a36896e0c7de867f
Miguel Company <miguelcompany@eprosima.com>
2024-09-30 15:25:09 +0200

    Refs #21690. Ensure same domain and topic name are used.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- test/dds/communication/dyn_network/launch_subscriber.bash
+++ test/dds/communication/dyn_network/launch_subscriber.bash
@@ -1,7 +1,7 @@
 echo "Putting down eth0 interface..."
 ifconfig eth0 down
 echo "Launching subscriber..."
-${EXAMPLE_DIR}/DDSCommunicationSubscriber --xmlfile ${EXAMPLE_DIR}/simple_reliable_profile.xml --samples 10 --rescan 2 &
+${EXAMPLE_DIR}/DDSCommunicationSubscriber --xmlfile ${EXAMPLE_DIR}/simple_reliable_profile.xml --samples 10 --seed 0 --magic T --rescan 2 &
 echo "Subscriber launched. Waiting 2 seconds and bring up eth0 interface..."
 sleep 2s
 ifconfig eth0 up
