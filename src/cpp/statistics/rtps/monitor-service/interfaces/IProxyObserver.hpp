4e1c6926cb930277d88b5a2587eba4a09ea7cbf1
Mario Dominguez <mariodominguez@eprosima.com>
2024-10-21 16:36:00 +0200

    Refs #21841: Update IProxyObserver
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- src/cpp/statistics/rtps/monitor-service/interfaces/IProxyObserver.hpp
+++ src/cpp/statistics/rtps/monitor-service/interfaces/IProxyObserver.hpp
@@ -20,10 +20,12 @@
 #ifndef _FASTDDS_STATISTICS_MONITOR_SERVICE_INTERFACES_IPROXYOBSERVER_HPP_
 #define _FASTDDS_STATISTICS_MONITOR_SERVICE_INTERFACES_IPROXYOBSERVER_HPP_
 
+#include <fastdds/dds/core/policy/QosPolicies.hpp>
 #include <fastdds/rtps/common/Guid.hpp>
 
 namespace eprosima {
 namespace fastdds {
+
 namespace statistics {
 namespace rtps {
 
@@ -41,6 +43,31 @@ struct IProxyObserver
     virtual bool on_local_entity_change(
             const fastdds::rtps::GUID_t& guid,
             bool is_alive) const = 0;
+
+    /**
+     * @brief Interface use to notify about any updates
+     * against remote entities incompatible qos matching.
+     *
+     * @param local_guid The GUID_t identifying the local entity
+     * @param remote_guid The GUID_t identifying the remote entity
+     * @param incompatible_qos The PolicyMask with the incompatible QoS
+     *
+     * @return Whether the implementor has been properly notified
+     */
+    virtual bool on_incompatible_qos_matching(
+            const fastdds::rtps::GUID_t& local_guid,
+            const fastdds::rtps::GUID_t& remote_guid,
+            const fastdds::dds::PolicyMask& incompatible_qos) const = 0;
+
+    /**
+     * @brief Method to notify the implementor that a remote proxy
+     * data has been removed. This is interesting to notify proxy removals
+     * independently of the the remote entity being matched or not.
+     *
+     * @return If the operation has been properly notified.
+     */
+    virtual bool on_remote_proxy_data_removed(
+            const fastdds::rtps::GUID_t& removed_proxy_guid) const = 0;
 };
 
 } // rtps
