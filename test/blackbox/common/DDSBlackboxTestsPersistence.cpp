ed12e9131eef7e5ffad7dad3bdd3ed286d0e451a
Ricardo González Moreno <ricardo@richiware.dev>
2024-10-29 14:11:33 +0100

    Refs #22021. Regression test
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/blackbox/common/DDSBlackboxTestsPersistence.cpp
+++ test/blackbox/common/DDSBlackboxTestsPersistence.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstdio>
 #include <thread>
 
 #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
