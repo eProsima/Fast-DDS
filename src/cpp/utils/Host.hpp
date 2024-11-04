8652e3a2520199a6f52569f8cb193ea629f29e68
cferreiragonz <carlosferreira@eprosima.com>
2024-11-04 11:53:19 +0100

    Refs #22056: compute_machine_id() method
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- src/cpp/utils/Host.hpp
+++ src/cpp/utils/Host.hpp
@@ -15,17 +15,31 @@
 #ifndef UTILS_HOST_HPP_
 #define UTILS_HOST_HPP_
 
+#include <fastcdr/cdr/fixed_size_string.hpp>
+
 #include <fastdds/dds/log/Log.hpp>
 #include <fastdds/rtps/common/LocatorList.hpp>
 #include <fastdds/utils/IPFinder.hpp>
 #include <fastdds/utils/md5.hpp>
+
+#if defined(_WIN32)
+#include <WinSock2.h> // Avoid conflicts with WinSock of Windows.h
+#include <windows.h>
+#include <process.h>
+#elif defined(__APPLE__)
+#include <IOKit/IOKitLib.h>
+#else
+#include <unistd.h>
+#include <fcntl.h>
+#endif // if defined(_WIN32)
+
 
 namespace eprosima {
 
 
 /**
  * This singleton generates a host_id based on system interfaces
- * ip addresses or mac addresses
+ * ip addresses, mac addresses or the machine UUID.
  */
 class Host
 {
@@ -51,6 +65,11 @@ public:
     inline uint48 mac_id() const
     {
         return mac_id_;
+    }
+
+    inline fastcdr::string_255 machine_id() const
+    {
+        return machine_id_;
     }
 
     static Host& instance()
@@ -131,10 +150,56 @@ private:
                 mac_id_.value[i + 1] = (id_ & 0xFF);
             }
         }
+
+        // Compute the machine id hash
+        machine_id_ = compute_machine_id();
+        if (machine_id_ == "")
+        {
+            EPROSIMA_LOG_WARNING(UTILS, "Cannot get machine id. Failing back to IP based ID");
+            machine_id_ = std::to_string(id_);
+        }
+    }
+
+
+    static std::string compute_machine_id()
+    {
+    #ifdef _WIN32
+        char machine_id[255];
+        DWORD BufferSize = sizeof(machine_id);
+        LONG res = RegGetValueA(HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Cryptography", "MachineGuid", RRF_RT_REG_SZ, NULL, machine_id, &BufferSize);
+        if (res == 0)
+        {
+            return std::string(machine_id);
+        }
+        return "";
+    #elif defined(__APPLE__)
+        io_registry_entry_t ioRegistryRoot = IORegistryEntryFromPath(kIOMasterPortDefault, "IOService:/");
+        CFStringRef uuidCf = (CFStringRef) IORegistryEntryCreateCFProperty(ioRegistryRoot, CFSTR(kIOPlatformUUIDKey), kCFAllocatorDefault, 0);
+        IOObjectRelease(ioRegistryRoot);
+        CFStringGetCString(uuidCf, buf, 255, kCFStringEncodingMacRoman);
+        CFRelease(uuidCf);
+        return std::string(buf, 255);
+    #else
+        int fd = open("/etc/machine-id", O_RDONLY);
+        if (fd == -1) {
+            return "";
+        }
+
+        char buffer[33] = {0};
+        ssize_t bytes_read = read(fd, buffer, 32);
+        close(fd);
+
+        if (bytes_read < 32) {
+            return "";
+        }
+
+        return std::string(buffer, 32);
+    #endif // if defined(_WIN32)
     }
 
     uint16_t id_;
     uint48 mac_id_;
+    fastcdr::string_255 machine_id_;
 };
 
 } // eprosima
