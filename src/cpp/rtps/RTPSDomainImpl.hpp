d831eb16ff5c404228e192a82961ef46d7479ca9
Mario Domínguez López <116071334+Mario-DL@users.noreply.github.com>
2024-11-14 11:03:19 +0100

    [22024] Improve `OpenSSL` lifecycle handling (#5384)
    * Refs #22024: Add BB test
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #22024: Make OpenSSLInit Mayers singleton
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #22024: Fix: Do not register atexit in OPenSSL. Instead, Comply with OpenSSL initialization and destruction.
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    * Refs #22024: Do not reference OpenSSLInit if security features are no present
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    
    ---------
    
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>
    (cherry picked from commit 44310c4f8efabfacc55d14604d25175d8f33070a)
    
    # Conflicts:
    #	src/cpp/rtps/RTPSDomainImpl.hpp

--- src/cpp/rtps/RTPSDomainImpl.hpp
+++ src/cpp/rtps/RTPSDomainImpl.hpp
@@ -31,7 +31,15 @@
 
 #include <utils/SystemInfo.hpp>
 
+<<<<<<< HEAD
 #include <utils/shared_memory/BoostAtExitRegistry.hpp>
+=======
+#if HAVE_SECURITY
+#include <security/OpenSSLInit.hpp>
+#endif // HAVE_SECURITY
+
+#include <fastdds/xtypes/type_representation/TypeObjectRegistry.hpp>
+>>>>>>> 44310c4f8 ([22024] Improve `OpenSSL` lifecycle handling (#5384))
 
 namespace eprosima {
 namespace fastrtps {
@@ -237,6 +245,9 @@ private:
     std::shared_ptr<eprosima::detail::BoostAtExitRegistry> boost_singleton_handler_ { eprosima::detail::
                                                                                               BoostAtExitRegistry::
                                                                                               get_instance() };
+#if HAVE_SECURITY
+    std::shared_ptr<security::OpenSSLInit> openssl_singleton_handler_{ security::OpenSSLInit::get_instance() };
+#endif // HAVE_SECURITY
 
     std::mutex m_mutex;
 
