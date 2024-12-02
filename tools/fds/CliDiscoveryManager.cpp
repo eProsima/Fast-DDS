dbcf28bed8ce9c133ad759a52b5fd2d50e78d46a
Mario Dominguez <mariodominguez@eprosima.com>
2024-12-02 09:50:57 +0100

    Refs #22352: Fix MacOS build
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- tools/fds/CliDiscoveryManager.cpp
+++ tools/fds/CliDiscoveryManager.cpp
@@ -24,6 +24,10 @@
 #include <stdlib.h>
 #include <fstream>
 
+#if __APPLE__
+#include <signal.h>
+#endif // if __APPLE__
+
 #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
 #include <fastdds/dds/log/Log.hpp>
 #include <fastdds/rtps/attributes/RTPSParticipantAttributes.hpp>
@@ -972,7 +976,7 @@ int CliDiscoveryManager::fastdds_discovery_auto(
     // Auto mode should check if there is a server created for the domain specified, if not, create one.
     int return_value = 0;
 
-    const option::Option* pOp = options[DOMAIN];
+    const option::Option* pOp = options[DOMAIN_OPT];
     DomainId_t id = get_domain_id(pOp);
 
     if (isServerRunning(id))
@@ -1011,7 +1015,7 @@ int CliDiscoveryManager::fastdds_discovery_start(
         return 1;
     }
 
-    const option::Option* pOp = options[DOMAIN];
+    const option::Option* pOp = options[DOMAIN_OPT];
     DomainId_t id = get_domain_id(pOp);
     if (isServerRunning(id))
     {
@@ -1050,7 +1054,7 @@ int CliDiscoveryManager::fastdds_discovery_stop(
     // Stop a server for the domain specified
     int return_value = 0;
 
-    const option::Option* pOp = options[DOMAIN];
+    const option::Option* pOp = options[DOMAIN_OPT];
     DomainId_t id = get_domain_id(pOp);
 
     if (!isServerRunning(id))
@@ -1099,7 +1103,7 @@ int CliDiscoveryManager::fastdds_discovery_add(
         return 1;
     }
 
-    const option::Option* pOp = options[DOMAIN];
+    const option::Option* pOp = options[DOMAIN_OPT];
     DomainId_t id = get_domain_id(pOp);
     if (!isServerRunning(id))
     {
@@ -1138,7 +1142,7 @@ int CliDiscoveryManager::fastdds_discovery_set(
         return 1;
     }
 
-    const option::Option* pOp = options[DOMAIN];
+    const option::Option* pOp = options[DOMAIN_OPT];
     DomainId_t id = get_domain_id(pOp);
     if (!isServerRunning(id))
     {
