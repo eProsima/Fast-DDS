cfd2c033476f3340b6334baa6286bc5f5536c87e
cferreiragonz <carlosferreira@eprosima.com>
2025-01-30 12:21:36 +0100

    Refs #22721: Easy Mode XML Tests
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- test/unittest/dds/participant/ParticipantTests.cpp
+++ test/unittest/dds/participant/ParticipantTests.cpp
@@ -81,6 +81,12 @@
 #define GET_PID getpid
 #endif // if defined(_WIN32)
 
+void stop_background_servers()
+{
+    // Stop server(s)
+    int res = std::system("fastdds discovery stop");
+    ASSERT_EQ(res, 0);
+}
 
 namespace eprosima {
 namespace fastdds {
@@ -1077,6 +1083,17 @@ void set_environment_variable(
 #endif // _WIN32
 }
 
+void set_easy_mode_environment_variable(
+        const std::string ip = "127.0.0.1"
+        )
+{
+#ifdef _WIN32
+    ASSERT_EQ(0, _putenv_s(rtps::EASY_MODE_URI, ip.c_str()));
+#else
+    ASSERT_EQ(0, setenv(rtps::EASY_MODE_URI, ip.c_str(), 1));
+#endif // _WIN32
+}
+
 void set_environment_file(
         const std::string& filename)
 {
@@ -1481,6 +1498,91 @@ TEST(ParticipantTests, ServerParticipantRemoteServerListConfiguration)
     DomainParticipantQos result_qos = participant->get_qos();
     EXPECT_EQ(RETCODE_OK, participant->set_qos(result_qos));
     EXPECT_EQ(RETCODE_OK, DomainParticipantFactory::get_instance()->delete_participant(participant));
+}
+
+TEST(ParticipantTests, EasyModeParticipantLoadsServiceDataWriterQos)
+{
+    // Use get_publisher_qos_from_profile to check if the profile is correctly loaded.
+    // But we cannot check the actual value of the max_blocking_time from qos because it belongs to the PublisherAttributes
+    {
+        // Default participant
+        DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(
+            (uint32_t)GET_PID() % 230, PARTICIPANT_QOS_DEFAULT);
+        ASSERT_NE(nullptr, participant);
+        Publisher* default_publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+        ASSERT_NE(default_publisher, nullptr);
+        PublisherQos profile_qos;
+        EXPECT_EQ(default_publisher->get_participant()->get_publisher_qos_from_profile("service", profile_qos),
+                RETCODE_BAD_PARAMETER);
+
+        EXPECT_EQ(RETCODE_OK, participant->delete_publisher(default_publisher));
+        EXPECT_EQ(RETCODE_OK, DomainParticipantFactory::get_instance()->delete_participant(participant));
+    }
+
+    {
+        // Easy mode participant
+        set_easy_mode_environment_variable();
+        DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(
+            (uint32_t)GET_PID() % 230, PARTICIPANT_QOS_DEFAULT);
+        ASSERT_NE(nullptr, participant);
+        Publisher* easy_mode_publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+        ASSERT_NE(easy_mode_publisher, nullptr);
+        PublisherQos profile_qos;
+        EXPECT_EQ(easy_mode_publisher->get_participant()->get_publisher_qos_from_profile("service", profile_qos),
+                RETCODE_OK);
+        TypeSupport type(new TopicDataTypeMock());
+        type.register_type(participant, "footype");
+        Topic* topic = participant->create_topic("footopic", "footype", TOPIC_QOS_DEFAULT);
+        ASSERT_NE(nullptr, topic);
+        DataWriter* data_writer = easy_mode_publisher->create_datawriter_with_profile(topic, "service");
+        ASSERT_NE(nullptr, data_writer);
+        DataWriterQos dw_qos;
+        EXPECT_EQ(data_writer->get_qos(dw_qos), RETCODE_OK);
+        EXPECT_EQ(dw_qos.reliability().max_blocking_time.seconds, 1);
+        EXPECT_EQ(dw_qos.reliability().max_blocking_time.nanosec, 0);
+        EXPECT_EQ(dw_qos.durability().kind, TRANSIENT_LOCAL_DURABILITY_QOS);  // Default value
+
+        EXPECT_EQ(RETCODE_OK, easy_mode_publisher->delete_datawriter(data_writer));
+        EXPECT_EQ(RETCODE_OK, participant->delete_topic(topic));
+        EXPECT_EQ(RETCODE_OK, participant->delete_publisher(easy_mode_publisher));
+        EXPECT_EQ(RETCODE_OK, DomainParticipantFactory::get_instance()->delete_participant(participant));
+        stop_background_servers();
+    }
+}
+
+TEST(ParticipantTests, EasyModeParticipantDoNotOverwriteCustomDataWriterQos)
+{
+    {
+        // Easy mode participant with existing profile
+        // set XML profile as environment variable: "export FASTDDS_DEFAULT_PROFILES_FILE=test_xml_service_easy_mode.xml"
+        eprosima::testing::set_environment_variable("FASTDDS_DEFAULT_PROFILES_FILE", "test_xml_service_easy_mode.xml");
+        set_easy_mode_environment_variable();
+        DomainParticipant* participant = DomainParticipantFactory::get_instance()->create_participant(
+            (uint32_t)GET_PID() % 230, PARTICIPANT_QOS_DEFAULT);
+        ASSERT_NE(nullptr, participant);
+        Publisher* easy_mode_publisher = participant->create_publisher(PUBLISHER_QOS_DEFAULT);
+        ASSERT_NE(easy_mode_publisher, nullptr);
+        PublisherQos profile_qos;
+        EXPECT_EQ(easy_mode_publisher->get_participant()->get_publisher_qos_from_profile("service", profile_qos),
+                RETCODE_OK);
+        TypeSupport type(new TopicDataTypeMock());
+        type.register_type(participant, "footype");
+        Topic* topic = participant->create_topic("footopic", "footype", TOPIC_QOS_DEFAULT);
+        ASSERT_NE(nullptr, topic);
+        DataWriter* data_writer = easy_mode_publisher->create_datawriter_with_profile(topic, "service");
+        ASSERT_NE(nullptr, data_writer);
+        DataWriterQos dw_qos;
+        EXPECT_EQ(data_writer->get_qos(dw_qos), RETCODE_OK);
+        EXPECT_EQ(dw_qos.reliability().max_blocking_time.seconds, 5);
+        EXPECT_EQ(dw_qos.reliability().max_blocking_time.nanosec, 0);
+        EXPECT_EQ(dw_qos.durability().kind, VOLATILE_DURABILITY_QOS);  // XML value
+
+        EXPECT_EQ(RETCODE_OK, easy_mode_publisher->delete_datawriter(data_writer));
+        EXPECT_EQ(RETCODE_OK, participant->delete_topic(topic));
+        EXPECT_EQ(RETCODE_OK, participant->delete_publisher(easy_mode_publisher));
+        EXPECT_EQ(RETCODE_OK, DomainParticipantFactory::get_instance()->delete_participant(participant));
+        stop_background_servers();
+    }
 }
 
 /**
