5c9f8dbe80e587279a3517f6270cbf5ceb46e459
cferreiragonz <carlosferreira@eprosima.com>
2025-04-01 12:25:42 +0200

    Refs #23001: Regression Test
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- test/blackbox/api/dds-pim/PubSubWriter.hpp
+++ test/blackbox/api/dds-pim/PubSubWriter.hpp
@@ -61,6 +61,8 @@ using eprosima::fastdds::rtps::UDPTransportDescriptor;
 using eprosima::fastdds::rtps::UDPv4TransportDescriptor;
 using eprosima::fastdds::rtps::UDPv6TransportDescriptor;
 using eprosima::fastdds::rtps::IPLocator;
+using eprosima::fastdds::rtps::BuiltinTransports;
+using eprosima::fastdds::rtps::BuiltinTransportsOptions;
 
 template<class TypeSupport>
 class PubSubWriter
@@ -1031,15 +1033,15 @@ public:
     }
 
     PubSubWriter& setup_transports(
-            eprosima::fastdds::rtps::BuiltinTransports transports)
+            BuiltinTransports transports)
     {
         participant_qos_.setup_transports(transports);
         return *this;
     }
 
     PubSubWriter& setup_transports(
-            eprosima::fastdds::rtps::BuiltinTransports transports,
-            const eprosima::fastdds::rtps::BuiltinTransportsOptions& options)
+            BuiltinTransports transports,
+            const BuiltinTransportsOptions& options)
     {
         participant_qos_.setup_transports(transports, options);
         return *this;
@@ -1048,9 +1050,10 @@ public:
     PubSubWriter& setup_large_data_tcp(
             bool v6 = false,
             const uint16_t& port = 0,
-            const uint32_t& tcp_negotiation_timeout = 0)
+            const BuiltinTransportsOptions& options = BuiltinTransportsOptions())
     {
         participant_qos_.transport().use_builtin_transports = false;
+        participant_qos_.transport().max_msg_size_no_frag = options.maxMessageSize;
 
         /* Transports configuration */
         // UDP transport for PDP over multicast
@@ -1060,6 +1063,9 @@ public:
         if (v6)
         {
             auto pdp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv6TransportDescriptor>();
+            pdp_transport->maxMessageSize = options.maxMessageSize;
+            pdp_transport->sendBufferSize = options.sockets_buffer_size;
+            pdp_transport->receiveBufferSize = options.sockets_buffer_size;
             participant_qos_.transport().user_transports.push_back(pdp_transport);
 
             auto data_transport = std::make_shared<eprosima::fastdds::rtps::TCPv6TransportDescriptor>();
@@ -1068,12 +1074,18 @@ public:
             data_transport->check_crc = false;
             data_transport->apply_security = false;
             data_transport->enable_tcp_nodelay = true;
-            data_transport->tcp_negotiation_timeout = tcp_negotiation_timeout;
+            data_transport->maxMessageSize = options.maxMessageSize;
+            data_transport->sendBufferSize = options.sockets_buffer_size;
+            data_transport->receiveBufferSize = options.sockets_buffer_size;
+            data_transport->tcp_negotiation_timeout = options.tcp_negotiation_timeout;
             participant_qos_.transport().user_transports.push_back(data_transport);
         }
         else
         {
             auto pdp_transport = std::make_shared<eprosima::fastdds::rtps::UDPv4TransportDescriptor>();
+            pdp_transport->maxMessageSize = options.maxMessageSize;
+            pdp_transport->sendBufferSize = options.sockets_buffer_size;
+            pdp_transport->receiveBufferSize = options.sockets_buffer_size;
             participant_qos_.transport().user_transports.push_back(pdp_transport);
 
             auto data_transport = std::make_shared<eprosima::fastdds::rtps::TCPv4TransportDescriptor>();
@@ -1082,7 +1094,10 @@ public:
             data_transport->check_crc = false;
             data_transport->apply_security = false;
             data_transport->enable_tcp_nodelay = true;
-            data_transport->tcp_negotiation_timeout = tcp_negotiation_timeout;
+            data_transport->maxMessageSize = options.maxMessageSize;
+            data_transport->sendBufferSize = options.sockets_buffer_size;
+            data_transport->receiveBufferSize = options.sockets_buffer_size;
+            data_transport->tcp_negotiation_timeout = options.tcp_negotiation_timeout;
             participant_qos_.transport().user_transports.push_back(data_transport);
         }
 
