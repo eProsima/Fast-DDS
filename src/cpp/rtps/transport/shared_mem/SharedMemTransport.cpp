e8674f87627a1801d7ab3c4b828702452ed35366
cferreiragonz <carlosferreira@eprosima.com>
2025-04-01 15:42:59 +0200

    Refs #23001: Remove stats buffer before alloc_buffer in SHM
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- src/cpp/rtps/transport/shared_mem/SharedMemTransport.cpp
+++ src/cpp/rtps/transport/shared_mem/SharedMemTransport.cpp
@@ -470,14 +470,13 @@ std::shared_ptr<SharedMemManager::Buffer> SharedMemTransport::copy_to_shared_buf
 
     assert(shared_mem_segment_);
 
-    std::shared_ptr<SharedMemManager::Buffer> shared_buffer =
-            shared_mem_segment_->alloc_buffer(total_bytes, max_blocking_time_point);
-    uint8_t* pos = static_cast<uint8_t*>(shared_buffer->data());
-
     // Statistics submessage is always the last buffer to be added
     // If statistics message is present, skip last buffer
     auto it_end = remove_statistics_buffer(buffers.back(), total_bytes) ? std::prev(buffers.end()) : buffers.end();
 
+    std::shared_ptr<SharedMemManager::Buffer> shared_buffer =
+            shared_mem_segment_->alloc_buffer(total_bytes, max_blocking_time_point);
+    uint8_t* pos = static_cast<uint8_t*>(shared_buffer->data());
 
     for (auto it = buffers.begin(); it != it_end; ++it)
     {
