c4b2b9bf587dc47b6f0fda2006b0f69776905590
Javier Gil Aviles <javiergil@eprosima.com>
2025-05-22 12:12:09 +0200

    Refs #23196: Add condition for string_builder nullptr on XMLDynamicParser.cpp in case bound is 0
    Signed-off-by: Javier Gil Aviles <javiergil@eprosima.com>

--- test/unittest/xmlparser/XMLParserTests.cpp
+++ test/unittest/xmlparser/XMLParserTests.cpp
@@ -90,6 +90,7 @@ TEST_F(XMLParserTests, regressions)
     EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/22843_profile_bin.xml", root));
     EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/22844_profile_bin.xml", root));
     EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/23030_profile_bin.xml", root));
+    EXPECT_EQ(XMLP_ret::XML_ERROR, XMLParser::loadXML("regressions/23196_profile_bin.xml", root));
     Log::Flush();
 }
 
