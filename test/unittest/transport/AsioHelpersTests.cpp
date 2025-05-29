1840a53f28de0db940c4e29451ae1fe6cdba3684
Miguel Company <miguelcompany@eprosima.com>
2025-05-29 08:52:40 +0200

    Fix included headers
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- test/unittest/transport/AsioHelpersTests.cpp
+++ test/unittest/transport/AsioHelpersTests.cpp
@@ -20,11 +20,10 @@
 #include <gtest/gtest.h>
 
 #include <fastdds/dds/log/Log.hpp>
-#include <fastdds/rtps/transport/UDPv4TransportDescriptor.hpp>
-#include <fastdds/utils/IPFinder.hpp>
-#include <fastdds/utils/IPLocator.hpp>
-
-#include <utils/Semaphore.hpp>
+#include <fastrtps/transport/UDPv4TransportDescriptor.h>
+#include <fastrtps/utils/IPFinder.h>
+#include <fastrtps/utils/IPLocator.h>
+#include <fastrtps/utils/Semaphore.h>
 
 #include <MockReceiverResource.h>
 #include <rtps/transport/asio_helpers.hpp>
