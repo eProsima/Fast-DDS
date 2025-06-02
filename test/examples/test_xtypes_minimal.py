d4976ba63bf44348ff3b467488e009c6213dc7b9
Mario Dominguez <mariodominguez@eprosima.com>
2025-06-02 09:59:10 +0200

    Refs #22593: Add xtypes examples to windows ci
    Signed-off-by: Mario Dominguez <mariodominguez@eprosima.com>

--- test/examples/test_xtypes_minimal.py
+++ test/examples/test_xtypes_minimal.py
@@ -20,7 +20,7 @@ def test_xtypes():
     out = ''
     try:
         out = subprocess.check_output(
-            '@DOCKER_EXECUTABLE@ compose -f xtypes_minimal.compose.yml up',
+            '"@DOCKER_EXECUTABLE@" compose -f xtypes_minimal.compose.yml up',
             stderr=subprocess.STDOUT,
             shell=True,
             timeout=30
