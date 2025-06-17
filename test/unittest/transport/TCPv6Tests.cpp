4d8758950f2fba57c76d9a44c7e4eca06d66deae
cferreiragonz <carlosferreira@eprosima.com>
2025-06-17 16:10:16 +0200

    Refs #22469: Disable grouping of locators in single channel for TCPv6
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- test/unittest/transport/TCPv6Tests.cpp
+++ test/unittest/transport/TCPv6Tests.cpp
@@ -368,13 +368,7 @@ TEST_F(TCPv6Tests, client_announced_local_port_uniqueness)
 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-    EXPECT_GT(receiveTransportUnderTest.get_channel_resources().size(), 2u);
-    std::set<std::shared_ptr<TCPChannelResource>> channels_created;
-    for (const auto& channel_resource : receiveTransportUnderTest.get_channel_resources())
-    {
-        channels_created.insert(channel_resource.second);
-    }
-    ASSERT_EQ(channels_created.size(), 2u);
+    EXPECT_EQ(receiveTransportUnderTest.get_channel_resources().size(), 2u);
 }
 
 #ifndef _WIN32
