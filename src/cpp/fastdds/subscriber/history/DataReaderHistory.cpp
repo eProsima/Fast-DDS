1f41964a78dba1a5efc93ff00aa39df6397c2495
Miguel Company <miguelcompany@eprosima.com>
2025-06-25 15:04:38 +0200

    Refs #22901. Handle key computation failure in DataReaderHistory.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- src/cpp/fastdds/subscriber/history/DataReaderHistory.cpp
+++ src/cpp/fastdds/subscriber/history/DataReaderHistory.cpp
@@ -215,8 +215,14 @@ bool DataReaderHistory::received_change_keep_all(
 {
     if (!compute_key_for_change_fn_(a_change))
     {
-        // Store the sample temporally only in ReaderHistory. When completed it will be stored in DataReaderHistory too.
-        return add_to_reader_history_if_not_full(a_change, rejection_reason);
+        if (!a_change->is_fully_assembled())
+        {
+            // Store the sample temporally only in ReaderHistory. When completed it will be stored in SubscriberHistory too.
+            return add_to_reader_history_if_not_full(a_change, rejection_reason);
+        }
+
+        rejection_reason = REJECTED_BY_INSTANCES_LIMIT;
+        return false;
     }
 
     bool ret_value = false;
@@ -251,8 +257,14 @@ bool DataReaderHistory::received_change_keep_last(
 {
     if (!compute_key_for_change_fn_(a_change))
     {
-        // Store the sample temporally only in ReaderHistory. When completed it will be stored in SubscriberHistory too.
-        return add_to_reader_history_if_not_full(a_change, rejection_reason);
+        if (!a_change->is_fully_assembled())
+        {
+            // Store the sample temporally only in ReaderHistory. When completed it will be stored in SubscriberHistory too.
+            return add_to_reader_history_if_not_full(a_change, rejection_reason);
+        }
+
+        rejection_reason = REJECTED_BY_INSTANCES_LIMIT;
+        return false;
     }
 
     bool ret_value = false;
