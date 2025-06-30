a0a2c3299b06f34cd3d3a9c927ab2a2a2a542437
Miguel Company <miguelcompany@eprosima.com>
2025-06-30 12:42:48 +0200

    Refs #22901. Extend fragment management unit test.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- test/unittest/rtps/common/CacheChangeTests.cpp
+++ test/unittest/rtps/common/CacheChangeTests.cpp
@@ -27,6 +27,7 @@ struct FragmentTestStep
 
     struct __Input
     {
+        bool is_key_only;
         uint32_t initial_fragment;
         uint16_t num_fragments;
     }
@@ -44,6 +45,7 @@ struct FragmentTestStep
         if (input.num_fragments > 0)
         {
             SerializedPayload_t payload(100);
+            payload.is_serialized_key = input.is_key_only;
             payload.length = 100;
             uut.add_fragments(payload, input.initial_fragment, input.num_fragments);
         }
@@ -70,57 +72,62 @@ TEST(CacheChange, FragmentManagement)
     {
         {
             "initial state",
-            {0, 0},
+            {false, 0, 0},
             {true, true, true, true, true, true, true, true, true, true}
         },
         {
             "received out-of-bounds (11)",
-            {11, 1},
+            {false, 11, 1},
             {true, true, true, true, true, true, true, true, true, true}
         },
         {
             "received (2)",
-            {2, 1},
+            {false, 2, 1},
             {true, false, true, true, true, true, true, true, true, true}
         },
         {
             "received (2) again",
-            {2, 1},
+            {false, 2, 1},
             {true, false, true, true, true, true, true, true, true, true}
         },
         {
             "received (2, 3)",
-            {2, 2},
+            {false, 2, 2},
             {true, false, false, true, true, true, true, true, true, true}
         },
         {
             "received (1)",
-            {1, 1},
+            {false, 1, 1},
             {false, false, false, true, true, true, true, true, true, true}
         },
         {
             "received (1) again",
-            {1, 1},
+            {false, 1, 1},
             {false, false, false, true, true, true, true, true, true, true}
         },
         {
             "received (9)",
-            {9, 1},
+            {false, 9, 1},
             {false, false, false, true, true, true, true, true, false, true}
         },
         {
             "received (8, 9, 10, 11)",
-            {8, 4},
+            {false, 8, 4},
+            {false, false, false, true, true, true, true, true, false, true}
+        },
+        {
+            "received key-only (7)",
+            {true, 7, 1},
             {false, false, false, true, true, true, true, true, false, true}
         },
         {
             "received (7)",
-            {7, 1},
+            {false, 7, 1},
             {false, false, false, true, true, true, false, true, false, true}
         },
         {
             "received (4, 5, 6)",
-            {4, 3},
+            {false, 4, 3},
             {false, false, false, false, false, false, false, true, false, true}
         }
     };
