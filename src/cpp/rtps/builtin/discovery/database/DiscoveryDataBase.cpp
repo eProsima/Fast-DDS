f616402036b9d0c9a95a69a0d7e1c4b26c5c4333
cferreiragonz <carlosferreira@eprosima.com>
2025-07-24 15:49:31 +0200

    Fix conflicts src
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>

--- src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
+++ src/cpp/rtps/builtin/discovery/database/DiscoveryDataBase.cpp
@@ -518,14 +518,7 @@ bool DiscoveryDataBase::process_edp_data_queue()
     // Lock(exclusive mode) mutex locally
     std::lock_guard<std::recursive_mutex> guard(mutex_);
 
-<<<<<<< HEAD
-    // Swap DATA queues
-    edp_data_queue_.Swap();
-
     eprosima::fastrtps::rtps::CacheChange_t* change;
-=======
-    eprosima::fastdds::rtps::CacheChange_t* change;
->>>>>>> ec666f72 (Solve Discovery Server race conditions (#5780))
     std::string topic_name;
 
     // Process all messages in the queque
@@ -606,52 +599,6 @@ void DiscoveryDataBase::match_new_server_(
     assert(our_data_it != participants_.end());
     add_pdp_to_send_(our_data_it->second.change());
 
-<<<<<<< HEAD
-=======
-    if (!is_superclient)
-    {
-        // To obtain a mesh topology with servers, we need to:
-        // - Make all known servers relevant to the new server
-        // - Make the new server relevant to all known servers
-        // - Send DATA(p) of all known servers to the new server
-        // - Send Data(p) of the new server to all other servers
-        for (auto& part : participants_)
-        {
-            if (part.first != server_guid_prefix_ && !part.second.is_client() && !part.second.is_superclient())
-            {
-                if (part.first == participant_prefix)
-                {
-                    std::lock_guard<std::recursive_mutex> guard(mutex_);
-                    bool resend_new_pdp = false;
-                    for (auto& server: servers_)
-                    {
-                        if (server != participant_prefix)
-                        {
-                            // Make all known servers relevant to the new server, but not matched
-                            part.second.add_or_update_ack_participant(server, ParticipantState::PENDING_SEND);
-                            resend_new_pdp = true;
-                        }
-                    }
-                    if (resend_new_pdp)
-                    {
-                        // Send DATA(p) of the new server to all other servers.
-                        add_pdp_to_send_(part.second.change());
-                    }
-                }
-                else
-                {
-                    // Make the new server relevant to all known servers
-                    part.second.add_or_update_ack_participant(participant_prefix, ParticipantState::PENDING_SEND);
-                    // Send DATA(p) of all known servers to the new participant
-                    add_pdp_to_send_(part.second.change());
-                }
-            }
-        }
-    }
-    // The resources needed for TCP new connections are created during the matching process when the
-    // DATA(p) is received by each server.
-
->>>>>>> ec666f72 (Solve Discovery Server race conditions (#5780))
     // Create virtual endpoints
     create_virtual_endpoints_(participant_prefix);
 }
@@ -777,12 +724,12 @@ void DiscoveryDataBase::update_participant_from_change_(
 {
     fastrtps::rtps::GUID_t change_guid = guid_from_change(ch);
 
-    assert(ch->kind == eprosima::fastdds::rtps::ALIVE);
+    assert(ch->kind == eprosima::fastrtps::rtps::ALIVE);
 
     // If the change corresponds to a previously removed participant (which hasn't yet been removed from the map since
     // the DATA(Up) is still unacked), update map with new data and behave as if it was a new participant.
     // Remove also the old change from the disposals collection, if it was added just before
-    if (participant_info.change()->kind != eprosima::fastdds::rtps::ALIVE)
+    if (participant_info.change()->kind != eprosima::fastrtps::rtps::ALIVE)
     {
         // Update the change data
         participant_info.participant_change_data(change_data);
@@ -804,7 +751,7 @@ void DiscoveryDataBase::update_participant_from_change_(
             // Match new server and create virtual endpoints
             // NOTE: match after having updated the change, so virtual endpoints are not discarded for having
             // an associated unalive participant
-            match_new_server_(change_guid.guidPrefix, change_data.is_superclient());
+            match_new_server_(change_guid.guidPrefix);
         }
 
         // Treat as a new participant found
@@ -822,16 +769,6 @@ void DiscoveryDataBase::update_participant_from_change_(
     else if (change_guid.guidPrefix != server_guid_prefix_ && change_data.is_local() &&
             DiscoveryDataBase::participant_data_has_changed_(participant_info, change_data))
     {
-<<<<<<< HEAD
-        // If the participant changes to server local, virtual endpoints must be added
-        // If it is local and server the only possibility is it was a remote server and it must be converted to local
-        if (!change_data.is_client())
-        {
-            match_new_server_(change_guid.guidPrefix);
-        }
-
-=======
->>>>>>> ec666f72 (Solve Discovery Server race conditions (#5780))
         // Update the change data
         participant_info.participant_change_data(change_data);
 
@@ -843,7 +780,7 @@ void DiscoveryDataBase::update_participant_from_change_(
         if (!change_data.is_client())
         {
             // NOTE: match after having updated the change in order to send the new Data(P)
-            match_new_server_(change_guid.guidPrefix, change_data.is_superclient());
+            match_new_server_(change_guid.guidPrefix);
         }
 
         // Treat as a new participant found
@@ -958,7 +895,7 @@ void DiscoveryDataBase::create_writers_from_change_(
         // NOTE: Processing a DATA(w) should always be preceded by the reception and processing of its corresponding
         // participant. However, one may receive a DATA(w) just after the participant has been removed, case in which the
         // former should no longer be processed.
-        std::map<eprosima::fastdds::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator writer_part_it =
+        std::map<eprosima::fastrtps::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator writer_part_it =
                 participants_.find(writer_guid.guidPrefix);
         if (writer_part_it == participants_.end())
         {
@@ -968,7 +905,7 @@ void DiscoveryDataBase::create_writers_from_change_(
             changes_to_release_.push_back(ch); // Release change so it can be reused
             return;
         }
-        else if (writer_part_it->second.change()->kind != fastdds::rtps::ChangeKind_t::ALIVE)
+        else if (writer_part_it->second.change()->kind != fastrtps::rtps::ChangeKind_t::ALIVE)
         {
             EPROSIMA_LOG_WARNING(DISCOVERY_DATABASE,
                     "Writer " << writer_guid << " is associated to a removed participant. Skipping");
@@ -997,22 +934,7 @@ void DiscoveryDataBase::create_writers_from_change_(
         new_updates_++;
 
         // Add entry to participants_[guid_prefix]::writers
-<<<<<<< HEAD
-        std::map<eprosima::fastrtps::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator writer_part_it =
-                participants_.find(writer_guid.guidPrefix);
-        if (writer_part_it != participants_.end())
-        {
-            writer_part_it->second.add_writer(writer_guid);
-        }
-        else
-        {
-            EPROSIMA_LOG_ERROR(DISCOVERY_DATABASE,
-                    "Writer " << writer_guid << " has no associated participant. Skipping");
-            return;
-        }
-=======
         writer_part_it->second.add_writer(writer_guid);
->>>>>>> ec666f72 (Solve Discovery Server race conditions (#5780))
 
         // Add writer to writers_by_topic_[topic_name]
         add_writer_to_topic_(writer_guid, topic_name);
@@ -1103,7 +1025,7 @@ void DiscoveryDataBase::create_readers_from_change_(
         // NOTE: Processing a DATA(r) should always be preceded by the reception and processing of its corresponding
         // participant. However, one may receive a DATA(r) just after the participant has been removed, case in which the
         // former should no longer be processed.
-        std::map<eprosima::fastdds::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator reader_part_it =
+        std::map<eprosima::fastrtps::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator reader_part_it =
                 participants_.find(reader_guid.guidPrefix);
         if (reader_part_it == participants_.end())
         {
@@ -1113,7 +1035,7 @@ void DiscoveryDataBase::create_readers_from_change_(
             changes_to_release_.push_back(ch); // Release change so it can be reused
             return;
         }
-        else if (reader_part_it->second.change()->kind != fastdds::rtps::ChangeKind_t::ALIVE)
+        else if (reader_part_it->second.change()->kind != fastrtps::rtps::ChangeKind_t::ALIVE)
         {
             EPROSIMA_LOG_WARNING(DISCOVERY_DATABASE,
                     "Reader " << reader_guid << " is associated to a removed participant. Skipping");
@@ -1142,22 +1064,7 @@ void DiscoveryDataBase::create_readers_from_change_(
         new_updates_++;
 
         // Add entry to participants_[guid_prefix]::readers
-<<<<<<< HEAD
-        std::map<eprosima::fastrtps::rtps::GuidPrefix_t, DiscoveryParticipantInfo>::iterator reader_part_it =
-                participants_.find(reader_guid.guidPrefix);
-        if (reader_part_it != participants_.end())
-        {
-            reader_part_it->second.add_reader(reader_guid);
-        }
-        else
-        {
-            EPROSIMA_LOG_ERROR(DISCOVERY_DATABASE,
-                    "Reader " << reader_guid << " has no associated participant. Skipping");
-            return;
-        }
-=======
         reader_part_it->second.add_reader(reader_guid);
->>>>>>> ec666f72 (Solve Discovery Server race conditions (#5780))
 
         // Add reader to readers_by_topic_[topic_name]
         add_reader_to_topic_(reader_guid, topic_name);
