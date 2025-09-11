84e1b8c0b7497c2aee4b8c06510f38f715cf5258
Carlos Ferreira González <carlosferreira@eprosima.com>
2025-09-11 15:38:11 +0200

    Use binary search in 'find_change_nts' (#5997)
    * Refs #23618: Use binary search in 'find_change_nts'
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    
    * Refs #23618: Take into account changes with same sequence number
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    
    ---------
    
    Signed-off-by: cferreiragonz <carlosferreira@eprosima.com>
    (cherry picked from commit 434af1bd7ba715a8a91fda945cf482393c5eb66d)

--- src/cpp/rtps/history/History.cpp
+++ src/cpp/rtps/history/History.cpp
@@ -23,6 +23,7 @@
 #include <fastdds/dds/log/Log.hpp>
 #include <fastdds/rtps/common/CacheChange.hpp>
 
+#include <rtps/common/ChangeComparison.hpp>
 #include <rtps/history/BasicPayloadPool.hpp>
 #include <rtps/history/CacheChangePool.h>
 
@@ -54,11 +55,15 @@ History::const_iterator History::find_change_nts(
         return const_iterator();
     }
 
-    return std::find_if(changesBegin(), changesEnd(), [this, ch](const CacheChange_t* chi)
-                   {
-                       // use the derived classes comparison criteria for searching
-                       return this->matches_change(chi, ch);
-                   });
+    // Use binary search to find the first change with the same sequence number
+    auto lb = std::lower_bound(changesBegin(), changesEnd(), ch, history_order_cmp);
+
+    if (lb != changesEnd() && matches_change(*lb, ch))
+    {
+        return lb;
+    }
+
+    return changesEnd();
 }
 
 bool History::matches_change(
