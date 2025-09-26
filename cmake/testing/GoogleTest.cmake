0bb401b132f6fd4fd2e23c60342621247a11fd44
Ricardo González <ricardo@richiware.dev>
2025-09-26 09:06:53 +0200

    Fix location of generate_google_test_win_wrapper.cmake (#6058)
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- cmake/testing/GoogleTest.cmake
+++ cmake/testing/GoogleTest.cmake
@@ -19,7 +19,7 @@ function(gtest_discover_tests TARGET)
     if (WIN32)
         add_custom_command(
             TARGET ${TARGET} POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> "-DRUNTIME_LIST=$<TARGET_RUNTIME_DLLS:${TARGET}>" -P ${CMAKE_SOURCE_DIR}/cmake/testing/generate_google_test_win_wrapper.cmake
+            COMMAND ${CMAKE_COMMAND} -DTARGET=${TARGET} -DCONFIG=$<CONFIG> "-DRUNTIME_LIST=$<TARGET_RUNTIME_DLLS:${TARGET}>" -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/generate_google_test_win_wrapper.cmake
             VERBATIM
             )
 
