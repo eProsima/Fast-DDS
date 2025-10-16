9f72bbdeb4c7973e95c73a50563e920b1cd003b3
Juan Lopez Fernandez <juanlopez@eprosima.com>
2025-10-16 11:11:06 +0200

    Uncrustify
    Signed-off-by: Juan Lopez Fernandez <juanlopez@eprosima.com>

--- src/cpp/rtps/writer/StatefulWriter.cpp
+++ src/cpp/rtps/writer/StatefulWriter.cpp
@@ -1510,7 +1510,8 @@ void StatefulWriter::check_acked_status()
                     listener_->on_writer_change_received_by_all(this, change);
 
                     // Stop if we got to either next_all_acked_notify_sequence_ or the first change
-                } while (seq > end_seq);
+                }
+                while (seq > end_seq);
             }
 
             next_all_acked_notify_sequence_ = min_low_mark + 1;
@@ -2081,7 +2082,8 @@ bool StatefulWriter::ack_timer_expired()
         do
         {
             last_sequence_number_++;
-        } while (!history_->get_change(
+        }
+        while (!history_->get_change(
             last_sequence_number_,
             getGuid(),
             &change) && last_sequence_number_ < next_sequence_number());
