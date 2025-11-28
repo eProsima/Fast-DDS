0edf762f8a41fcca578141570765e55a26d9e6f7
Emilio Cuesta <emiliocuesta@eprosima.com>
2025-11-28 06:39:57 +0100

    Almost done with the RTPS fix
    Signed-off-by: Emilio Cuesta <emiliocuesta@eprosima.com>

--- src/cpp/rtps/history/WriterHistory.cpp
+++ src/cpp/rtps/history/WriterHistory.cpp
@@ -63,7 +63,7 @@ static CacheChange_t* initialize_change(
     reserved_change->kind = change_kind;
     if ((WITH_KEY == writer->getAttributes().topicKind) && !handle.isDefined())
     {
-        EPROSIMA_LOG_WARNING(RTPS_WRITER, "Changes in KEYED Writers need a valid instanceHandle");
+        EPROSIMA_LOG_WARNING(RTPS_WRITER, "Changes in KEYED Writers need a valid instanceHandle. The data will be handled as if it came from an unkeyed topic.");
     }
     reserved_change->instanceHandle = handle;
     reserved_change->writerGUID = writer->getGuid();
@@ -485,6 +485,7 @@ void WriterHistory::set_fragments(
     }
     if (ChangeKind_t::ALIVE != change->kind && TopicKind_t::WITH_KEY == mp_writer->getAttributes().topicKind)
     {
+        // TODO(ecuesta): Review
         inline_qos_size += fastdds::dds::ParameterSerializer<Parameter_t>::PARAMETER_KEY_SIZE;
         inline_qos_size += fastdds::dds::ParameterSerializer<Parameter_t>::PARAMETER_STATUS_SIZE;
     }
