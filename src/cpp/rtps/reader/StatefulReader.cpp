9aae0df7d6406841e9a8a2b81eed3e4ac641216f
Emilio Cuesta <emiliocuesta@eprosima.com>
2025-11-28 11:01:19 +0100

    Adding errors and logs for DDS case
    Signed-off-by: Emilio Cuesta <emiliocuesta@eprosima.com>

--- src/cpp/rtps/reader/StatefulReader.cpp
+++ src/cpp/rtps/reader/StatefulReader.cpp
@@ -825,6 +825,9 @@ bool StatefulReader::process_data_frag_msg(
                          */
                         if (fastdds::dds::REJECTED_BY_UNKNOWN_INSTANCE == rejection_reason)
                         {
+                            EPROSIMA_LOG_ERROR(RTPS_READER, "Change received from " << work_change->writerGUID << " with sequence number: "
+                                            << work_change->sequenceNumber <<
+                                            " ignored. Could not compute key in keyed topic.");
                             pWP->irrelevant_change_set(work_change->sequenceNumber);
                             has_to_notify = true;
                         }
@@ -1199,6 +1202,9 @@ bool StatefulReader::change_received(
              */
             if (fastdds::dds::REJECTED_BY_UNKNOWN_INSTANCE == rejection_reason)
             {
+                EPROSIMA_LOG_ERROR(RTPS_READER, "Change received from " << a_change->writerGUID << " with sequence number: "
+                                                                       << a_change->sequenceNumber <<
+                        " ignored. Could not compute key in keyed topic.");
                 prox->irrelevant_change_set(a_change->sequenceNumber);
                 NotifyChanges(prox);
             }
