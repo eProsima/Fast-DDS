9d2e73768e218fa1d729727ab419597918b0bb8e
Ricardo González Moreno <ricardo@richiware.dev>
2025-12-23 11:35:26 +0100

    Refs #23968. Fix compilation
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/mock/rtps/ReaderLocator/rtps/writer/ReaderLocator.hpp
+++ test/mock/rtps/ReaderLocator/rtps/writer/ReaderLocator.hpp
@@ -200,7 +200,7 @@ public:
     bool send(
             const std::vector<eprosima::fastdds::rtps::NetworkBuffer>& /*buffers*/,
             const uint32_t& /*total_bytes*/,
-            std::chrono::steady_clock::time_point /*max_blocking_time_point*/) const override
+            std::chrono::steady_clock::time_point /*max_blocking_time_point*/) override
     {
         return true;
     }
