0b70d700de75f90ef04ef44819b3ff91fd918ce6
Miguel Company <miguelcompany@eprosima.com>
2026-01-05 12:10:20 +0100

    Refs #24027. Apply suggestions from code review.
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>

--- src/cpp/rtps/participant/rtps_participant_types.hpp
+++ src/cpp/rtps/participant/rtps_participant_types.hpp
@@ -1,7 +1,21 @@
-#ifndef FASTDDS_RTPS_PARTICIPANT_TYPES_HPP
-#define FASTDDS_RTPS_PARTICIPANT_TYPES_HPP
+// Copyright 2026 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include <rtps/participant/RTPSParticipantImpl.hpp>
+#ifndef RTPS_PARTICIPANT__RTPS_PARTICIPANT_TYPES_HPP
+#define RTPS_PARTICIPANT__RTPS_PARTICIPANT_TYPES_HPP
+
+#include "RTPSParticipantImpl.hpp"
 
 namespace eprosima {
 namespace fastdds {
@@ -13,4 +27,4 @@ using RTPSParticipantImplType = RTPSParticipantImpl;
 } // namespace fastdds
 } // namespace eprosima
 
-#endif // FASTDDS_RTPS_PARTICIPANT_TYPES_HPP
+#endif // RTPS_PARTICIPANT__RTPS_PARTICIPANT_TYPES_HPP
