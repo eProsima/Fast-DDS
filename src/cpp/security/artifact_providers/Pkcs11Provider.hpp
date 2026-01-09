4c91a7f9ae62d380842b2c3f79eadd4579efaba3
Miguel Company <miguelcompany@eprosima.com>
2026-01-09 08:43:44 +0100

    Improve Open SSL includes (#6240)
    Signed-off-by: Miguel Company <miguelcompany@eprosima.com>
    (cherry picked from commit f5aaab565b39953c036360bf2d1b044dd8f7975b)

--- src/cpp/security/artifact_providers/Pkcs11Provider.hpp
+++ src/cpp/security/artifact_providers/Pkcs11Provider.hpp
@@ -19,9 +19,9 @@
 #ifndef _SECURITY_ARTIFACTPROVIDERS_PKCS11PROVIDER_HPP_
 #define _SECURITY_ARTIFACTPROVIDERS_PKCS11PROVIDER_HPP_
 
-#include <openssl/engine.h>
-#include <openssl/err.h>
-#include <openssl/ssl.h>
+#include <string>
+
+#include <openssl/types.h>
 
 #include <rtps/security/exceptions/SecurityException.h>
 
@@ -48,16 +48,13 @@ public:
 
 private:
 
-    EVP_PKEY* load_private_key_impl(
-            X509* certificate,
-            const std::string& file,
-            const std::string& password,
-            SecurityException& exception);
-
     SecurityException initialization_exception_;
     bool has_initialization_error_ = false;
+
+#if !defined(OPENSSL_NO_ENGINE)
     ENGINE* pkcs11_ = nullptr;
     UI_METHOD* ui_method_ = nullptr;
+#endif // !defined(OPENSSL_NO_ENGINE)
 };
 
 } // namespace detail
