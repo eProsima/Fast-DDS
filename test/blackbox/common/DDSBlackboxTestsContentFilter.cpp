693ecb60b43a75acf39d47439ad61cc0efb8a66b
Ricardo González Moreno <ricardo@richiware.dev>
2026-01-14 09:01:24 +0100

    Refs #24038. Apply new uncrustify
    Signed-off-by: Ricardo González Moreno <ricardo@richiware.dev>

--- test/blackbox/common/DDSBlackboxTestsContentFilter.cpp
+++ test/blackbox/common/DDSBlackboxTestsContentFilter.cpp
@@ -160,7 +160,8 @@ public:
         {
             case communication_type::INTRAPROCESS:
                 library_settings.intraprocess_delivery = eprosima::fastdds::IntraprocessDeliveryType::INTRAPROCESS_FULL;
-                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(library_settings);
+                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(
+                    library_settings);
                 break;
             case communication_type::DATASHARING:
                 enable_datasharing = true;
@@ -182,7 +183,8 @@ public:
         {
             case communication_type::INTRAPROCESS:
                 library_settings.intraprocess_delivery = eprosima::fastdds::IntraprocessDeliveryType::INTRAPROCESS_OFF;
-                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(library_settings);
+                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(
+                    library_settings);
                 break;
             case communication_type::DATASHARING:
                 break;
