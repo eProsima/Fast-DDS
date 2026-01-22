2f2ca428973e7b8ade55f45df3d1b9ac0521999a
Raül <raulojeda@eprosima.com>
2026-01-22 14:38:03 +0100

    Uncrustify
    Signed-off-by: Raül <raulojeda@eprosima.com>

--- test/blackbox/common/BlackboxTestsDiscovery.cpp
+++ test/blackbox/common/BlackboxTestsDiscovery.cpp
@@ -65,7 +65,8 @@ public:
         {
             case INTRAPROCESS:
                 library_settings.intraprocess_delivery = eprosima::fastdds::IntraprocessDeliveryType::INTRAPROCESS_FULL;
-                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(library_settings);
+                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(
+                    library_settings);
                 break;
             case DATASHARING:
                 enable_datasharing = true;
@@ -83,7 +84,8 @@ public:
         {
             case INTRAPROCESS:
                 library_settings.intraprocess_delivery = eprosima::fastdds::IntraprocessDeliveryType::INTRAPROCESS_OFF;
-                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(library_settings);
+                eprosima::fastdds::dds::DomainParticipantFactory::get_instance()->set_library_settings(
+                    library_settings);
                 break;
             case DATASHARING:
                 enable_datasharing = false;
