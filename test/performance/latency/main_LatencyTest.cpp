d19278d0c3a4a2120669aacb192b6e8891b93f92
Emilio Cuesta <emiliocuesta@eprosima.com>
2026-01-26 12:05:05 +0100

    Modify performance tests to properly test access security
    Signed-off-by: Emilio Cuesta <emiliocuesta@eprosima.com>

--- test/performance/latency/main_LatencyTest.cpp
+++ test/performance/latency/main_LatencyTest.cpp
@@ -445,6 +445,8 @@ int main(
             return -1;
         }
 
+        // Subscriber
+        // Auth
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.plugin", "builtin.PKI-DH"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_ca",
                 "file://" + certs_path + "/maincacert.pem"));
@@ -453,11 +455,17 @@ int main(
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.private_key",
                 "file://" + certs_path + "/mainsubkey.pem"));
         sub_part_property_policy.properties().emplace_back(Property("dds.sec.crypto.plugin", "builtin.AES-GCM-GMAC"));
-        sub_part_property_policy.properties().emplace_back("rtps.participant.rtps_protection_kind", "ENCRYPT");
+        // Access
+        sub_part_property_policy.properties().emplace_back(Property("dds.sec.access.plugin", "builtin.Access-Permissions"));
+        sub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.permissions_ca",
+                "file://" + certs_path + "/maincacert.pem"));
+        sub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.governance",
+                "file://" + certs_path + "/governance_performance_tests.smime"));
+        sub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.permissions",
+                "file://" + certs_path + "/permissions_performance_tests.smime"));
 
-        sub_property_policy.properties().emplace_back("rtps.endpoint.submessage_protection_kind", "ENCRYPT");
-        sub_property_policy.properties().emplace_back("rtps.endpoint.payload_protection_kind", "ENCRYPT");
-
+        // Publisher
+        // Auth
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.plugin", "builtin.PKI-DH"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.identity_ca",
                 "file://" + certs_path + "/maincacert.pem"));
@@ -466,9 +474,14 @@ int main(
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.auth.builtin.PKI-DH.private_key",
                 "file://" + certs_path + "/mainpubkey.pem"));
         pub_part_property_policy.properties().emplace_back(Property("dds.sec.crypto.plugin", "builtin.AES-GCM-GMAC"));
-        pub_part_property_policy.properties().emplace_back("rtps.participant.rtps_protection_kind", "ENCRYPT");
-        pub_property_policy.properties().emplace_back("rtps.endpoint.submessage_protection_kind", "ENCRYPT");
-        pub_property_policy.properties().emplace_back("rtps.endpoint.payload_protection_kind", "ENCRYPT");
+        // Access
+        pub_part_property_policy.properties().emplace_back(Property("dds.sec.access.plugin", "builtin.Access-Permissions"));
+        pub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.permissions_ca",
+                "file://" + certs_path + "/maincacert.pem"));
+        pub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.governance",
+                "file://" + certs_path + "/governance_performance_tests.smime"));
+        pub_part_property_policy.properties().emplace_back(Property("dds.sec.access.builtin.Access-Permissions.permissions",
+                "file://" + certs_path + "/permissions_performance_tests.smime"));
     }
 #endif // if HAVE_SECURITY
 
