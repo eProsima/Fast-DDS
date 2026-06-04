5467b2112f27ae99e034fbcd061faac5223c73ca
Carlos Ferreira González <carlosferreira@eprosima.com>
2026-06-04 10:40:19 +0200

    Refs #24365: Avoid UBSan with statistics narrow operation
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>

--- src/cpp/statistics/fastdds/domain/DomainParticipant.cpp
+++ src/cpp/statistics/fastdds/domain/DomainParticipant.cpp
@@ -82,7 +82,7 @@ DomainParticipant* DomainParticipant::narrow(
         eprosima::fastdds::dds::DomainParticipant* domain_participant)
 {
 #ifdef FASTDDS_STATISTICS
-    return static_cast<DomainParticipant*>(domain_participant);
+    return reinterpret_cast<DomainParticipant*>(domain_participant);
 #else
     (void)domain_participant;
     return nullptr;
@@ -93,7 +93,7 @@ const DomainParticipant* DomainParticipant::narrow(
         const eprosima::fastdds::dds::DomainParticipant* domain_participant)
 {
 #ifdef FASTDDS_STATISTICS
-    return static_cast<const DomainParticipant*>(domain_participant);
+    return reinterpret_cast<const DomainParticipant*>(domain_participant);
 #else
     (void)domain_participant;
     return nullptr;
