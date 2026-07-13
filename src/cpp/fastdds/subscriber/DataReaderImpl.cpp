47e68781e93a4faf85af1ca51db7eb94275f3cbe
Carlos Ferreira González <carlosferreira@eprosima.com>
2026-07-13 09:04:44 +0200

    Uncrustify
    Signed-off-by: Carlos Ferreira González <carlosferreira@eprosima.com>

--- src/cpp/fastdds/subscriber/DataReaderImpl.cpp
+++ src/cpp/fastdds/subscriber/DataReaderImpl.cpp
@@ -1617,11 +1617,11 @@ ReturnCode_t DataReaderImpl::check_qos(
             qos.history().depth > qos.resource_limits().max_samples_per_instance)
     {
         EPROSIMA_LOG_WARNING(RTPS_QOS_CHECK,
-                "HISTORY DEPTH '" << qos.history().depth <<
-                "' is inconsistent with max_samples_per_instance: '" <<
-                qos.resource_limits().max_samples_per_instance <<
-                "'. Consistency rule: depth <= max_samples_per_instance." <<
-                " Effectively using max_samples_per_instance as depth.");
+                "HISTORY DEPTH '" << qos.history().depth
+                                  << "' is inconsistent with max_samples_per_instance: '"
+                                  << qos.resource_limits().max_samples_per_instance
+                                  << "'. Consistency rule: depth <= max_samples_per_instance."
+                                  << " Effectively using max_samples_per_instance as depth.");
     }
     // Check for nanoseconds in all duration policies
     if (!utils::is_duration_consistent(qos.deadline().period))
